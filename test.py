fd182b6ea5d4bb83325891c34e04d0f7bc62c673
loicmagne <loic.magne@outlook.com>
2022-05-02 14:49:01 +0200

    init file

--- test.py
+++ test.py
@@ -1,8 +1,6 @@
 from sentence_transformers import SentenceTransformer
-from mteb.evaluation import *
+from mteb import MTEB
 
 model = SentenceTransformer('average_word_embeddings_komninos')
 eval = MTEB(task_types=['Reranking'])
-print(eval.available_tasks)
-print(eval.selected_tasks)
 eval.run(model)
