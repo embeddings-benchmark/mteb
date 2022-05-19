96848c182b5230657956cc6b1160f65a2cc488de
loicmagne <loic.magne@outlook.com>
2022-05-19 23:39:14 +0200

    bucc

---
+++ mteb/tasks/BitextMining/BUCCBitextMining.py
@@ -0,0 +1,23 @@
+from ...abstasks import AbsTaskBitextMining, CrosslingualTask
+
+_LANGUAGES = [
+  'de-en',
+  'fr-en',
+  'ru-en',
+  'zh-en'
+]
+
+class BUCCBitextMining(AbsTaskBitextMining, CrosslingualTask):
+    @property
+    def description(self):
+        return {
+            "name": "BUCC",
+            "hf_hub_name": "mteb/bucc-bitext-mining",
+            "description": "BUCC bitext mining dataset",
+            "reference": "https://comparable.limsi.fr/bucc2018/bucc2018-task.html",
+            "type": "BitextMining",
+            "category": "s2s",
+            "available_splits": ["test"],
+            "available_langs": _LANGUAGES,
+            "main_score": "error",
+        }
