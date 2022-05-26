2b17b4a1552e71f298e23cbaea461ecc1efe4e17
Nouamane Tazi <nouamane98@gmail.com>
2022-05-27 01:36:48 +0200

    fix cmd.py arguments

--- mteb/cmd.py
+++ mteb/cmd.py
@@ -1,6 +1,6 @@
 # entry point for the library
 # example call:
-#   mteb --model average_word_embeddings_komninos --tasks Banking77Classification EmotionClassification  --k 5 --device 0 --batch_size 32 --seed 42 --output_dir /tmp/mteb_output --n_splits 5 --samples_per_label 8 --verbose 3
+#   mteb --model average_word_embeddings_komninos --tasks Banking77Classification EmotionClassification  --k 5 --device 0 --batch_size 32 --seed 42 --output_folder /tmp/mteb_output --n_splits 5 --samples_per_label 8 --verbosity 3
 
 import argparse
 import logging
@@ -20,24 +20,51 @@ def main():
         required=True,
         help="Model to use. Use pre-trained model name from https://huggingface.co/models",
     )
-    parser.add_argument("--tasks", nargs="+", type=str, default=None, help="Tasks to run. Use task name from #TODO")
-    parser.add_argument("--k", type=int, default=5, help="Number of nearest neighbors to use for classification")
-    parser.add_argument("--device", type=int, default=0, help="Device to use for computation")
+    parser.add_argument(
+        "--task_types",
+        nargs="+",
+        type=str,
+        default=None,
+        help="List of task types (Clustering, Retrieval..) to be evaluated. If None, all tasks will be evaluated",
+    )
+    parser.add_argument(
+        "--task_categories",
+        nargs="+",
+        type=str,
+        default=None,
+        help="List of task categories (s2s, p2p..) to be evaluated. If None, all tasks will be evaluated",
+    )
+    parser.add_argument(
+        "-t",
+        "--tasks",
+        nargs="+",
+        type=str,
+        default=None,
+        help="List of tasks to be evaluated. If specified, the other arguments are ignored.",
+    )
+    parser.add_argument("--device", type=int, default=None, help="Device to use for computation")
     parser.add_argument("--batch_size", type=int, default=32, help="Batch size for computation")
     parser.add_argument("--seed", type=int, default=42, help="Random seed for computation")
-    parser.add_argument("--output_dir", type=str, default="mteb_output", help="Output directory for results")
-    parser.add_argument("--n_splits", type=int, default=5, help="Number of splits for bootstrapping")
+    parser.add_argument("--output_folder", type=str, default="results/res", help="Output directory for results")
     parser.add_argument(
-        "--samples_per_label", type=int, default=8, help="Number of samples per label for bootstrapping"
+        "--method",
+        type=str,
+        default=None,
+        help="Method to use for evaluation. Can be 'kNN' or 'logReg'",
     )
-    parser.add_argument("--verbose", type=int, default=1, help="Verbosity level")
+    parser.add_argument("--k", type=int, default=None, help="Number of nearest neighbors to use for classification")
+    parser.add_argument("--n_splits", type=int, default=None, help="Number of splits for bootstrapping")
+    parser.add_argument(
+        "--samples_per_label", type=int, default=None, help="Number of samples per label for bootstrapping"
+    )
+    parser.add_argument("-v", "--verbosity", type=int, default=1, help="Verbosity level")
     args = parser.parse_args()
 
     logger.info("Running with parameters: %s", args)
 
-    model = SentenceTransformer(args.model)
-    eval = MTEB(task_list=args.tasks)
-    eval.run(model)
+    model = SentenceTransformer(args.model, device=args.device)
+    eval = MTEB(**vars(args))
+    eval.run(model, verbosity=args.verbosity, output_folder=args.output_folder)
 
 
 if __name__ == "__main__":
