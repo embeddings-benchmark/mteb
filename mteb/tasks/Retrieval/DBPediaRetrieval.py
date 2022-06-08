50d67005e826c1fd29731e02f8e5dd2210d44bfb
loicmagne <loic.magne@outlook.com>
2022-06-08 03:56:45 +0200

    dbpedia

---
+++ mteb/tasks/Retrieval/DBPediaRetrieval.py
@@ -0,0 +1,18 @@
+from ...abstasks.AbsTaskRetrieval import AbsTaskRetrieval
+from ...abstasks.BeIRTask import BeIRTask
+
+
+class DBPedia(AbsTaskRetrieval, BeIRTask):
+    @property
+    def description(self):
+        return {
+            "name": "DBPedia",
+            "beir_name": "dbpedia-entity",
+            "description": "DBpedia-Entity is a standard test collection for entity search over the DBpedia knowledge base",
+            "reference": "https://github.com/iai-group/DBpedia-Entity/",
+            "type": "Retrieval",
+            "category": "s2s",
+            "eval_splits": ["dev","test"],
+            "eval_langs": ["en"],
+            "main_score": "map",
+        }
