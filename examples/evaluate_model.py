b581d00bc384f5623a970b146a9614f93115b251
Nouamane Tazi <nouamane98@gmail.com>
2022-06-30 22:51:25 +0000

    update example

--- examples/evaluate_model.py
+++ examples/evaluate_model.py
@@ -9,7 +9,7 @@ logging.basicConfig(level=logging.INFO)
 
 model_name = "average_word_embeddings_komninos"
 model = SentenceTransformer(model_name)
-evaluation = MTEB(task_types=["Clustering"])
-evaluation.run(model, output_folder=f"results/{model_name}")
+evaluation = MTEB(task_langs=["en"])
+evaluation.run(model, output_folder=f"results/{model_name}", eval_splits=["test"])
 
 print("--DONE--")
