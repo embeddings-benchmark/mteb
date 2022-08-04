7ce4bb16db369258e1f88e4118baf247203a8e77
Niklas Muennighoff <n.muennighoff@gmail.com>
2022-08-04 10:05:47 +0200

    Fix Mind Reference
    Two other notes:
    - The renaming can create confusion as there exists a test set just that I assume we don't have the labels
    - MIND uses AUC & MRR & NDCG scores, not MAP, see https://msnews.github.io/

--- mteb/tasks/Reranking/MindSmallReranking.py
+++ mteb/tasks/Reranking/MindSmallReranking.py
@@ -8,7 +8,7 @@ class MindSmallReranking(AbsTaskReranking):
             "name": "MindSmallReranking",
             "hf_hub_name": "mteb/mind_small",
             "description": "Microsoft News Dataset: A Large-Scale English Dataset for News Recommendation Research",
-            "reference": "https://www.microsoft.com/en-us/research/uploads/prod/2019/03/nl4se18LinkSO.pdf",
+            "reference": "https://msnews.github.io/assets/doc/ACL2020_MIND.pdf",
             "type": "Reranking",
             "category": "s2s",
             "eval_splits": ["test"],
