fb42e89b94eb0ad4ce3f5530296f006c7898bb61
Niklas Muennighoff <n.muennighoff@gmail.com>
2022-09-04 14:03:01 +0200

    Update MSMARCOv2Retrieval.py

--- mteb/tasks/Retrieval/MSMARCOv2Retrieval.py
+++ mteb/tasks/Retrieval/MSMARCOv2Retrieval.py
@@ -9,7 +9,7 @@ class MSMARCOv2(AbsTaskRetrieval, BeIRTask):
             "name": "MSMARCOv2",
             "beir_name": "msmarco-v2",
             "reference": "https://microsoft.github.io/msmarco/TREC-Deep-Learning.html",
-            "description": "MS MARCO is a collection of datasets focused on deep learning in search"
+            "description": "MS MARCO is a collection of datasets focused on deep learning in search",
             "type": "Retrieval",
             "category": "s2s",
             "eval_splits": ["dev1", "dev2"],
