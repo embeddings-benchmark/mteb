37b7b7915128dbc70e87ec5914058d48be00dfbe
Niklas Muennighoff <n.muennighoff@gmail.com>
2022-11-08 12:01:37 +0400

    Update BeIRTask.py

--- mteb/abstasks/BeIRTask.py
+++ mteb/abstasks/BeIRTask.py
@@ -19,6 +19,8 @@ class BeIRTask(AbsTask):
 
         USE_BEIR_DEVELOPMENT = False
         try:
+            raise ImportError("MTEB is temporarily incompatible with HFDataLoader")
+                              
             if self.description["beir_name"].startswith("cqadupstack"):
                 raise ImportError("CQADupstack is incompatible with latest BEIR")
             from beir.datasets.data_loader_hf import HFDataLoader as BeirDataLoader
