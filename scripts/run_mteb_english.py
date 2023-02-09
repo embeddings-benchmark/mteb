6ff57d3498cdaefec7658560e210df1faad047c3
Niklas Muennighoff <n.muennighoff@gmail.com>
2023-02-09 20:22:41 +0100

    Update run_mteb_english.py

--- scripts/run_mteb_english.py
+++ scripts/run_mteb_english.py
@@ -1,4 +1,4 @@
-"""Example script for benchmarking all of datasets constituting the MTEB English Leaderboard & Average scores"""
+"""Example script for benchmarking all datasets constituting the MTEB English Leaderboard & Average scores"""
 
 import argparse
 import logging
@@ -104,5 +104,5 @@ model = SentenceTransformer(model_name)
 for task in TASK_LIST:
     logger.info(f"Running task: {task}")
     eval_splits = ["dev"] if task == "MSMARCO" else ["test"]
-    evaluation = MTEB(tasks=[task], task_langs=["en"])
+    evaluation = MTEB(tasks=[task], task_langs=["en"]) # Remove "en" for running all languages
     evaluation.run(model, output_folder=f"results/{model_name}", eval_splits=eval_splits)
