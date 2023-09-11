7d764000ad66ab9903c6433dc2e85f174e2cd3e3
guenthermi <guenthermi50@gmail.com>
2023-09-11 18:02:08 +0200

    feat: add news groups dataset

--- mteb/tasks/Classification/LongDocumentClassification.py
+++ mteb/tasks/Classification/LongDocumentClassification.py
@@ -16,3 +16,20 @@ class PatentClassification(AbsTaskClassification):
             "main_score": "accuracy",
             "revision": "01ab216c0a33cb4163a3ac0f69b9e573017382fd",
         }
+
+
+class NewsGroupsClassification(AbsTaskClassification):
+    @property
+    def description(self):
+        return {
+            "name": "NewsGroupsClassification",
+            "hf_hub_name": "jinaai/20newsgroups",
+            "description": "Category classification evaluation based on the 20 newsgroups dataset",
+            "reference": "http://people.csail.mit.edu/jrennie/20Newsgroups/",
+            "category": "p2p",
+            "type": "Classification",
+            "eval_splits": ["test"],
+            "eval_langs": ["en"],
+            "main_score": "accuracy",
+            "revision": "dc56aa588069550b1399f624ee90fc1017515079",
+        }
