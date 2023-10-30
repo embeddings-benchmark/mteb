ae67a56ee49a216f3e3cf3b20a06578eb25d3176
Isabelle Mohr <isabelle.mohr@jina.ai>
2023-10-30 10:52:27 +0100

    feat: update revision id of wikicitiesclustering task

--- mteb/tasks/Clustering/WikiCitiesClustering.py
+++ mteb/tasks/Clustering/WikiCitiesClustering.py
@@ -9,7 +9,7 @@ class WikiCitiesClustering(AbsTaskClustering):
             "hf_hub_name": "jinaai/cities_wiki_clustering",
             "description": (
                 "Clustering of Wikipedia articles of cities by country from https://huggingface.co/datasets/wikipedia."
-                "Test set includes cities from 133 countries."
+                "Test set includes 126 countries, and a total of 3531 cities."
             ),
             "reference": "https://huggingface.co/datasets/wikipedia",
             "type": "Clustering",
@@ -17,5 +17,5 @@ class WikiCitiesClustering(AbsTaskClustering):
             "eval_splits": ["test"],
             "eval_langs": ["en"],
             "main_score": "v_measure",
-            "revision": "2af05f538fd902351ce5ec7ac84f23f71d52bae1",
+            "revision": "ddc9ee9242fa65332597f70e967ecc38b9d734fa",
         }
