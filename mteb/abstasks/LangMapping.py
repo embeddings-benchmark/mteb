5a06efd8df177da8d9b37108f397802a6599798f
Gabriel Sequeira <gsequeira@openstudio.fr>
2023-11-07 16:06:03 +0100

    add some documentation

--- mteb/abstasks/LangMapping.py
+++ mteb/abstasks/LangMapping.py
@@ -1,5 +1,7 @@
+"""The `LANG_MAPPING` dictionary is mapping bigram, trigram language codes to each others, to include various datasets that use one or the other"""
 LANG_MAPPING = {
     "fr" : "fra",
     "fra" : "fr",
     "en" : "eng"
-} #TODO: Add some lang codes
+} 
+#TODO: Add some lang codes
