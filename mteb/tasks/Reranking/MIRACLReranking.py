f3f7e067fadb2723ee7d89e7bafa6faa927eaa90
Isabelle Mohr <isabelle.mohr@jina.ai>
2023-11-09 14:14:47 +0100

    feat: add miracl reranking task for german

---
+++ mteb/tasks/Reranking/MIRACLReranking.py
@@ -0,0 +1,26 @@
+from collections import defaultdict
+
+import datasets
+
+from mteb.abstasks.AbsTaskReranking import AbsTaskReranking
+
+
+class MIRACLReranking(AbsTaskReranking):
+    @property
+    def description(self):
+        return {
+            'name': 'MIRACL',
+            'hf_hub_name': 'jinaai/miracl',
+            'reference': 'https://project-miracl.github.io/',
+            'description': (
+                'MIRACL (Multilingual Information Retrieval Across a Continuum of Languages) is a multilingual '
+                'retrieval dataset that focuses on search across 18 different languages. This task focuses on '
+                'the German subset, uing the dev set containing 305 queries.'
+            ),
+            'type': 'Reranking',
+            'category': 's2p',
+            'eval_splits': ['dev'],
+            'eval_langs': ['de'],
+            'main_score': 'map',
+            'revision': '8741c3b61cd36ed9ca1b3d4203543a41793239e2',
+        }
--- mteb/tasks/Reranking/__init__.py
+++ mteb/tasks/Reranking/__init__.py
@@ -3,3 +3,4 @@ from .MindSmallReranking import *
 from .SciDocsReranking import *
 from .StackOverflowDupQuestions import *
 from .CMTEBReranking import *
+from .MIRACLReranking import *
