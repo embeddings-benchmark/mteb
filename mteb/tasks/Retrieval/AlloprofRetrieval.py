b615ff42e9f68305f59b92dbcd2836ae66342d8d
mciancone@openstudio.fr <mciancone@openstudio.fr>
2023-11-10 09:47:00 +0100

    adding dataset processing for mteb

--- mteb/tasks/Retrieval/AlloprofRetrieval.py
+++ mteb/tasks/Retrieval/AlloprofRetrieval.py
@@ -4,13 +4,12 @@ from ...abstasks.AbsTaskRetrieval import AbsTaskRetrieval
 
 
 class AlloprofRetrieval(AbsTaskRetrieval):
-
     @property
     def description(self):
         return {
-            'name': 'AlloprofRetrieval',
-            'hf_hub_name': 'lyon-nlp/alloprof',
-            'reference': 'https://huggingface.co/datasets/antoinelb7/alloprof',
+            "name": "AlloprofRetrieval",
+            "hf_hub_name": "lyon-nlp/alloprof",
+            "reference": "https://huggingface.co/datasets/antoinelb7/alloprof",
             "description": (
                 "This dataset was provided by AlloProf, an organisation in Quebec, Canada offering resources and a help forum"
                 "curated by a large number of teachers to students on all subjects taught from in primary and secondary school"
@@ -26,25 +25,15 @@ class AlloprofRetrieval(AbsTaskRetrieval):
         if self.data_loaded:
             return
         # fetch both subsets of the dataset
-        corpus_raw = datasets.load_dataset(self.description['hf_hub_name'], "documents")
-        queries_raw = datasets.load_dataset(self.description['hf_hub_name'], "queries")
+        corpus_raw = datasets.load_dataset(self.description["hf_hub_name"], "documents")
+        queries_raw = datasets.load_dataset(self.description["hf_hub_name"], "queries")
 
-        self.queries = {
-            "test": {
-                str(q["id"]):q["text"] for q
-                in queries_raw["queries"]
-                }
-            }
-        self.corpus = {
-            "test": {
-                str(d["uuid"]):{"text":d["text"]} for d
-                in corpus_raw["documents"]
-                }
-            }
+        self.queries = {"test": {str(q["id"]): q["text"] for q in queries_raw["queries"]}}
+        self.corpus = {"test": {str(d["uuid"]): {"text": d["text"]} for d in corpus_raw["documents"]}}
 
         self.relevant_docs = {"test": {}}
         for q in queries_raw["queries"]:
             for r in q["relevant"]:
-                self.relevant_docs["test"][str(q["id"])] = {r:1}
+                self.relevant_docs["test"][str(q["id"])] = {r: 1}
 
         self.data_loaded = True
