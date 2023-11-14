09947f8916de33841f17e308f79bf0364c6fd98b
Gabriel Sequeira <gsequeira@openstudio.fr>
2023-11-14 11:41:15 +0100

    alphabetic order

--- mteb/tasks/Classification/__init__.py
+++ mteb/tasks/Classification/__init__.py
@@ -3,6 +3,7 @@ from .AmazonPolarityClassification import *
 from .AmazonReviewsClassification import *
 from .AngryTweetsClassification import *
 from .Banking77Classification import *
+from .CMTEBClassification import *
 from .DalajClassification import *
 from .DanishPoliticalCommentsClassification import *
 from .DKHateClassification import *
@@ -14,12 +15,11 @@ from .MassiveIntentClassification import *
 from .MassiveScenarioClassification import *
 from .MTOPDomainClassification import *
 from .MTOPIntentClassification import *
-from .NoRecClassification import *
 from .NordicLangClassification import *
+from .NoRecClassification import *
 from .NorwegianParliamentClassification import *
+from .PolishClassification import *
 from .ScalaClassification import *
 from .SweRecClassification import *
 from .ToxicConversationsClassification import *
 from .TweetSentimentExtractionClassification import *
-from .CMTEBClassification import *
-from .PolishClassification import *
