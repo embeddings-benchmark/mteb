4b18a7e49b07b94f4eff215a1d0f1cc7b69302fd
rasdani <73563550+rasdani@users.noreply.github.com>
2024-01-05 19:27:46 +0100

    Update scripts/run_mteb_german.py
    Co-authored-by: Niklas Muennighoff <n.muennighoff@gmail.com>

--- scripts/run_mteb_german.py
+++ scripts/run_mteb_german.py
@@ -9,19 +9,10 @@ from sentence_transformers import SentenceTransformer
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger("main")
 
-if os.path.exists("results/de/multilingual-e5-small/GermanQuAD-Retrieval.json"):
-    os.remove("results/de/multilingual-e5-small/GermanQuAD-Retrieval.json")
-
-if os.path.exists("results/pl/multilingual-e5-small/GermanQuAD-Retrieval.json"):
-    os.remove("results/pl/multilingual-e5-small/GermanQuAD-Retrieval.json")
-
-
-retrieval_tasks = ["GermanQuAD-Retrieval"]
-
-tasks =  retrieval_tasks
+tasks = ["GermanQuAD-Retrieval"]
 
 model_name = "intfloat/multilingual-e5-small"
 model = SentenceTransformer(model_name)
 
 evaluation = MTEB(tasks=tasks)
-evaluation.run(model, output_folder=f"results/de/{model_name.split('/')[-1]}")
+evaluation.run(model, overwrite_results=True, output_folder=f"results/de/{model_name.split('/')[-1]}")
