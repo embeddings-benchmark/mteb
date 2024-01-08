d23eac312e8e8c067c5f7fb1354829cafee0de3b
rasdani <73563550+rasdani@users.noreply.github.com>
2024-01-08 19:13:15 +0100

    remove code for writing JSONL dataset

--- scripts/data/germanquad/process_data.py
+++ scripts/data/germanquad/process_data.py
@@ -7,13 +7,6 @@ from datasets import load_dataset, Dataset, DatasetDict, Features, Value
 dataset = load_dataset("deepset/germanquad")
 dataset.pop("train")
 
-PATH = "scripts/data/germanquad/"
-
-corpus_file_path = PATH + "corpus.jsonl"
-queries_file_path = PATH + "queries.jsonl"
-os.makedirs(PATH + "qrels/", exist_ok=True)
-qrels_file_path = PATH + "qrels/test.tsv"
-
 # Deduplicate contexts and map them uniquely one-to-one to ids
 context_to_id = {}
 
@@ -21,40 +14,36 @@ corpus_data = {"_id": [], "text": []}
 queries_data = {"_id": [], "text": []}
 qrels_data = {"query-id": [], "corpus-id": [], "score": []}
 
-with open(corpus_file_path, 'w') as file:
-    for item in dataset["test"]:
-        # Check if the context is already in the dictionary
-        if item["context"] not in context_to_id:
-            context_to_id[item["context"]] = "c" + str(item["id"])
-            entry = {
-                "_id": context_to_id[item["context"]],
-                "text": item["context"]
-            }
-            corpus_data["_id"].append(entry["_id"])
-            corpus_data["text"].append(entry["text"])
-
-with open(queries_file_path, 'w') as file:
-    for item in dataset["test"]:
+for item in dataset["test"]:
+    # Check if the context is already in the dictionary
+    if item["context"] not in context_to_id:
+        context_to_id[item["context"]] = "c" + str(item["id"])
         entry = {
-            "_id": "q" + str(item["id"]),
-            "text": item["question"]
+            "_id": context_to_id[item["context"]],
+            "text": item["context"]
         }
-        queries_data["_id"].append(entry["_id"])
-        queries_data["text"].append(entry["text"])
+        corpus_data["_id"].append(entry["_id"])
+        corpus_data["text"].append(entry["text"])
+
+for item in dataset["test"]:
+    entry = {
+        "_id": "q" + str(item["id"]),
+        "text": item["question"]
+    }
+    queries_data["_id"].append(entry["_id"])
+    queries_data["text"].append(entry["text"])
 
 # this maps queries to relevant documents
-with open(qrels_file_path, 'w') as file:
-    file.write('\t'.join(["query-id", "corpus-id", "score"]) + '\n')
-    for item in dataset["test"]:
-        corpus_id = context_to_id[item["context"]]
-        entry = {
-            "query-id": "q" + str(item["id"]),
-            "corpus-id": corpus_id,
-            "score": 1
-        }
-        qrels_data["query-id"].append(entry["query-id"])
-        qrels_data["corpus-id"].append(entry["corpus-id"])
-        qrels_data["score"].append(entry["score"])
+for item in dataset["test"]:
+    corpus_id = context_to_id[item["context"]]
+    entry = {
+        "query-id": "q" + str(item["id"]),
+        "corpus-id": corpus_id,
+        "score": 1
+    }
+    qrels_data["query-id"].append(entry["query-id"])
+    qrels_data["corpus-id"].append(entry["corpus-id"])
+    qrels_data["score"].append(entry["score"])
 
 corpus_features = Features({
     "_id": Value("string"),
