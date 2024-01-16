9440435ea573f116897202ba88eb2291e5b66888
Isabelle Mohr <isabelle.mohr@jina.ai>
2024-01-16 16:03:31 +0100

    feat: use hf repo with all reranking langs

--- mteb/tasks/Reranking/MIRACLReranking.py
+++ mteb/tasks/Reranking/MIRACLReranking.py
@@ -3,7 +3,7 @@ from ...abstasks.AbsTaskReranking import AbsTaskReranking
 import datasets
 
 
-class MIRACLReranking(AbsTaskReranking):
+class MIRACLRerankingDE(AbsTaskReranking):
     @property
     def description(self):
         return {
@@ -34,5 +34,38 @@ class MIRACLReranking(AbsTaskReranking):
             self.description["hf_hub_name"],
             self.description["eval_langs"][0],
             revision=self.description.get("revision", None),
+        )
+        self.data_loaded = True
+
+class MIRACLRerankingES(AbsTaskReranking):
+    @property
+    def description(self):
+        return {
+            'name': 'MIRACLES',
+            'hf_hub_name': 'jinaai/miracl-es',
+            'reference': 'https://project-miracl.github.io/',
+            'description': (
+                'MIRACL (Multilingual Information Retrieval Across a Continuum of Languages) is a multilingual '
+                'retrieval dataset that focuses on search across 18 different languages. This task focuses on '
+                'the Spanish subset, using the test set containing 648 queries and 6443 passages.'
+            ),
+            'type': 'Reranking',
+            'category': 's2p',
+            'eval_splits': ['test'],
+            'eval_langs': ['es'],
+            'main_score': 'map',
+            'revision': '',
+        }
+
+    def load_data(self, **kwargs):
+        """
+        Load dataset from HuggingFace hub
+        """
+        if self.data_loaded:
+            return
+
+        # TODO: add split argument
+        self.dataset = datasets.load_dataset(
+            self.description["hf_hub_name"], 'es', revision=self.description.get("revision", None)
         )
         self.data_loaded = True
