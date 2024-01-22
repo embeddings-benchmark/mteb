602d34e6d6dd1c6f85588e0e49a5ec755d69385c
Imene Kerboua <imenelydia.kr@gmail.com>
2024-01-22 14:23:50 +0100

    Fix DiaBLa by inheriting CrossLingual class

--- mteb/tasks/BitextMining/DiaBLaBitextMining.py
+++ mteb/tasks/BitextMining/DiaBLaBitextMining.py
@@ -1,9 +1,10 @@
+import json
 import datasets
 
-from ...abstasks import AbsTaskBitextMining
+from ...abstasks import AbsTaskBitextMining, CrosslingualTask
 
 
-class DiaBLaBitextMining(AbsTaskBitextMining):
+class DiaBLaBitextMining(AbsTaskBitextMining, CrosslingualTask):
     @property
     def description(self):
         return {
@@ -18,7 +19,7 @@ class DiaBLaBitextMining(AbsTaskBitextMining):
             "type": "BitextMining",
             "category": "s2s",
             "eval_splits": ["test"],
-            "eval_langs": ["fr"],
+            "eval_langs": ["fr", "en"],
             "main_score": "f1",
             "revision": "5345895c56a601afe1a98519ce3199be60a27dba",
         }
@@ -30,14 +31,29 @@ class DiaBLaBitextMining(AbsTaskBitextMining):
         if self.data_loaded:
             return
 
-        self.dataset = datasets.load_dataset(
-            self.description["hf_hub_name"],
-            revision=self.description.get("revision", None),
-        )
+        self.dataset = {}
+        for lang in self.langs:
+            print(lang)
+            self.dataset[lang] = datasets.load_dataset(
+                self.description["hf_hub_name"],
+                revision=self.description.get("revision", None),
+            )
+
         self.dataset_transform()
         self.data_loaded = True
 
     def dataset_transform(self):
+        def create_columns(row):
+            """Put all French texts in column 'sentence1' and English texts in 'sentence2' column"""
+            row["orig_lang"] = row["utterance_meta"]["lang"]
+            row["sentence1"] = row["orig"] if row["orig_lang"] == "french" else row["ref"]
+            row["sentence2"] = row["orig"] if not row["orig_lang"] == "french" else row["ref"]
+            return row
+
         # Convert to standard format
-        self.dataset = self.dataset.rename_column("orig", "sentence1")
-        self.dataset = self.dataset.rename_column("ref", "sentence2")
+        for lang in self.langs:
+            self.dataset[lang] = self.dataset[lang].map(create_columns)
+            if lang == "en":
+                self.dataset[lang]["test"] = self.dataset[lang]["test"].rename_column("sentence2", "sentence_1")
+                self.dataset[lang]["test"] = self.dataset[lang]["test"].rename_column("sentence1", "sentence2")
+                self.dataset[lang]["test"] = self.dataset[lang]["test"].rename_column("sentence_1", "sentence1")
