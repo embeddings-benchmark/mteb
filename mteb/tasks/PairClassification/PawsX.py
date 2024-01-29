ab23dd15e1a535a9f426457ad85d046cc3b40a7a
Michael <michael.guenther@jina.ai>
2024-01-29 12:02:53 +0100

    support multilingual pair classification

--- mteb/tasks/PairClassification/PawsX.py
+++ mteb/tasks/PairClassification/PawsX.py
@@ -1,9 +1,10 @@
 import datasets
 
+from ...abstasks import MultilingualTask
 from ...abstasks.AbsTaskPairClassification import AbsTaskPairClassification
 
 
-class PawsX(AbsTaskPairClassification):
+class PawsX(MultilingualTask, AbsTaskPairClassification):
     @property
     def description(self):
         return {
@@ -14,7 +15,7 @@ class PawsX(AbsTaskPairClassification):
             "category": "s2s",
             "type": "PairClassification",
             "eval_splits": ["test"],
-            "eval_langs": ["de"],
+            "eval_langs": ["de", "en", "es", "fr", "ja", "ko", "zh"],
             "main_score": "ap",
             "revision": "8a04d940a42cd40658986fdd8e3da561533a3646",
         }
@@ -23,29 +24,32 @@ class PawsX(AbsTaskPairClassification):
         if self.data_loaded:
             return
 
-        hf_dataset = datasets.load_dataset(
-            self.description["hf_hub_name"],
-            self.description.get('eval_langs', ['en'])[0],
-            revision=self.description.get("revision", None),
-        )
+        print(kwargs, self.is_multilingual, self.langs)
+        self.dataset = dict()
+        for lang in self.langs:
+            hf_dataset = datasets.load_dataset(
+                self.description["hf_hub_name"],
+                lang,
+                revision=self.description.get("revision", None),
+            )
 
-        sent1 = []
-        sent2 = []
-        labels = []
+            sent1 = []
+            sent2 = []
+            labels = []
 
-        for line in hf_dataset['test']:
-            sent1.append(line['sentence1'])
-            sent2.append(line['sentence2'])
-            labels.append(line['label'])
+            for line in hf_dataset["test"]:
+                sent1.append(line["sentence1"])
+                sent2.append(line["sentence2"])
+                labels.append(line["label"])
 
-        self.dataset = {
-            'test': [
-                {
-                    'sent1': sent1,
-                    'sent2': sent2,
-                    'labels': labels,
-                }
-            ]
-        }
+            self.dataset[lang] = {
+                "test": [
+                    {
+                        "sent1": sent1,
+                        "sent2": sent2,
+                        "labels": labels,
+                    }
+                ]
+            }
 
         self.data_loaded = True
