b6cbec6b71b578df748a6ad22d03eff3f2691313
Kenneth Enevoldsen <kennethcenevoldsen@gmail.com>
2024-03-15 11:52:44 +0100

    fix: Added missing init files

---
+++ mteb/tasks/PairClassification/__init__.py
@@ -0,0 +1,7 @@
+from .zh.CMTEBPairClassification import *
+from .multilingual.OpusparcusPC import *
+from .pl.PolishPC import *
+from .multilingual.PawsX import *
+from .en.SprintDuplicateQuestionsPC import *
+from .en.TwitterSemEval2015PC import *
+from .en.TwitterURLCorpusPC import *
--- mteb/tasks/PairClassification/pl/__init__.py
+++
@@ -1,7 +0,0 @@
-from ..zh.CMTEBPairClassification import *
-from ..multilingual.OpusparcusPC import *
-from .PolishPC import *
-from ..multilingual.PawsX import *
-from ..en.SprintDuplicateQuestionsPC import *
-from ..en.TwitterSemEval2015PC import *
-from ..en.TwitterURLCorpusPC import *
