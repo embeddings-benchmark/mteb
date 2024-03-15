236614add5de4e848bc0f1db8ad997f451ae2906
Martin Bernstorff <martinbernstorff@gmail.com>
2024-03-15 18:30:03 +0100

    tests: do not run tests on collection (#249)
    test: update test_all_abstasks.py

--- tests/test_all_abstasks.py
+++ tests/test_all_abstasks.py
@@ -7,17 +7,19 @@ from mteb.tasks.BitextMining import BUCCBitextMining
 
 logging.basicConfig(level=logging.INFO)
 
-model = SentenceTransformer("average_word_embeddings_komninos")
-eval = MTEB(
-    tasks=[
-        BUCCBitextMining(),
-        "Banking77Classification",
-        "TwentyNewsgroupsClustering",
-        "SciDocsRR",
-        "SprintDuplicateQuestions",
-        "NFCorpus",
-        "STS12",
-        "SummEval",
-    ]
-)
-eval.run(model)
+
+def test_mteb_tasks():
+    model = SentenceTransformer("average_word_embeddings_komninos")
+    eval = MTEB(
+        tasks=[
+            BUCCBitextMining(),
+            "Banking77Classification",
+            "TwentyNewsgroupsClustering",
+            "SciDocsRR",
+            "SprintDuplicateQuestions",
+            "NFCorpus",
+            "STS12",
+            "SummEval",
+        ]
+    )
+    eval.run(model)
