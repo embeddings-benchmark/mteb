364be7f4a26275263c9a82de594e47aaf28a1bcf
Kenneth Enevoldsen <kennethcenevoldsen@gmail.com>
2024-03-19 17:11:34 +0100

    restructing the readme

--- tests/test_all_abstasks.py
+++ tests/test_all_abstasks.py
@@ -1,25 +1,61 @@
 import logging
+from typing import Union
 
+import pytest
 from sentence_transformers import SentenceTransformer
 
 from mteb import MTEB
+from mteb.abstasks import AbsTask
 from mteb.tasks.BitextMining import BUCCBitextMining
 
 logging.basicConfig(level=logging.INFO)
+from mteb import MTEB
 
 
-def test_mteb_tasks():
+def test_two_mteb_tasks():
+    """
+    Test that two tasks can be fetched and run
+    """
     model = SentenceTransformer("average_word_embeddings_komninos")
     eval = MTEB(
         tasks=[
-            BUCCBitextMining(),
-            "Banking77Classification",
-            "TwentyNewsgroupsClustering",
-            "SciDocsRR",
-            "SprintDuplicateQuestions",
-            "NFCorpus",
             "STS12",
             "SummEval",
         ]
     )
-    eval.run(model)
+    eval.run(model, output_folder="tests/results", overwrite_results=True)
+
+
+@pytest.mark.parametrize(
+    "task",
+    [
+        BUCCBitextMining(),
+        "TwentyNewsgroupsClustering",
+        "Banking77Classification",
+        "SciDocsRR",
+        "SprintDuplicateQuestions",
+        "NFCorpus",
+        "STS12",
+        "SummEval",
+    ],
+)
+@pytest.mark.parametrize(
+    "model_name",
+    [
+        "average_word_embeddings_komninos",
+    ],
+)
+def test_mteb_task(task: Union[str, AbsTask], model_name: str):
+    """
+    Test that a task can be fetched and run
+    """
+    model = SentenceTransformer(model_name)
+    eval = MTEB(tasks=[task])
+    eval.run(model, output_folder="tests/results", overwrite_results=True)
+
+
+def test_all_tasks_fetch():
+    """
+    Test that all tasks can be fetched
+    """
+    MTEB.mteb_tasks()
