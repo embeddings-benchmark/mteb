e1ae0d367eda70e6be31f0a43e20aed7cf75fedd
Kenneth Enevoldsen <kennethcenevoldsen@gmail.com>
2024-03-31 17:17:28 +0200

    fix: Fixed mispecified rev. id for datasets (#298)
    * fix: fixed wrong rev. id for ToxicConversationsClassification
    
    * fix: fixed wrong rev. id with RedditClusteringP2P

--- mteb/tasks/Clustering/en/RedditClusteringP2P.py
+++ mteb/tasks/Clustering/en/RedditClusteringP2P.py
@@ -16,7 +16,7 @@ class RedditClusteringP2P(AbsTaskClustering):
         eval_splits=["test"],
         eval_langs=["en"],
         main_score="v_measure",
-        revision="24640382cdbf8abc73003fb0fa6d111a705499eb",
+        revision="385e3cb46b4cfa89021f56c4380204149d0efe33",
         date=None,
         form=None,
         domains=None,
