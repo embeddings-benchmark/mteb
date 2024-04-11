c1d23f44b607727645c7b23871e8c8bd72cb1320
Imene Kerboua <imenelydia.kr@gmail.com>
2024-04-11 10:29:26 +0200

    fix: remove remaining

--- mteb/tasks/PairClassification/__init__.py
+++ mteb/tasks/PairClassification/__init__.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 from .eng.SprintDuplicateQuestionsPC import *
 from .eng.TwitterSemEval2015PC import *
 from .eng.TwitterURLCorpusPC import *
-from .multilingual.OpusparcusPC import *
 from .multilingual.PawsX import *
 from .pol.PolishPC import *
 from .zho.CMTEBPairClassification import *
