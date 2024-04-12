6c1bafb5d5ac291412cfabbf539d3c01dea6ebe8
lm <loic.magne@outlook.com>
2024-04-12 09:40:27 +0200

    256 sentences

--- mteb/tasks/BitextMining/__init__.py
+++ mteb/tasks/BitextMining/__init__.py
@@ -5,6 +5,6 @@ from .multilingual.BUCCBitextMining import *
 from .multilingual.DiaBLaBitextMining import *
 from .multilingual.FloresBitextMining import *
 from .multilingual.NorwegianCourtsBitextMining import *
-from .multilingual.OpenSubtitles250BitextMining import *
+from .multilingual.OpenSubtitles1kBitextMining import *
 from .multilingual.TatoebaBitextMining import *
 from .nb.norwegian_courts_bitext_mining import *
