217780222368252531fd769d52e2cf1ffe70149a
David Stap <dd.stap@gmail.com>
2024-04-17 20:17:54 +0200

    update with results

--- mteb/tasks/BitextMining/multilingual/TedTalksBitextMining.py
+++ mteb/tasks/BitextMining/multilingual/TedTalksBitextMining.py
@@ -4,9 +4,8 @@ from typing import Any
 
 from datasets import DatasetDict, load_dataset
 
+from mteb.abstasks import AbsTaskBitextMining, CrosslingualTask
 from mteb.abstasks.TaskMetadata import TaskMetadata
-
-from ....abstasks import AbsTaskBitextMining, CrosslingualTask
 
 # Mapping from ISO 639-3 + script to ISO 639-1 used in TED
 _ISO6393_to_ISO6391 = {
@@ -14,60 +13,59 @@ _ISO6393_to_ISO6391 = {
     "aze_Latn": "az",
     "bel_Cyrl": "be",
     "bul_Cyrl": "bg",
-    # "ben_Beng": "bn",
-    # "bos_Latn": "bs",
-    # "ces_Latn": "cs",
-    # "dan_Latn": "da",
-    # "deu_Latn": "de",
-    # "ell_Grek": "el",
-    # "eng_Latn": "en",
-    # "epo_Latn": "eo",
-    # "spa_Latn": "es",
-    # "est_Latn": "et",
-    # "eus_Latn": "eu",
-    # "fas_Arab": "fa", # this seems new language
-    # "fin_Latn": "fi",
-    # "fra_Latn": "fr",
-    # "glg_Latn": "gl",
-    # "heb_Hebr": "he",
-    # "hin_Deva": "hi",
-    # "hrv_Latn": "hr",
-    # "hun_Latn": "hu",
-    # "hye_Armn": "hy",
-    # "ind_Latn": "id",
-    # "ita_Latn": "it",
-    # "jpn_Jpan": "ja",
-    # "kat_Geor": "ka",
-    # "kaz_Cyrl": "kk",
-    # "kor_Hang": "ko",
-    # "kur_Arab": "ku", # this seems new language
-    # "lit_Latn": "lt",
-    # "mkd_Cyrl": "mk",
-    # "mon_Cyrl": "mn", # this seems new language
-    # "mar_Deva": "mr",
-    # "msa_Latn": "ms", # this seems new language
-    # "mya_Mymr": "my",
-    # "nob_Latn": "nb",
-    # "nld_Latn": "nl",
-    # "pol_Latn": "pl",
-    # "por_Latn": "pt",
-    # "ron_Latn": "ro",
-    # "rus_Cyrl": "ru",
-    # "slk_Latn": "sk",
-    # "slv_Latn": "sl",
-    # "sqi_Latn": "sq", # this seems new language
-    # "srp_Cyrl": "sr",
-    # "swe_Latn": "sv",
-    # "tam_Taml": "ta",
-    # "tha_Thai": "th",
-    # "tur_Latn": "tr",
-    # "ukr_Cyrl": "uk",
-    # "urd_Arab": "ur",
-    # "vie_Latn": "vi",
-    # "zho_Hans": "zh-cn", # new dialect
-    # "zho_Hant": "zh-tw", # new dialect
+    "ben_Beng": "bn",
+    "bos_Latn": "bs",
+    "ces_Latn": "cs",
+    "dan_Latn": "da",
+    "deu_Latn": "de",
+    "ell_Grek": "el",
+    "eng_Latn": "en",
+    "epo_Latn": "eo",
+    "spa_Latn": "es",
+    "est_Latn": "et",
+    "eus_Latn": "eu",
+    "fas_Arab": "fa",
+    "fin_Latn": "fi",
+    "fra_Latn": "fr",
+    "glg_Latn": "gl",
+    "heb_Hebr": "he",
+    "hin_Deva": "hi",
+    "hrv_Latn": "hr",
+    "hun_Latn": "hu",
+    "hye_Armn": "hy",
+    "ind_Latn": "id",
+    "ita_Latn": "it",
+    "jpn_Jpan": "ja",
+    "kat_Geor": "ka",
+    "kaz_Cyrl": "kk",
+    "kor_Hang": "ko",
+    "kur_Arab": "ku",
+    "lit_Latn": "lt",
+    "mkd_Cyrl": "mk",
+    "mon_Cyrl": "mn",
+    "mar_Deva": "mr",
+    "msa_Latn": "ms",
+    "mya_Mymr": "my",
+    "nob_Latn": "nb",
+    "nld_Latn": "nl",
+    "pol_Latn": "pl",
+    "por_Latn": "pt",
+    "ron_Latn": "ro",
+    "rus_Cyrl": "ru",
+    "slk_Latn": "sk",
+    "slv_Latn": "sl",
+    "sqi_Latn": "sq",
+    "srp_Cyrl": "sr",
+    "swe_Latn": "sv",
+    "tam_Taml": "ta",
+    "tha_Thai": "th",
+    "tur_Latn": "tr",
+    "ukr_Cyrl": "uk",
+    "urd_Arab": "ur",
+    "vie_Latn": "vi",
+    "zho_Hans": "zh-cn",
+    "zho_Hant": "zh-tw",
 }
-_ISO6391_to_ISO6393 = {v: k for k, v in _ISO6393_to_ISO6391.items()}
 
 _SPLIT = ["train"]
 
@@ -90,7 +88,7 @@ def extend_lang_pairs() -> dict[str, list[str]]:
     return hf_lang_subset2isolang
 
 
-_ISO6393_to_ISO6391_MAPPING = extend_lang_pairs()
+_EVAL_LANGS = extend_lang_pairs()
 
 
 class TedTalksBitextMining(AbsTaskBitextMining, CrosslingualTask):
@@ -106,17 +104,17 @@ class TedTalksBitextMining(AbsTaskBitextMining, CrosslingualTask):
         type="BitextMining",
         category="s2s",
         eval_splits=_SPLIT,
-        eval_langs=_ISO6393_to_ISO6391_MAPPING,
+        eval_langs=_EVAL_LANGS,
         main_score="f1",
-        date=None,
-        form=None,
+        date=("2018-06-01", "2018-06-01"),
+        form=["written"],
         domains=["Spoken"],
         task_subtypes=None,
-        license=None,
-        socioeconomic_status=None,
-        annotations_creators=None,
+        license="CC-BY-NC-ND-4.0",
+        socioeconomic_status="medium",
+        annotations_creators="human-annotated",
         dialect=None,
-        text_creation=None,
+        text_creation="created",
         n_samples={"train": _N},
         avg_character_length={},
         bibtex_citation="""
@@ -138,7 +136,6 @@ class TedTalksBitextMining(AbsTaskBitextMining, CrosslingualTask):
     url = "https://aclanthology.org/N18-2084",
     doi = "10.18653/v1/N18-2084",
     pages = "529--535",
-    abstract = "The performance of Neural Machine Translation (NMT) systems often suffers in low-resource scenarios where sufficiently large-scale parallel corpora cannot be obtained. Pre-trained word embeddings have proven to be invaluable for improving performance in natural language analysis tasks, which often suffer from paucity of data. However, their utility for NMT has not been extensively explored. In this work, we perform five sets of experiments that analyze when we can expect pre-trained word embeddings to help in NMT tasks. We show that such embeddings can be surprisingly effective in some cases {--} providing gains of up to 20 BLEU points in the most favorable setting.",
 }
 """,
     )
@@ -151,12 +148,11 @@ class TedTalksBitextMining(AbsTaskBitextMining, CrosslingualTask):
             return
         self.dataset = {}
         for lang in self.langs:
-            self.metadata_dict["dataset"]["path"] = "davidstap/ted_talks"
             self.dataset[lang] = DatasetDict(
                 {
                     "train": load_dataset(
                         name="_".join(
-                            [_ISO6393_to_ISO6391[l] for l in lang.split("-")]
+                            sorted([_ISO6393_to_ISO6391[l] for l in lang.split("-")])
                         ),
                         split=f"train[:{_N}]",
                         **self.metadata_dict["dataset"],
