a41ca047ad67c4d8880d85556e926dc3ab55c346
Ömer Veysel Çağatan <72755761+asparius@users.noreply.github.com>
2024-04-17 15:48:24 +0300

    Update TurkishMovieSentimentClassification.py

--- mteb/tasks/Classification/tur/TurkishMovieSentimentClassification.py
+++ mteb/tasks/Classification/tur/TurkishMovieSentimentClassification.py
@@ -19,7 +19,7 @@ class TurkishMovieSentimentClassification(AbsTaskClassification):
         eval_splits=["test"],
         eval_langs=["tur-Latn"],
         main_score="accuracy",
-        date=("2013-01-01","2014-01-01"),
+        date=("01-01-2013","11-08-2013"),
         form=["written"],
         domains=["Reviews"],
         task_subtypes=["Sentiment/Hate speech"],
