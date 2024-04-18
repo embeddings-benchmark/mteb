1dc3cca0bb9506bb0e6507ab436a1f5a61d14491
Kenneth Enevoldsen <kennethcenevoldsen@gmail.com>
2024-04-18 15:00:54 +0200

    Added experiments on downsampling

--- exp.py
+++ exp.py
@@ -1,52 +1,47 @@
-from mteb import ArxivClusteringP2P
+"""
+Experimental script to test the effect of downsampling on the results of the MTEB benchmark. Will not be submitted.
+"""
 
-task = ArxivClusteringP2P() 
+from mteb import ArxivClusteringS2S
+
+target_task = ArxivClusteringS2S
+task = target_task()
 
 task.load_data()
 
 
-# checking that there are no duplicates
-# for i in range(len(task.dataset["test"]["sentences"])):
-#     print(
-#         len(task.dataset["test"]["sentences"][i]),
-#         len(set(task.dataset["test"]["sentences"][i])),
-#     )
-
-# t = list(zip(task.dataset["test"]["sentences"][9], task.dataset["test"]["labels"][9]))
-
-# checking that there are no cross duplicates
-
 t = []
 for i in range(len(task.dataset["test"]["sentences"])):
-    t += list(zip(task.dataset["test"]["sentences"][i], task.dataset["test"]["labels"][i]))
-    
-# assert len(t) == len(set(t)) # this is false
+    t += list(
+        zip(task.dataset["test"]["sentences"][i], task.dataset["test"]["labels"][i])
+    )
+
+# assert len(t) == len(set(t)) # this is false(!)
 
 from sentence_transformers import SentenceTransformer
+
 from mteb import MTEB
 
-# bench = MTEB(tasks=[task])
+bench = MTEB(tasks=[task])
 
 model_name = "sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2"
 model = SentenceTransformer(model_name)
 
-# bench.run(model, output_folder="tmp_res/results_before", overwrite_results=True)
+bench.run(model, output_folder="tmp_res/results_before", overwrite_results=False)
 
 # downsample task
 dataset = {"test": []}
 
 for i in range(len(task.dataset["test"]["sentences"])):
-    # dataset["test"]["sentences"].append(task.dataset["test"]["sentences"][i][:1000])
-    # dataset["test"]["labels"].append(task.dataset["test"]["labels"][i][:1000])
     dataset["test"].append(
         {
-            "sentences": task.dataset["test"]["sentences"][i][:5000],
-            "labels": task.dataset["test"]["labels"][i][:5000],
+            "sentences": task.dataset["test"]["sentences"][i][:10000],
+            "labels": task.dataset["test"]["labels"][i][:10000],
         }
     )
-downsampled_task = ArxivClusteringP2P()
+downsampled_task = target_task()
 downsampled_task.dataset = dataset
 downsampled_task.data_loaded = True
 
 bench = MTEB(tasks=[downsampled_task])
-bench.run(model, output_folder="tmp_res/results_after_5k", overwrite_results=True)
+bench.run(model, output_folder="tmp_res/results_after_10k", overwrite_results=True)
