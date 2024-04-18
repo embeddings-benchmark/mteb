a0fa60b32335e31c0aa6dbb2861d14c3c3ce06e8
Manan Dey <manandey1@gmail.com>
2024-04-18 15:00:52 +0530

    fix: Id clickbait (#411)
    * add indonesian clickbait
    
    * Update points.md
    
    * update
    
    ---------
    
    Co-authored-by: Manan Dey <159106637+manandey-sfdx@users.noreply.github.com>

--- mteb/tasks/Classification/__init__.py
+++ mteb/tasks/Classification/__init__.py
@@ -20,6 +20,7 @@ from .eng.ToxicConversationsClassification import *
 from .eng.TweetSentimentExtractionClassification import *
 from .est.estonian_valence import *
 from .hin.HindiDiscourseClassification import *
+from .ind.IndonesianIdClickbaitClassification import *
 from .jpn.WRIMEClassification import *
 from .mkd.MacedonianTweetSentimentClassification import *
 from .multilingual.AmazonCounterfactualClassification import *
