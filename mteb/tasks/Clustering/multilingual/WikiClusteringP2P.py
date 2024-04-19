eb5b386efc6bb965efa5a6b07a5b52d83b792788
Jonathan HR <39084161+Rysias@users.noreply.github.com>
2024-04-19 12:50:47 +0200

    Add Wikiclustering benchmark for multiple languages:)) (#376)
    * add tasks and leaderboard update
    
    * change names to the right ones:))
    
    * update points and start migrating to multilingual format
    
    * add metadata and working revision
    
    * bump revision to newest version of data
    
    * Update mteb/tasks/Clustering/multilingual/WikiClusteringP2P.py
    
    Co-authored-by: Kenneth Enevoldsen <kennethcenevoldsen@gmail.com>
    
    * Update mteb/tasks/Clustering/multilingual/WikiClusteringP2P.py
    
    Co-authored-by: Kenneth Enevoldsen <kennethcenevoldsen@gmail.com>
    
    * Update mteb/tasks/Clustering/multilingual/WikiClusteringP2P.py
    
    Co-authored-by: Kenneth Enevoldsen <kennethcenevoldsen@gmail.com>
    
    * Update mteb/tasks/Clustering/multilingual/WikiClusteringP2P.py
    
    Co-authored-by: Kenneth Enevoldsen <kennethcenevoldsen@gmail.com>
    
    * Update mteb/tasks/Clustering/multilingual/WikiClusteringP2P.py
    
    Co-authored-by: Kenneth Enevoldsen <kennethcenevoldsen@gmail.com>
    
    * update to match main
    
    * add date and fix literal
    
    * add result for MiniLM-L12-v2
    
    * add scores for intfloat model
    
    ---------
    
    Co-authored-by: Kenneth Enevoldsen <kennethcenevoldsen@gmail.com>

---
+++ mteb/tasks/Clustering/multilingual/WikiClusteringP2P.py
@@ -0,0 +1,40 @@
+from __future__ import annotations
+
+from mteb.abstasks import AbsTaskClustering, MultilingualTask
+from mteb.abstasks.TaskMetadata import TaskMetadata
+
+_LANGUAGES = {
+    "da": ["dan-Latn"],
+    "lv": ["lav-Latn"],
+    "gv": ["glv-Latn"],
+    "sq": ["sqi-Latn"],
+}
+
+class WikiClusteringP2P(AbsTaskClustering, MultilingualTask):
+    metadata = TaskMetadata(
+        name="WikiClusteringP2P",
+        description="Clustering of wikipedia articles inspired by BlubrbsClusteringP2P. Labels are taken from top-level categories of the respective languages (e.g., https://lv.wikipedia.org/wiki/Kategorija:Pamatkategorijas).",
+        reference="https://github.com/Rysias/wiki-clustering",
+        dataset={
+            "path": "ryzzlestrizzle/multi-wiki-clustering-p2p",
+            "revision": "7f1d0674aff24ac2b8c3c216e9128ba0f91a5cf4",
+        },
+        type="Clustering",
+        category="p2p",
+        eval_splits=["test"],
+        eval_langs=_LANGUAGES,
+        main_score="v_measure",
+        date=("2001-01-15", "2024-04-15"),
+        form=["written"],
+        domains=["Encyclopaedic"],
+        task_subtypes=["Thematic clustering"],
+        license="cc-by-sa-3.0",
+        socioeconomic_status="mixed",
+        annotations_creators="derived",
+        dialect=[],
+        text_creation="created",
+        bibtex_citation=None, # None exists
+        n_samples={"test": 40960},
+        avg_character_length={"test": 570.6},
+    )
+
