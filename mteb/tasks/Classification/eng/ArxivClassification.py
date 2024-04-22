a40dfc927c9eb58d7a07b7674abeb1bb557080c8
shreeya-dhakal <ssdhakal57@gmail.com>
2024-04-21 17:58:08 -0700

    add dataset

---
+++ mteb/tasks/Classification/eng/ArxivClassification.py
@@ -0,0 +1,34 @@
+from __future__ import annotations
+
+from mteb.abstasks.TaskMetadata import TaskMetadata
+
+from ....abstasks import AbsTaskClassification
+
+
+class ArxivClassification(AbsTaskClassification):
+    metadata = TaskMetadata(
+        name="Arxiv Classification",
+        description="Classification Dataset of Arxiv Papers",
+        dataset={
+            "path": "ccdv/arxiv-classification",
+            "revision": "f9bd92144ed76200d6eb3ce73a8bd4eba9ffdc85",
+        },
+        reference="https://ieeexplore.ieee.org/document/8675939",
+        type="Classification",
+        category="p2p",
+        eval_splits=["test"],
+        eval_langs=["eng-Latn"],
+        main_score="accuracy",
+        date=None,
+        form=None,
+        domains=None,
+        task_subtypes=None,
+        license=None,
+        socioeconomic_status=None,
+        annotations_creators=None,
+        dialect=None,
+        text_creation=None,
+        bibtex_citation=None,
+        n_samples={"test": 2500},
+        avg_character_length=None,
+    )
