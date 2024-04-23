75277f46b7bccda84607f98238019d7183fd0b03
shreeya-dhakal <ssdhakal57@gmail.com>
2024-04-22 22:43:20 -0700

    correct language ISO code and name for Swahili

--- mteb/tasks/Summarization/__init__.py
+++ mteb/tasks/Summarization/__init__.py
@@ -4,5 +4,5 @@ from .deu.SummEvalDeSummarization import *
 from .eng.SummEvalSummarization import *
 from .fra.SummEvalFrSummarization import *
 from .spa.SummEvalEsSummarization import *
-from .swe.SummEvalSwSummarization import *
+from .swh.SummEvalSwSummarization import *
 from .tur.SummEvalTrSummarization import *
