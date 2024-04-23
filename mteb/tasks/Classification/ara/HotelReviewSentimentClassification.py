15bfb81d6949e799e7bd9ae9bf571f7c274b4ff8
Isaac Chung <chungisaac1217@gmail.com>
2024-04-23 12:26:16 +0300

    specify split

--- mteb/tasks/Classification/ara/HotelReviewSentimentClassification.py
+++ mteb/tasks/Classification/ara/HotelReviewSentimentClassification.py
@@ -44,4 +44,4 @@ class HotelReviewSentimentClassification(AbsTaskClassification):
     )
 
     def dataset_transform(self):
-        self.stratified_subsampling()
+        self.stratified_subsampling(splits=["train"])
