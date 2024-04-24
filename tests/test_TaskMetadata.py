cfc70367a1c8b2deef038e105313307bf3fd9d7b
Lasse <lasseh0310@gmail.com>
2024-04-24 15:13:12 +0200

    fix: don't change bibtex citation to "" for historic datasets

--- tests/test_TaskMetadata.py
+++ tests/test_TaskMetadata.py
@@ -3,6 +3,9 @@ import logging
 import pytest
 
 from mteb.abstasks.TaskMetadata import TaskMetadata
+from mteb.get_tasks import get_tasks
+from mteb.tasks import *
+from tests.historic_datasets import HISTORIC_DATASETS
 
 
 def test_given_dataset_config_then_it_is_valid():
@@ -28,7 +31,7 @@ def test_given_dataset_config_then_it_is_valid():
         annotations_creators=None,
         dialect=None,
         text_creation=None,
-        bibtex_citation=None,
+        bibtex_citation="",
         avg_character_length=None,
         n_samples=None,
     )
@@ -56,7 +59,7 @@ def test_given_missing_dataset_path_then_it_throws():
             annotations_creators=None,
             dialect=None,
             text_creation=None,
-            bibtex_citation=None,
+            bibtex_citation="",
             avg_character_length=None,
             n_samples=None,
         )
@@ -85,7 +88,7 @@ def test_given_missing_revision_path_then_it_throws():
             annotations_creators=None,
             dialect=None,
             text_creation=None,
-            bibtex_citation=None,
+            bibtex_citation="",
             avg_character_length=None,
             n_samples=None,
         )
@@ -112,7 +115,7 @@ def test_given_none_revision_path_then_it_logs_warning(caplog):
             annotations_creators=None,
             dialect=None,
             text_creation=None,
-            bibtex_citation=None,
+            bibtex_citation="",
             avg_character_length=None,
             n_samples=None,
         )
@@ -126,4 +129,82 @@ def test_given_none_revision_path_then_it_logs_warning(caplog):
         assert (
             warning_logs[0].message == "Revision missing for the dataset test/dataset. "
             "It is encourage to specify a dataset revision for reproducability."
+        )
+
+
+def test_unfilled_metadata_is_not_filled():
+    assert (
+        TaskMetadata(
+            name="MyTask",
+            dataset={
+                "path": "test/dataset",
+                "revision": "1.0",
+            },
+            description="testing",
+            reference=None,
+            type="Classification",
+            category="s2s",
+            eval_splits=["test"],
+            eval_langs=["eng-Latn"],
+            main_score="map",
+            date=None,
+            form=None,
+            domains=None,
+            license=None,
+            task_subtypes=None,
+            socioeconomic_status=None,
+            annotations_creators=None,
+            dialect=None,
+            text_creation=None,
+            bibtex_citation="",
+            avg_character_length=None,
+            n_samples=None,
+        ).is_filled()
+        is False
+    )
+
+
+def test_filled_metadata_is_filled():
+    assert (
+        TaskMetadata(
+            name="MyTask",
+            dataset={
+                "path": "test/dataset",
+                "revision": "1.0",
+            },
+            description="testing",
+            reference="https://aclanthology.org/W19-6138/",
+            type="Classification",
+            category="s2s",
+            eval_splits=["test"],
+            eval_langs=["eng-Latn"],
+            main_score="map",
+            date=("2021-01-01", "2021-12-31"),
+            form=["written"],
+            domains=["Non-fiction"],
+            license="mit",
+            task_subtypes=["Thematic clustering"],
+            socioeconomic_status="high",
+            annotations_creators="expert-annotated",
+            dialect=None,
+            text_creation="found",
+            bibtex_citation="Someone et al",
+            avg_character_length={"train": 1},
+            n_samples={"train": 1},
+        ).is_filled()
+        is True
+    )
+
+
+def test_all_metadata_is_filled():
+    all_tasks = get_tasks()
+
+    unfilled_metadata = []
+    for task in all_tasks:
+        if task.metadata.name not in HISTORIC_DATASETS:
+            if not task.metadata.is_filled():
+                unfilled_metadata.append(task.metadata.name)
+    if unfilled_metadata:
+        raise ValueError(
+            f"The metadata of the following datasets is not filled: {unfilled_metadata}"
         )
