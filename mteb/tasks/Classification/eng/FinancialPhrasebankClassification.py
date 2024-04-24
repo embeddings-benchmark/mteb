f7e74559c256454bafede0ef263120021a781195
Dipam <dipam44@gmail.com>
2024-04-23 20:34:20 -0700

    PR comments

--- mteb/tasks/Classification/eng/FinancialPhrasebankClassification.py
+++ mteb/tasks/Classification/eng/FinancialPhrasebankClassification.py
@@ -21,14 +21,14 @@ class FinancialPhrasebankClassification(AbsTaskClassification):
         eval_langs=["eng-Latn"],
         main_score="accuracy",
         date=None,
-        form=None,
+        form=["written"],
         domains=["News"],
-        task_subtypes=None,
+        task_subtypes=["Sentiment/Hate speech"],
         license="cc-by-nc-sa-3.0",
-        socioeconomic_status=None,
+        socioeconomic_status="medium",
         annotations_creators="expert-annotated",
-        dialect=None,
-        text_creation=None,
+        dialect=[],
+        text_creation="found",
         bibtex_citation="""
             @article{Malo2014GoodDO,
             title={Good debt or bad debt: Detecting semantic orientations in economic texts},
@@ -39,17 +39,8 @@ class FinancialPhrasebankClassification(AbsTaskClassification):
             }
         """,
         n_samples={"train": 4840},
-        avg_character_length=None,  # Optional: You can calculate this based on the dataset if needed
+        avg_character_length={"train": 121.96},
     )
 
-    @property
-    def metadata_dict(self) -> dict[str, str]:
-        metadata_dict = dict(self.metadata)
-        metadata_dict["n_experiments"] = 10
-        metadata_dict["samples_per_label"] = 128
-        return metadata_dict
-
     def dataset_transform(self):
-        self.dataset = self.dataset.map(
-            lambda examples: {"text": examples["sentence"]}, remove_columns=["sentence"]
-        )
+        self.dataset = self.dataset.rename_column("sentence", "text")
