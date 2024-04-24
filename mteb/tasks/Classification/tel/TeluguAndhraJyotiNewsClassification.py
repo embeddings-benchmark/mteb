c2855a8faf9e2fa6a11538c6e8de7638d26cf382
Saiteja Utpala <saitejautpala@gmail.com>
2024-04-25 00:51:22 +0530

    correct dialect setting

--- mteb/tasks/Classification/tel/TeluguAndhraJyotiNewsClassification.py
+++ mteb/tasks/Classification/tel/TeluguAndhraJyotiNewsClassification.py
@@ -25,7 +25,7 @@ class TeluguAndhraJyotiNewsClassification(AbsTaskClassification):
         license="MIT",
         socioeconomic_status="mixed",
         annotations_creators="derived",
-        dialect=None,
+        dialect=[],
         text_creation="found",
         bibtex_citation=None,
         n_samples={"train": 17312, "test": 4329},
