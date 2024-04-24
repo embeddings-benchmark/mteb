b1d615d06eae2b0a89ca663faed4b22db82f0d14
Kenneth Enevoldsen <kennethcenevoldsen@gmail.com>
2024-04-24 09:59:58 +0200

    Added meta

--- mteb/tasks/Clustering/eng/ArxivClusteringP2PFast.py
+++ mteb/tasks/Clustering/eng/ArxivClusteringP2PFast.py
@@ -1,9 +1,7 @@
 from __future__ import annotations
 
-import datasets
-
-from mteb.abstasks.AbsTaskClusteringFast import clustering_downsample
 from mteb.abstasks.AbsTaskClustering import AbsTaskClustering
+from mteb.abstasks.AbsTaskClusteringFast import clustering_downsample
 from mteb.abstasks.TaskMetadata import TaskMetadata
 
 
@@ -12,7 +10,7 @@ class ArxivClusteringP2PFast(AbsTaskClustering):
     # simply downsample each cluster.
 
     metadata = TaskMetadata(
-        name="ArxivClusteringP2P.v2",
+        name="ArxivClusteringP2P.v3",
         description="Clustering of titles+abstract from arxiv. Clustering of 30 sets, either on the main or secondary category",
         reference="https://www.kaggle.com/Cornell-University/arxiv",
         dataset={
@@ -24,20 +22,19 @@ class ArxivClusteringP2PFast(AbsTaskClustering):
         eval_splits=["test"],
         eval_langs=["eng-Latn"],
         main_score="v_measure",
-        date=None,
-        form=None,
-        domains=None,
-        task_subtypes=None,
-        license=None,
-        socioeconomic_status=None,
-        annotations_creators=None,
-        dialect=None,
-        text_creation=None,
-        bibtex_citation=None,
+        date=("1991-01-01", "2021-01-01"),  # 1991-01-01 is the first arxiv paper
+        form=["written"],
+        domains=["Academic"],
+        task_subtypes=[],
+        license="CC0",
+        socioeconomic_status="high",
+        annotations_creators="derived",
+        dialect=[],
+        text_creation="found",
+        bibtex_citation=None,  # None found
         n_samples={"test": 250_000},
         avg_character_length={"test": 1009.98},
     )
-
 
     def dataset_transform(self):
         ds = clustering_downsample(self.dataset, self.seed)
