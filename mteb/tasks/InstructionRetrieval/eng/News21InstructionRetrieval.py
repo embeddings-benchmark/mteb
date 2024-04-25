5f9e5b50e2c9c0fff14f063c3c098ee6f8d83970
Isaac Chung <chungisaac1217@gmail.com>
2024-04-25 06:00:49 +0000

    use old datasets to match revisions

--- mteb/tasks/InstructionRetrieval/eng/News21InstructionRetrieval.py
+++ mteb/tasks/InstructionRetrieval/eng/News21InstructionRetrieval.py
@@ -11,7 +11,7 @@ class News21InstructionRetrieval(AbsTaskInstructionRetrieval):
         description="Measuring retrieval instruction following ability on News21 narratives.",
         reference="https://arxiv.org/abs/2403.15246",
         dataset={
-            "path": "jhu-clsp/news21-instructions",
+            "path": "jhu-clsp/news21-instructions-old",
             "revision": "e0144086b45fe31ac125e9ac1a83b6a409bb6ca6",
         },
         type="InstructionRetrieval",
