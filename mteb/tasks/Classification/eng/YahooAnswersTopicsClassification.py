8bd6db3811bc410bc7ad0910adf5ddfc24e0496d
Dipam Vasani <dipam44@gmail.com>
2024-04-26 12:11:44 -0700

    commit suggestion
    Co-authored-by: Imene Kerboua <33312980+imenelydiaker@users.noreply.github.com>

--- mteb/tasks/Classification/eng/YahooAnswersTopicsClassification.py
+++ mteb/tasks/Classification/eng/YahooAnswersTopicsClassification.py
@@ -41,10 +41,7 @@ class YahooAnswersTopicsClassification(AbsTaskClassification):
         return metadata_dict
 
     def dataset_transform(self):
-        self.dataset = self.dataset.map(
-            lambda examples: examples,
-            remove_columns=["id", "question_title", "question_content"],
-        )
+        self.dataset = self.dataset.remove_columns(["id", "question_title", "question_content"])
 
         self.dataset = self.dataset.rename_columns(
             {"topic": "label", "best_answer": "text"}
