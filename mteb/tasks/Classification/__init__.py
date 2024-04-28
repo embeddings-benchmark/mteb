28f465da3e1a8a32bac9253e67bdab2a946f0308
Akash Kundu <112017800+Akash190104@users.noreply.github.com>
2024-04-28 15:13:59 +0000

    added AJGT corpus

--- mteb/tasks/Classification/__init__.py
+++ mteb/tasks/Classification/__init__.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from .ara.AJGT import *
 from .ara.HotelReviewSentimentClassification import *
 from .ara.RestaurantReviewSentimentClassification import *
 from .ara.TweetEmotionClassification import *
