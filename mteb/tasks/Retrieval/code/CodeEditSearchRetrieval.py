fdf32b868dac9456730169f7a84f521b78b97ed8
cassanof <federico.cassano@federico.codes>
2024-04-28 22:41:23 -0400

    citation

--- mteb/tasks/Retrieval/code/CodeEditSearchRetrieval.py
+++ mteb/tasks/Retrieval/code/CodeEditSearchRetrieval.py
@@ -47,7 +47,7 @@ class CodeEditSearchRetrieval(MultilingualTask, AbsTaskRetrieval):
         annotations_creators="derived",
         dialect=[],
         text_creation="found",
-        bibtex_citation=None,
+        bibtex_citation="@article{muennighoff2023octopack, title={OctoPack: Instruction Tuning Code Large Language Models}, author={Niklas Muennighoff and Qian Liu and Armel Zebaze and Qinkai Zheng and Binyuan Hui and Terry Yue Zhuo and Swayam Singh and Xiangru Tang and Leandro von Werra and Shayne Longpre}, journal={arXiv preprint arXiv:2308.07124}, year={2023} }",
         n_samples={
             _EVAL_SPLIT: 1000,
         },
