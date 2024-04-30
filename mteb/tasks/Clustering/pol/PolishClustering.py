c08530364d9c8068b6ce073be4c4d55803736900
rposwiata <rposwiata@opi.org.pl>
2024-05-01 00:39:33 +0200

    Add new Polish clustering tasks based on PLSC - Polish Library of Science Corpus

--- mteb/tasks/Clustering/pol/PolishClustering.py
+++ mteb/tasks/Clustering/pol/PolishClustering.py
@@ -33,3 +33,63 @@ class EightTagsClustering(AbsTaskClustering):
         n_samples=None,
         avg_character_length=None,
     )
+
+
+class PlscClusteringS2S(AbsTaskClustering):
+    metadata = TaskMetadata(
+        name="PlscClusteringS2S",
+        description="Clustering of polish article titles from Library of Science (https://bibliotekanauki.pl/), either "
+                    "on the scientific field or discipline.",
+        reference="https://huggingface.co/datasets/rafalposwiata/plsc",
+        dataset={
+            "path": "PL-MTEB/plsc-clustering-s2s",
+            "revision": "45451181fd30822c844cec1c795b48a5685a1081",
+        },
+        type="Clustering",
+        category="s2s",
+        eval_splits=["test"],
+        eval_langs=["pol-Latn"],
+        main_score="v_measure",
+        date=("2022-04-04", "2023-09-12"),
+        form=["written"],
+        domains=["Academic"],
+        task_subtypes=["Topic classification"],
+        license="cc0-1.0",
+        socioeconomic_status="high",
+        annotations_creators="derived",
+        dialect=[],
+        text_creation="found",
+        bibtex_citation="",
+        n_samples={"test": 259764},
+        avg_character_length={"test": 85.25},
+    )
+
+
+class PlscClusteringP2P(AbsTaskClustering):
+    metadata = TaskMetadata(
+        name="PlscClusteringP2P",
+        description="Clustering of polish article titles+abstracts from Library of Science "
+                    "(https://bibliotekanauki.pl/), either on the scientific field or discipline.",
+        reference="https://huggingface.co/datasets/rafalposwiata/plsc",
+        dataset={
+            "path": "PL-MTEB/plsc-clustering-p2p",
+            "revision": "cbc0d22dadb3ff596e4cbf200d8725f9023ef773",
+        },
+        type="Clustering",
+        category="s2s",
+        eval_splits=["test"],
+        eval_langs=["pol-Latn"],
+        main_score="v_measure",
+        date=("2022-04-04", "2023-09-12"),
+        form=["written"],
+        domains=["Academic"],
+        task_subtypes=["Topic classification"],
+        license="cc0-1.0",
+        socioeconomic_status="high",
+        annotations_creators="derived",
+        dialect=[],
+        text_creation="found",
+        bibtex_citation="",
+        n_samples={"test": 259764},
+        avg_character_length={"test": 960.98},
+    )
