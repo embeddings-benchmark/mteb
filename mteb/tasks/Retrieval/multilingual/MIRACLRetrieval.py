8d122d0b77f0082ef0d16a73eeedb57a4a50fd39
Nandan Thakur <nandant@gmail.com>
2024-05-02 14:38:35 -0400

    added initial MIRACL Retrieval for yoruba

--- mteb/tasks/Retrieval/multilingual/MIRACLRetrieval.py
+++ mteb/tasks/Retrieval/multilingual/MIRACLRetrieval.py
@@ -7,83 +7,141 @@ from mteb.abstasks.TaskMetadata import TaskMetadata
 from ....abstasks import MultilingualTask
 from ....abstasks.AbsTaskRetrieval import AbsTaskRetrieval
 
-_EVAL_SPLIT = "test"
+_EVAL_SPLIT = "dev"
 
-_LANGS = {"de": ["deu-Latn"], "es": ["spa-Latn"]}
+_LANGUAGES = {
+    "yo": ["yor-Latn"],
+}
+
+# _LANGUAGES = {
+#     "ar": ["ara-Arab"],
+#     "bn": ["ben-Beng"],
+#     "de": ["deu-Latn"],
+#     "en": ["eng-Latn"],
+#     "es": ["spa-Latn"],
+#     "fa": ["fas-Arab"],
+#     "fi": ["fin-Latn"],
+#     "fr": ["fra-Latn"],
+#     "hi": ["hin-Deva"],
+#     "id": ["ind-Latn"],
+#     "ja": ["jpn-Jpan"],
+#     "ko": ["kor-Kore"],
+#     "ru": ["rus-Cyrl"],
+#     "sw": ["swa-Latn"],
+#     "te": ["tel-Telu"],
+#     "th": ["tha-Thai"],
+#     "yo": ["yor-Latn"],
+#     "zh": ["zho-Hans"],
+#     }
 
 
 def _load_miracl_data(
-    path: str, langs: list, split: str, cache_dir: str = None, revision: str = None
+    path: str, 
+    langs: list, 
+    splits: str, 
+    cache_dir: str = None, 
+    revision: str = None
 ):
-    queries = {lang: {split: {}} for lang in langs}
-    corpus = {lang: {split: {}} for lang in langs}
-    relevant_docs = {lang: {split: {}} for lang in langs}
+    corpus = {lang: {split: None for split in splits} for lang in langs}
+    queries = {lang: {split: None for split in splits} for lang in langs}
+    relevant_docs = {lang: {split: None for split in splits} for lang in langs}
 
+    split = _EVAL_SPLIT
+    
     for lang in langs:
-        data = datasets.load_dataset(
+        # Load corpus data (Can be several millions for languages)
+        corpus_identifier = f"corpus-{lang}"
+        corpus_data = datasets.load_dataset(
             path,
-            lang,
-            split=split,
+            corpus_identifier,
             cache_dir=cache_dir,
             revision=revision,
+            trust_remote_code=True,
         )
-        # Generate unique IDs for queries and documents
-        query_id_counter = 1
-        document_id_counter = 1
-
-        for row in data:
+        corpus[lang][split] = {}
+        for row in corpus_data["corpus"]:
+            docid = row["docid"]
+            doc_title = row["title"]
+            doc_text = row["text"]
+            corpus[lang][split][docid] = {"title": doc_title, "text": doc_text}
+        
+        # Load queries data
+        queries_identifier = f"queries-{lang}"
+        queries_data = datasets.load_dataset(
+            path,
+            queries_identifier,
+            cache_dir=cache_dir,
+            revision=revision,
+            trust_remote_code=True,
+        )
+        queries[lang][split] = {}
+        for row in queries_data["queries"]:
+            query_id = row["query_id"]
             query_text = row["query"]
-            positive_texts = row["positive"]
-            negative_texts = row["negative"]
-
-            # Assign unique ID to the query
-            query_id = f"Q{query_id_counter}"
             queries[lang][split][query_id] = query_text
-            query_id_counter += 1
 
-            # Add positive and negative texts to corpus with unique IDs
-            for text in positive_texts + negative_texts:
-                doc_id = f"D{document_id_counter}"
-                corpus[lang][split][doc_id] = {"text": text}
-                document_id_counter += 1
+        # Load relevant documents data
+        qrels_identifier = f"{lang}"
+        qrels_data = datasets.load_dataset(
+            path,
+            qrels_identifier,
+            cache_dir=cache_dir,
+            revision=revision,
+            trust_remote_code=True,
+        )
+        relevant_docs[lang][split] = {}
+        for row in qrels_data[split]:
+            query_id = row["query_id"]
+            doc_id = row["docid"]
+            score = row["score"]
+            if query_id not in relevant_docs[lang][split]:
+                relevant_docs[lang][split][query_id] = {}
+            relevant_docs[lang][split][query_id][doc_id] = score
 
-                # Add relevant document information to relevant_docs for positive texts only
-                if text in positive_texts:
-                    if query_id not in relevant_docs[lang][split]:
-                        relevant_docs[lang][split][query_id] = {}
-                    relevant_docs[lang][split][query_id][doc_id] = 1
+    corpus = datasets.DatasetDict(corpus)
+    queries = datasets.DatasetDict(queries)
+    relevant_docs = datasets.DatasetDict(relevant_docs)
 
-        corpus = datasets.DatasetDict(corpus)
-        queries = datasets.DatasetDict(queries)
-        relevant_docs = datasets.DatasetDict(relevant_docs)
-
-        return corpus, queries, relevant_docs
+    return corpus, queries, relevant_docs
 
 
 class MIRACLRetrieval(MultilingualTask, AbsTaskRetrieval):
     metadata = TaskMetadata(
         name="MIRACLRetrieval",
-        description="MIRACLRetrieval",
-        reference=None,
+        description="MIRACL (Multilingual Information Retrieval Across a Continuum of Languages) is a multilingual retrieval dataset that focuses on search across 18 different languages.",
+        reference="http://miracl.ai",
         dataset={
-            "path": "jinaai/miracl",
-            "revision": "d28a029f35c4ff7f616df47b0edf54e6882395e6",
+            "path": "miracl/mmteb-miracl",
+            "revision": "main",
         },
         type="Retrieval",
         category="s2p",
         eval_splits=[_EVAL_SPLIT],
-        eval_langs=_LANGS,
+        eval_langs=_LANGUAGES,
         main_score="ndcg_at_10",
-        date=None,
-        form=None,
-        domains=None,
-        task_subtypes=None,
-        license=None,
-        socioeconomic_status=None,
-        annotations_creators=None,
-        dialect=None,
-        text_creation=None,
-        bibtex_citation=None,
+        date=("2022-06-01", "2023-01-30"),
+        form=["written"],
+        domains=["Encyclopaedic"],
+        task_subtypes=[],
+        license="CC BY-SA 4.0",
+        socioeconomic_status="low",
+        annotations_creators="expert-annotated",
+        dialect=[],
+        text_creation="created",
+        bibtex_citation="""@article{10.1162/tacl_a_00595,
+    author = {Zhang, Xinyu and Thakur, Nandan and Ogundepo, Odunayo and Kamalloo, Ehsan and Alfonso-Hermelo, David and Li, Xiaoguang and Liu, Qun and Rezagholizadeh, Mehdi and Lin, Jimmy},
+    title = "{MIRACL: A Multilingual Retrieval Dataset Covering 18 Diverse Languages}",
+    journal = {Transactions of the Association for Computational Linguistics},
+    volume = {11},
+    pages = {1114-1131},
+    year = {2023},
+    month = {09},
+    abstract = "{MIRACL is a multilingual dataset for ad hoc retrieval across 18 languages that collectively encompass over three billion native speakers around the world. This resource is designed to support monolingual retrieval tasks, where the queries and the corpora are in the same language. In total, we have gathered over 726k high-quality relevance judgments for 78k queries over Wikipedia in these languages, where all annotations have been performed by native speakers hired by our team. MIRACL covers languages that are both typologically close as well as distant from 10 language families and 13 sub-families, associated with varying amounts of publicly available resources. Extensive automatic heuristic verification and manual assessments were performed during the annotation process to control data quality. In total, MIRACL represents an investment of around five person-years of human annotator effort. Our goal is to spur research on improving retrieval across a continuum of languages, thus enhancing information access capabilities for diverse populations around the world, particularly those that have traditionally been underserved. MIRACL is available at http://miracl.ai/.}",
+    issn = {2307-387X},
+    doi = {10.1162/tacl_a_00595},
+    url = {https://doi.org/10.1162/tacl\_a\_00595},
+    eprint = {https://direct.mit.edu/tacl/article-pdf/doi/10.1162/tacl\_a\_00595/2157340/tacl\_a\_00595.pdf},
+}""",
         n_samples=None,
         avg_character_length=None,
     )
@@ -94,8 +152,8 @@ class MIRACLRetrieval(MultilingualTask, AbsTaskRetrieval):
 
         self.corpus, self.queries, self.relevant_docs = _load_miracl_data(
             path=self.metadata_dict["dataset"]["path"],
-            langs=self.langs,
-            split=self.metadata_dict["eval_splits"][0],
+            langs=self.metadata.eval_langs,
+            splits=self.metadata_dict["eval_splits"],
             cache_dir=kwargs.get("cache_dir", None),
             revision=self.metadata_dict["dataset"]["revision"],
         )
