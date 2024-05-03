b763d522f59ffd20aff56354898766b3be2cf254
alessia <borghini.1834906@studenti.uniroma1.it>
2024-05-03 18:37:17 +0200

    Add 5 new datasets for languages with poor support

---
+++ mteb/tasks/Classification/fra/FrenchBookReviews.py
@@ -0,0 +1,39 @@
+from __future__ import annotations
+
+from mteb.abstasks import AbsTaskClassification
+from mteb.abstasks.TaskMetadata import TaskMetadata
+
+class FrenchBookReviews(AbsTaskClassification):
+    metadata = TaskMetadata(
+        name="FrenchBookReviews",
+        dataset={
+            "path": "Abirate/french_book_reviews",
+            "revision": "534725e03fec6f560dbe8166e8ae3825314a6290",
+        },
+        description="It is a French book reviews dataset containing a huge number of reader reviews on French books. Each review is pared with a rating that ranges from 0.5 to 5 (with 0.5 increment).",
+        reference="https://huggingface.co/datasets/Abirate/french_book_reviews",
+        type="Classification",
+        category="s2s",
+        eval_splits=["train"],
+        eval_langs=["fra-Latn"],
+        main_score="accuracy",
+        date=("2022-01-01", "2023-01-01"),
+        form=["written"],
+        domains=["Reviews"],
+        task_subtypes=[],
+        license="CC0",
+        socioeconomic_status="mixed",
+        annotations_creators="derived",
+        dialect=[],
+        text_creation="found",
+        bibtex_citation="""
+        """,
+        n_samples={"train": 2048},
+        avg_character_length={"train": 311.5},
+    )
+
+    def dataset_transform(self):
+        self.dataset = self.dataset.rename_columns({"reader_review": "text"})
+        self.dataset = self.stratified_subsampling(
+            self.dataset, seed=self.seed, splits=["train"]
+        )
---
+++ mteb/tasks/Classification/multilingual/LanguageClassification.py
@@ -0,0 +1,62 @@
+from __future__ import annotations
+
+from mteb.abstasks.TaskMetadata import TaskMetadata
+
+from ....abstasks import AbsTaskClassification
+
+_LANGUAGES = {
+    "ar": ["ara-Arab"],
+    "bg": ["bul-Cyrl"],
+    "de": ["deu-Latn"],
+    "el": ["ell-Grek"],
+    "en": ["eng-Latn"],
+    "es": ["spa-Latn"],
+    "fr": ["fra-Latn"],
+    "hi": ["hin-Deva"],
+    "it": ["ita-Latn"],
+    "ja": ["jpn-Jpan"],
+    "nl": ["nld-Latn"],
+    "pl": ["pol-Latn"],
+    "pt": ["por-Latn"],
+    "ru": ["rus-Cyrl"],
+    "sw": ["swa-Latn"],
+    "th": ["tha-Thai"],
+    "tr": ["tur-Latn"],
+    "ur": ["urd-Arab"],
+    "vi": ["vie-Latn"],
+    "zh": ["cmn-Hans"],
+}
+
+class LanguageClassification(AbsTaskClassification):
+    metadata = TaskMetadata(
+        name="LanguageClassification",
+        dataset={
+            "path": "papluca/language-identification",
+            "revision": "aa56583bf2bc52b0565770607d6fc3faebecf9e2",
+        },
+        description="A language identification dataset for 20 languages.",
+        reference="https://huggingface.co/datasets/papluca/language-identification",
+        category="s2s",
+        type="Classification",
+        eval_splits=["test"],
+        eval_langs=_LANGUAGES,
+        main_score="accuracy",
+        date=("2021-11-01", "2021-11-30"),
+        form=["written"],
+        domains=["Reviews", "Web", "Non-fiction", "Fiction", "Government"],
+        task_subtypes=["Language identification"],
+        license="Not specified",
+        socioeconomic_status="mixed",
+        annotations_creators="derived",
+        dialect=[],
+        text_creation="found",
+        bibtex_citation="",
+        n_samples={"test": 2048},
+        avg_character_length={"test": 107.8},
+    )
+
+    def dataset_transform(self) -> None:
+        self.dataset = self.dataset.rename_columns({"labels": "label"})
+        self.dataset = self.stratified_subsampling(
+            self.dataset, seed=self.seed, splits=["test"]
+        )
