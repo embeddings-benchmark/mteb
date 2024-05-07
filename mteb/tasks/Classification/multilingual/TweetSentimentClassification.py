e92c760a16d607a4386b0ec88ce1d7ec43169e76
lm <loic.magne@outlook.com>
2024-05-07 11:38:24 +0200

    TweetSentimentClassification

--- mteb/tasks/Classification/multilingual/TweetSentimentClassification.py
+++ mteb/tasks/Classification/multilingual/TweetSentimentClassification.py
@@ -17,11 +17,12 @@ _LANGUAGES = {
 
 
 class TweetSentimentClassification(MultilingualTask, AbsTaskClassification):
+    fast_loading = True
     metadata = TaskMetadata(
         name="TweetSentimentClassification",
         dataset={
-            "path": "cardiffnlp/tweet_sentiment_multilingual",
-            "revision": "14b13edfbc4046892f6011d114c29c0f83170589",
+            "path": "mteb/tweet_sentiment_multilingual",
+            "revision": "9136186884fac29b7cda6357bb35ddc685dccdb9",
         },
         description="A multilingual Sentiment Analysis dataset consisting of tweets in 8 different languages.",
         reference="https://aclanthology.org/2022.lrec-1.27",
@@ -64,4 +65,3 @@ class TweetSentimentClassification(MultilingualTask, AbsTaskClassification):
             self.dataset[lang] = self.stratified_subsampling(
                 self.dataset[lang], n_samples=256, seed=self.seed, splits=["test"]
             )
-            print(self.dataset)
