9479f2ac0a36adda53e0bb5affe1f905a246eadb
mmhamdy <mohamdy2772@gmail.com>
2024-05-12 02:22:20 +0300

    add romanian reviews

--- mteb/tasks/Classification/__init__.py
+++ mteb/tasks/Classification/__init__.py
@@ -85,6 +85,7 @@ from .ory.OdiaNewsClassification import *
 from .pan.PunjabiNewsClassification import *
 from .pol.PolishClassification import *
 from .por.HateSpeechPortugueseClassification import *
+from .ron.RomanianReviewsSentiment import *
 from .ron.RomanianSentimentClassification import *
 from .san.SanskritShlokasClassification import *
 from .sin.SinhalaNewsClassification import *
