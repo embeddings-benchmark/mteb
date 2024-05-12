398ee9b94783c69b05264f1ac5219315cebe0113
Wissam Siblini <wissam.siblini92@gmail.com>
2024-05-12 11:27:44 +0200

    Adding Klue-NLI dataset

--- mteb/tasks/PairClassification/__init__.py
+++ mteb/tasks/PairClassification/__init__.py
@@ -6,6 +6,7 @@ from .eng.SprintDuplicateQuestionsPC import *
 from .eng.TwitterSemEval2015PC import *
 from .eng.TwitterURLCorpusPC import *
 from .hye.ArmenianParaphrasePC import *
+from .kor.KlueNLI import *
 from .multilingual.OpusparcusPC import *
 from .multilingual.PawsX import *
 from .multilingual.XNLI import *
