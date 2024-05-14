9e8308343ce0a3f38efae38545bdda5cb85b0caa
Isaac Chung <chungisaac1217@gmail.com>
2024-05-14 14:29:29 +0300

    add task count per lang table

--- docs/create_tasks_table.py
+++ docs/create_tasks_table.py
@@ -1,9 +1,18 @@
 from __future__ import annotations
 
 import re
+from typing import get_args
 from pathlib import Path
 
 import mteb
+
+from mteb.abstasks.TaskMetadata import TASK_TYPE, PROGRAMMING_LANGS
+
+
+def is_task_to_skip(task: mteb.AbsTask):
+    if isinstance(task, mteb.CrosslingualTask):
+        return True
+    return False
 
 
 def author_from_bibtex(bibtex: str | None) -> str:
@@ -56,15 +65,49 @@ def create_tasks_table(tasks: list[mteb.AbsTask]) -> str:
     return table
 
 
-def insert_table(file_path, table):
-    """Insert table in the in <!-- TABLE START --> and <!-- TABLE END -->"""
+def create_task_lang_table(tasks: list[mteb.AbsTask]) -> str:
+    
+    table_dict = {}
+    ## Group by language. If it is a multilingual dataset, 1 is added to all languages present.
+    ## For now, we skip cross lingual datasets.
+    for task in tasks:
+        if is_task_to_skip(task):
+            continue
+
+        for lang in task.metadata.languages:
+            if lang in PROGRAMMING_LANGS:
+                lang = "code"
+            if table_dict.get(lang) is None:
+                table_dict[lang] = {k: 0 for k in sorted(get_args(TASK_TYPE))}
+            table_dict[lang][task.metadata.type] += 1
+    
+    task_names_md = " | ".join(sorted(get_args(TASK_TYPE)))
+    horizontal_line_md = "---|---" * len(sorted(get_args(TASK_TYPE)))
+    table = """
+| Language | {} |
+|{}|
+""".format(task_names_md, horizontal_line_md)
+    
+    langs = list(table_dict.keys())
+    langs.sort()
+    for lang in langs:
+        table += f"| {lang} "
+        for _ , num in table_dict[lang].items():
+            table += f"| {num} "
+        table += "|\n"
+
+    return table
+
+
+def insert_tables(file_path: str, tables: list[str], tags: list[str] = ["TASKS TABLE"]):
+    """Insert tables within <!-- TABLE START --> and <!-- TABLE END --> or similar tags."""
     with open(file_path, "r") as file:
         md = file.read()
 
-    start = "<!-- TABLE START -->"
-    end = "<!-- TABLE END -->"
-
-    md = md.replace(md[md.index(start) + len(start) : md.index(end)], table)
+    for table, tag in zip(tables, tags):
+        start = f"<!-- {tag} START -->"
+        end = f"<!-- {tag} END -->"
+        md = md.replace(md[md.index(start) + len(start) : md.index(end)], table)
 
     with open(file_path, "w") as file:
         file.write(md)
@@ -74,11 +117,12 @@ def main():
     tasks = mteb.get_tasks()
     tasks = sorted(tasks, key=lambda x: x.metadata.name)
 
-    table = create_tasks_table(tasks)
+    tasks_table = create_tasks_table(tasks)
+    task_lang_table = create_task_lang_table(tasks)
 
     file_path = Path(__file__).parent / "tasks.md"
 
-    insert_table(file_path, table)
+    insert_tables(file_path, tables=[tasks_table, task_lang_table], tags=['TASKS TABLE', 'TASK LANG TABLE'])
 
 
 if __name__ == "__main__":
