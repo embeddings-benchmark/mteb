11e01bab6187a4b28d73a6fabc4d513500382923
rasdani <73563550+rasdani@users.noreply.github.com>
2024-05-15 00:00:59 +0000

    update license and README of dataset

--- scripts/data/wikipedia_retrieval/create_data.py
+++ scripts/data/wikipedia_retrieval/create_data.py
@@ -1,4 +1,6 @@
 from datasets import load_dataset, Dataset, DatasetDict, Features, Value
+from huggingface_hub import hf_hub_download, HfApi
+import io
 
 languages = ["de"]
 
@@ -17,32 +19,43 @@ for lang in languages:
     queries_dict = {row["title"]: "q" + row["_id"] for row in ds_queries}
     corpus = ds_corpus.map(lambda x: {"_id": x["_id"], "title": x["title"], "text": x["text"]}, remove_columns=ds_corpus.column_names)
     qrels = ds_corpus.map(lambda x: apply_query_id(x, queries_dict=queries_dict), remove_columns=ds_corpus.column_names)
-    # breakpoint()
 
     corpus_features = Features({"_id": Value("string"), "title": Value("string"), "text": Value("string")})
     queries_features = Features({"_id": Value("string"), "text": Value("string")})
     qrels_features = Features({"query-id": Value("string"), "corpus-id": Value("string"), "score": Value("float32")})
-
-    # corpus_dataset = Dataset.from_dict(corpus, features=corpus_features, split="test")
-    # queries_dataset = Dataset.from_dict(queries, features=queries_features, split="test")
-    # qrels_dataset = Dataset.from_dict(qrels, features=qrels_features, split="test")
-
-    # ds_dict = DatasetDict({"corpus": corpus_dataset, "queries": queries_dataset, "qrels": qrels_dataset})
-    # ds_dict.save_to_disk(f"dataset/wikipedia-2023-11-retrieval-{lang}")
 
     corpus = corpus.cast(corpus_features)
     queries = queries.cast(queries_features)
     qrels = qrels.cast(qrels_features)
 
     ds_dict = DatasetDict({"corpus": corpus, "queries": queries, "qrels": qrels})
-    # ds_dict.save_to_disk(f"datasets/wikipedia-2023-11-retrieval-{lang}")
-    # ds_dict.push_to_hub(f"ellamind/wikipedia-2023-11-retrieval-{lang}")
 
-    corpus.push_to_hub(f"ellamind/wikipedia-2023-11-retrieval-{lang}", config_name="corpus", split="test")
-    queries.push_to_hub(f"ellamind/wikipedia-2023-11-retrieval-{lang}", config_name="queries", split="test")
-    qrels.push_to_hub(f"ellamind/wikipedia-2023-11-retrieval-{lang}", config_name="qrels", split="test")
 
-    # qrels_datadict = DatasetDict({"test": qrels_dataset})
+    repo_id = f"ellamind/wikipedia-2023-11-retrieval-{lang}"
+    corpus.push_to_hub(repo_id, config_name="corpus")
+    queries.push_to_hub(repo_id, config_name="queries")
+    qrels.push_to_hub(repo_id, config_name="default")
 
-    # corpus_datadict.save_to_disk(f"scripts/data/wikipedia-retrieval-{lang}/corpus_{lang}")
-    # qrels_datadict.save_to_disk(f"scripts/data/germanquad/qrels_{lang}")
+    # Download the README from the repository
+    readme_path = hf_hub_download(repo_id=repo_id, filename="README.md", repo_type="dataset")
+
+    with open(readme_path, "r") as f:
+        readme_content = f.read()
+
+    readme = """
+This dataset is derived from Cohere's wikipedia-2023-11 dataset, which is in turn derived from `wikimedia/wikipedia`.
+The dataset is licensed under the Creative Commons CC BY-SA 3.0 license.
+    """
+    # Prepend the license key to the YAML header and append the custom README
+    if "- license: " not in readme_content and readme not in readme_content:
+        license = "cc-by-sa-3.0"
+
+        updated_readme = readme_content.replace(
+        "---\ndataset_info:",
+        "---\nlicense: {license}\ndataset_info:"
+        ).format(license=license)
+        updated_readme += readme
+
+        api = HfApi()
+        readme_bytes = updated_readme.encode("utf-8")
+        api.upload_file(path_or_fileobj=readme_bytes, path_in_repo="README.md", repo_id=repo_id, repo_type="dataset")
