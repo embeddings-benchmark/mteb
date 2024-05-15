ebefa8ab2715945c864638c195ff883e82bbe409
Isaac Chung <chungisaac1217@gmail.com>
2024-05-15 12:11:16 +0000

    reddit cluster s2s to fast

--- mteb/tasks/Clustering/eng/RedditClustering.py
+++ mteb/tasks/Clustering/eng/RedditClustering.py
@@ -2,10 +2,11 @@ from __future__ import annotations
 
 from mteb.abstasks.TaskMetadata import TaskMetadata
 
-from ....abstasks.AbsTaskClustering import AbsTaskClustering
+from ....abstasks.AbsTaskClustering import AbsTaskClustering, AbsTaskClusteringFast
 
 
 class RedditClustering(AbsTaskClustering):
+    superseeded_by = "RedditFastClusteringS2S"
     metadata = TaskMetadata(
         name="RedditClustering",
         description="Clustering of titles from 199 subreddits. Clustering of 25 sets, each with 10-50 classes, and each class with 100 - 1000 sentences.",
@@ -32,3 +33,53 @@ class RedditClustering(AbsTaskClustering):
         n_samples={"test": 420464},
         avg_character_length={"test": 64.7},
     )
+
+
+class RedditFastClusteringS2S(AbsTaskClusteringFast):
+    metadata = TaskMetadata(
+        name="RedditClustering",
+        description="Clustering of titles from 199 subreddits. Clustering of 25 sets, each with 10-50 classes, and each class with 100 - 1000 sentences.",
+        reference="https://arxiv.org/abs/2104.07081",
+        dataset={
+            "path": "mteb/reddit-clustering",
+            "revision": "24640382cdbf8abc73003fb0fa6d111a705499eb",
+        },
+        type="Clustering",
+        category="s2s",
+        eval_splits=["test"],
+        eval_langs=["eng-Latn"],
+        main_score="v_measure",
+        date=("2021-01-01", "2021-04-14"),
+        form=["written"],
+        domains=["Web", "Social"],
+        task_subtypes=["Thematic clustering"],
+        license="Not specified",
+        socioeconomic_status="mixed",
+        annotations_creators="derived",
+        dialect=[],
+        text_creation="found",
+        bibtex_citation="""@article{geigle:2021:arxiv,
+        author    = {Gregor Geigle and 
+                        Nils Reimers and 
+                        Andreas R{\"u}ckl{\'e} and
+                        Iryna Gurevych},
+        title     = {TWEAC: Transformer with Extendable QA Agent Classifiers},
+        journal   = {arXiv preprint},
+        volume    = {abs/2104.07081},
+        year      = {2021},
+        url       = {http://arxiv.org/abs/2104.07081},
+        archivePrefix = {arXiv},
+        eprint    = {2104.07081}
+        }""",
+        n_samples={"test": 2048},
+        avg_character_length={"test": 64.7},
+    )
+
+    def dataset_transform(self):
+        self.dataset = self.stratified_subsampling(
+            self.dataset,
+            self.seed,
+            self.metadata.eval_splits,
+            label="labels",
+            n_samples=2048,
+        )
