d11b75312cfdadade61b68731898f98d1c97c1cb
rasdani <73563550+rasdani@users.noreply.github.com>
2024-05-15 19:38:12 +0000

    multilingual reranking dataset

--- scripts/data/wikipedia_reranking/create_data.py
+++ scripts/data/wikipedia_reranking/create_data.py
@@ -3,7 +3,12 @@ from huggingface_hub import hf_hub_download, HfApi
 import io
 from collections import defaultdict
 
-languages = ["de", "bn"]
+# languages = [
+#     "de", "bn", "it", "pt",
+# ]
+languages = ["nl", "cs", "ro", "bg", "sr", "fi", "fa", "hi", "da", "en"]
+
+# languages = ["de", "bn"]
 
 def map_corpus_to_query(example, negatives_dict):
     title = example["title"]
@@ -11,6 +16,7 @@ def map_corpus_to_query(example, negatives_dict):
     negatives = negatives_dict[title]
     return {"query": example["text"], "positive": positive, "negative": negatives}
 
+ds_dict = DatasetDict()
 for lang in languages:
     ds_queries = load_dataset(f"rasdani/cohere-wikipedia-2023-11-{lang}-queries", split="train")
     ds_corpus = load_dataset(f"rasdani/cohere-wikipedia-2023-11-{lang}-1.5k-articles", split="train")
@@ -22,29 +28,33 @@ for lang in languages:
 
     ds = ds_queries.map( lambda x: map_corpus_to_query(x, negatives_dict), remove_columns=ds_queries.column_names)
 
-    repo_id = f"ellamind/wikipedia-2023-11-reranking-{lang}"
-    ds.push_to_hub(repo_id, config_name="corpus", split="test")
+# ds_dict[lang] = ds
 
-    # Download the README from the repository
-    readme_path = hf_hub_download(repo_id=repo_id, filename="README.md", repo_type="dataset")
+    repo_id = f"ellamind/wikipedia-2023-11-reranking-multilingual"
+    # ds_dict.push_to_hub(repo_id, config_name="corpus", split="test")
+    # ds_dict.push_to_hub(repo_id, config_name=lang)
+    ds.push_to_hub(repo_id, config_name=lang, split="test")
 
-    with open(readme_path, "r") as f:
-        readme_content = f.read()
+# Download the README from the repository
+readme_path = hf_hub_download(repo_id=repo_id, filename="README.md", repo_type="dataset")
 
-    readme = """
+with open(readme_path, "r") as f:
+    readme_content = f.read()
+
+readme = """
 This dataset is derived from Cohere's wikipedia-2023-11 dataset, which is in turn derived from `wikimedia/wikipedia`.
 The dataset is licensed under the Creative Commons CC BY-SA 3.0 license.
-    """
-    # Prepend the license key to the YAML header and append the custom README
-    if "- license: " not in readme_content and readme not in readme_content:
-        license = "cc-by-sa-3.0"
+"""
+# Prepend the license key to the YAML header and append the custom README
+if "- license: " not in readme_content and readme not in readme_content:
+    license = "cc-by-sa-3.0"
 
-        updated_readme = readme_content.replace(
-        "---\ndataset_info:",
-        "---\nlicense: {license}\ndataset_info:"
-        ).format(license=license)
-        updated_readme += readme
+    updated_readme = readme_content.replace(
+    "---\ndataset_info:",
+    "---\nlicense: {license}\ndataset_info:"
+    ).format(license=license)
+    updated_readme += readme
 
-        api = HfApi()
-        readme_bytes = updated_readme.encode("utf-8")
-        api.upload_file(path_or_fileobj=readme_bytes, path_in_repo="README.md", repo_id=repo_id, repo_type="dataset")
+    api = HfApi()
+    readme_bytes = updated_readme.encode("utf-8")
+    api.upload_file(path_or_fileobj=readme_bytes, path_in_repo="README.md", repo_id=repo_id, repo_type="dataset")
