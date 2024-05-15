e8ac0ceea12e43b37bb19e7180137b06ab348fbf
rasdani <73563550+rasdani@users.noreply.github.com>
2024-05-15 22:23:03 +0000

    WIP: reranking with multilingual dataset

--- mteb/tasks/Reranking/multilingual/WikipediaRerankingMultilingual.py
+++ mteb/tasks/Reranking/multilingual/WikipediaRerankingMultilingual.py
@@ -4,6 +4,23 @@ from mteb.abstasks.TaskMetadata import TaskMetadata
 
 from ....abstasks.AbsTaskReranking import AbsTaskReranking
 
+_EVAL_LANGS = {
+    "bg": ["bul-Cyrl"],
+    "bn": ["ben-Beng"],
+    "cs": ["ces-Latn"],
+    "da": ["dan-Latn"],
+    "de": ["deu-Latn"],
+    "en": ["eng-Latn"],
+    "fa": ["fas-Arab"],
+    "fi": ["fin-Latn"],
+    "hi": ["hin-Deva"],
+    "it": ["ita-Latn"],
+    "nl": ["nld-Latn"],
+    "pt": ["por-Latn"],
+    "ro": ["ron-Latn"],
+    "sr": ["srp-Cyrl"],
+}
+_CONFIG_LIST = list(_EVAL_LANGS.keys())
 
 class WikipediaRerankingMultilingual(AbsTaskReranking):
     metadata = TaskMetadata(
@@ -14,26 +31,12 @@ class WikipediaRerankingMultilingual(AbsTaskReranking):
         dataset={
             "path": "ellamind/wikipedia-2023-11-reranking-multilingual",
             "revision": "b20adcd11a02dc0226edbe8b5951e99ee0bb091c",
+            # "config": _CONFIG_LIST,
         },
         type="Reranking",
         category="s2p",
         eval_splits=["test"],
-        eval_langs={
-            "bg": ["bul-Cyrl"],
-            "bn": ["ben-Beng"],
-            "cs": ["ces-Latn"],
-            "da": ["dan-Latn"],
-            "de": ["deu-Latn"],
-            "en": ["eng-Latn"],
-            "fa": ["fas-Arab"],
-            "fi": ["fin-Latn"],
-            "hi": ["hin-Deva"],
-            "it": ["ita-Latn"],
-            "nl": ["nld-Latn"],
-            "pt": ["por-Latn"],
-            "ro": ["ron-Latn"],
-            "sr": ["srp-Cyrl"],
-        },
+        eval_langs=_EVAL_LANGS,
         main_score="map",
         date=("2023-11-01", "2024-05-15"),
         form=["written"],
@@ -63,3 +66,11 @@ class WikipediaRerankingMultilingual(AbsTaskReranking):
         },
         avg_character_length={"test": 452},
     )
+
+    def load_data(self, config: str, **kwargs):
+        """Load dataset from HuggingFace hub"""
+        if self.data_loaded:
+            return
+        self.dataset = datasets.load_dataset(config=config, **self.metadata_dict["dataset"])  # type: ignore
+        self.dataset_transform()
+        self.data_loaded = True
