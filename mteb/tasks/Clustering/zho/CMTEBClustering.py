bffc6ea5f266e881270b3641da9f9f21c5010616
Isaac Chung <chungisaac1217@gmail.com>
2024-05-17 11:50:49 +0000

    convert CLSClustering to fast

--- mteb/tasks/Clustering/zho/CMTEBClustering.py
+++ mteb/tasks/Clustering/zho/CMTEBClustering.py
@@ -1,11 +1,124 @@
 from __future__ import annotations
 
+import itertools
+
+from datasets import Dataset, DatasetDict
+
+from mteb.abstasks.AbsTaskClustering import AbsTaskClustering
+from mteb.abstasks.AbsTaskClusteringFast import AbsTaskClusteringFast
 from mteb.abstasks.TaskMetadata import TaskMetadata
 
-from ....abstasks.AbsTaskClustering import AbsTaskClustering
+NUM_SAMPLES = 2048
+
+
+class CLSClusteringFastS2S(AbsTaskClusteringFast):
+    metadata = TaskMetadata(
+        name="CLSClusteringFastS2S",
+        description="Clustering of titles from CLS dataset. Clustering of 13 sets on the main category.",
+        reference="https://arxiv.org/abs/2209.05034",
+        dataset={
+            "path": "C-MTEB/CLSClusteringS2S",
+            "revision": "e458b3f5414b62b7f9f83499ac1f5497ae2e869f",
+        },
+        type="Clustering",
+        category="s2s",
+        eval_splits=["test"],
+        eval_langs=["cmn-Hans"],
+        main_score="v_measure",
+        date=("2022-01-01", "2022-09-12"),
+        form=["written"],
+        domains=["Academic"],
+        task_subtypes=["Thematic clustering", "Topic classification"],
+        license="Apache-2.0",
+        socioeconomic_status="high",
+        annotations_creators="derived",
+        dialect=[],
+        text_creation="found",
+        bibtex_citation="""@misc{li2022csl,
+            title={CSL: A Large-scale Chinese Scientific Literature Dataset}, 
+            author={Yudong Li and Yuqing Zhang and Zhe Zhao and Linlin Shen and Weijie Liu and Weiquan Mao and Hui Zhang},
+            year={2022},
+            eprint={2209.05034},
+            archivePrefix={arXiv},
+            primaryClass={cs.CL}
+        }""",
+        n_samples={"test": NUM_SAMPLES},
+        avg_character_length={},
+    )
+
+    def dataset_transform(self):
+        ds = dict()
+        for split in self.metadata.eval_splits:
+            labels = list(itertools.chain.from_iterable(self.dataset[split]["labels"]))
+            sentences = list(
+                itertools.chain.from_iterable(self.dataset[split]["sentences"])
+            )
+            ds[split] = Dataset.from_dict({"labels": labels, "sentences": sentences})
+        self.dataset = DatasetDict(ds)
+        self.dataset = self.stratified_subsampling(
+            self.dataset,
+            self.seed,
+            self.metadata.eval_splits,
+            label="labels",
+            n_samples=NUM_SAMPLES,
+        )
+
+
+class CLSClusteringFastP2P(AbsTaskClusteringFast):
+    metadata = TaskMetadata(
+        name="CLSClusteringFastP2P",
+        description="Clustering of titles + abstract from CLS dataset. Clustering of 13 sets on the main category.",
+        reference="https://arxiv.org/abs/2209.05034",
+        dataset={
+            "path": "C-MTEB/CLSClusteringP2P",
+            "revision": "4b6227591c6c1a73bc76b1055f3b7f3588e72476",
+        },
+        type="Clustering",
+        category="p2p",
+        eval_splits=["test"],
+        eval_langs=["cmn-Hans"],
+        main_score="v_measure",
+        date=("2022-01-01", "2022-09-12"),
+        form=["written"],
+        domains=["Academic"],
+        task_subtypes=["Thematic clustering", "Topic classification"],
+        license="Apache-2.0",
+        socioeconomic_status="high",
+        annotations_creators="derived",
+        dialect=[],
+        text_creation="found",
+        bibtex_citation="""@misc{li2022csl,
+            title={CSL: A Large-scale Chinese Scientific Literature Dataset}, 
+            author={Yudong Li and Yuqing Zhang and Zhe Zhao and Linlin Shen and Weijie Liu and Weiquan Mao and Hui Zhang},
+            year={2022},
+            eprint={2209.05034},
+            archivePrefix={arXiv},
+            primaryClass={cs.CL}
+        }""",
+        n_samples={"test": NUM_SAMPLES},
+        avg_character_length={},
+    )
+
+    def dataset_transform(self):
+        ds = dict()
+        for split in self.metadata.eval_splits:
+            labels = list(itertools.chain.from_iterable(self.dataset[split]["labels"]))
+            sentences = list(
+                itertools.chain.from_iterable(self.dataset[split]["sentences"])
+            )
+            ds[split] = Dataset.from_dict({"labels": labels, "sentences": sentences})
+        self.dataset = DatasetDict(ds)
+        self.dataset = self.stratified_subsampling(
+            self.dataset,
+            self.seed,
+            self.metadata.eval_splits,
+            label="labels",
+            n_samples=NUM_SAMPLES,
+        )
 
 
 class CLSClusteringS2S(AbsTaskClustering):
+    superseeded_by = "CLSClusteringFastS2S"
     metadata = TaskMetadata(
         name="CLSClusteringS2S",
         description="Clustering of titles from CLS dataset. Clustering of 13 sets on the main category.",
@@ -29,12 +142,13 @@ class CLSClusteringS2S(AbsTaskClustering):
         dialect=None,
         text_creation=None,
         bibtex_citation=None,
-        n_samples=None,
+        n_samples={"test": 100000},
         avg_character_length=None,
     )
 
 
 class CLSClusteringP2P(AbsTaskClustering):
+    superseeded_by = "CLSClusteringFastP2P"
     metadata = TaskMetadata(
         name="CLSClusteringP2P",
         description="Clustering of titles + abstract from CLS dataset. Clustering of 13 sets on the main category.",
@@ -58,7 +172,7 @@ class CLSClusteringP2P(AbsTaskClustering):
         dialect=None,
         text_creation=None,
         bibtex_citation=None,
-        n_samples=None,
+        n_samples={"test": 100000},
         avg_character_length=None,
     )
 
