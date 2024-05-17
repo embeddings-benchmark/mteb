e5241d158304568f2146a151156eb6387bda0e54
Isaac Chung <chungisaac1217@gmail.com>
2024-05-17 12:01:58 +0000

    convert thunews to fast

--- mteb/tasks/Clustering/zho/CMTEBClustering.py
+++ mteb/tasks/Clustering/zho/CMTEBClustering.py
@@ -177,7 +177,99 @@ class CLSClusteringP2P(AbsTaskClustering):
     )
 
 
+class ThuNewsClusteringFastS2S(AbsTaskClusteringFast):
+    metadata = TaskMetadata(
+        name="ThuNewsClusteringFastS2S",
+        dataset={
+            "path": "C-MTEB/ThuNewsClusteringS2S",
+            "revision": "8a8b2caeda43f39e13c4bc5bea0f8a667896e10d",
+        },
+        description="Clustering of titles from the THUCNews dataset",
+        reference="http://thuctc.thunlp.org/",
+        type="Clustering",
+        category="s2s",
+        eval_splits=["test"],
+        eval_langs=["cmn-Hans"],
+        main_score="v_measure",
+        date=("2006-01-01", "2007-01-01"),
+        form=["written"],
+        domains=["News"],
+        task_subtypes=['Thematic clustering', 'Topic classification'],
+        license="Not specified",
+        socioeconomic_status="mixed",
+        annotations_creators="derived",
+        dialect=[],
+        text_creation="found",
+        bibtex_citation="",
+        n_samples={"test": NUM_SAMPLES},
+        avg_character_length={},
+    )
+
+    def dataset_transform(self):
+        ds = dict()
+        for split in self.metadata.eval_splits:
+            labels = list(itertools.chain.from_iterable(self.dataset[split]["labels"]))
+            sentences = list(
+                itertools.chain.from_iterable(self.dataset[split]["sentences"])
+            )
+            ds[split] = Dataset.from_dict({"labels": labels, "sentences": sentences})
+        self.dataset = DatasetDict(ds)
+        self.dataset = self.stratified_subsampling(
+            self.dataset,
+            self.seed,
+            self.metadata.eval_splits,
+            label="labels",
+            n_samples=NUM_SAMPLES,
+        )
+
+
+class ThuNewsClusteringFastP2P(AbsTaskClusteringFast):
+    metadata = TaskMetadata(
+        name="ThuNewsClusteringFastP2P",
+        dataset={
+            "path": "C-MTEB/ThuNewsClusteringP2P",
+            "revision": "5798586b105c0434e4f0fe5e767abe619442cf93",
+        },
+        description="Clustering of titles + abstracts from the THUCNews dataset",
+        reference="http://thuctc.thunlp.org/",
+        type="Clustering",
+        category="p2p",
+        eval_splits=["test"],
+        eval_langs=["cmn-Hans"],
+        main_score="v_measure",
+        date=("2006-01-01", "2007-01-01"),
+        form=["written"],
+        domains=["News"],
+        task_subtypes=['Thematic clustering', 'Topic classification'],
+        license="Not specified",
+        socioeconomic_status="mixed",
+        annotations_creators="derived",
+        dialect=[],
+        text_creation="found",
+        bibtex_citation="",
+        n_samples={"test": NUM_SAMPLES},
+        avg_character_length={},
+    )
+
+    def dataset_transform(self):
+        ds = dict()
+        for split in self.metadata.eval_splits:
+            labels = list(itertools.chain.from_iterable(self.dataset[split]["labels"]))
+            sentences = list(
+                itertools.chain.from_iterable(self.dataset[split]["sentences"])
+            )
+            ds[split] = Dataset.from_dict({"labels": labels, "sentences": sentences})
+        self.dataset = DatasetDict(ds)
+        self.dataset = self.stratified_subsampling(
+            self.dataset,
+            self.seed,
+            self.metadata.eval_splits,
+            label="labels",
+            n_samples=NUM_SAMPLES,
+        )
+
 class ThuNewsClusteringS2S(AbsTaskClustering):
+    superseeded_by = "ThuNewsClusteringFastS2S"
     metadata = TaskMetadata(
         name="ThuNewsClusteringS2S",
         dataset={
@@ -201,12 +293,13 @@ class ThuNewsClusteringS2S(AbsTaskClustering):
         dialect=None,
         text_creation=None,
         bibtex_citation=None,
-        n_samples=None,
+        n_samples={"test": 100000},
         avg_character_length=None,
     )
 
 
 class ThuNewsClusteringP2P(AbsTaskClustering):
+    superseeded_by = "ThuNewsClusteringFastP2P"
     metadata = TaskMetadata(
         name="ThuNewsClusteringP2P",
         dataset={
@@ -230,6 +323,6 @@ class ThuNewsClusteringP2P(AbsTaskClustering):
         dialect=None,
         text_creation=None,
         bibtex_citation=None,
-        n_samples=None,
+        n_samples={"test": 100000},
         avg_character_length=None,
     )
