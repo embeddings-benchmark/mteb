ca7266e6c81d8155a42d3532b725126c9c896a94
Xing Han Lu <21180505+xhluca@users.noreply.github.com>
2024-05-18 02:58:28 -0400

    fix: Add PublicHealthQA (#750)
    * Create new PublicHealthQA class and add to `mteb/tasks/Retrieval/__init__.py`
    
    * Add retriever results
    
    * Update metadata
    
    * Update mteb/tasks/Retrieval/multilingual/PublicHealthQARetrieval.py
    
    Co-authored-by: Wissam Siblini <36303760+wissam-sib@users.noreply.github.com>
    
    * Update mteb/tasks/Retrieval/multilingual/PublicHealthQARetrieval.py
    
    Co-authored-by: Wissam Siblini <36303760+wissam-sib@users.noreply.github.com>
    
    * Update mteb/tasks/Retrieval/multilingual/PublicHealthQARetrieval.py
    
    Co-authored-by: Isaac Chung <chungisaac1217@gmail.com>
    
    * Add contribution points
    
    ---------
    
    Co-authored-by: Wissam Siblini <36303760+wissam-sib@users.noreply.github.com>
    Co-authored-by: Isaac Chung <chungisaac1217@gmail.com>

--- mteb/tasks/Retrieval/__init__.py
+++ mteb/tasks/Retrieval/__init__.py
@@ -71,6 +71,7 @@ from .multilingual.MLQARetrieval import *
 from .multilingual.MultiLongDocRetrieval import *
 from .multilingual.NeuCLIR2022Retrieval import *
 from .multilingual.NeuCLIR2023Retrieval import *
+from .multilingual.PublicHealthQARetrieval import *
 from .multilingual.XMarketRetrieval import *
 from .multilingual.XPQARetrieval import *
 from .multilingual.XQuADRetrieval import *
