f233121c31bb200b8d7c385bd19e4a57711fcdeb
malteos <git@i.mieo.de>
2024-05-23 10:42:42 +0200

    fix: Added new dataset XStance pair classification (#737)
    * Added xstance dataset
    
    * Adding points, fixed hard-coded data loading

--- mteb/tasks/PairClassification/__init__.py
+++ mteb/tasks/PairClassification/__init__.py
@@ -15,6 +15,7 @@ from .multilingual.OpusparcusPC import *
 from .multilingual.PawsX import *
 from .multilingual.RTE3 import *
 from .multilingual.XNLI import *
+from .multilingual.XStance import *
 from .pol.PolishPC import *
 from .por.Assin2RTE import *
 from .por.SickBrPC import *
