65b1d38ec5a983166320f8507def54b9d4d1ab60
mciancone <mathieu.ciancone@gmail.com>
2024-05-24 13:57:12 +0200

    adapt load_dataset for dataset update

--- mteb/tasks/Reranking/fra/AlloprofReranking.py
+++ mteb/tasks/Reranking/fra/AlloprofReranking.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+import datasets
+
 from mteb.abstasks.TaskMetadata import TaskMetadata
 
 from ....abstasks.AbsTaskReranking import AbsTaskReranking
@@ -12,7 +14,7 @@ class AlloprofReranking(AbsTaskReranking):
         reference="https://huggingface.co/datasets/antoinelb7/alloprof",
         dataset={
             "path": "lyon-nlp/mteb-fr-reranking-alloprof-s2p",
-            "revision": "e40c8a63ce02da43200eccb5b0846fcaa888f562",
+            "revision": "e40c8a63ce02da43200eccb5b0846fcaa888f562", # TODO : change
         },
         type="Reranking",
         category="s2s",
@@ -29,6 +31,35 @@ class AlloprofReranking(AbsTaskReranking):
         dialect=None,
         text_creation=None,
         bibtex_citation=None,
-        n_samples=None,
+        n_samples={"test": 2316, "train": 9264},
         avg_character_length=None,
     )
+
+    def load_data(self):
+
+        if self.data_loaded:
+            return
+
+        self.dataset = datasets.load_dataset(
+            name="queries",
+            **self.metadata_dict["dataset"],
+            split=self.metadata["eval_splits"][0]
+        )
+        self.documents = datasets.load_dataset(
+            name="documents",
+            **self.metadata_dict["dataset"],
+            split="test"
+        )
+        # replace documents ids in positive and negative column by their respective texts
+        self.dataset = self.dataset.map(lambda x: {
+            "positive": [
+                self.documents["text"][self.documents["doc_id"].index(docid)]
+                for docid in x["positive"]
+            ],
+            "negative": [
+                self.documents["text"][self.documents["doc_id"].index(docid)]
+                for docid in x["negative"]
+            ]
+        })
+
+        self.data_loaded = True
