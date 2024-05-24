cf5152eafae6e663147a92f18af2fd6e830a03fc
Kenneth Enevoldsen <kennethcenevoldsen@gmail.com>
2024-05-24 12:25:41 +0200

    add model meta to create reproducible workflow
    - Add outline for model meta object
    - Added a single model as a an example
    - Added test for the reproducible workflow
    
    The intention is that a reproducible workflow should then look like:
    
    ```
    # assuming the same mteb and sent. trf. version
    
    model_meta = mteb.get_model(model_name)
    task = mteb.get_task(task_name)
    
    model = model_meta.load_model() # load model either using custom loader or sentence transformer (with revision)
    
    eval = MTEB(tasks=[task])
    eval.run(model, output_folder="tests/results", overwrite_results=True)
    ```
    
    For running models we can the simply have tasks like:
    
    1) implement model
    2) ensures that it runs on all tasks types
    
    Running the models then become simple:
    
    ```
    eval = MTEB(tasks=mteb.get_tasks())
    for mdl_name in models:
       model_meta = mteb.get_model(mdl_name)
       mdl = model_meta.load_model()
       eval.run(mteb.get_model(mdl)
    ```
    
    We can start with this already now e.g. on classification tasks.

---
+++ mteb/model_meta.py
@@ -0,0 +1,78 @@
+from __future__ import annotations
+
+from datetime import date
+from functools import partial
+from typing import Any, Callable, Literal
+
+from pydantic import BaseModel, BeforeValidator, TypeAdapter
+from sentence_transformers import SentenceTransformer
+from typing_extensions import Annotated
+
+from mteb.encoder_interface import Encoder, EncoderWithQueryCorpusEncode
+
+from .languages import ISO_LANGUAGE_SCRIPT
+
+Frameworks = Literal["Sentence Transformers", "PyTorch"]
+
+pastdate_adapter = TypeAdapter(date)
+STR_DATE = Annotated[
+    str, BeforeValidator(lambda value: str(pastdate_adapter.validate_python(value)))
+]  # Allows the type to be a string, but ensures that the string is a valid date
+
+
+def sentence_transformers_loader(model_name: str, revision: str) -> SentenceTransformer:
+    return SentenceTransformer(model_name_or_path=model_name, revision=revision)
+
+
+class ModelMeta(BaseModel):
+    """The model metadata object.
+
+    Attributes:
+        loader: the function that loads the model. If None it will just default to loading the model using the sentence transformer library.
+        name: The name of the model, ideally the name on huggingface.
+        n_parameters: The number of parameters in the model. Can be None if the the number of parameters is not known (e.g. for proprietary models) or
+            if the loader returns a SentenceTransformer model from which it can be derived.
+        memory_usage: The amount of memory the model uses. Can be None if the memory usage is not known (e.g. for proprietary models).
+        max_tokens: The maximum number of tokens the model can handle. Can be None if the maximum number of tokens is not known (e.g. for proprietary
+            models).
+        embedding_dimension: The dimension of the embeddings produced by the model. Currently all models are assumed to produce fixed-size embeddings.
+        revision: The revision number of the model.
+        release_date: The date the model was released.
+        license: The license under which the model is released. Required if open_source is True.
+        open_source: Whether the model is open source or proprietary.
+        framework: The framework the model is implemented in, can be a list of frameworks.
+        languages: The languages the model is intended for specified as a 3 letter language code followed by a script code e.g. "eng-Latn" for English
+            in the Latin script.
+    """
+
+    name: str
+    revision: str
+    release_date: STR_DATE
+    languages: list[ISO_LANGUAGE_SCRIPT]
+    loader: Callable[..., Encoder | EncoderWithQueryCorpusEncode] | None = None
+    n_parameters: int | None = None
+    memory_usage: float | None = None
+    max_tokens: int | None = None
+    embedding_dimension: int | None = None
+    license: str | None = None
+    open_source: bool | None = None
+    framework: list[Frameworks] = []
+
+    def to_dict(self):
+        dict_repr = self.model_dump()
+        loader = dict_repr.pop("loader", None)
+        dict_repr["loader"] = loader.__name__ if loader is not None else None
+        return dict_repr
+
+    def load_model(self, **kwargs: Any) -> Encoder | EncoderWithQueryCorpusEncode:
+        if self.loader is None:
+            loader = partial(
+                sentence_transformers_loader,
+                model_name=self.name,
+                revision=self.revision,
+            )
+        else:
+            loader = self.loader
+
+        model: Encoder | EncoderWithQueryCorpusEncode = loader(**kwargs)  # type: ignore
+        return model
---
+++ mteb/models/__init__.py
@@ -0,0 +1,22 @@
+import sentence_transformers_models
+
+from mteb.model_meta import ModelMeta
+
+
+def get_model(model_name: str) -> ModelMeta:
+    """A function to fetch a model metadata object by name.
+
+    Args:
+        model_name: Name of the model to fetch
+    """
+    return models[model_name]
+
+
+model_modules = [sentence_transformers_models]
+models = {}
+
+
+for module in model_modules:
+    for mdl in module.__dict__.values():
+        if isinstance(mdl, ModelMeta):
+            models[mdl.name] = mdl
