0ceaff6fa7ed76f5915c66d6b2ac1b46a789f19e
xhluca <git@xinghanlu.com>
2024-05-25 01:23:26 -0400

    Update dates, and indicate dotos

--- mteb/tasks/Reranking/eng/WebLINXCandidatesReranking.py
+++ mteb/tasks/Reranking/eng/WebLINXCandidatesReranking.py
@@ -11,16 +11,16 @@ class WebLINXCandidatesReranking(AbsTaskReranking):
         description="WEBLINX is a large-scale benchmark of 100K interactions across 2300 expert demonstrations of conversational web navigation. The reranking task focuses on finding relevant elements at every given step in the trajectory.",
         reference="https://mcgill-nlp.github.io/weblinx",
         dataset={
-            "path": "https://huggingface.co/datasets/xhluca/WebLINX-testing",
-            "revision": "main",
+            "path": "https://huggingface.co/datasets/xhluca/WebLINX-testing",  # TODO: change to the actual path
+            "revision": "main", # TODO: change to commit hash
         },
         type="Reranking",
         category="p2p",
         eval_splits=["validation", "test_iid", "test_cat", "test_geo", "test_vis", "test_web"],
         eval_langs=["eng-Latn"],
         main_score="recall_at_10",
-        date=("2023-01-01", "2023-12-31",),
-        form='written',
+        date=("2023-03-01", "2023-10-30"),
+        form=['written'],
         domains=['Academic', 'Web'],
         task_subtypes=['Code retrieval', 'Conversational retrieval'],
         license="CC BY-NC-SA 4.0",
