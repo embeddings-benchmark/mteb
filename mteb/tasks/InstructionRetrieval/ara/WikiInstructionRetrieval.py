c5850f4af012cc25c5d2b8763fc2ee2517c6616e
Ruqyai <rbinsafi@gmail.com>
2024-05-25 12:33:35 +0300

    Renaming columns

--- mteb/tasks/InstructionRetrieval/ara/WikiInstructionRetrieval.py
+++ mteb/tasks/InstructionRetrieval/ara/WikiInstructionRetrieval.py
@@ -5,6 +5,7 @@ from mteb.abstasks.TaskMetadata import TaskMetadata
 
 from ....abstasks.AbsTaskInstructionRetrieval import AbsTaskInstructionRetrieval
 
+from datasets import load_dataset
 
 class WikiInstructionRetrieval(AbsTaskInstructionRetrieval):
     metadata = TaskMetadata(
@@ -14,6 +15,7 @@ class WikiInstructionRetrieval(AbsTaskInstructionRetrieval):
         dataset={
             "path": "wiki_qa_ar",
             "revision": "90f1673b95f7ca68ffc7d8bd1451f8e304819f49",
+            "name": "plain_text",
         },
         type="InstructionRetrieval",
         category="s2p",
@@ -30,6 +32,19 @@ class WikiInstructionRetrieval(AbsTaskInstructionRetrieval):
         dialect=[],
         text_creation="found",
         bibtex_citation=""" """,
-        n_samples={"eng": 19919 * 2},
-        avg_character_length={"eng": 2768.749235474006},
+        n_samples={"ara": 19919 * 2},
+        avg_character_length={"ara": 2768.749235474006},
     )
+
+
+    def load_data(self, **kwargs):
+        dataset = load_dataset(
+            self.metadata.dataset["path"],
+            name=self.metadata.dataset["name"],
+            revision=self.metadata.dataset["revision"]
+        )
+
+        dataset = dataset["test"].select_columns(["question", "answer"])
+        dataset = dataset.rename_column("question", "title")
+        dataset = dataset.rename_column("answer", "text")
+        self.corpus = dataset.to_dict()
