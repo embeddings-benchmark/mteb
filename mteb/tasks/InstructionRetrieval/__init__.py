5aeeb9dd3e40de5c898224901fb9494175dfed99
Ruqyai <rbinsafi@gmail.com>
2024-05-25 05:30:51 +0300

    Adding the task in init file

--- mteb/tasks/InstructionRetrieval/__init__.py
+++ mteb/tasks/InstructionRetrieval/__init__.py
@@ -3,3 +3,4 @@ from __future__ import annotations
 from .eng.Core17InstructionRetrieval import *
 from .eng.News21InstructionRetrieval import *
 from .eng.Robust04InstructionRetrieval import *
+from .ara.WikipediaInstructionRetrieval import *
