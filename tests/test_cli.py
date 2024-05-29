f8be487020d2f63093b97e16640a1d41cc6c0a8c
Isaac Chung <chungisaac1217@gmail.com>
2024-05-29 12:54:03 +0000

    tests (not dataset missing ones) passing

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -16,17 +16,18 @@ def test_available_tasks():
 def test_run_task(
     model_name: str = "average_word_embeddings_komninos",
     task_name="BornholmBitextMining",
+    model_revision="21eec43590414cb8e3a6f654857abed0483ae36e",
 ):
-    command = f"mteb -m {model_name} -t {task_name} --verbosity 3 --output_folder tests/results/test_model"
+    command = f"mteb -m {model_name} -t {task_name} --verbosity 3 --output_folder tests/results/test_model --model_revision {model_revision}"
     result = subprocess.run(command, shell=True, capture_output=True, text=True)
     assert result.returncode == 0, "Command failed"
 
-    path = Path("tests/results/test_model")
-    assert path.exists(), "Output folder not created"
-    json_files = list(path.glob("*.json"))
+    path = Path(f"tests/results/test_model")
+    results_path = Path(f"tests/results/test_model/{model_revision}")
+    assert results_path.exists(), "Output folder not created"
     assert "model_meta.json" in [
-        f.name for f in json_files
+        f.name for f in list(path.glob("*.json"))
     ], "model_meta.json not found in output folder"
     assert f"{task_name}.json" in [
-        f.name for f in json_files
+        f.name for f in list(results_path.glob("*.json"))
     ], f"{task_name} not found in output folder"
