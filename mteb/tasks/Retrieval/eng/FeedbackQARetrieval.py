0618cdf54c1b0e82cbbd033aef53392a78fb27ba
cslizc <54827718+cslizc@users.noreply.github.com>
2024-05-30 16:04:03 -0400

    Update mteb/tasks/Retrieval/eng/FeedbackQARetrieval.py
    Co-authored-by: Xing Han Lu <21180505+xhluca@users.noreply.github.com>

--- mteb/tasks/Retrieval/eng/FeedbackQARetrieval.py
+++ mteb/tasks/Retrieval/eng/FeedbackQARetrieval.py
@@ -27,7 +27,27 @@ class FeedbackQARetrieval(AbsTaskRetrieval):
         annotations_creators=None,
         dialect=None,
         text_creation=None,
-        bibtex_citation=None,
+        bibtex_citation="""
+@inproceedings{li-etal-2022-using,
+    title = "Using Interactive Feedback to Improve the Accuracy and Explainability of Question Answering Systems Post-Deployment",
+    author = "Li, Zichao  and
+      Sharma, Prakhar  and
+      Lu, Xing Han  and
+      Cheung, Jackie  and
+      Reddy, Siva",
+    editor = "Muresan, Smaranda  and
+      Nakov, Preslav  and
+      Villavicencio, Aline",
+    booktitle = "Findings of the Association for Computational Linguistics: ACL 2022",
+    month = may,
+    year = "2022",
+    address = "Dublin, Ireland",
+    publisher = "Association for Computational Linguistics",
+    url = "https://aclanthology.org/2022.findings-acl.75",
+    doi = "10.18653/v1/2022.findings-acl.75",
+    pages = "926--937"
+}
+"""
         n_samples=None,
         avg_character_length=None,
     )
