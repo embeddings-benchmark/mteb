3f58e8136839e9b6dee4a211431b75347db1e570
Niklas Muennighoff <n.muennighoff@gmail.com>
2024-05-31 21:41:48 -0700

    Update revision

--- mteb/models/sentence_transformers_models.py
+++ mteb/models/sentence_transformers_models.py
@@ -14,6 +14,6 @@ paraphrase_multilingual_MiniLM_L12_v2 = ModelMeta(
     name="sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2",
     languages=[], # TODO:  ar, bg, ca, cs, da, de, el, en, es, et, fa, fi, fr, fr-ca, gl, gu, he, hi, hr, hu, hy, id, it, ja, ka, ko, ku, lt, lv, mk, mn, mr, ms, my, nb, nl, pl, pt, pt-br, ro, ru, sk, sl, sq, sr, sv, th, tr, uk, ur, vi, zh-cn, zh-tw
     open_source=True,
-    revision="e4ce9877abf3edfe10b0d82785e83bdcb973e22e",  # TODO: update revision
+    revision="bf3bf13ab40c3157080a7ab344c831b9ad18b5eb",
     release_date="2019-11-01", # release date of paper 
 )
