8d7005a287c01df8c77340babd3a883569b6e7c2
Niklas Muennighoff <n.muennighoff@gmail.com>
2024-06-04 19:14:30 -0700

    Use embedding mode to save memory

--- mteb/models/gritlm.py
+++ mteb/models/gritlm.py
@@ -9,7 +9,7 @@ logger = logging.getLogger(__name__)
 try:
   from gritlm import GritLM
   gritlm7b = ModelMeta(
-    loader=partial(GritLM, "GritLM/GritLM-7B"),
+    loader=partial(GritLM, "GritLM/GritLM-7B", mode='embedding'),
     name="GritLM/GritLM-7B",
     languages=[],
     open_source=True,
@@ -17,7 +17,7 @@ try:
     release_date="2024-02-15",
   )
   gritlm8x7b = ModelMeta(
-    loader=partial(GritLM, "GritLM/GritLM-8x7B"),
+    loader=partial(GritLM, "GritLM/GritLM-8x7B", mode='embedding'),
     name="GritLM/GritLM-8x7B",
     languages=[],
     open_source=True,
