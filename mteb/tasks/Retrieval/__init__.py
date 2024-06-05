8962c0cac128121368bd56c63228c32520c1db22
Muennighoff <n.muennighoff@gmail.com>
2024-06-04 19:19:10 -0700

    Format

--- mteb/tasks/Retrieval/__init__.py
+++ mteb/tasks/Retrieval/__init__.py
@@ -29,8 +29,8 @@ from .eng.CQADupstackUnixRetrieval import *
 from .eng.CQADupstackWebmastersRetrieval import *
 from .eng.CQADupstackWordpressRetrieval import *
 from .eng.DBPediaRetrieval import *
-from .eng.FeedbackQARetrieval import *
 from .eng.FaithDialRetrieval import *
+from .eng.FeedbackQARetrieval import *
 from .eng.FEVERRetrieval import *
 from .eng.FiQA2018Retrieval import *
 from .eng.HagridRetrieval import *
