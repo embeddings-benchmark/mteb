878f226fa51d65cc22fd00f6e6a7e697316a16fe
akshita_sukhlecha <sukhlecha.akshita@gmail.com>
2024-06-09 18:40:46 -0700

    Multilingual/BelebeleRetrieval: convert to cross lingual task

--- mteb/tasks/Retrieval/multilingual/BelebeleRetrieval.py
+++ mteb/tasks/Retrieval/multilingual/BelebeleRetrieval.py
@@ -2,140 +2,152 @@ from __future__ import annotations
 
 from datasets import load_dataset
 
-from mteb.abstasks.TaskMetadata import TaskMetadata
-
-from ....abstasks import MultilingualTask
-from ....abstasks.AbsTaskRetrieval import AbsTaskRetrieval
+from mteb.abstasks import AbsTaskRetrieval, CrosslingualTask, TaskMetadata
 
 _EVAL_SPLIT = "test"
 
-_LANGS = {
-    "acm-Arab": ["acm-Arab"],
-    "afr-Latn": ["afr-Latn"],
-    "als-Latn": ["als-Latn"],
-    "amh-Ethi": ["amh-Ethi"],
-    "apc-Arab": ["apc-Arab"],
-    "arb-Arab": ["arb-Arab"],
-    "arb-Latn": ["arb-Latn"],
-    "ars-Arab": ["ars-Arab"],
-    "ary-Arab": ["ary-Arab"],
-    "arz-Arab": ["arz-Arab"],
-    "asm-Beng": ["asm-Beng"],
-    "azj-Latn": ["azj-Latn"],
-    "bam-Latn": ["bam-Latn"],
-    "ben-Beng": ["ben-Beng"],
-    "ben-Latn": ["ben-Latn"],
-    "bod-Tibt": ["bod-Tibt"],
-    "bul-Cyrl": ["bul-Cyrl"],
-    "cat-Latn": ["cat-Latn"],
-    "ceb-Latn": ["ceb-Latn"],
-    "ces-Latn": ["ces-Latn"],
-    "ckb-Arab": ["ckb-Arab"],
-    "dan-Latn": ["dan-Latn"],
-    "deu-Latn": ["deu-Latn"],
-    "ell-Grek": ["ell-Grek"],
-    "eng-Latn": ["eng-Latn"],
-    "est-Latn": ["est-Latn"],
-    "eus-Latn": ["eus-Latn"],
-    "fin-Latn": ["fin-Latn"],
-    "fra-Latn": ["fra-Latn"],
-    "fuv-Latn": ["fuv-Latn"],
-    "gaz-Latn": ["gaz-Latn"],
-    "grn-Latn": ["grn-Latn"],
-    "guj-Gujr": ["guj-Gujr"],
-    "hat-Latn": ["hat-Latn"],
-    "hau-Latn": ["hau-Latn"],
-    "heb-Hebr": ["heb-Hebr"],
-    "hin-Deva": ["hin-Deva"],
-    "hin-Latn": ["hin-Latn"],
-    "hrv-Latn": ["hrv-Latn"],
-    "hun-Latn": ["hun-Latn"],
-    "hye-Armn": ["hye-Armn"],
-    "ibo-Latn": ["ibo-Latn"],
-    "ilo-Latn": ["ilo-Latn"],
-    "ind-Latn": ["ind-Latn"],
-    "isl-Latn": ["isl-Latn"],
-    "ita-Latn": ["ita-Latn"],
-    "jav-Latn": ["jav-Latn"],
-    "jpn-Jpan": ["jpn-Jpan"],
-    "kac-Latn": ["kac-Latn"],
-    "kan-Knda": ["kan-Knda"],
-    "kat-Geor": ["kat-Geor"],
-    "kaz-Cyrl": ["kaz-Cyrl"],
-    "kea-Latn": ["kea-Latn"],
-    "khk-Cyrl": ["khk-Cyrl"],
-    "khm-Khmr": ["khm-Khmr"],
-    "kin-Latn": ["kin-Latn"],
-    "kir-Cyrl": ["kir-Cyrl"],
-    "kor-Hang": ["kor-Hang"],
-    "lao-Laoo": ["lao-Laoo"],
-    "lin-Latn": ["lin-Latn"],
-    "lit-Latn": ["lit-Latn"],
-    "lug-Latn": ["lug-Latn"],
-    "luo-Latn": ["luo-Latn"],
-    "lvs-Latn": ["lvs-Latn"],
-    "mal-Mlym": ["mal-Mlym"],
-    "mar-Deva": ["mar-Deva"],
-    "mkd-Cyrl": ["mkd-Cyrl"],
-    "mlt-Latn": ["mlt-Latn"],
-    "mri-Latn": ["mri-Latn"],
-    "mya-Mymr": ["mya-Mymr"],
-    "nld-Latn": ["nld-Latn"],
-    "nob-Latn": ["nob-Latn"],
-    "npi-Deva": ["npi-Deva"],
-    "npi-Latn": ["npi-Latn"],
-    "nso-Latn": ["nso-Latn"],
-    "nya-Latn": ["nya-Latn"],
-    "ory-Orya": ["ory-Orya"],
-    "pan-Guru": ["pan-Guru"],
-    "pbt-Arab": ["pbt-Arab"],
-    "pes-Arab": ["pes-Arab"],
-    "plt-Latn": ["plt-Latn"],
-    "pol-Latn": ["pol-Latn"],
-    "por-Latn": ["por-Latn"],
-    "ron-Latn": ["ron-Latn"],
-    "rus-Cyrl": ["rus-Cyrl"],
-    "shn-Mymr": ["shn-Mymr"],
-    "sin-Latn": ["sin-Latn"],
-    "sin-Sinh": ["sin-Sinh"],
-    "slk-Latn": ["slk-Latn"],
-    "slv-Latn": ["slv-Latn"],
-    "sna-Latn": ["sna-Latn"],
-    "snd-Arab": ["snd-Arab"],
-    "som-Latn": ["som-Latn"],
-    "sot-Latn": ["sot-Latn"],
-    "spa-Latn": ["spa-Latn"],
-    "srp-Cyrl": ["srp-Cyrl"],
-    "ssw-Latn": ["ssw-Latn"],
-    "sun-Latn": ["sun-Latn"],
-    "swe-Latn": ["swe-Latn"],
-    "swh-Latn": ["swh-Latn"],
-    "tam-Taml": ["tam-Taml"],
-    "tel-Telu": ["tel-Telu"],
-    "tgk-Cyrl": ["tgk-Cyrl"],
-    "tgl-Latn": ["tgl-Latn"],
-    "tha-Thai": ["tha-Thai"],
-    "tir-Ethi": ["tir-Ethi"],
-    "tsn-Latn": ["tsn-Latn"],
-    "tso-Latn": ["tso-Latn"],
-    "tur-Latn": ["tur-Latn"],
-    "ukr-Cyrl": ["ukr-Cyrl"],
-    "urd-Arab": ["urd-Arab"],
-    "urd-Latn": ["urd-Latn"],
-    "uzn-Latn": ["uzn-Latn"],
-    "vie-Latn": ["vie-Latn"],
-    "war-Latn": ["war-Latn"],
-    "wol-Latn": ["wol-Latn"],
-    "xho-Latn": ["xho-Latn"],
-    "yor-Latn": ["yor-Latn"],
-    "zho-Hans": ["zho-Hans"],
-    "zho-Hant": ["zho-Hant"],
-    "zsm-Latn": ["zsm-Latn"],
-    "zul-Latn": ["zul-Latn"]
-}
+_LANGUAGES = [
+    "acm_Arab",
+    "afr_Latn",
+    "als_Latn",
+    "amh_Ethi",
+    "apc_Arab",
+    "arb_Arab",
+    "arb_Latn",
+    "ars_Arab",
+    "ary_Arab",
+    "arz_Arab",
+    "asm_Beng",
+    "azj_Latn",
+    "bam_Latn",
+    "ben_Beng",
+    "ben_Latn",
+    "bod_Tibt",
+    "bul_Cyrl",
+    "cat_Latn",
+    "ceb_Latn",
+    "ces_Latn",
+    "ckb_Arab",
+    "dan_Latn",
+    "deu_Latn",
+    "ell_Grek",
+    "eng_Latn",
+    "est_Latn",
+    "eus_Latn",
+    "fin_Latn",
+    "fra_Latn",
+    "fuv_Latn",
+    "gaz_Latn",
+    "grn_Latn",
+    "guj_Gujr",
+    "hat_Latn",
+    "hau_Latn",
+    "heb_Hebr",
+    "hin_Deva",
+    "hin_Latn",
+    "hrv_Latn",
+    "hun_Latn",
+    "hye_Armn",
+    "ibo_Latn",
+    "ilo_Latn",
+    "ind_Latn",
+    "isl_Latn",
+    "ita_Latn",
+    "jav_Latn",
+    "jpn_Jpan",
+    "kac_Latn",
+    "kan_Knda",
+    "kat_Geor",
+    "kaz_Cyrl",
+    "kea_Latn",
+    "khk_Cyrl",
+    "khm_Khmr",
+    "kin_Latn",
+    "kir_Cyrl",
+    "kor_Hang",
+    "lao_Laoo",
+    "lin_Latn",
+    "lit_Latn",
+    "lug_Latn",
+    "luo_Latn",
+    "lvs_Latn",
+    "mal_Mlym",
+    "mar_Deva",
+    "mkd_Cyrl",
+    "mlt_Latn",
+    "mri_Latn",
+    "mya_Mymr",
+    "nld_Latn",
+    "nob_Latn",
+    "npi_Deva",
+    "npi_Latn",
+    "nso_Latn",
+    "nya_Latn",
+    "ory_Orya",
+    "pan_Guru",
+    "pbt_Arab",
+    "pes_Arab",
+    "plt_Latn",
+    "pol_Latn",
+    "por_Latn",
+    "ron_Latn",
+    "rus_Cyrl",
+    "shn_Mymr",
+    "sin_Latn",
+    "sin_Sinh",
+    "slk_Latn",
+    "slv_Latn",
+    "sna_Latn",
+    "snd_Arab",
+    "som_Latn",
+    "sot_Latn",
+    "spa_Latn",
+    "srp_Cyrl",
+    "ssw_Latn",
+    "sun_Latn",
+    "swe_Latn",
+    "swh_Latn",
+    "tam_Taml",
+    "tel_Telu",
+    "tgk_Cyrl",
+    "tgl_Latn",
+    "tha_Thai",
+    "tir_Ethi",
+    "tsn_Latn",
+    "tso_Latn",
+    "tur_Latn",
+    "ukr_Cyrl",
+    "urd_Arab",
+    "urd_Latn",
+    "uzn_Latn",
+    "vie_Latn",
+    "war_Latn",
+    "wol_Latn",
+    "xho_Latn",
+    "yor_Latn",
+    "zho_Hans",
+    "zho_Hant",
+    "zsm_Latn",
+    "zul_Latn"
+]
 
 
-class BelebeleRetrieval(MultilingualTask, AbsTaskRetrieval):
+def get_lang_pairs() -> dict[str, list[str]]:
+    # add all possible language pairs
+    lang_pairs = {}
+    for x in _LANGUAGES:
+        for y in _LANGUAGES:
+            xx = x.replace('_', '-')
+            yy = y.replace('_', '-')
+            pair = f"{xx}-{yy}"
+            lang_pairs[pair] = [xx, yy]
+    return lang_pairs
+
+
+_LANGUAGES_MAPPING = get_lang_pairs()
+
+
+class BelebeleRetrieval(CrosslingualTask, AbsTaskRetrieval):
     metadata = TaskMetadata(
         name="BelebeleRetrieval",
         dataset={
@@ -148,7 +160,7 @@ class BelebeleRetrieval(MultilingualTask, AbsTaskRetrieval):
         type="Retrieval",
         category="s2p",
         eval_splits=[_EVAL_SPLIT],
-        eval_langs=_LANGS,
+        eval_langs=_LANGUAGES_MAPPING,
         reference="https://arxiv.org/abs/2308.16884",
         main_score="ndcg_at_10",
         license="CC-BY-SA-4.0",
@@ -176,33 +188,41 @@ class BelebeleRetrieval(MultilingualTask, AbsTaskRetrieval):
 
         self.dataset = load_dataset(**self.metadata_dict["dataset"])
 
-        self.queries = {lang: {_EVAL_SPLIT: {}} for lang in self.hf_subsets}
-        self.corpus = {lang: {_EVAL_SPLIT: {}} for lang in self.hf_subsets}
-        self.relevant_docs = {lang: {_EVAL_SPLIT: {}} for lang in self.hf_subsets}
+        self.queries = {lang_pair: {_EVAL_SPLIT: {}} for lang_pair in self.hf_subsets}
+        self.corpus = {lang_pair: {_EVAL_SPLIT: {}} for lang_pair in self.hf_subsets}
+        self.relevant_docs = {lang_pair: {_EVAL_SPLIT: {}} for lang_pair in self.hf_subsets}
 
-        for lang in self.hf_subsets:
-            belebele_lang = _LANGS[lang][0].replace("-", "_")
-            ds = self.dataset[belebele_lang]
+        for lang_pair in self.hf_subsets:
+            languages = self.metadata_dict["eval_langs"][lang_pair]
+            lang_corpus, lang_question = languages[0].replace("-", "_"), languages[1].replace("-", "_")
+            ds_corpus = self.dataset[lang_corpus]
+            ds_question = self.dataset[lang_question]
 
             question_ids = {
-                question: _id for _id, question in enumerate(set(ds["question"]))
-            }
-            context_ids = {
-                passage: _id for _id, passage in enumerate(set(ds["flores_passage"]))
+                question: _id for _id, question in enumerate(set(ds_question["question"]))
             }
 
-            for row in ds:
+            link_to_context_id = {}
+            context_idx = 0
+            for row in ds_corpus:
+                if row['link'] not in link_to_context_id:
+                    context_id = f"C{context_idx}"
+                    link_to_context_id[row['link']] = context_id
+                    self.corpus[lang_pair][_EVAL_SPLIT][context_id] = {
+                        "title": "",
+                        "text": row['flores_passage']
+                    }
+                    context_idx = context_idx + 1
+
+            for row in ds_question:
                 query = row["question"]
                 query_id = f"Q{question_ids[query]}"
-                self.queries[lang][_EVAL_SPLIT][query_id] = query
-                context = row["flores_passage"]
-                context_id = f"C{context_ids[context]}"
-                self.corpus[lang][_EVAL_SPLIT][context_id] = {
-                    "title": "",
-                    "text": context,
-                }
-                if query_id not in self.relevant_docs[lang][_EVAL_SPLIT]:
-                    self.relevant_docs[lang][_EVAL_SPLIT][query_id] = {}
-                self.relevant_docs[lang][_EVAL_SPLIT][query_id][context_id] = 1
+                self.queries[lang_pair][_EVAL_SPLIT][query_id] = query
+
+                context_link = row["link"]
+                context_id = link_to_context_id[context_link]
+                if query_id not in self.relevant_docs[lang_pair][_EVAL_SPLIT]:
+                    self.relevant_docs[lang_pair][_EVAL_SPLIT][query_id] = {}
+                self.relevant_docs[lang_pair][_EVAL_SPLIT][query_id][context_id] = 1
 
         self.data_loaded = True
