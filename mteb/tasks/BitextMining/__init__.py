664dc98db6b923d41881d5d0591e7edf5f528755
gentaiscool <gentaindrawinata@gmail.com>
2024-06-13 16:25:42 -0400

    add NollySentiBitextMining

--- mteb/tasks/BitextMining/__init__.py
+++ mteb/tasks/BitextMining/__init__.py
@@ -11,6 +11,7 @@ from .multilingual.IN22ConvBitextMining import *
 from .multilingual.IN22GenBitextMining import *
 from .multilingual.IndicGenBenchFloresBitextMining import *
 from .multilingual.IWSLT2017BitextMinig import *
+from .multilingual.NollySentiBitextMining import *
 from .multilingual.NorwegianCourtsBitextMining import *
 from .multilingual.NTREXBitextMining import *
 from .multilingual.RomaTalesBitextMining import *
