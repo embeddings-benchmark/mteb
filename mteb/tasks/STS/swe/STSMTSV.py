949b926b841c8ea3c2ab9eb0c8d2f37dcd86c57a
Henil Panchal <henilp105@gmail.com>
2024-06-14 04:52:48 +0000

    dataset added

--- mteb/tasks/STS/swe/STSMTSV.py
+++ mteb/tasks/STS/swe/STSMTSV.py
@@ -41,7 +41,7 @@ class STSMTSV(AbsTaskSTS):
       primaryClass={id='cs.CL' full_name='Computation and Language' is_active=True alt_name='cmp-lg' in_archive='cs' is_general=False description='Covers natural language processing. Roughly includes material in ACM Subject Class I.2.7. Note that work on artificial languages (programming languages, logics, formal systems) that does not explicitly address natural-language issues broadly construed (natural-language processing, computational linguistics, speech, text retrieval, etc.) is not appropriate for this area.'}
         }""",
         n_samples={"train": 5749, "test": 1379,'validation':1500},
-        avg_character_length=None,
+        avg_character_length={"test":3.45},
     )
 
     @property
