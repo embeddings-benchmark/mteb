dffafd215373ea2a2dec290d936b6dfe144cde83
Henil Panchal <henilp105@gmail.com>
2024-06-14 06:21:41 +0000

    add dataset

---
+++ mteb/tasks/STS/ind/STSES.py
@@ -0,0 +1,62 @@
+from __future__ import annotations
+
+from mteb.abstasks.TaskMetadata import TaskMetadata
+
+from ....abstasks.AbsTaskSTS import AbsTaskSTS
+
+_EVAL_SPLIT = "test"
+
+
+class STSIDMT(AbsTaskSTS):
+    metadata = TaskMetadata(
+        name="STSIDMT",
+        dataset={
+            "path": "LazarusNLP/stsb_mt_id",
+            "revision": "0912bb6c9393c76d62a7c5ee81c4c817ff47c9f4",
+            "trust_remote_code": True,
+        },
+        description="Indonesian test sets from STS-B translated using Google Translate API",
+        reference="https://huggingface.co/datasets/LazarusNLP/stsb_mt_id",
+        type="STS",
+        category="s2s",
+        eval_splits=[_EVAL_SPLIT],
+        eval_langs=["ind-Latn"],
+        main_score="cosine_spearman",
+        date=None,
+        form=None,
+        domains=None,
+        task_subtypes=None,
+        license=None,
+        socioeconomic_status=None,
+        annotations_creators=None,
+        dialect=None,
+        text_creation=None,
+        bibtex_citation="""@software{wongso_2024_10983756,
+  author       = {Wongso, Wilson and
+                  Joyoadikusumo, Ananto and
+                  Setiawan, David Samuel and
+                  Limcorn, Steven},
+  title        = {LazarusNLP/indonesian-sentence-embeddings: v0.0.1},
+  month        = apr,
+  year         = 2024,
+  publisher    = {Zenodo},
+  version      = {v0.0.1},
+  doi          = {10.5281/zenodo.10983756},
+  url          = {https://doi.org/10.5281/zenodo.10983756}
+}
+""",
+        n_samples={"test":},
+        avg_character_length={"test":3.45},
+    )
+
+    @property
+    def metadata_dict(self) -> dict[str, str]:
+        metadata_dict = super().metadata_dict
+        metadata_dict["min_score"] = 0
+        metadata_dict["max_score"] = 5
+        return metadata_dict
+
+    def dataset_transform(self) -> None:
+        self.dataset = self.dataset.rename_column("correlation", "score")
+        self.dataset = self.dataset.rename_column("text_1", "sentence1")
+        self.dataset = self.dataset.rename_column("text_2", "sentence2")
---
+++ ret.py
@@ -0,0 +1,15 @@
+import mteb
+from sentence_transformers import SentenceTransformer
+
+# Define the sentence-transformers model name
+# model_name = "average_word_embeddings_komninos"
+model_name = "sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2"
+model = SentenceTransformer(model_name)
+tasks = mteb.get_tasks(tasks=["STSMTSV"])
+evaluation = mteb.MTEB(tasks=tasks)
+results = evaluation.run(model, output_folder=f"results/{model_name}")
+model_name = "intfloat/multilingual-e5-small"
+model = SentenceTransformer(model_name)
+tasks = mteb.get_tasks(tasks=["STSMTSV"])
+evaluation = mteb.MTEB(tasks=tasks)
+results = evaluation.run(model, output_folder=f"results/{model_name}")
