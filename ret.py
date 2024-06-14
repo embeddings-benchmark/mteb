5f31f7e6c3b4e4b3c0fe14f5f2c2cdf858489147
Henil Panchal <henilp105@gmail.com>
2024-06-14 06:26:25 +0000

    add dataset

--- ret.py
+++ ret.py
@@ -5,11 +5,11 @@ from sentence_transformers import SentenceTransformer
 # model_name = "average_word_embeddings_komninos"
 model_name = "sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2"
 model = SentenceTransformer(model_name)
-tasks = mteb.get_tasks(tasks=["STSMTSV"])
+tasks = mteb.get_tasks(tasks=["STSIDMT"])
 evaluation = mteb.MTEB(tasks=tasks)
 results = evaluation.run(model, output_folder=f"results/{model_name}")
 model_name = "intfloat/multilingual-e5-small"
 model = SentenceTransformer(model_name)
-tasks = mteb.get_tasks(tasks=["STSMTSV"])
+tasks = mteb.get_tasks(tasks=["STSIDMT"])
 evaluation = mteb.MTEB(tasks=tasks)
 results = evaluation.run(model, output_folder=f"results/{model_name}")
