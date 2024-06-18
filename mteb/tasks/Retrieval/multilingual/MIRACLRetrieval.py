f6c07a682120e1438568e416cd987a61d61f74c4
Imene Kerboua <imenelydia.kr@gmail.com>
2024-06-18 09:34:16 +0200

    Fix tests for ignore_identical_ids=True

--- mteb/tasks/Retrieval/multilingual/MIRACLRetrieval.py
+++ mteb/tasks/Retrieval/multilingual/MIRACLRetrieval.py
@@ -97,7 +97,7 @@ def _load_miracl_data(
     return corpus, queries, relevant_docs
 
 
-class MIRACLRetrieval(MultilingualTask, AbsTaskRetrieval):    
+class MIRACLRetrieval(MultilingualTask, AbsTaskRetrieval):
     metadata = TaskMetadata(
         name="MIRACLRetrieval",
         description="MIRACL (Multilingual Information Retrieval Across a Continuum of Languages) is a multilingual retrieval dataset that focuses on search across 18 different languages.",
--- tests/test_RetrievalEvaluator.py
+++ tests/test_RetrievalEvaluator.py
@@ -28,13 +28,13 @@ class TestRetrievalEvaluator:
                     "0": {"0": 1.0, "1": 0.9, "2": 0.8},
                     "1": {"0": 0.0, "1": 1.0, "2": 0.0},
                 },
-                False, 
+                False,
                 {
-                "ndcg": {'NDCG@1': 1.0, 'NDCG@2': 1.0, 'NDCG@3': 1.0},
-                "map": {"MAP@1": 0.75, "MAP@2": 1.0, "MAP@3": 1.0},
-                "recall": {"Recall@1": 0.75, "Recall@2": 1.0, "Recall@3": 1.0},
-                "precision": {"P@1": 1.0, "P@2": 0.75, "P@3": 0.5},
-                }
+                    "ndcg": {"NDCG@1": 1.0, "NDCG@2": 1.0, "NDCG@3": 1.0},
+                    "map": {"MAP@1": 0.75, "MAP@2": 1.0, "MAP@3": 1.0},
+                    "recall": {"Recall@1": 0.75, "Recall@2": 1.0, "Recall@3": 1.0},
+                    "precision": {"P@1": 1.0, "P@2": 0.75, "P@3": 0.5},
+                },
             ),
             (
                 # Qid: {Docid: Relevance}
@@ -46,17 +46,23 @@ class TestRetrievalEvaluator:
                     "0": {"0": 1.0, "1": 0.9, "2": 0.8},
                     "1": {"0": 0.0, "1": 1.0, "2": 0.0},
                 },
-                True, 
+                True,
                 {
-                "ndcg": {'NDCG@1': 1.0, 'NDCG@2': 1.0, 'NDCG@3': 1.0, 'NDCG(no self)@1': 0.5, 'NDCG(no self)@2': 0.30657, 'NDCG(no self)@3': 0.30657},
-                 "map": {"MAP@1": 0.25, "MAP@2": 0.25, "MAP@3": 0.25},
-                "recall": {"Recall@1": 0.25, "Recall@2": 0.25, "Recall@3": 0.25},
-                "precision": {"P@1": 0.5, "P@2": 0.25, "P@3": 0.16667},
-                }
+                    "ndcg": {
+                        "NDCG@1": 0.5,
+                        "NDCG@2": 0.30657,
+                        "NDCG@3": 0.30657
+                    },
+                    "map": {"MAP@1": 0.25, "MAP@2": 0.25, "MAP@3": 0.25},
+                    "recall": {"Recall@1": 0.25, "Recall@2": 0.25, "Recall@3": 0.25},
+                    "precision": {"P@1": 0.5, "P@2": 0.25, "P@3": 0.16667},
+                },
             ),
         ],
     )
-    def test_metrics_at_k(self, relevant_docs, results, ignore_identical_ids, expected_metrics):
+    def test_metrics_at_k(
+        self, relevant_docs, results, ignore_identical_ids, expected_metrics
+    ):
         ndcg, _map, recall, precision = self.evaluator.evaluate(
             relevant_docs,
             results,
