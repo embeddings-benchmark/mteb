1a31a2c72ec0a96f6165e0ee6e0610de21fef629
Artem Snegirev <artem.s.snegirev@gmail.com>
2024-06-19 20:15:31 +0300

    move models to a separate file

--- mteb/models/sentence_transformers_models.py
+++ mteb/models/sentence_transformers_models.py
@@ -89,36 +89,3 @@ labse = ModelMeta(
     revision="e34fab64a3011d2176c99545a93d5cbddc9a91b7",  # can be any
     release_date="2019-11-01",  # release date of paper
 )
-
-rubert_tiny2 = ModelMeta(
-    name="cointegrated/rubert-tiny2",
-    languages=["rus_Cyrl"],
-    open_source=True,
-    revision="dad72b8f77c5eef6995dd3e4691b758ba56b90c3",  # can be any
-    release_date="2021-10-28",
-)
-
-rubert_tiny = ModelMeta(
-    name="cointegrated/rubert-tiny",
-    languages=["rus_Cyrl"],
-    open_source=True,
-    revision="5441c5ea8026d4f6d7505ec004845409f1259fb1",  # can be any
-    release_date="2021-05-24",
-)
-
-
-sbert_large_nlu_ru = ModelMeta(
-    name="ai-forever/sbert_large_nlu_ru",
-    languages=["rus_Cyrl"],
-    open_source=True,
-    revision="af977d5dfa46a3635e29bf0ef383f2df2a08d47a",  # can be any
-    release_date="2020-11-20",
-)
-
-sbert_large_mt_nlu_ru = ModelMeta(
-    name="ai-forever/sbert_large_mt_nlu_ru",
-    languages=["rus_Cyrl"],
-    open_source=True,
-    revision="05300876c2b83f46d3ddd422a7f17e45cf633bb0",  # can be any
-    release_date="2021-05-18",
-)
