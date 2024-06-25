6b3c28c172479794e10a34ff146ebb2c0eacb9d7
Isaac Chung <chungisaac1217@gmail.com>
2024-06-25 19:14:20 +0000

    does not need as Jina works out of the box with ST

--- mteb/models/jina_models.py
+++
@@ -1,9 +0,0 @@
-from mteb.model_meta import ModelMeta
-
-jina_embeddings_v2_base_en = ModelMeta(
-    name="jinaai/jina-embeddings-v2-base-en",
-    languages=["eng_Latn"],
-    open_source=True,
-    revision="f84569a8c478c7bb2ea3bc187034859870940deb",
-    release_date="2023-10-30",  # release date of paper
-)
