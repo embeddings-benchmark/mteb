24408fd97c4e58af462c1ada75851d44c3c8faea
Isaac Chung <chungisaac1217@gmail.com>
2024-06-25 18:58:00 +0000

    add models

--- mteb/models/bge_models.py
+++ mteb/models/bge_models.py
@@ -56,3 +56,12 @@ bge_base_en_v1_5 = ModelMeta(
     revision="a5beb1e3e68b9ab74eb54cfd186867f64f240e1a",
     release_date="2023-09-11",  # initial commit of hf model.
 )
+
+bge_large_en_v1_5 = ModelMeta(
+    loader=partial(BGEWrapper, model_name="BAAI/bge-large-en-v1.5"),  # type: ignore
+    name="BAAI/bge-large-en-v1.5",
+    languages=["eng_Latn"],
+    open_source=True,
+    revision="d4aa6901d3a41ba39fb536a557fa166f842b0e09",
+    release_date="2023-09-12",  # initial commit of hf model.
+)
---
+++ mteb/models/jina_models.py
@@ -0,0 +1,9 @@
+from mteb.model_meta import ModelMeta
+
+paraphrase_multilingual_MiniLM_L12_v2 = ModelMeta(
+    name="jinaai/jina-embeddings-v2-base-en",
+    languages=["eng_Latn"],
+    open_source=True,
+    revision="f84569a8c478c7bb2ea3bc187034859870940deb", 
+    release_date="2023-10-30",  # release date of paper
+)
