cead90a7cb87392a43202eeb1f33a3c2456e5f03
Kenneth Enevoldsen <kennethcenevoldsen@gmail.com>
2024-06-26 15:06:52 +0200

    Added normalization to encode

--- mteb/models/nomic_models.py
+++ mteb/models/nomic_models.py
@@ -1,9 +1,30 @@
+from __future__ import annotations
+
 from functools import partial
 
-from mteb.model_meta import ModelMeta, sentence_transformers_loader
+from sentence_transformers import SentenceTransformer
+
+from mteb.model_meta import ModelMeta
+
+
+class SentenceTransformerWithNormalization(SentenceTransformer):
+    def encode(self, sentences, *args, **kwargs):
+        if "normalize_embeddings" not in kwargs:
+            kwargs["normalize_embeddings"] = True
+
+        return super().encode(sentences, *args, **kwargs)
+
+
+def sentence_transformers_loader(
+    model_name: str, revision: str | None, **kwargs
+) -> SentenceTransformer:
+    return SentenceTransformerWithNormalization(
+        model_name_or_path=model_name, revision=revision, **kwargs
+    )
+
 
 nomic_embed = ModelMeta(
-    loader=partial(
+    loader=partial(  # type: ignore
         sentence_transformers_loader,
         trust_remote_code=True,
         model_name="nomic-ai/nomic-embed-text-v1.5",
@@ -20,3 +41,4 @@ if __name__ == "__main__":
     import mteb
 
     mdl = mteb.get_model(nomic_embed.name, nomic_embed.revision)
+    emb = mdl.encode(["test"], convert_to_tensor=True)
