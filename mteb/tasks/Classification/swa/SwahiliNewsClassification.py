d2932ce3af48d95d111488fb915199a4fa78befc
Michael Mollel <47386502+msamwelmollel@users.noreply.github.com>
2024-06-27 15:42:16 +0100

    swahili_news_classfication

---
+++ mteb/tasks/Classification/swa/SwahiliNewsClassification.py
@@ -0,0 +1,41 @@
+from __future__ import annotations
+
+from mteb.abstasks.TaskMetadata import TaskMetadata
+
+from ....abstasks import AbsTaskClassification
+
+class SwahiliNewsClassification(AbsTaskClassification):
+    metadata = TaskMetadata(
+        name="SwahiliNewsClassification",
+        description="Dataset for Swahili News Classification, categorized with 5 domains.",
+        reference="Building and Optimizing Swahili Language Models: Techniques, Embeddings, and Datasets",
+        dataset={
+            "path": "Mollel/SwahiliNewsClassfication",
+        },
+        type="Classification",
+        category="s2s",
+        eval_splits=["train"],
+        eval_langs=["swa-Latn"],
+        main_score="accuracy",
+        date=("2019-01-01", "2023-05-01"),
+        form=["written"],
+        dialect=[],
+        domains=["News"],
+        task_subtypes=[],
+        license="CC BY-NC-SA 4.0",
+        socioeconomic_status="mixed",
+        annotations_creators="derived",
+        text_creation="found",
+        bibtex_citation="""
+        """,
+        n_samples={"train": 2048},
+        avg_character_length={"train": 2107.1962725684334},
+    )
+
+    def dataset_transform(self) -> None:
+        self.dataset = self.dataset.rename_columns(
+            {"content": "text", "category": "label"}
+        )
+        self.dataset = self.stratified_subsampling(
+            self.dataset, seed=self.seed, splits=["train"]
+        )
