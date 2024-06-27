99bd4e869c9413174c1753880005a30e35ce48be
Michael Mollel <47386502+msamwelmollel@users.noreply.github.com>
2024-06-27 19:21:17 +0100

    update_two

--- mteb/tasks/Classification/swa/SwahiliNewsClassification.py
+++ mteb/tasks/Classification/swa/SwahiliNewsClassification.py
@@ -2,15 +2,17 @@ from __future__ import annotations
 
 from mteb.abstasks.TaskMetadata import TaskMetadata
 
-from ....abstasks import AbsTaskClassification
+# from ....abstasks import AbsTaskClassification
+from mteb.abstasks.AbsTaskClassification import AbsTaskClassification
 
 class SwahiliNewsClassification(AbsTaskClassification):
     metadata = TaskMetadata(
         name="SwahiliNewsClassification",
-        description="Dataset for Swahili News Classification, categorized with 5 domains.",
-        reference="Building and Optimizing Swahili Language Models: Techniques, Embeddings, and Datasets",
+        description="Dataset for Swahili News Classification, categorized with 5 domains. Building and Optimizing Swahili Language Models: Techniques, Embeddings, and Datasets",
+        reference="https://www.sartify.com/",
         dataset={
-            "path": "Mollel/SwahiliNewsClassfication",
+            "path": "Mollel/SwahiliNewsClassification",
+            "revision": "fadfd9f2989740dc6c9aa020cac97b318ec41cc3",
         },
         type="Classification",
         category="s2s",
