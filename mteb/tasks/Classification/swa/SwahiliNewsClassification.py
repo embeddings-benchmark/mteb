7e4ba0634feb41b27c9e17b3adbe0e1df914af4b
Michael Mollel <47386502+msamwelmollel@users.noreply.github.com>
2024-07-02 12:14:12 +0100

    fix: The Swahili Classification Task (#998)
    * swahili_news_classfication
    
    * update_two
    
    * Update Swahili addin source and refeence
    
    * Update SwahiliNewsClassification.py
    
    * Update SwahiliNewsClassification.py
    
    * addition info about group
    
    * remove unnecessary line

---
+++ mteb/tasks/Classification/swa/SwahiliNewsClassification.py
@@ -0,0 +1,50 @@
+from __future__ import annotations
+
+from mteb.abstasks.AbsTaskClassification import AbsTaskClassification
+from mteb.abstasks.TaskMetadata import TaskMetadata
+
+
+class SwahiliNewsClassification(AbsTaskClassification):
+    metadata = TaskMetadata(
+        name="SwahiliNewsClassification",
+        description="Dataset for Swahili News Classification, categorized with 6 domains (Local News (Kitaifa), International News (Kimataifa), Finance News (Uchumi), Health News (Afya), Sports News (Michezo), and Entertainment News (Burudani)). Building and Optimizing Swahili Language Models: Techniques, Embeddings, and Datasets",
+        reference="https://huggingface.co/datasets/Mollel/SwahiliNewsClassification",
+        dataset={
+            "path": "Mollel/SwahiliNewsClassification",
+            "revision": "cd89b0993ca6a71240f7fb16223c64f6fe4a27b7",
+        },
+        type="Classification",
+        category="s2s",
+        eval_splits=["train"],
+        eval_langs=["swa-Latn"],
+        main_score="accuracy",
+        date=("2019-01-01", "2023-05-01"),
+        form=["written"],
+        dialect=[],
+        domains=["News"],
+        task_subtypes=[],
+        license="CC BY-NC-SA 4.0",
+        socioeconomic_status="mixed",
+        annotations_creators="derived",
+        text_creation="found",
+        bibtex_citation="""
+        @inproceedings{davis2020swahili,
+        title = "Swahili: News Classification Dataset (0.2)",
+        author = "Davis, David",
+        year = "2020",
+        publisher = "Zenodo",
+        doi = "10.5281/zenodo.5514203",
+        url = "https://doi.org/10.5281/zenodo.5514203"
+        }
+        """,
+        n_samples={"train": 2048},
+        avg_character_length={"train": 2438.2308135942326},
+    )
+
+    def dataset_transform(self) -> None:
+        self.dataset = self.dataset.rename_columns(
+            {"content": "text", "category": "label"}
+        )
+        self.dataset = self.stratified_subsampling(
+            self.dataset, seed=self.seed, splits=["train"]
+        )
