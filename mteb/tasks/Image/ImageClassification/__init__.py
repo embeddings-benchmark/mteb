b828754ba462b3bd75cf4fceaeea18f639540163
Isaac Chung <chungisaac1217@gmail.com>
2024-07-19 13:18:36 +0000

    add FGVC aircraft dataset

--- mteb/tasks/Image/ImageClassification/__init__.py
+++ mteb/tasks/Image/ImageClassification/__init__.py
@@ -2,4 +2,5 @@ from __future__ import annotations
 
 from .eng.Caltech101Classification import *
 from .eng.CIFAR import *
+from .eng.FGVCAircraftClassification import *
 from .eng.OxfordFlowersClassification import *
