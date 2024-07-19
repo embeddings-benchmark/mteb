2320a2d09f9c05d0a5dd0c14cd7d15e9832e0578
Isaac Chung <chungisaac1217@gmail.com>
2024-07-19 13:19:08 +0000

    add food 101 dataset

--- mteb/tasks/Image/ImageClassification/__init__.py
+++ mteb/tasks/Image/ImageClassification/__init__.py
@@ -3,4 +3,5 @@ from __future__ import annotations
 from .eng.Caltech101Classification import *
 from .eng.CIFAR import *
 from .eng.FGVCAircraftClassification import *
+from .eng.Food101Classification import *
 from .eng.OxfordFlowersClassification import *
