1daa91fc7e505d584ae2c6ccab430c17972efaec
Isaac Chung <chungisaac1217@gmail.com>
2024-07-19 13:50:30 +0000

    remove comments

--- mteb/tasks/Image/ImageClassification/eng/OxfordPetsClassification.py
+++ mteb/tasks/Image/ImageClassification/eng/OxfordPetsClassification.py
@@ -4,8 +4,6 @@ from mteb.abstasks.TaskMetadata import TaskMetadata
 
 from .....abstasks import AbsTaskImageClassification
 
-## mteb run -m openai/clip-vit-base-patch32 --model_revision 3d74acf9a28c67741b2f4f2ea7635f0aaf6f0268 --output_folder results-mieb -t OxfordPets
-
 
 class OxfordPetsClassification(AbsTaskImageClassification):
     metadata = TaskMetadata(
