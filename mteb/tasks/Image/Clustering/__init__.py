a847238c5d0f6e40d08a1ee7f24b610f851462d0
Isaac Chung <chungisaac1217@gmail.com>
2024-07-20 19:19:36 +0000

    add CIFAR clustering

--- mteb/tasks/Image/Clustering/__init__.py
+++ mteb/tasks/Image/Clustering/__init__.py
@@ -1,3 +1,4 @@
 from __future__ import annotations
 
+from .eng.CIFAR import *
 from .eng.TinyImageNet import *
---
+++ mteb/tasks/Image/Clustering/eng/CIFAR.py
@@ -0,0 +1,88 @@
+from __future__ import annotations
+
+from mteb.abstasks.TaskMetadata import TaskMetadata
+
+from .....abstasks import AbsTaskImageClustering
+
+
+class CIFAR10Clustering(AbsTaskImageClustering):
+    metadata = TaskMetadata(
+        name="CIFAR10Clustering",
+        description="Clustering images from 10 classes.",
+        reference="https://huggingface.co/datasets/uoft-cs/cifar10",
+        dataset={
+            "path": "uoft-cs/cifar10",
+            "revision": "0b2714987fa478483af9968de7c934580d0bb9a2",
+        },
+        type="Clustering",
+        category="i2t",
+        eval_splits=["test"],
+        eval_langs=["eng-Latn"],
+        main_score="accuracy",
+        date=(
+            "2008-01-01",
+            "2009-01-01",
+        ),  # Estimated range for the collection of reviews
+        domains=["Web"],
+        task_subtypes=["Object recognition"],
+        license="Not specified",
+        socioeconomic_status="mixed",
+        annotations_creators="derived",
+        dialect=[],
+        modalities=["image"],
+        sample_creation="created",
+        bibtex_citation=""" @TECHREPORT{Krizhevsky09learningmultiple,
+            author = {Alex Krizhevsky},
+            title = {Learning multiple layers of features from tiny images},
+            institution = {},
+            year = {2009}
+        }
+        """,
+        descriptive_stats={
+            "n_samples": {"test": 10000},
+            "avg_character_length": {"test": 431.4},
+        },
+    )
+    image_column_name: str = "img"
+
+
+class CIFAR100Clustering(AbsTaskImageClustering):
+    metadata = TaskMetadata(
+        name="CIFAR100Clustering",
+        description="Clustering images from 100 classes.",
+        reference="https://huggingface.co/datasets/uoft-cs/cifar100",
+        dataset={
+            "path": "uoft-cs/cifar100",
+            "revision": "aadb3af77e9048adbea6b47c21a81e47dd092ae5",
+        },
+        type="Clustering",
+        category="i2t",
+        eval_splits=["test"],
+        eval_langs=["eng-Latn"],
+        main_score="accuracy",
+        date=(
+            "2008-01-01",
+            "2009-01-01",
+        ),  # Estimated range for the collection of reviews
+        domains=["Web"],
+        task_subtypes=["Object recognition"],
+        license="Not specified",
+        socioeconomic_status="mixed",
+        annotations_creators="derived",
+        dialect=[],
+        modalities=["image"],
+        sample_creation="created",
+        bibtex_citation=""" @TECHREPORT{Krizhevsky09learningmultiple,
+            author = {Alex Krizhevsky},
+            title = {Learning multiple layers of features from tiny images},
+            institution = {},
+            year = {2009}
+        }
+        """,
+        descriptive_stats={
+            "n_samples": {"test": 10000},
+            "avg_character_length": {"test": 431.4},
+        },
+    )
+    image_column_name: str = "img"
+    label_column_name: str = "fine_label"
