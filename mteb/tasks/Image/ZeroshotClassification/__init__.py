51a66bafe0cb46545673394a08ceed59cdce340c
Isaac Chung <chungisaac1217@gmail.com>
2024-07-20 12:06:53 +0000

    add aircraft and food zero shot

--- mteb/tasks/Image/ZeroshotClassification/__init__.py
+++ mteb/tasks/Image/ZeroshotClassification/__init__.py
@@ -2,4 +2,6 @@ from __future__ import annotations
 
 from .eng.Caltech101 import *
 from .eng.CIFAR import *
+from .eng.FGVCAircraft import *
+from .eng.Food101 import *
 from .eng.RenderedSST2 import *
