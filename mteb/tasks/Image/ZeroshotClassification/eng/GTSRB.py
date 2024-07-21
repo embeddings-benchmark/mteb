b454dac25e685faddf952e7271000b1229cd5c6d
Imene Kerboua <imenelydia.kr@gmail.com>
2024-07-21 18:43:04 +0200

    Add country211 zero shot classification

--- mteb/tasks/Image/ZeroshotClassification/eng/GTSRB.py
+++ mteb/tasks/Image/ZeroshotClassification/eng/GTSRB.py
@@ -6,7 +6,7 @@ from mteb.abstasks import AbsTaskZeroshotClassification
 from mteb.abstasks.TaskMetadata import TaskMetadata
 
 
-class GTSRBZeroShotClassification(AbsTaskZeroshotClassification):
+class GTSRBClassification(AbsTaskZeroshotClassification):
     metadata = TaskMetadata(
         name="GTSRBZeroShot",
         description="""The German Traffic Sign Recognition Benchmark (GTSRB) is a multi-class classification dataset for traffic signs. It consists of dataset of more than 50,000 traffic sign images. The dataset comprises 43 classes with unbalanced class frequencies.""",
