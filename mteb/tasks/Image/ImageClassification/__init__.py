935f621a5f5c553eb98cfbcf55d022f58c4dcec0
Isaac Chung <chungisaac1217@gmail.com>
2024-07-22 04:42:58 +0300

    [MIEB] Add more image classification and zero shot datasets (#1105)
    * add STL10 linear probe and zero shot
    
    * add RESISC45 linear probe and zeor shot
    
    * add Describable textures linear probe and zero shot
    
    * fix spacing lint
    
    * add SUN397 linear probe and zero shot

--- mteb/tasks/Image/ImageClassification/__init__.py
+++ mteb/tasks/Image/ImageClassification/__init__.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from .eng.BirdsnapClassification import *
 from .eng.Caltech101Classification import *
 from .eng.CIFAR import *
+from .eng.DTDClassification import *
 from .eng.EuroSATClassification import *
 from .eng.FER2013Classification import *
 from .eng.FGVCAircraftClassification import *
@@ -10,4 +11,7 @@ from .eng.Food101Classification import *
 from .eng.MNISTClassification import *
 from .eng.OxfordFlowersClassification import *
 from .eng.OxfordPetsClassification import *
+from .eng.RESISC45Classification import *
 from .eng.StanfordCarsClassification import *
+from .eng.STL10Classification import *
+from .eng.SUN397Classification import *
--- mteb/tasks/Image/ZeroshotClassification/__init__.py
+++ mteb/tasks/Image/ZeroshotClassification/__init__.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from .eng.Birdsnap import *
 from .eng.Caltech101 import *
 from .eng.CIFAR import *
+from .eng.DTD import *
 from .eng.EuroSAT import *
 from .eng.FER2013 import *
 from .eng.FGVCAircraft import *
@@ -10,4 +11,7 @@ from .eng.Food101 import *
 from .eng.MNIST import *
 from .eng.OxfordPets import *
 from .eng.RenderedSST2 import *
+from .eng.RESISC45 import *
 from .eng.StanfordCars import *
+from .eng.STL10 import *
+from .eng.SUN397 import *
