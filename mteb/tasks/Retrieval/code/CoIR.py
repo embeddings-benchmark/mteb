54f11d9dc1da776124d9a8a14ad612ab8a694720
l00856372 <lee.yi.quan@huawei.com>
2024-07-26 14:47:26 +0800

    update monolingual tasks for coir

--- mteb/tasks/Retrieval/code/CoIR.py
+++ mteb/tasks/Retrieval/code/CoIR.py
@@ -16,7 +16,7 @@ class CoIRTasks:
         queries_corpus_dataset = load_dataset(f"CoIR-Retrieval/{task_name}-queries-corpus")
         qrels_dataset = load_dataset(f"CoIR-Retrieval/{task_name}-qrels")
 
-        corpus_data = [q for q in queries_corpus_dataset['corpus'] if q['partition'] == _EVAL_SPLIT]
+        corpus_data = [q for q in queries_corpus_dataset['corpus']]
         query_data = [q for q in queries_corpus_dataset['queries'] if q['partition'] == _EVAL_SPLIT]
         
         # corpus handling 
@@ -96,6 +96,314 @@ class CodeContestRetrieval(CoIRTasks, AbsTaskRetrieval):
         """,
         descriptive_stats={
             "n_samples":{_EVAL_SPLIT: 3765}   
+        }
+    )
+    
+    def load_data(self, **kwargs):
+        self._load_data()
+        
+class SyntheticText2SQLRetrieval(CoIRTasks, AbsTaskRetrieval):
+    metadata = TaskMetadata(
+        name="SyntheticText2SQL",
+        dataset={
+            "path": "CoIR-Retrieval/synthetic-text2sql",
+            "revision": "",
+            "name": "synthetic-text2sql",
+        },
+        reference="https://huggingface.co/CoIR-Retrieval",
+        description=("Retrieving relevant SQL code in response to natural language questions"),
+        type="Retrieval",
+        modalities=['text'],
+        sample_creation="found",
+        category="p2p",
+        eval_splits=[_EVAL_SPLIT],
+        eval_langs=["python-Code"],
+        main_score="ndcg_at_10",
+        date=("2020-01-01", "2021-11-08"),  
+        form=["written"],
+        domains=["Programming"],
+        task_subtypes=["Code retrieval"],
+        license="Not specified",
+        annotations_creators="derived",
+        dialect=[],
+        text_creation="found",
+        bibtex_citation="""
+            @misc{li2024coircomprehensivebenchmarkcode,
+            title={CoIR: A Comprehensive Benchmark for Code Information Retrieval Models}, 
+            author={Xiangyang Li and Kuicai Dong and Yi Quan Lee and Wei Xia and Yichun Yin and Hao Zhang and Yong Liu and Yasheng Wang and Ruiming Tang},
+            year={2024},
+            eprint={2407.02883},
+            archivePrefix={arXiv},
+            primaryClass={cs.IR},
+            url={https://arxiv.org/abs/2407.02883}, 
+        }
+        """,
+        descriptive_stats={
+            "n_samples":{_EVAL_SPLIT: 5851}   
+        }
+    )
+    
+    def load_data(self, **kwargs):
+        self._load_data()
+        
+class CodeTranslationContestRetrieval(CoIRTasks, AbsTaskRetrieval):
+    metadata = TaskMetadata(
+        name="CodeTranslationContestRetrieval",
+        dataset={
+            "path": "CoIR-Retrieval/codetrans-contest",
+            "revision": "",
+            "name": "codetrans-contest",
+        },
+        reference="https://huggingface.co/CoIR-Retrieval",
+        description=("Retrieving semantically equivalent C++ code from its counterpart written in Python."),
+        type="Retrieval",
+        modalities=['text'],
+        sample_creation="found",
+        category="p2p",
+        eval_splits=[_EVAL_SPLIT],
+        eval_langs=["python-Code"],
+        main_score="ndcg_at_10",
+        date=("2020-01-01", "2021-11-08"),  
+        form=["written"],
+        domains=["Programming"],
+        task_subtypes=["Code retrieval"],
+        license="Not specified",
+        annotations_creators="derived",
+        dialect=[],
+        text_creation="found",
+        bibtex_citation="""
+            @misc{li2024coircomprehensivebenchmarkcode,
+            title={CoIR: A Comprehensive Benchmark for Code Information Retrieval Models}, 
+            author={Xiangyang Li and Kuicai Dong and Yi Quan Lee and Wei Xia and Yichun Yin and Hao Zhang and Yong Liu and Yasheng Wang and Ruiming Tang},
+            year={2024},
+            eprint={2407.02883},
+            archivePrefix={arXiv},
+            primaryClass={cs.IR},
+            url={https://arxiv.org/abs/2407.02883}, 
+        }
+        """,
+        descriptive_stats={
+            "n_samples":{_EVAL_SPLIT: 221}   
+        }
+    )
+    
+    def load_data(self, **kwargs):
+        self._load_data()
+
+class CodeTranslationDLRetrieval(CoIRTasks, AbsTaskRetrieval):
+    metadata = TaskMetadata(
+        name="CodeTranslationDLRetrieval",
+        dataset={
+            "path": "CoIR-Retrieval/codetrans-dl",
+            "revision": "",
+            "name": "codetrans-dl",
+        },
+        reference="https://huggingface.co/CoIR-Retrieval",
+        description=("Retrieving semantically equivalent code written using 2 different machine learning frameworks"),
+        type="Retrieval",
+        modalities=['text'],
+        sample_creation="found",
+        category="p2p",
+        eval_splits=[_EVAL_SPLIT],
+        eval_langs=["python-Code"],
+        main_score="ndcg_at_10",
+        date=("2020-01-01", "2021-11-08"),  
+        form=["written"],
+        domains=["Programming"],
+        task_subtypes=["Code retrieval"],
+        license="Not specified",
+        annotations_creators="derived",
+        dialect=[],
+        text_creation="found",
+        bibtex_citation="""
+            @misc{li2024coircomprehensivebenchmarkcode,
+            title={CoIR: A Comprehensive Benchmark for Code Information Retrieval Models}, 
+            author={Xiangyang Li and Kuicai Dong and Yi Quan Lee and Wei Xia and Yichun Yin and Hao Zhang and Yong Liu and Yasheng Wang and Ruiming Tang},
+            year={2024},
+            eprint={2407.02883},
+            archivePrefix={arXiv},
+            primaryClass={cs.IR},
+            url={https://arxiv.org/abs/2407.02883}, 
+        }
+        """,
+        descriptive_stats={
+            "n_samples":{_EVAL_SPLIT: 180}   
+        }
+    )
+    
+    def load_data(self, **kwargs):
+        self._load_data()
+        
+class StackoverflowQARetrieval(CoIRTasks, AbsTaskRetrieval):
+    metadata = TaskMetadata(
+        name="StackoverflowQARetrieval",
+        dataset={
+            "path": "CoIR-Retrieval/stackoverflow-qa",
+            "revision": "",
+            "name": "stackoverflow-qa",
+        },
+        reference="https://huggingface.co/CoIR-Retrieval",
+        description=("Retrieving relevant answer in text with some code snippets given a natural language question"),
+        type="Retrieval",
+        modalities=['text'],
+        sample_creation="found",
+        category="p2p",
+        eval_splits=[_EVAL_SPLIT],
+        eval_langs=["python-Code"],
+        main_score="ndcg_at_10",
+        date=("2018-01-01", "2024-05-01"),  
+        form=["written"],
+        domains=["Programming"],
+        task_subtypes=["Code retrieval"],
+        license="Not specified",
+        annotations_creators="derived",
+        dialect=[],
+        text_creation="found",
+        bibtex_citation="""
+            @misc{li2024coircomprehensivebenchmarkcode,
+            title={CoIR: A Comprehensive Benchmark for Code Information Retrieval Models}, 
+            author={Xiangyang Li and Kuicai Dong and Yi Quan Lee and Wei Xia and Yichun Yin and Hao Zhang and Yong Liu and Yasheng Wang and Ruiming Tang},
+            year={2024},
+            eprint={2407.02883},
+            archivePrefix={arXiv},
+            primaryClass={cs.IR},
+            url={https://arxiv.org/abs/2407.02883}, 
+        }
+        """,
+        descriptive_stats={
+            "n_samples":{_EVAL_SPLIT: 221}   
+        }
+    )
+    
+    def load_data(self, **kwargs):
+        self._load_data()
+        
+class CodeFeedbackMTRetrieval(CoIRTasks, AbsTaskRetrieval):
+    metadata = TaskMetadata(
+        name="CodeFeedbackMTRetrieval",
+        dataset={
+            "path": "CoIR-Retrieval/codefeedback-mt",
+            "revision": "",
+            "name": "codefeedback-mt",
+        },
+        reference="https://huggingface.co/CoIR-Retrieval",
+        description=("Retrieving relevant answer in text with some code snippets given a natural language question and preceding dialogue history."),
+        type="Retrieval",
+        modalities=['text'],
+        sample_creation="found",
+        category="p2p",
+        eval_splits=[_EVAL_SPLIT],
+        eval_langs=["python-Code"],
+        main_score="ndcg_at_10",
+        date=("2020-01-01", "2021-11-08"),  
+        form=["written"],
+        domains=["Programming"],
+        task_subtypes=["Code retrieval"],
+        license="Not specified",
+        annotations_creators="derived",
+        dialect=[],
+        text_creation="found",
+        bibtex_citation="""
+            @misc{li2024coircomprehensivebenchmarkcode,
+            title={CoIR: A Comprehensive Benchmark for Code Information Retrieval Models}, 
+            author={Xiangyang Li and Kuicai Dong and Yi Quan Lee and Wei Xia and Yichun Yin and Hao Zhang and Yong Liu and Yasheng Wang and Ruiming Tang},
+            year={2024},
+            eprint={2407.02883},
+            archivePrefix={arXiv},
+            primaryClass={cs.IR},
+            url={https://arxiv.org/abs/2407.02883}, 
+        }
+        """,
+        descriptive_stats={
+            "n_samples":{_EVAL_SPLIT: 13277}   
+        }
+    )
+    
+    def load_data(self, **kwargs):
+        self._load_data()
+        
+class CodeFeedbackSTRetrieval(CoIRTasks, AbsTaskRetrieval):
+    metadata = TaskMetadata(
+        name="CodeFeedbackSTRetrieval",
+        dataset={
+            "path": "CoIR-Retrieval/codefeedback-st",
+            "revision": "",
+            "name": "codefeedback-mt",
+        },
+        reference="https://huggingface.co/CoIR-Retrieval",
+        description=("Retrieving relevant answer in text with some code snippets given a natural language question."),
+        type="Retrieval",
+        modalities=['text'],
+        sample_creation="found",
+        category="p2p",
+        eval_splits=[_EVAL_SPLIT],
+        eval_langs=["python-Code"],
+        main_score="ndcg_at_10",
+        date=("2020-01-01", "2021-11-08"),  
+        form=["written"],
+        domains=["Programming"],
+        task_subtypes=["Code retrieval"],
+        license="Not specified",
+        annotations_creators="derived",
+        dialect=[],
+        text_creation="found",
+        bibtex_citation="""
+            @misc{li2024coircomprehensivebenchmarkcode,
+            title={CoIR: A Comprehensive Benchmark for Code Information Retrieval Models}, 
+            author={Xiangyang Li and Kuicai Dong and Yi Quan Lee and Wei Xia and Yichun Yin and Hao Zhang and Yong Liu and Yasheng Wang and Ruiming Tang},
+            year={2024},
+            eprint={2407.02883},
+            archivePrefix={arXiv},
+            primaryClass={cs.IR},
+            url={https://arxiv.org/abs/2407.02883}, 
+        }
+        """,
+        descriptive_stats={
+            "n_samples":{_EVAL_SPLIT: 31306}   
+        }
+    )
+    
+    def load_data(self, **kwargs):
+        self._load_data()
+        
+class WebQueryCodeRetrieval(CoIRTasks, AbsTaskRetrieval):
+    metadata = TaskMetadata(
+        name="WebQueryCodeRetrieval",
+        dataset={
+            "path": "CoIR-Retrieval/cosqa",
+            "revision": "",
+            "name": "cosqa",
+        },
+        reference="https://huggingface.co/CoIR-Retrieval",
+        description=("Retrieving relevant code solutions in response to natural language problems."),
+        type="Retrieval",
+        modalities=['text'],
+        sample_creation="found",
+        category="p2p",
+        eval_splits=[_EVAL_SPLIT],
+        eval_langs=["python-Code"],
+        main_score="ndcg_at_10",
+        date=("2020-01-01", "2021-11-08"),  
+        form=["written"],
+        domains=["Programming"],
+        task_subtypes=["Code retrieval"],
+        license="Not specified",
+        annotations_creators="derived",
+        dialect=[],
+        text_creation="found",
+        bibtex_citation="""
+            @misc{li2024coircomprehensivebenchmarkcode,
+            title={CoIR: A Comprehensive Benchmark for Code Information Retrieval Models}, 
+            author={Xiangyang Li and Kuicai Dong and Yi Quan Lee and Wei Xia and Yichun Yin and Hao Zhang and Yong Liu and Yasheng Wang and Ruiming Tang},
+            year={2024},
+            eprint={2407.02883},
+            archivePrefix={arXiv},
+            primaryClass={cs.IR},
+            url={https://arxiv.org/abs/2407.02883}, 
+        }
+        """,
+        descriptive_stats={
+            "n_samples":{_EVAL_SPLIT: 31306}   
         }
     )
     
