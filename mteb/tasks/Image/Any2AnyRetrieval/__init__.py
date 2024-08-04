da470bd2305227042ca712cdd3775457de297274
gowitheflow-1998 <jsbs54@durham.ac.uk>
2024-08-04 23:20:21 +0100

    add fashion200k & fashionIQ test passed

--- mteb/tasks/Image/Any2AnyRetrieval/__init__.py
+++ mteb/tasks/Image/Any2AnyRetrieval/__init__.py
@@ -1,3 +1,4 @@
 from __future__ import annotations
 
 from .eng.CIRRIT2IRetrieval import *
+from .eng.FashionIQIT2IRetrieval import *
--- mteb/tasks/Image/I2TRetrieval/__init__.py
+++ mteb/tasks/Image/I2TRetrieval/__init__.py
@@ -1,3 +1,4 @@
 from __future__ import annotations
 
+from .eng.Fashion200kI2TRetrieval import *
 from .eng.MSCOCOI2TRetrieval import *
