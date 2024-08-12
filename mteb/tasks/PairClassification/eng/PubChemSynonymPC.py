678dbc9dc57a47dc99329896f41e626c2af5d914
Ali Shiraee <a.shiraee@gmail.com>
2024-08-12 08:43:12 -0400

    Add PubChem Synonyms PairClassification task

---
+++ mteb/tasks/PairClassification/eng/PubChemSynonymPC.py
@@ -0,0 +1,71 @@
+from __future__ import annotations
+
+
+from typing import Any
+
+import datasets
+
+from mteb.abstasks.AbsTaskPairClassification import AbsTaskPairClassification
+from mteb.abstasks.TaskMetadata import TaskMetadata
+
+
+_DATASET_COLUMN_MAP = {
+    "sentence1": "title",
+    "sentence2": "synonyms",
+    "labels": "labels",
+}
+
+
+class PubChemSynonymPC(AbsTaskPairClassification):
+    metadata = TaskMetadata(
+        name="PubChemSynonymPC",
+        description="""TBW""",
+        reference="https://pubchem.ncbi.nlm.nih.gov/",
+        dataset={
+            "path": "BASF-We-Create-Chemistry/PubChemSynonymPC",
+            "revision": "dcd82d56e7a8388db23aa962c99d59fe9a2ba7e3"
+        },
+        type="PairClassification",
+        category="s2s",
+        modalities=["text"],
+        eval_splits=["test"],
+        eval_langs=["eng-Latn"],
+        main_score="max_f1",
+        date=None,
+        domains=None,
+        task_subtypes=None,
+        license=None,
+        annotations_creators=None,
+        dialect=None,
+        sample_creation="created",
+        bibtex_citation=None,
+        descriptive_stats={}
+    )
+
+    def load_data(self, **kwargs: Any) -> None:
+        """Load dataset from HuggingFace hub"""
+        if self.data_loaded:
+            return
+
+        _dataset = datasets.load_dataset(
+            self.metadata_dict["dataset"]["path"],
+            revision=self.metadata_dict["dataset"]["revision"],
+            trust_remote_code=True,
+        )
+        self.dataset = _dataset
+        self.dataset_transform()
+        self.data_loaded = True
+
+    def dataset_transform(self):
+        _dataset = {}
+
+        for split in self.metadata.eval_splits:
+            hf_dataset = self.dataset[split]
+            _dataset[split] = [
+                {
+                    "sentence1": hf_dataset[_DATASET_COLUMN_MAP["sentence1"]],
+                    "sentence2": hf_dataset[_DATASET_COLUMN_MAP["sentence2"]],
+                    "labels": hf_dataset[_DATASET_COLUMN_MAP["labels"]],
+                }
+            ]
+        self.dataset = _dataset
