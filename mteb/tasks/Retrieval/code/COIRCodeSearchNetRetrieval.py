22f2a72c2b07762f082f2821466847c096419172
LYQ <leeyiquan95@gmail.com>
2024-08-16 00:29:10 +0800

    fix metclass error

--- mteb/tasks/Retrieval/code/COIRCodeSearchNetRetrieval.py
+++ mteb/tasks/Retrieval/code/COIRCodeSearchNetRetrieval.py
@@ -2,18 +2,74 @@ from __future__ import annotations
 
 import logging
 
+import datasets
+
 from mteb.abstasks import MultilingualTask
 from mteb.abstasks.AbsTaskRetrieval import AbsTaskRetrieval
 from mteb.abstasks.TaskMetadata import TaskMetadata
-from mteb.tasks.Retrieval.code.CodeSearchNetCCRetrieval import (
-    _load_code_search_code_retrieval,
-)
 
 _LANGS = ["python", "javascript", "go", "ruby", "java", "php"]
 _EVAL_SPLIT = "test"
 
 logger = logging.getLogger(__name__)
 
+def _load_code_search_code_retrieval(
+    path: str, langs: list, splits: str, cache_dir: str = None, revision: str = None
+):
+    corpus = {lang: {split: {} for split in splits} for lang in langs}
+    queries = {lang: {split: {} for split in splits} for lang in langs}
+    relevant_docs = {lang: {split: {} for split in splits} for lang in langs}
+
+    split = _EVAL_SPLIT
+
+    for lang in langs:
+        qrels_data = datasets.load_dataset(
+            path,
+            name=f"{lang}-qrels",
+            cache_dir=cache_dir,
+            revision=revision,
+            trust_remote_code=True,
+        )[split]
+
+        for row in qrels_data:
+            query_id = row["query-id"]
+            doc_id = row["corpus-id"]
+            score = row["score"]
+            if query_id not in relevant_docs[lang][split]:
+                relevant_docs[lang][split][query_id] = {}
+            relevant_docs[lang][split][query_id][doc_id] = score
+
+        corpus_data = datasets.load_dataset(
+            path,
+            name=f"{lang}-corpus",
+            cache_dir=cache_dir,
+            revision=revision,
+            trust_remote_code=True,
+        )["corpus"]
+
+        for row in corpus_data:
+            doc_id = row["_id"]
+            doc_title = row["title"]
+            doc_text = row["text"]
+            corpus[lang][split][doc_id] = {"title": doc_title, "text": doc_text}
+
+        queries_data = datasets.load_dataset(
+            path,
+            name=f"{lang}-queries",
+            cache_dir=cache_dir,
+            revision=revision,
+            trust_remote_code=True,
+        )["queries"].filter(lambda x: x["partition"] == "test")
+
+        for row in queries_data:
+            query_id = row["_id"]
+            query_text = row["text"]
+            queries[lang][split][query_id] = query_text
+
+        queries = queries
+        logger.info("Loaded %d %s Queries.", len(queries), split.upper())
+
+    return corpus, queries, relevant_docs
 
 class COIRCodeSearchNetRetrieval(MultilingualTask, AbsTaskRetrieval):
     _EVAL_SPLIT = "test"
