f0fb32675525a1d4bb97fa577db64038253c305e
Márton Kardos <power.up1163@gmail.com>
2024-09-24 12:54:42 +0200

    Added get_benchmarks to __init__

--- mteb/__init__.py
+++ mteb/__init__.py
@@ -5,14 +5,13 @@ from importlib.metadata import version
 from mteb.benchmarks.benchmarks import (MTEB_MAIN_EN, MTEB_MAIN_RU,
                                         MTEB_RETRIEVAL_LAW,
                                         MTEB_RETRIEVAL_WITH_INSTRUCTIONS, CoIR)
-from mteb.benchmars import get_benchmark, get_benchmarks
 from mteb.evaluation import *
 from mteb.load_results import load_results
 from mteb.models import get_model, get_model_meta, get_model_metas
 from mteb.overview import TASKS_REGISTRY, get_task, get_tasks
 
 from .benchmarks.benchmarks import Benchmark
-from .benchmarks.get_benchmark import get_benchmark
+from .benchmarks.get_benchmark import get_benchmark, get_benchmarks
 
 __version__ = version("mteb")  # fetch version from install metadata
 
@@ -28,7 +27,9 @@ __all__ = [
     "get_task",
     "get_model",
     "get_model_meta",
+    "get_model_metas",
     "load_results",
     "Benchmark",
     "get_benchmark",
+    "get_benchmarks",
 ]
