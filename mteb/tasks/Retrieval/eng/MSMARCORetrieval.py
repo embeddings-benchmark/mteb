b754f1a578cc6f6868f0666e7a0e2ac9158fe13c
Orion Weller <31665361+orionw@users.noreply.github.com>
2024-09-28 11:33:09 -0700

    fix: Downsample large retrieval datasets (#1236)
    * most tasks
    
    * lint
    
    * fix other issues
    
    * refactor
    
    * lint and docs
    
    * add polish
    
    * keep case sensitive mteb paths
    
    * add potential points
    
    * fix points
    
    * fix test about metadata
    
    * update tasks and stats
    
    * lint

--- mteb/tasks/Retrieval/eng/MSMARCORetrieval.py
+++ mteb/tasks/Retrieval/eng/MSMARCORetrieval.py
@@ -76,3 +76,62 @@ class MSMARCO(AbsTaskRetrieval):
             },
         },
     )
+
+
+class MSMARCOHardNegatives(AbsTaskRetrieval):
+    ignore_identical_ids = True
+
+    metadata = TaskMetadata(
+        name="MSMARCOHardNegatives",
+        dataset={
+            "path": "mteb/MSMARCO_test_top_250_only_w_correct-v2",
+            "revision": "67c0b4f7f15946e0b15cf6cf3b8993d04cb3efc6",
+        },
+        description="MS MARCO is a collection of datasets focused on deep learning in search. The hard negative version has been created by pooling the 250 top documents per query from BM25, e5-multilingual-large and e5-mistral-instruct.",
+        reference="https://microsoft.github.io/msmarco/",
+        type="Retrieval",
+        category="s2p",
+        modalities=["text"],
+        eval_splits=["test"],
+        eval_langs=["eng-Latn"],
+        main_score="ndcg_at_10",
+        date=None,
+        domains=None,
+        task_subtypes=None,
+        license=None,
+        annotations_creators=None,
+        dialect=None,
+        sample_creation=None,
+        bibtex_citation="""@article{DBLP:journals/corr/NguyenRSGTMD16,
+  author    = {Tri Nguyen and
+               Mir Rosenberg and
+               Xia Song and
+               Jianfeng Gao and
+               Saurabh Tiwary and
+               Rangan Majumder and
+               Li Deng},
+  title     = {{MS} {MARCO:} {A} Human Generated MAchine Reading COmprehension Dataset},
+  journal   = {CoRR},
+  volume    = {abs/1611.09268},
+  year      = {2016},
+  url       = {http://arxiv.org/abs/1611.09268},
+  archivePrefix = {arXiv},
+  eprint    = {1611.09268},
+  timestamp = {Mon, 13 Aug 2018 16:49:03 +0200},
+  biburl    = {https://dblp.org/rec/journals/corr/NguyenRSGTMD16.bib},
+  bibsource = {dblp computer science bibliography, https://dblp.org}
+}
+}""",
+        descriptive_stats={
+            "n_samples": {"test": 43},
+            "avg_character_length": {
+                "test": {
+                    "average_document_length": 355.2909668633681,
+                    "average_query_length": 32.74418604651163,
+                    "num_documents": 8812,
+                    "num_queries": 43,
+                    "average_relevant_docs_per_query": 95.3953488372093,
+                }
+            },
+        },
+    )
--- mteb/tasks/Retrieval/multilingual/NeuCLIR2023Retrieval.py
+++ mteb/tasks/Retrieval/multilingual/NeuCLIR2023Retrieval.py
@@ -129,3 +129,155 @@ class NeuCLIR2023Retrieval(MultilingualTask, AbsTaskRetrieval):
             revision=self.metadata_dict["dataset"]["revision"],
         )
         self.data_loaded = True
+
+
+def load_neuclir_data_hard_negatives(
+    path: str,
+    langs: list,
+    eval_splits: list,
+    cache_dir: str | None = None,
+    revision: str | None = None,
+):
+    split = "test"
+    corpus = {lang: {split: None for split in eval_splits} for lang in langs}
+    queries = {lang: {split: None for split in eval_splits} for lang in langs}
+    relevant_docs = {lang: {split: None for split in eval_splits} for lang in langs}
+
+    for lang in langs:
+        corpus_identifier = f"corpus-{lang}"
+        corpus_data = datasets.load_dataset(
+            path,
+            corpus_identifier,
+            cache_dir=cache_dir,
+            revision=revision,
+            trust_remote_code=True,
+        )
+        corpus[lang][split] = {}
+        for row in corpus_data["corpus"]:
+            docid = row["_id"]
+            doc_title = row["title"]
+            doc_text = row["text"]
+            corpus[lang][split][docid] = {"title": doc_title, "text": doc_text}
+
+        # Load queries data
+        queries_identifier = f"queries-{lang}"
+        queries_data = datasets.load_dataset(
+            path,
+            queries_identifier,
+            cache_dir=cache_dir,
+            revision=revision,
+            trust_remote_code=True,
+        )
+        queries[lang][split] = {}
+        for row in queries_data["queries"]:
+            query_id = row["_id"]
+            query_text = row["text"]
+            queries[lang][split][query_id] = query_text
+
+        # Load relevant documents data
+        qrels_identifier = f"{lang}"
+        qrels_data = datasets.load_dataset(
+            path,
+            qrels_identifier,
+            cache_dir=cache_dir,
+            revision=revision,
+            trust_remote_code=True,
+        )
+        relevant_docs[lang][split] = {}
+        for row in qrels_data[split]:
+            query_id = row["query-id"]
+            doc_id = row["corpus-id"]
+            score = row["score"]
+            if query_id not in relevant_docs[lang][split]:
+                relevant_docs[lang][split][query_id] = {}
+            relevant_docs[lang][split][query_id][doc_id] = score
+
+    corpus = datasets.DatasetDict(corpus)
+    queries = datasets.DatasetDict(queries)
+    relevant_docs = datasets.DatasetDict(relevant_docs)
+
+    return corpus, queries, relevant_docs
+
+
+class NeuCLIR2023RetrievalHardNegatives(MultilingualTask, AbsTaskRetrieval):
+    metadata = TaskMetadata(
+        name="NeuCLIR2023RetrievalHardNegatives",
+        description="The task involves identifying and retrieving the documents that are relevant to the queries. The hard negative version has been created by pooling the 250 top documents per query from BM25, e5-multilingual-large and e5-mistral-instruct.",
+        reference="https://neuclir.github.io/",
+        dataset={
+            "path": "mteb/neuclir-2023-hard-negatives",
+            "revision": "5d47e924e632c333d3f087d945642af93b008d2b",
+            "trust_remote_code": True,
+        },
+        type="Retrieval",
+        category="s2p",
+        modalities=["text"],
+        eval_splits=["test"],
+        eval_langs=_LANGUAGES,
+        main_score="ndcg_at_20",
+        date=("2022-08-01", "2023-06-30"),
+        domains=["News", "Written"],
+        task_subtypes=[],
+        license="odc-by",
+        annotations_creators="expert-annotated",
+        dialect=[],
+        sample_creation="found",
+        bibtex_citation="""@misc{lawrie2024overview,
+      title={Overview of the TREC 2023 NeuCLIR Track}, 
+      author={Dawn Lawrie and Sean MacAvaney and James Mayfield and Paul McNamee and Douglas W. Oard and Luca Soldaini and Eugene Yang},
+      year={2024},
+      eprint={2404.08071},
+      archivePrefix={arXiv},
+      primaryClass={cs.IR}
+}""",
+        descriptive_stats={
+            "n_samples": None,
+            "avg_character_length": {
+                "test": {
+                    "average_document_length": 2236.175955333482,
+                    "average_query_length": 54.10267857142857,
+                    "num_documents": 49433,
+                    "num_queries": 224,
+                    "average_relevant_docs_per_query": 61.816964285714285,
+                    "hf_subset_descriptive_stats": {
+                        "fas": {
+                            "average_document_length": 2895.869857421016,
+                            "average_query_length": 65.89189189189189,
+                            "num_documents": 15921,
+                            "num_queries": 74,
+                            "average_relevant_docs_per_query": 68.08108108108108,
+                        },
+                        "rus": {
+                            "average_document_length": 2724.294762109928,
+                            "average_query_length": 74.41333333333333,
+                            "num_documents": 16247,
+                            "num_queries": 75,
+                            "average_relevant_docs_per_query": 63.053333333333335,
+                        },
+                        "zho": {
+                            "average_document_length": 1168.4984071821605,
+                            "average_query_length": 22.16,
+                            "num_documents": 17265,
+                            "num_queries": 75,
+                            "average_relevant_docs_per_query": 54.4,
+                        },
+                    },
+                }
+            },
+        },
+    )
+
+    def load_data(self, **kwargs):
+        if self.data_loaded:
+            return
+
+        self.corpus, self.queries, self.relevant_docs = (
+            load_neuclir_data_hard_negatives(
+                path=self.metadata_dict["dataset"]["path"],
+                langs=self.metadata.eval_langs,
+                eval_splits=self.metadata_dict["eval_splits"],
+                cache_dir=kwargs.get("cache_dir", None),
+                revision=self.metadata_dict["dataset"]["revision"],
+            )
+        )
+        self.data_loaded = True
