456df8214a8168a644e7278e6de1a5f8d7183355
Roman Solomatin <36135455+Samoed@users.noreply.github.com>
2024-09-29 18:17:10 +0300

    add Touche2020v3

--- mteb/tasks/Retrieval/eng/Touche2020Retrieval.py
+++ mteb/tasks/Retrieval/eng/Touche2020Retrieval.py
@@ -1,8 +1,7 @@
 from __future__ import annotations
 
+from mteb.abstasks.AbsTaskRetrieval import AbsTaskRetrieval
 from mteb.abstasks.TaskMetadata import TaskMetadata
-
-from ....abstasks.AbsTaskRetrieval import AbsTaskRetrieval
 
 
 class Touche2020(AbsTaskRetrieval):
@@ -55,5 +54,46 @@ class Touche2020(AbsTaskRetrieval):
                     "average_relevant_docs_per_query": 19.020408163265305,
                 }
             },
+        },
+    )
+
+
+class Touche2020v3(AbsTaskRetrieval):
+    metadata = TaskMetadata(
+        name="Touche2020v3",
+        description="Touché Task 1: Argument Retrieval for Controversial Questions",
+        reference="https://github.com/castorini/touche-error-analysis",
+        dataset={
+            "path": "mteb/webis-touche2020-v3",
+            "revision": "431886eaecc48f067a3975b70d0949ea2862463c",
+        },
+        type="Retrieval",
+        category="s2p",
+        modalities=["text"],
+        eval_splits=["test"],
+        eval_langs=["eng-Latn"],
+        main_score="ndcg_at_10",
+        date=None,
+        domains=None,
+        task_subtypes=None,
+        license="cc-by-sa-4.0",
+        annotations_creators=None,
+        dialect=None,
+        sample_creation=None,
+        bibtex_citation="""@INPROCEEDINGS{Thakur_etal_SIGIR2024,
+   author = "Nandan Thakur and Luiz Bonifacio and Maik {Fr\"{o}be} and Alexander Bondarenko and Ehsan Kamalloo and Martin Potthast and Matthias Hagen and Jimmy Lin",
+   title = "Systematic Evaluation of Neural Retrieval Models on the {Touch\'{e}} 2020 Argument Retrieval Subset of {BEIR}",
+   booktitle = "Proceedings of the 47th International ACM SIGIR Conference on Research and Development in Information Retrieval",
+   year = 2024,
+   address_ = "Washington, D.C."
+}""",
+        descriptive_stats={
+            "test": {
+                "average_document_length": 2096.391812518931,
+                "average_query_length": 43.42857142857143,
+                "num_documents": 303732,
+                "num_queries": 49,
+                "average_relevant_docs_per_query": 34.93877551020408,
+            }
         },
     )
