d273405fde849e85fa88ac567db65cb128411a27
Daniel Buades Marcos <daniel.buades@clinia.com>
2024-09-30 11:43:25 -0400

    feat: readd arctic models due to metadata

---
+++ mteb/models/arctic_models.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+
+from mteb.model_meta import ModelMeta
+
+arctic_m_v1_5 = ModelMeta(
+    name="Snowflake/snowflake-arctic-embed-m-v1.5",
+    languages=["eng_Latn"],
+    open_source=True,
+    revision="97eab2e17fcb7ccb8bb94d6e547898fa1a6a0f47",
+    release_date="2024-07-08",  # initial commit of hf model.
+)
