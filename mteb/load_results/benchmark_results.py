50062e3daf2c802c82ad9aa731e917a961e74972
Márton Kardos <power.up1163@gmail.com>
2024-10-02 15:01:45 +0200

    Added additional arguments to get_scores on two classes

--- mteb/load_results/benchmark_results.py
+++ mteb/load_results/benchmark_results.py
@@ -1,14 +1,16 @@
 import json
 from collections import defaultdict
 from pathlib import Path
-from typing import Iterable, Literal
+from typing import Any, Callable, Iterable, Literal
 
 import numpy as np
 import pandas as pd
 from pydantic import BaseModel, ConfigDict
 
-from mteb.abstasks import AbsTask
-from mteb.abstasks.TaskMetadata import TASK_CATEGORY, TASK_DOMAIN, TASK_TYPE
+from mteb.abstasks.AbsTask import AbsTask, ScoresDict
+from mteb.abstasks.TaskMetadata import (ISO_LANGUAGE_SCRIPT, TASK_CATEGORY,
+                                        TASK_DOMAIN, TASK_TYPE)
+from mteb.languages import ISO_LANGUAGE
 from mteb.load_results.task_results import TaskResult
 from mteb.models.overview import get_model_metas
 from mteb.overview import get_tasks
@@ -39,6 +41,10 @@ def restrict_task_results(res: TaskResult, task: AbsTask) -> TaskResult:
     new_res = {**res.to_dict(), "scores": new_scores}
     new_res = TaskResult.from_dict(new_res)
     return new_res
+
+
+Split = str
+Score = Any
 
 
 class ModelResult(BaseModel):
@@ -87,8 +93,24 @@ class ModelResult(BaseModel):
             task_results=new_task_results,
         )
 
-    def get_scores(self) -> dict[str, float]:
-        return {res.task_name: res.get_score() for res in self.task_results}
+    def get_scores(
+        self,
+        splits: list[Split] | None = None,
+        languages: list[ISO_LANGUAGE | ISO_LANGUAGE_SCRIPT] | None = None,
+        scripts: list[ISO_LANGUAGE_SCRIPT] | None = None,
+        getter: Callable[[ScoresDict], Score] = lambda scores: scores["main_score"],
+        aggregation: Callable[[list[Score]], Any] = np.mean,
+    ) -> dict[str, float]:
+        return {
+            res.task_name: res.get_score(
+                splits=splits,
+                languages=languages,
+                scripts=scripts,
+                getter=getter,
+                aggregation=aggregation,
+            )
+            for res in self.task_results
+        }
 
     def __iter__(self):
         return iter(self.task_results)
@@ -157,17 +179,48 @@ class BenchmarkResults(BaseModel):
                 new_model_results.append(model_res)
         return type(self)(model_results=new_model_results)
 
-    def to_table(self, format: Literal["wide", "long"] = "wide") -> pd.DataFrame:
+    def get_scores(
+        self,
+        splits: list[Split] | None = None,
+        languages: list[ISO_LANGUAGE | ISO_LANGUAGE_SCRIPT] | None = None,
+        scripts: list[ISO_LANGUAGE_SCRIPT] | None = None,
+        getter: Callable[[ScoresDict], Score] = lambda scores: scores["main_score"],
+        aggregation: Callable[[list[Score]], Any] = np.mean,
+    ) -> list[dict[str, Any]]:
+        res = []
+        for model_res in self:
+            model_scores = model_res.get_scores(
+                splits=splits,
+                languages=languages,
+                scripts=scripts,
+                getter=getter,
+                aggregation=aggregation,
+            )
+            res.append(
+                {
+                    "model": model_res.model_name,
+                    "revision": model_res.model_revision,
+                    **model_scores,
+                }
+            )
+        return res
+
+    def to_table(
+        self,
+        splits: list[Split] | None = None,
+        languages: list[ISO_LANGUAGE | ISO_LANGUAGE_SCRIPT] | None = None,
+        scripts: list[ISO_LANGUAGE_SCRIPT] | None = None,
+        getter: Callable[[ScoresDict], Score] = lambda scores: scores["main_score"],
+        aggregation: Callable[[list[Score]], Any] = np.mean,
+        format: Literal["wide", "long"] = "wide",
+    ) -> pd.DataFrame:
         if format == "wide":
-            entries = []
-            for model_res in self:
-                entry = dict(
-                    model_name=model_res.model_name,
-                    model_revision=model_res.model_revision,
-                )
-                for task_res in model_res:
-                    entry[task_res.task_name] = task_res.get_score()
-                entries.append(entry)
+            entries = self.get_scores(
+                splits=splits,
+                languages=languages,
+                getter=getter,
+                aggregation=aggregation,
+            )
             return pd.DataFrame(entries).set_index(["model_name", "model_revision"])
         elif format == "long":
             entries = []
@@ -177,7 +230,12 @@ class BenchmarkResults(BaseModel):
                         model_name=model_res.model_name,
                         model_revision=model_res.model_revision,
                         task_name=task_res.task_name,
-                        score=task_res.get_score(),
+                        score=task_res.get_score(
+                            splits=splits,
+                            languages=languages,
+                            getter=getter,
+                            aggregation=aggregation,
+                        ),
                         mteb_version=task_res.mteb_version,
                         dataset_revision=task_res.dataset_revision,
                         evaluation_time=task_res.evaluation_time,
