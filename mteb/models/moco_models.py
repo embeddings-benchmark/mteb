a691aa71c76ba2f4d175be35c2e5acf2760052cd
Isaac Chung <chungisaac1217@gmail.com>
2024-10-16 20:40:55 +0000

    make lint

--- mteb/models/moco_models.py
+++ mteb/models/moco_models.py
@@ -16,11 +16,12 @@ def mocov3_loader(**kwargs):
         import timm
     except ImportError:
         raise ImportError("Please install `pip install timm` to use MOCOv3 models.")
-    
+
     class MOCOv3Wrapper:
         """A wrapper class for MOCOv3 models that supports image encoding.
         Text encoding and text-image fusion are not supported.
         """
+
         def __init__(
             self,
             model_name: str = "nyu-visionx/moco-v3-vit-b",
@@ -29,22 +30,28 @@ def mocov3_loader(**kwargs):
         ):
             self.model_name = model_name
             self.device = device
-            name = 'vit_base_patch16_224'
-            if 'vit-l' in model_name:
-                name = 'vit_large_patch16_224'
-            model = timm.create_model(name, pretrained=True, num_classes=0, pretrained_cfg_overlay=dict(hf_hub_id=model_name))
+            name = "vit_base_patch16_224"
+            if "vit-l" in model_name:
+                name = "vit_large_patch16_224"
+            model = timm.create_model(
+                name,
+                pretrained=True,
+                num_classes=0,
+                pretrained_cfg_overlay={"hf_hub_id": model_name},
+            )
 
             self.model = model.eval()
 
             # get model specific transforms (normalization, resize)
             data_config = timm.data.resolve_model_data_config(self.model)
-            self.processor = timm.data.create_transform(**data_config, is_training=False)
-
+            self.processor = timm.data.create_transform(
+                **data_config, is_training=False
+            )
 
         @staticmethod
         def get_text_embeddings(texts: list[str], batch_size: int = 32):
             raise ValueError("MOCO models only support image encoding.")
-        
+
         def get_image_embeddings(
             self,
             images: list[Image.Image] | DataLoader,
@@ -54,16 +61,26 @@ def mocov3_loader(**kwargs):
 
             if isinstance(images, DataLoader):
                 import torchvision.transforms.functional as F
+
                 with torch.no_grad():
                     for batch in tqdm(images):
-                        inputs = torch.vstack([self.processor(F.to_pil_image(b.to("cpu"))).unsqueeze(0) for b in batch])
-                        output = self.model(inputs)  # output is (batch_size, num_features) shaped tensor
+                        inputs = torch.vstack(
+                            [
+                                self.processor(F.to_pil_image(b.to("cpu"))).unsqueeze(0)
+                                for b in batch
+                            ]
+                        )
+                        output = self.model(
+                            inputs
+                        )  # output is (batch_size, num_features) shaped tensor
                         all_image_embeddings.append(output)
             else:
                 with torch.no_grad():
                     for i in tqdm(range(0, len(images), batch_size)):
                         batch_images = images[i : i + batch_size]
-                        output = self.model(self.processor(batch_images))  # output is (batch_size, num_features) shaped tensor
+                        output = self.model(
+                            self.processor(batch_images)
+                        )  # output is (batch_size, num_features) shaped tensor
                         all_image_embeddings.append(output)
 
             all_image_embeddings = torch.cat(all_image_embeddings, dim=0)
@@ -98,8 +115,9 @@ def mocov3_loader(**kwargs):
                 return text_embeddings
             elif image_embeddings is not None:
                 return image_embeddings
-    
+
     return MOCOv3Wrapper(**kwargs)
+
 
 mocov3_vit_base = ModelMeta(
     loader=partial(
