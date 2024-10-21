a3649a73cb81716f68481e246fc7ca7099f1dfe4
Jamie-Stirling <stirlingj00@gmail.com>
2024-10-21 16:23:47 +0100

    split ROxford, RParis into easy, medium and hard

--- mteb/tasks/Image/Any2AnyRetrieval/eng/RParisI2IRetrieval.py
+++ mteb/tasks/Image/Any2AnyRetrieval/eng/RParisI2IRetrieval.py
@@ -4,14 +4,14 @@ from mteb.abstasks.Image.AbsTaskAny2AnyRetrieval import AbsTaskAny2AnyRetrieval
 from mteb.abstasks.TaskMetadata import TaskMetadata
 
 
-class RParisI2IRetrieval(AbsTaskAny2AnyRetrieval):
+class RParisEasyI2IRetrieval(AbsTaskAny2AnyRetrieval):
     metadata = TaskMetadata(
-        name="RParisI2IRetrieval",
-        description="Retrieve photos of landmarks in Paris.",
-        reference="https://openaccess.thecvf.com/content_cvpr_2018/html/Radenovic_Revisiting_Oxford_and_CVPR_2018_paper.html",
+        name="RParisEasyI2IRetrieval",
+        description="Retrieve photos of landmarks in Paris, France.",
+        reference="https://openaccess.thecvf.com/content_cvpr_2018/html/Radenovic_Revisiting_Paris_and_CVPR_2018_paper.html",
         dataset={
-            "path": "JamieSJS/r-paris",
-            "revision": "bafc3a08fdffd72558021ce3a41250833d7e0e88",
+            "path": "JamieSJS/r-paris-easy",
+            "revision": "a7293da8a341de665ee4dcb2f209281df342d80b",
         },
         type="Retrieval",
         category="i2i",
@@ -35,16 +35,106 @@ class RParisI2IRetrieval(AbsTaskAny2AnyRetrieval):
 }
         """,
         descriptive_stats={
-            "n_samples": {"test": 6392},
+            "n_samples": {"test": 70},
             "avg_character_length": {
                 "test": {
                     "average_document_length": 0.0,
                     "average_query_length": 0.0,
-                    "num_documents": 6392,
-                    "num_queries": 6392,
-                    "average_relevant_docs_per_query": 734,
+                    "num_documents": 6322,
+                    "num_queries": 70,
+                    "average_relevant_docs_per_query": 98.2,
                 }
             },
         },
     )
-    skip_first_result = True
+    skip_first_result = False
+
+class RParisMediumI2IRetrieval(AbsTaskAny2AnyRetrieval):
+    metadata = TaskMetadata(
+        name="RParisMediumI2IRetrieval",
+        description="Retrieve photos of landmarks in Paris, France.",
+        reference="https://openaccess.thecvf.com/content_cvpr_2018/html/Radenovic_Revisiting_Paris_and_CVPR_2018_paper.html",
+        dataset={
+            "path": "JamieSJS/r-paris-medium",
+            "revision": "900267b49003a086979e8d52f6942624236bfc34",
+        },
+        type="Retrieval",
+        category="i2i",
+        eval_splits=["test"],
+        eval_langs=["eng-Latn"],
+        main_score="cv_recall_at_1",
+        date=("2009-01-01", "2010-04-01"),
+        domains=["Web"],
+        task_subtypes=["Object recognition"],
+        license="Not specified",
+        annotations_creators="derived",
+        dialect=[],
+        modalities=["image"],
+        sample_creation="created",
+        bibtex_citation="""@inproceedings{radenovic2018revisiting,
+  title={Revisiting oxford and paris: Large-scale image retrieval benchmarking},
+  author={Radenovi{\'c}, Filip and Iscen, Ahmet and Tolias, Giorgos and Avrithis, Yannis and Chum, Ond{\v{r}}ej},
+  booktitle={Proceedings of the IEEE conference on computer vision and pattern recognition},
+  pages={5706--5715},
+  year={2018}
+}
+        """,
+        descriptive_stats={
+            "n_samples": {"test": 70},
+            "avg_character_length": {
+                "test": {
+                    "average_document_length": 0.0,
+                    "average_query_length": 0.0,
+                    "num_documents": 6322,
+                    "num_queries": 70,
+                    "average_relevant_docs_per_query": 147.9,
+                }
+            },
+        },
+    )
+    skip_first_result = False
+
+class RParisHardI2IRetrieval(AbsTaskAny2AnyRetrieval):
+    metadata = TaskMetadata(
+        name="RParisHardI2IRetrieval",
+        description="Retrieve photos of landmarks in Paris, France.",
+        reference="https://openaccess.thecvf.com/content_cvpr_2018/html/Radenovic_Revisiting_Paris_and_CVPR_2018_paper.html",
+        dataset={
+            "path": "JamieSJS/r-paris-hard",
+            "revision": "fd121b6592fe946616fa85116703b94a4c61fd63",
+        },
+        type="Retrieval",
+        category="i2i",
+        eval_splits=["test"],
+        eval_langs=["eng-Latn"],
+        main_score="cv_recall_at_1",
+        date=("2009-01-01", "2010-04-01"),
+        domains=["Web"],
+        task_subtypes=["Object recognition"],
+        license="Not specified",
+        annotations_creators="derived",
+        dialect=[],
+        modalities=["image"],
+        sample_creation="created",
+        bibtex_citation="""@inproceedings{radenovic2018revisiting,
+  title={Revisiting oxford and paris: Large-scale image retrieval benchmarking},
+  author={Radenovi{\'c}, Filip and Iscen, Ahmet and Tolias, Giorgos and Avrithis, Yannis and Chum, Ond{\v{r}}ej},
+  booktitle={Proceedings of the IEEE conference on computer vision and pattern recognition},
+  pages={5706--5715},
+  year={2018}
+}
+        """,
+        descriptive_stats={
+            "n_samples": {"test": 70},
+            "avg_character_length": {
+                "test": {
+                    "average_document_length": 0.0,
+                    "average_query_length": 0.0,
+                    "num_documents": 6322,
+                    "num_queries": 70,
+                    "average_relevant_docs_per_query": 35.7,
+                }
+            },
+        },
+    )
+    skip_first_result = False
