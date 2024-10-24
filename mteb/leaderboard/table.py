26f05b9dc192eec4ee17f88120a17f4bad1703ca
Márton Kardos <power.up1163@gmail.com>
2024-10-24 14:22:01 +0200

    Updated layout

--- mteb/leaderboard/table.py
+++ mteb/leaderboard/table.py
@@ -1,12 +1,15 @@
 from __future__ import annotations
 
+import gradio as gr
 import numpy as np
 import pandas as pd
 
 from mteb.overview import get_task
 
 
-def scores_to_table(scores_long: list[dict]):
+def scores_to_tables(scores_long: list[dict]):
+    if not scores_long:
+        return gr.DataFrame(), gr.DataFrame()
     data = pd.DataFrame.from_records(scores_long)
     data["task_type"] = data["task_name"].map(
         lambda task_name: get_task(task_name).metadata.type
@@ -27,12 +30,16 @@ def scores_to_table(scores_long: list[dict]):
     per_task = data.pivot(
         index=["model_name", "model_revision"], columns="task_name", values="score"
     )
+    to_remove = per_task.isna().any(axis="columns")
     overall_mean = (
         data.groupby(["model_name", "model_revision"])[["score"]]
         .agg(np.nanmean)
         .rename(columns={"score": "mean"})
     )
-    joint_table = overall_mean.join([typed_mean, mean_per_type, per_task]).reset_index()
+    per_task = per_task[~to_remove]
+    mean_per_type = mean_per_type[~to_remove]
+    overall_mean = overall_mean[~to_remove]
+    joint_table = overall_mean.join([typed_mean, mean_per_type]).reset_index()
     joint_table = joint_table.sort_values("mean", ascending=False)
     joint_table = joint_table.rename(
         columns={
@@ -42,4 +49,12 @@ def scores_to_table(scores_long: list[dict]):
         }
     )
     joint_table = joint_table.drop(columns=["model_revision"])
-    return joint_table
+    per_task = per_task.rename(
+        columns={
+            "model_name": "Model",
+            "mean_by_task_type": "Mean by Task Type",
+            "mean": "Mean",
+        }
+    )
+    per_task = per_task.reset_index().drop(columns=["model_revision"])
+    return joint_table, per_task
