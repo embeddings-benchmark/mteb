7463853241b17db0fd739d8ed6ad528fd42852f8
yjoonjang <yjoonjang34@gmail.com>
2024-11-01 20:45:37 +0900

    feat: add AutoRAG Korean embedding retrieval benchmark

---
+++ mteb/tasks/Retrieval/kor/AutoRAGRetrieval.py
@@ -0,0 +1,33 @@
+from __future__ import annotations
+
+from mteb.abstasks.TaskMetadata import TaskMetadata
+
+from ....abstasks.AbsTaskRetrieval import AbsTaskRetrieval
+
+class AutoRAGRetrieval(AbsTaskRetrieval):
+    metadata = TaskMetadata(
+        name="AutoRAGRetrieval",
+        description="AutoRAGRetrieval",
+        reference=None,
+        dataset={
+            "path": "nlpai-lab/markers_bm",
+            "revision": "e252a2b2644140a68961e64f10c107e5c036119b"
+        },
+        type="Retrieval",
+        category="s2p",
+        eval_splits=["test"],
+        eval_langs=["kor-Hang"],
+        main_score="ndcg_at_10",
+        date=None,
+        form=None,
+        domains=None,
+        task_subtypes=None,
+        license=None,
+        socioeconomic_status=None,
+        annotations_creators=None,
+        dialect=None,
+        text_creation=None,
+        bibtex_citation="""""",
+        n_samples=None,
+        avg_character_length=None,
+    )
