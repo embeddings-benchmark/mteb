cab4eb8888bdf11b7a92114df7013761522caaa9
yjoonjang <yjoonjang34@gmail.com>
2024-11-01 21:14:24 +0900

    fix: add metadata for AutoRAGRetrieval

--- mteb/tasks/Retrieval/kor/AutoRAGRetrieval.py
+++ mteb/tasks/Retrieval/kor/AutoRAGRetrieval.py
@@ -28,7 +28,22 @@ class AutoRAGRetrieval(AbsTaskRetrieval):
         annotations_creators=None,
         dialect=None,
         text_creation=None,
-        bibtex_citation="""""",
-        n_samples=None,
-        avg_character_length=None,
+        bibtex_citation="""@misc{AutoRAG,
+  author = "Dongkyu Kim and Byoungwook Kim and Donggeon Han",
+  title = "AutoRAG",
+  year = {2024},
+  url = {https://github.com/Marker-Inc-Korea/AutoRAG}
+}""",
+        descriptive_stats={
+            "n_samples": None,
+            "avg_character_length": {
+                "dev": {
+                    "average_document_length": 305.15457788347203,
+                    "average_query_length": 22.62442640310625,
+                    "num_documents": 9251,
+                    "num_queries": 2833,
+                    "average_relevant_docs_per_query": 1.0,
+                }
+            },
+        },
     )
