66176a0a64651785faecdf07f9f96baf0b0a4d61
chenghao xiao <85804993+gowitheflow-1998@users.noreply.github.com>
2024-11-11 03:33:06 +0800

    [mieb] mieb scripts (siglip rerun & linear probing ablation & params count) (#1429)
    * mieb scripts
    
    * lint

---
+++ scripts/run_mieb_rerun_siglip.py
@@ -0,0 +1,30 @@
+from __future__ import annotations
+
+import mteb
+
+for model_name in [
+    "google/siglip-so400m-patch14-384",
+    "google/siglip-base-patch16-256-multilingual",
+    "google/siglip-base-patch16-256",
+    "google/siglip-base-patch16-512",
+    "google/siglip-base-patch16-384",
+    "google/siglip-base-patch16-224",
+    "google/siglip-large-patch16-256",
+    "google/siglip-large-patch16-384",
+]:
+    model = mteb.get_model(model_name)
+    tasks = mteb.get_tasks(
+        task_types=[
+            "Any2AnyRetrieval",
+            "Any2AnyMultiChoice",
+            "Any2TextMutipleChoice",
+            "ImageClustering",
+            "ImageClassification",
+            "ImageMultilabelClassification",
+            "ImageTextPairClassification",
+            # "VisualSTS",  # visual sts does not need rerun as will be the same after fixed.
+            "ZeroShotClassification",
+        ]
+    )
+    evaluation = mteb.MTEB(tasks=tasks)
+    results = evaluation.run(model, output_folder="results-mieb-final/siglip_rerun")
