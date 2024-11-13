9c58518080f6640f1e644c18fe8f174cdb2530e6
Orion Weller <31665361+orionw@users.noreply.github.com>
2024-11-13 11:30:38 -0500

    Consolidate Retrieval/Reranking/Instruction Variants (#1359)
    * update
    
    * merged retrieval; working
    
    * update tasks; working multilingual
    
    * everything working except instructions
    
    * working instructions; just need cleanup
    
    * add metadata for all but MindSmall
    
    * faster evaluation; mindsmall can compute in reasonable time
    
    * fix bad merge of docs
    
    * lint
    
    * fix test
    
    * qa
    
    * updated mindsmall
    
    * lint
    
    * fix debug
    
    * Update mteb/abstasks/dataloaders.py
    
    Co-authored-by: Roman Solomatin <samoed.roman@gmail.com>
    
    * lint
    
    ---------
    
    Co-authored-by: Roman Solomatin <samoed.roman@gmail.com>

--- mteb/tasks/InstructionReranking/eng/Robust04InstructionRetrieval.py
+++ mteb/tasks/InstructionReranking/eng/Robust04InstructionRetrieval.py
@@ -2,19 +2,19 @@ from __future__ import annotations
 
 from mteb.abstasks.TaskMetadata import TaskMetadata
 
-from ....abstasks.AbsTaskInstructionRetrieval import AbsTaskInstructionRetrieval
+from ....abstasks.AbsTaskReranking import AbsTaskReranking
 
 
-class Robust04InstructionRetrieval(AbsTaskInstructionRetrieval):
+class Robust04InstructionRetrieval(AbsTaskReranking):
     metadata = TaskMetadata(
         name="Robust04InstructionRetrieval",
         description="Measuring retrieval instruction following ability on Robust04 narratives for the FollowIR benchmark.",
         reference="https://arxiv.org/abs/2403.15246",
         dataset={
-            "path": "jhu-clsp/robust04-instructions",
-            "revision": "a5a1c4fe2bc528ac12e83f8cdf82178da85d2f1d",
+            "path": "jhu-clsp/robust04-instructions-mteb",
+            "revision": "0a3efedfcac0a7f859c46cff3a0fac0f8747b28f",
         },
-        type="InstructionRetrieval",
+        type="InstructionReranking",
         category="s2p",
         modalities=["text"],
         eval_splits=["test"],
