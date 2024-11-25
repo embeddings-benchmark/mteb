f8819442cc74a587817f8d0c08e2a356486e1579
Roman Solomatin <36135455+Samoed@users.noreply.github.com>
2024-11-25 18:33:25 +0300

    add rest of the stat

--- tests/test_TaskMetadata.py
+++ tests/test_TaskMetadata.py
@@ -531,10 +531,6 @@ def test_empty_descriptive_stat_in_new_datasets(task: AbsTask):
     # DON'T ADD NEW DATASETS TO THIS LIST
     # THIS IS ONLY INTENDED FOR HISTORIC DATASETS
     exceptions = [
-        "MSMARCOv2",
-        "NeuCLIR2022Retrieval",
-        "NeuCLIR2023Retrieval",
-        "FloresBitextMining",
         "FilipinoHateSpeechClassification",
     ]
 
