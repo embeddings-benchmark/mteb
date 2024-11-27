b9285094e52f3b99c415d199cb7fb5b083f52ac8
Márton Kardos <power.up1163@gmail.com>
2024-11-27 13:47:44 +0100

    Added join_revisions, made get_scores failsafe

--- mteb/load_results/benchmark_results.py
+++ mteb/load_results/benchmark_results.py
@@ -5,9 +5,11 @@ import warnings
 from collections import defaultdict
 from collections.abc import Iterable
 from pathlib import Path
-from typing import Any, Callable, Literal
+from typing import Any, Callable, Literal, Optional
 
 import numpy as np
+import pandas as pd
+from packaging.version import InvalidVersion, Version
 from pydantic import BaseModel, ConfigDict
 
 from mteb.abstasks.AbsTask import AbsTask, ScoresDict
@@ -91,36 +93,45 @@ class ModelResult(BaseModel):
         format: Literal["wide", "long"] = "wide",
     ) -> dict | list:
         if format == "wide":
-            scores = {
-                res.task_name: res.get_score(
-                    splits=splits,
-                    languages=languages,
-                    scripts=scripts,
-                    getter=getter,
-                    aggregation=aggregation,
-                )
-                for res in self.task_results
-            }
+            scores = {}
+            for res in self.task_results:
+                try:
+                    scores[res.task_name] = res.get_score(
+                        splits=splits,
+                        languages=languages,
+                        scripts=scripts,
+                        getter=getter,
+                        aggregation=aggregation,
+                    )
+                except Exception as e:
+                    warnings.warn(
+                        f"Couldn't get scores for {res.task_name} due to {e}."
+                    )
             return scores
         if format == "long":
             entries = []
             for task_res in self.task_results:
-                entry = dict(  # noqa
-                    model_name=self.model_name,
-                    model_revision=self.model_revision,
-                    task_name=task_res.task_name,
-                    score=task_res.get_score(
-                        splits=splits,
-                        languages=languages,
-                        getter=getter,
-                        aggregation=aggregation,
-                    ),
-                    mteb_version=task_res.mteb_version,
-                    dataset_revision=task_res.dataset_revision,
-                    evaluation_time=task_res.evaluation_time,
-                    kg_co2_emissions=task_res.kg_co2_emissions,
-                )
-                entries.append(entry)
+                try:
+                    entry = dict(  # noqa
+                        model_name=self.model_name,
+                        model_revision=self.model_revision,
+                        task_name=task_res.task_name,
+                        score=task_res.get_score(
+                            splits=splits,
+                            languages=languages,
+                            getter=getter,
+                            aggregation=aggregation,
+                        ),
+                        mteb_version=task_res.mteb_version,
+                        dataset_revision=task_res.dataset_revision,
+                        evaluation_time=task_res.evaluation_time,
+                        kg_co2_emissions=task_res.kg_co2_emissions,
+                    )
+                    entries.append(entry)
+                except Exception as e:
+                    warnings.warn(
+                        f"Couldn't get scores for {task_res.task_name} due to {e}."
+                    )
             return entries
 
     def __iter__(self):
@@ -200,6 +211,8 @@ class BenchmarkResults(BaseModel):
         n_parameters_range: tuple[int | None, int | None] = (None, None),
         use_instructions: bool | None = None,
     ) -> BenchmarkResults:
+        if model_names is None:
+            model_names = [model_res.model_name for model_res in self]
         model_metas = get_model_metas(
             model_names=model_names,
             languages=languages,
@@ -208,12 +221,63 @@ class BenchmarkResults(BaseModel):
             n_parameters_range=n_parameters_range,
             use_instructions=use_instructions,
         )
-        model_revision_pairs = {(meta.name, meta.revision) for meta in model_metas}
+        models = {meta.name for meta in model_metas}
+        # model_revision_pairs = {(meta.name, meta.revision) for meta in model_metas}
         new_model_results = []
         for model_res in self:
-            if (model_res.model_name, model_res.model_revision) in model_revision_pairs:
+            if model_res.model_name in models:
                 new_model_results.append(model_res)
         return type(self).model_construct(model_results=new_model_results)
+
+    def join_revisions(self):
+        def parse_version(version_str: str) -> Optional[Version]:
+            try:
+                return Version(version_str)
+            except (InvalidVersion, TypeError):
+                return None
+
+        def keep_best(group: pd.DataFrame) -> pd.DataFrame:
+            is_main_revision = group["revision"] == group["main_revision"]
+            if is_main_revision.sum() == 1:
+                return group[is_main_revision]
+            if group["mteb_version"].notna().any():
+                group = group.dropna(subset=["mteb_version"])
+                group = group.sort_values("mteb_version", ascending=False)
+                return group.head(n=1)
+            return group.head(n=1)
+
+        records = []
+        for model_result in self:
+            for task_result in model_result:
+                records.append(
+                    dict(
+                        model=model_result.model_name,
+                        revision=model_result.model_revision,
+                        task_name=task_result.task_name,
+                        mteb_version=task_result.mteb_version,
+                        task_result=task_result,
+                    )
+                )
+        task_df = pd.DataFrame.from_records(records)
+        model_to_main_revision = {
+            meta.name: meta.revision for meta in get_model_metas()
+        }
+        task_df["main_revision"] = task_df["model"].map(model_to_main_revision)
+        task_df["mteb_version"] = task_df["mteb_version"].map(parse_version)
+        task_df = (
+            task_df.groupby(["model", "task_name"])
+            .apply(keep_best)
+            .reset_index(drop=True)
+        )
+        model_results = []
+        for (model, model_revision), group in task_df.groupby(["model", "revision"]):
+            model_result = ModelResult.model_construct(
+                model_name=model,
+                model_revision=model_revision,
+                task_results=list(group["task_result"]),
+            )
+            model_results.append(model_result)
+        return BenchmarkResults.model_construct(model_results=model_results)
 
     def get_scores(
         self,
