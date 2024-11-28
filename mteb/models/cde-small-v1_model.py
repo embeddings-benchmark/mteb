eb04a8741ef7afb09579c744c635c24dbcf7a82a
Yash Thapliyal <123337877+YashDThapliyal@users.noreply.github.com>
2024-11-27 20:18:29 -0800

    update code for cde-small-v1 model

--- mteb/models/cde-small-v1_model.py
+++ mteb/models/cde-small-v1_model.py
@@ -1,7 +1,6 @@
 import mteb
-
-# load a model from the hub (or for a custom implementation see https://github.com/embeddings-benchmark/mteb/blob/main/docs/reproducible_workflow.md)
-
+from sentence_transformers import SentenceTransformer
+from transformers import AutoTokenizer, AutoModel
 
 model = mteb.get_model(
     "jxm/cde-small-v1",
@@ -11,10 +10,30 @@ model = mteb.get_model(
         "passage": "search_document: "
     }
 )
-
-tasks = mteb.get_benchmark("MTEB(eng, classic)") # or use a specific benchmark
-
+tasks = mteb.get_tasks(
+    tasks=[
+        # classification
+        "AmazonCounterfactualClassification", 
+        # clustering
+        "RedditClustering",
+        # pair classification
+        "TwitterSemEval2015",
+        # reranking
+        "AskUbuntuDupQuestions",
+        # retrieval
+        "SCIDOCS", 
+#         # sts
+        "STS22",
+#         # summarization
+        "SummEval",
+        ]
+)
 evaluation = mteb.MTEB(tasks=tasks)
-evaluation.run(model, output_folder="results")
+results = evaluation.run(
+    model,
+    output_folder="results",
+    extra_kwargs={"batch_size": 8},
+    overwrite_results=True
+)
 
-
+#print(results)
