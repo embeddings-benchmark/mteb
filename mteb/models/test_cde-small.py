200029d877b2f353748ff4f465f73a52c5bc048f
Yash Thapliyal <123337877+YashDThapliyal@users.noreply.github.com>
2024-12-23 13:33:08 -0800

    create a test

---
+++ mteb/models/test_cde-small.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+
+import mteb
+
+model_name = "jxm/cde-small-v1"
+revision = "main"
+
+model = mteb.get_model(
+    "jxm/cde-small-v1",
+    trust_remote_code=True,
+    model_prompts={"query": "search_query: ", "passage": "search_document: "},
+)
+print("succesffully got the model!")
+
+print(mteb.get_model_meta(model_name, revision))
+
+#test 
