0025f1066c0fb412da47871d6a7af9847c22db6f
Roman Solomatin <36135455+Samoed@users.noreply.github.com>
2025-01-03 22:06:25 +0300

    use all prompts

--- mteb/models/nomic_models.py
+++ mteb/models/nomic_models.py
@@ -213,10 +213,10 @@ nomic_modern_bert_embed = ModelMeta(
         NomicWrapper,
         model_name="nomic-ai/modernbert-embed-base",
         revision="5960f1566fb7cb1adf1eb6e816639cf4646d9b12",
-        model_prompts={
-            PromptType.query.value: "search_query: ",
-            PromptType.passage.value: "search_document: ",
-        },
+        # model_prompts={
+        #     PromptType.query.value: "search_query: ",
+        #     PromptType.passage.value: "search_document: ",
+        # },
     ),
     name="nomic-ai/modernbert-embed-base",
     languages=["eng-Latn"],
