766aad28a073bcc121d0954e17977bd0344c964e
Isaac Chung <chungisaac1217@gmail.com>
2025-01-03 09:54:54 +0000

    use the real get_model_meta

--- tests/test_models/test_model_loading.py
+++ tests/test_models/test_model_loading.py
@@ -4,7 +4,7 @@ import logging
 
 import pytest
 
-from mteb import get_model
+from mteb import get_model, get_model_meta
 from mteb.models.overview import MODEL_REGISTRY
 
 logging.basicConfig(level=logging.INFO)
@@ -13,7 +13,7 @@ logging.basicConfig(level=logging.INFO)
 @pytest.mark.parametrize("model_name", MODEL_REGISTRY.keys())
 def test_get_all_models_below_n_param_threshold(model_name: str):
     """Test that we can get all models with a number of parameters below a threshold."""
-    model_meta = MODEL_REGISTRY.get_model_meta(model_name=model_name)
+    model_meta = get_model_meta(model_name=model_name)
     assert model_meta is not None
     if model_meta.n_parameters is not None and model_meta.n_parameters < 2e9:
         m = get_model(model_name)
