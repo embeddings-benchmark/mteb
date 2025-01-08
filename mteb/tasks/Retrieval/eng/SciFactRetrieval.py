7e16fa2565b2058e12303a1feedbd0d4dea96a41
Kenneth Enevoldsen <kennethcenevoldsen@gmail.com>
2025-01-08 19:10:24 +0100

    feat: reduce logging for load_results()
    - redacts missing subsets to avoid 100+ subsets printed
    - reduce to logging.info
    - removed splits that are commonly never evaluated on and thus also the errors for them being missing
    
    The second part removed quite a few warnings (4930 to XX)
    
    It also seems like the splits were accidentally included in some of the MMTEB benchmark.
    
    This will remove those splits from those benchmarks (which are all in beta). We will have to recompute the tables for the paper though (we should do that anyway)
    
    Other potential thing to consider:
    
    - Scifact is included in MTEB(Medical). I have removed the "train" split from it as I think that was a mistake. (checked other dataset in benchmark)
    
    Here is a count of the current top errors:
    ```py
    {
        "MassiveScenarioClassification: Missing splits {'validation'}": 238,  # included in e.g. mteb(fra)
        "MassiveIntentClassification: Missing splits {'validation'}": 237, # included in e.g. mteb(fra)
        "MassiveScenarioClassification: Missing subsets {'af', 'da', ...} for split test": 230,
        "AmazonReviewsClassification: Missing splits {'validation'}": 229, # included in e.g. mteb(deu)
        "MassiveIntentClassification: Missing subsets {'af', 'da', ...} for split test": 228,
        "STS22: Missing subsets {'fr-pl', 'de-en', ...} for split test": 223,
        "AmazonReviewsClassification: Missing subsets {'es', 'ja', ...} for split test": 196,
        "MTOPDomainClassification: Missing splits {'validation'}": 195, # included in mteb(fra)
        "MTOPIntentClassification: Missing splits {'validation'}": 194, # included in mteb(fra)
        "AmazonCounterfactualClassification: Missing splits {'validation'}": 189, # included in mteb(deu)
        "MTOPDomainClassification: Missing subsets {'es', 'th', ...} for split test": 165,
        "STS17: Missing subsets {'en-ar', 'es-es', ...} for split test": 164,
        "MTOPIntentClassification: Missing subsets {'es', 'th', ...} for split test": 164,
        "AmazonCounterfactualClassification: Missing subsets {'de', 'ja', ...} for split test": 148,
    }
    ```

--- mteb/tasks/Retrieval/eng/SciFactRetrieval.py
+++ mteb/tasks/Retrieval/eng/SciFactRetrieval.py
@@ -17,7 +17,7 @@ class SciFact(AbsTaskRetrieval):
         type="Retrieval",
         category="s2p",
         modalities=["text"],
-        eval_splits=["train", "test"],
+        eval_splits=["test"],
         eval_langs=["eng-Latn"],
         main_score="ndcg_at_10",
         date=None,
