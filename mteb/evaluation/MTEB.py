216aa7cd14ca741c6d950af76dc8d9a1b3a50fba
sam021313 <40773225+sam021313@users.noreply.github.com>
2025-01-12 12:42:43 +0100

    fix: _run_eval no co tracking

--- mteb/evaluation/MTEB.py
+++ mteb/evaluation/MTEB.py
@@ -589,7 +589,6 @@ class MTEB:
                             task,
                             model,
                             split,
-                            output_folder,
                             subsets_to_run=subsets_to_run,
                             encode_kwargs=encode_kwargs,
                             **kwargs,
