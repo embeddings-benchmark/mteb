4fabb0944f32e59523c7253e3741bdc88edc821a
sam021313 <40773225+sam021313@users.noreply.github.com>
2025-01-13 18:47:24 +0100

    test: add test for bm25s and ColBERT

---
+++ tests/test_benchmark/test_models.py
@@ -0,0 +1,42 @@
+from __future__ import annotations
+
+import pytest
+
+import mteb
+from mteb import MTEB
+from mteb.abstasks import AbsTask
+
+from .mock_tasks import MockRetrievalTask
+
+
+
+@pytest.mark.parametrize("model", ["colbert-ir/colbertv2.0"])
+@pytest.mark.parametrize("task", [MockRetrievalTask()])
+@pytest.mark.skip(reason="Needs pylate")
+def test_colbert_model_e2e(task: AbsTask, model: str):
+    eval_splits = ["test"]
+
+    evaluation = MTEB(tasks=[task])
+
+    results = evaluation.run(
+        model,
+        eval_splits=eval_splits,
+        corpus_chunk_size=500,
+    )
+    result = results[0]
+
+    assert result.scores["test"][0]["ndcg_at_1"] == 1.0
+
+
+def test_bm25s_e2e():
+    # fails for dataset smaller then 1000
+    model = mteb.get_model("bm25s")
+    tasks = mteb.get_tasks(tasks=["NFCorpus"])
+    eval_splits = ["test"]
+
+    evaluation = MTEB(tasks=tasks)
+
+    results = evaluation.run(model, eval_splits=eval_splits)
+    result = results[0]
+
+    assert result.scores["test"][0]["ndcg_at_1"] == 0.42879
