1fceddd6b08b590f14fbb53b502fe00ef3e54c97
Isaac Chung <chungisaac1217@gmail.com>
2025-01-15 10:32:14 +0000

    fix typo: exclude MIEB mock tests

--- tests/test_tasks/test_all_abstasks.py
+++ tests/test_tasks/test_all_abstasks.py
@@ -97,7 +97,7 @@ def test_dataset_availability():
         t
         for t in tasks
         if t.metadata.name not in MOCK_TASK_TEST_GRID_AS_STRING
-        if t.metadata.name not in MOCK_TASK_TEST_GRID_AS_STRING
+        if t.metadata.name not in MOCK_MIEB_TASK_GRID_AS_STRING
         and t.metadata.name
         != "AfriSentiLangClassification"  # HOTFIX: Issue#1777. Remove this line when issue is resolved.
     ]
