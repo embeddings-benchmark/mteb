6c425f42211fcdd48cfe4b227109936745ef7af8
sam021313 <40773225+sam021313@users.noreply.github.com>
2025-01-16 14:50:19 +0100

    update tests skip

--- tests/test_benchmark/test_models.py
+++ tests/test_benchmark/test_models.py
@@ -11,10 +11,10 @@ from .mock_tasks import MockRetrievalTask
 
 @pytest.mark.parametrize("model", ["colbert-ir/colbertv2.0"])
 @pytest.mark.parametrize("task", [MockRetrievalTask()])
-@pytest.mark.skip(reason="Needs mteb[pylate]")
 def test_colbert_model_e2e(task: AbsTask, model: str):
+    pytest.importorskip("pylate", reason="pylate not installed")
     eval_splits = ["test"]
-
+    model = mteb.get_model(model)
     evaluation = MTEB(tasks=[task])
 
     results = evaluation.run(
@@ -27,9 +27,11 @@ def test_colbert_model_e2e(task: AbsTask, model: str):
     assert result.scores["test"][0]["ndcg_at_1"] == 1.0
 
 
-@pytest.mark.skip(reason="Needs mteb[bm25s]")
 def test_bm25s_e2e():
     # fails for dataset smaller then 1000
+    pytest.importorskip("bm25s", reason="bm25s not installed")
+    pytest.importorskip("Stemmer", reason="PyStemmer not installed")
+
     model = mteb.get_model("bm25s")
     tasks = mteb.get_tasks(tasks=["NFCorpus"])
     eval_splits = ["test"]
