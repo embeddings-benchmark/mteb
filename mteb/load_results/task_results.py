66fb5707c8fd7c10621a5f988ff08063fa3f76b7
Kenneth Enevoldsen <kennethcenevoldsen@gmail.com>
2025-01-19 12:35:16 +0100

    format

--- mteb/load_results/task_results.py
+++ mteb/load_results/task_results.py
@@ -1,10 +1,10 @@
 from __future__ import annotations
 
-from collections.abc import Iterable
 import json
 import logging
 from argparse import Namespace
 from collections import defaultdict
+from collections.abc import Iterable
 from functools import cached_property
 from importlib.metadata import version
 from pathlib import Path
