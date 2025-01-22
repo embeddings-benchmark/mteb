df434c6495abf4bb3b9e683dfbeb99b9d851b161
Márton Kardos <power.up1163@gmail.com>
2025-01-22 10:55:40 +0100

    Added metadata for amazon titan

--- mteb/models/misc_models.py
+++ mteb/models/misc_models.py
@@ -1776,3 +1776,22 @@ ember_v1 = ModelMeta(
     training_datasets=None,
     superseded_by=None,
 )
+amazon_titan_text_embeddings_v2 = ModelMeta(
+    name="amazon/Titan-text-embeddings-v2",
+    revision="1",
+    release_date="2024-04-30",
+    languages=["eng_Latn"],
+    n_parameters=None,
+    max_tokens=None,
+    embed_dim=None,
+    license="proprietary",
+    open_weights=False,
+    public_training_code=None,
+    public_training_data=None,
+    framework=[],
+    reference="https://huggingface.co/amazon/Titan-text-embeddings-v2",
+    similarity_fn_name="cosine",
+    use_instructions=False,
+    training_datasets=None,
+    superseded_by=None,
+)
