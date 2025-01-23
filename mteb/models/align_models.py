9164d09c253454ac31165fd9473b62fa210af651
Isaac Chung <chungisaac1217@gmail.com>
2025-01-23 12:43:56 +0000

    fix meta

--- mteb/models/align_models.py
+++ mteb/models/align_models.py
@@ -148,15 +148,15 @@ align_base = ModelMeta(
     embed_dim=768,
     license=None,
     open_weights=True,
-    public_training_code=True,  # https://github.com/kakaobrain/coyo-align
+    public_training_code="https://github.com/kakaobrain/coyo-align",
     public_training_data=True,
     framework=["PyTorch"],
     reference="https://huggingface.co/kakaobrain/align-base",
     similarity_fn_name=None,
     use_instructions=False,
-    training_datasets=[
+    training_datasets={
         # COYO-700M
-    ],
+    },
 )
 
 if __name__ == "__main__":
