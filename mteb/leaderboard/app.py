92b5658811f158da1c88beedfa61f3ba9b5a0d13
Wissam Siblini <wissam.siblini92@gmail.com>
2025-01-30 09:55:26 +0100

    Adding a banner to the new MMTEB leaderboard

--- mteb/leaderboard/app.py
+++ mteb/leaderboard/app.py
@@ -234,6 +234,13 @@ head = """
 """
 
 with gr.Blocks(fill_width=True, theme=gr.themes.Base(), head=head) as demo:
+
+    gr.Markdown("""
+    ## MMTEB: Massive Multilingual Text Embedding Benchmark
+
+    The MMTEB leaderboard compares text embedding models on 1000+ languages. Check out the [paper](https://openreview.net/pdf?id=zl3pfz4VCV) for details on datasets, languages and tasks. And you can contribute! 🤗 To add a model, please refer to the documentation in the [GitHub repository](https://github.com/embeddings-benchmark/mteb/path/to/doc) ✨
+    """)
+
     with gr.Row():
         with gr.Column(scale=5):
             gr.Markdown(
