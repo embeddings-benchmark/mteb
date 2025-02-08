2abad3990f2d7d624733c308696e1793d8138738
Roman Solomatin <36135455+Samoed@users.noreply.github.com>
2025-02-08 15:54:39 +0300

    add abstask imports

--- mteb/abstasks/__init__.py
+++ mteb/abstasks/__init__.py
@@ -12,17 +12,17 @@ from .AbsTaskRetrieval import AbsTaskRetrieval
 from .AbsTaskSpeedTask import AbsTaskSpeedTask
 from .AbsTaskSTS import AbsTaskSTS
 from .AbsTaskSummarization import AbsTaskSummarization
-from .Image.AbsTaskAny2AnyMultiChoice import AbsTaskAny2AnyMultiChoice
-from .Image.AbsTaskAny2AnyRetrieval import AbsTaskAny2AnyRetrieval
-from .Image.AbsTaskAny2TextMultipleChoice import AbsTaskAny2TextMultipleChoice
-from .Image.AbsTaskImageClassification import AbsTaskImageClassification
-from .Image.AbsTaskImageClustering import AbsTaskImageClustering
-from .Image.AbsTaskImageMultilabelClassification import (
+from .Image import (
+    AbsTaskAny2AnyMultiChoice,
+    AbsTaskAny2AnyRetrieval,
+    AbsTaskAny2TextMultipleChoice,
+    AbsTaskImageClassification,
+    AbsTaskImageClustering,
     AbsTaskImageMultilabelClassification,
+    AbsTaskImageTextPairClassification,
+    AbsTaskVisualSTS,
+    AbsTaskZeroshotClassification,
 )
-from .Image.AbsTaskImageTextPairClassification import AbsTaskImageTextPairClassification
-from .Image.AbsTaskVisualSTS import AbsTaskVisualSTS
-from .Image.AbsTaskZeroshotClassification import AbsTaskZeroshotClassification
 from .TaskMetadata import TaskMetadata
 
 __all__ = [
