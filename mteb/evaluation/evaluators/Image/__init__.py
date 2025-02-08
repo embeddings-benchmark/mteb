cae6c9e7768453ea664c7fbd67a2de3a5f34c956
Roman Solomatin <36135455+Samoed@users.noreply.github.com>
2025-02-08 15:51:19 +0300

    fix imports

--- mteb/evaluation/evaluators/Image/__init__.py
+++ mteb/evaluation/evaluators/Image/__init__.py
@@ -1,17 +1,17 @@
 from __future__ import annotations
 
-from Any2AnyMultiChoiceEvaluator import Any2AnyMultiChoiceEvaluator
-from Any2AnyRetrievalEvaluator import Any2AnyRetrievalEvaluator
-from Any2TextMultipleChoiceEvaluator import Any2TextMultipleChoiceEvaluator
-from ClassificationEvaluator import (
+from .Any2AnyMultiChoiceEvaluator import Any2AnyMultiChoiceEvaluator
+from .Any2AnyRetrievalEvaluator import Any2AnyRetrievalEvaluator
+from .Any2TextMultipleChoiceEvaluator import Any2TextMultipleChoiceEvaluator
+from .ClassificationEvaluator import (
     ImagekNNClassificationEvaluator,
     ImagekNNClassificationEvaluatorPytorch,
     ImagelogRegClassificationEvaluator,
 )
-from ClusteringEvaluator import ImageClusteringEvaluator
-from ImageTextPairClassificationEvaluator import ImageTextPairClassificationEvaluator
-from VisualSTSEvaluator import VisualSTSEvaluator
-from ZeroshotClassificationEvaluator import ZeroshotClassificationEvaluator
+from .ClusteringEvaluator import ImageClusteringEvaluator
+from .ImageTextPairClassificationEvaluator import ImageTextPairClassificationEvaluator
+from .VisualSTSEvaluator import VisualSTSEvaluator
+from .ZeroshotClassificationEvaluator import ZeroshotClassificationEvaluator
 
 __all__ = [
     "Any2AnyMultiChoiceEvaluator",
