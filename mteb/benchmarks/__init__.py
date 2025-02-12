65f3407e91100b6176052e2e5137f8e88c4eaece
Kenneth Enevoldsen <kennethcenevoldsen@gmail.com>
2025-02-12 11:31:04 +0100

    fix: Add versioning to MTEB benchmarks (#2024)
    * Add versioning to MTEB benchmarks
    
    - Following suggestion made in #2001 I added version to MTEB benchmarks
    - changed the name of the MTEB(Chinese) to MTEB(cmn, v1). Though we could go for MTEB(Chinese, v1) assuming it is a group (also covering chinese other than mandarin)
    - change the name of FaMTEB(fas, beta) to MTEB(fas)
    - did a minor formatting of imports due to a circular import error
    - moved the Benchmark object out of the file with the benchmarks
      - this is still >1000 lines so we could split it up to "external_benchmarks", "monolingual_benchmarks", "domain_specific_benchmarks", "multilingual_benchmarks"
    
    Fixes #2001
    
    * format
    
    * update tests
    
    * updated for backward compatibility
    
    * ibid
    
    * Update mteb/benchmarks/get_benchmark.py
    
    Co-authored-by: Roman Solomatin <samoed.roman@gmail.com>
    
    * Update mteb/benchmarks/get_benchmark.py
    
    Co-authored-by: Roman Solomatin <samoed.roman@gmail.com>
    
    * add missing import
    
    ---------
    
    Co-authored-by: Roman Solomatin <samoed.roman@gmail.com>

--- mteb/benchmarks/__init__.py
+++ mteb/benchmarks/__init__.py
@@ -1,4 +1,16 @@
 from __future__ import annotations
 
+from mteb.benchmarks.benchmark import Benchmark
 from mteb.benchmarks.benchmarks import *
-from mteb.benchmarks.get_benchmark import *
+from mteb.benchmarks.get_benchmark import (
+    BENCHMARK_REGISTRY,
+    get_benchmark,
+    get_benchmarks,
+)
+
+__all__ = [
+    "BENCHMARK_REGISTRY",
+    "get_benchmark",
+    "get_benchmarks",
+    "Benchmark",
+]
--- mteb/benchmarks/get_benchmark.py
+++ mteb/benchmarks/get_benchmark.py
@@ -1,9 +1,33 @@
 from __future__ import annotations
 
 import difflib
+import logging
+import warnings
 
 import mteb.benchmarks.benchmarks as benchmark_module
-from mteb.benchmarks.benchmarks import Benchmark
+from mteb.benchmarks.benchmarks import (
+    C_MTEB,
+    FA_MTEB,
+    MTEB_DEU,
+    MTEB_EN,
+    MTEB_ENG_CLASSIC,
+    MTEB_EU,
+    MTEB_FRA,
+    MTEB_INDIC,
+    MTEB_JPN,
+    MTEB_KOR,
+    MTEB_MAIN_RU,
+    MTEB_POL,
+    MTEB_RETRIEVAL_LAW,
+    MTEB_RETRIEVAL_MEDICAL,
+    MTEB_RETRIEVAL_WITH_INSTRUCTIONS,
+    SEB,
+    Benchmark,
+    MTEB_code,
+    MTEB_multilingual,
+)
+
+logger = logging.getLogger(__name__)
 
 BENCHMARK_REGISTRY = {
     inst.name: inst
@@ -11,10 +35,37 @@ BENCHMARK_REGISTRY = {
     if isinstance(inst, Benchmark)
 }
 
+PREVIOUS_BENCHMARK_NAMES = {
+    "MTEB(eng)": MTEB_EN.name,
+    "MTEB(eng, classic)": MTEB_ENG_CLASSIC.name,
+    "MTEB(rus)": MTEB_MAIN_RU.name,
+    "MTEB(Retrieval w/Instructions)": MTEB_RETRIEVAL_WITH_INSTRUCTIONS.name,
+    "MTEB(law)": MTEB_RETRIEVAL_LAW.name,
+    "MTEB(Medical)": MTEB_RETRIEVAL_MEDICAL.name,
+    "MTEB(Scandinavian)": SEB.name,
+    "MTEB(fra)": MTEB_FRA.name,
+    "MTEB(deu)": MTEB_DEU.name,
+    "MTEB(kor)": MTEB_KOR.name,
+    "MTEB(pol)": MTEB_POL.name,
+    "MTEB(code)": MTEB_code.name,
+    "MTEB(Multilingual)": MTEB_multilingual.name,
+    "MTEB(jpn)": MTEB_JPN.name,
+    "MTEB(Indic)": MTEB_INDIC.name,
+    "MTEB(Europe)": MTEB_EU.name,
+    "MTEB(Chinese)": C_MTEB.name,
+    "FaMTEB(fas, beta)": FA_MTEB.name,
+}
+
 
 def get_benchmark(
     benchmark_name: str,
 ) -> Benchmark:
+    if benchmark_name in PREVIOUS_BENCHMARK_NAMES:
+        warnings.warn(
+            f"Using the previous benchmark name '{benchmark_name}' is deprecated. Please use '{PREVIOUS_BENCHMARK_NAMES[benchmark_name]}' instead.",
+            DeprecationWarning,
+        )
+        benchmark_name = PREVIOUS_BENCHMARK_NAMES[benchmark_name]
     if benchmark_name not in BENCHMARK_REGISTRY:
         close_matches = difflib.get_close_matches(
             benchmark_name, BENCHMARK_REGISTRY.keys()
@@ -32,4 +83,4 @@ def get_benchmarks(
 ) -> list[Benchmark]:
     if names is None:
         names = list(BENCHMARK_REGISTRY.keys())
-    return [BENCHMARK_REGISTRY[name] for name in names]
+    return [get_benchmark(name) for name in names]
