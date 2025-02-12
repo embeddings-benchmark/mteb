0d72520eac2badddc6770be2e12a889417a3deac
Isaac Chung <chungisaac1217@gmail.com>
2025-02-12 12:02:26 +0000

    fix import

--- mteb/tasks/aggregated_tasks/__init__.py
+++ mteb/tasks/aggregated_tasks/__init__.py
@@ -3,6 +3,14 @@ from __future__ import annotations
 from .CQADupStackNLRetrieval import CQADupstackNLRetrieval
 from .CQADupStackRetrieval import CQADupstackRetrieval
 from .CQADupStackRetrievalFa import CQADupstackRetrievalFa
+from .STS17MultilingualVisualSTSEng import STS17MultilingualVisualSTSEng
+from .STS17MultilingualVisualSTSMultilingual import (
+    STS17MultilingualVisualSTSMultilingual,
+)
+from .STSBenchmarkMultilingualVisualSTSEng import STSBenchmarkMultilingualVisualSTSEng
+from .STSBenchmarkMultilingualVisualSTSMultilingual import (
+    STSBenchmarkMultilingualVisualSTSMultilingual,
+)
 from .SynPerChatbotConvSAClassification import SynPerChatbotConvSAClassification
 
 __all__ = [
@@ -10,4 +18,8 @@ __all__ = [
     "CQADupstackRetrievalFa",
     "SynPerChatbotConvSAClassification",
     "CQADupstackNLRetrieval",
+    "STS17MultilingualVisualSTSEng",
+    "STS17MultilingualVisualSTSMultilingual",
+    "STSBenchmarkMultilingualVisualSTSEng",
+    "STSBenchmarkMultilingualVisualSTSMultilingual",
 ]
