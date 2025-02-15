0a87566cd48e737ecf590424e307caef30cac83d
Isaac Chung <chungisaac1217@gmail.com>
2025-02-15 12:11:56 +0000

    split into mieb(eng) and mieb(multilingual)

--- mteb/tasks/aggregated_tasks/__init__.py
+++ mteb/tasks/aggregated_tasks/__init__.py
@@ -3,14 +3,6 @@ from __future__ import annotations
 from .CQADupStackNLRetrieval import CQADupstackNLRetrieval
 from .CQADupStackRetrieval import CQADupstackRetrieval
 from .CQADupStackRetrievalFa import CQADupstackRetrievalFa
-from .STS17MultilingualVisualSTSEng import STS17MultilingualVisualSTSEng
-from .STS17MultilingualVisualSTSMultilingual import (
-    STS17MultilingualVisualSTSMultilingual,
-)
-from .STSBenchmarkMultilingualVisualSTSEng import STSBenchmarkMultilingualVisualSTSEng
-from .STSBenchmarkMultilingualVisualSTSMultilingual import (
-    STSBenchmarkMultilingualVisualSTSMultilingual,
-)
 from .SynPerChatbotConvSAClassification import SynPerChatbotConvSAClassification
 
 __all__ = [
@@ -18,8 +10,4 @@ __all__ = [
     "CQADupstackRetrievalFa",
     "SynPerChatbotConvSAClassification",
     "CQADupstackNLRetrieval",
-    "STS17MultilingualVisualSTSEng",
-    "STS17MultilingualVisualSTSMultilingual",
-    "STSBenchmarkMultilingualVisualSTSEng",
-    "STSBenchmarkMultilingualVisualSTSMultilingual",
 ]
