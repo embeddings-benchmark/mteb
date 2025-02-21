07eda3c4a2d695f6113657afa48cacdad2be1fc6
Animesh Jha <jha.animesh01@gmail.com>
2025-02-20 19:17:10 -0800

    readd changes lost in rebase

--- mteb/encoder_interface.py
+++ mteb/encoder_interface.py
@@ -212,20 +212,15 @@ class ImageEncoder:
 
 
 class AudioEncoder:
-    """Interface for audio encoder designed based on <?>.
-    There is not a perfect 1-1 match, e.g. device can be None here.
-    The intention here is to define the current interface and adapt to as close to MTEB as possible
-    and align as much as possible with sentencetransformers.
-    """
+    """Interface for audio encoders."""
 
     def __init__(
-        self,
-        device: str | None,
-        **kwargs: Any,
+        self, device: str | None = None, sample_rate: int = 16000, **kwargs: Any
     ):
-        pass
+        self.device = device
+        self.sample_rate = sample_rate
 
-    def encode(  # current a 1-1 match with Encoder.encode
+    def encode(
         self,
         sentences: Sequence[str],
         *,
@@ -235,29 +230,16 @@ class AudioEncoder:
     ) -> np.ndarray:
         pass
 
-    def get_audio_embeddings(
-        # Seems like sentence transformers use a singular encode for both images and text. Not sure if we want to do the same.
-        # If not it might be ideal to redefine Encoder.encode
-        self,
-        audio: list[Audio] | DataLoader,
-        **kwargs,
-        # removed batch_size, it is not required that it will accept kwargs
-    ) -> np.ndarray:  # added standard output (I believe we actually expect tensors in the code, but would like to be consistent)
+    def get_audio_embeddings(self, audio: AudioBatch, **kwargs: Any) -> np.ndarray:
         pass
 
-    def get_text_embeddings(  # any reason for this?
-        self,
-        texts: list[str],
-        **kwargs,
-    ) -> np.ndarray:
+    def get_text_embeddings(self, texts: list[str], **kwargs: Any) -> np.ndarray:
         pass
 
-    def get_fused_embeddings(  # hmm what if I have a document with images at specific positions?
+    def get_fused_embeddings(
         self,
+        audio: AudioBatch | None = None,
         texts: list[str] | None = None,
-        audio: list[Audio] | DataLoader | None = None,
-        # the requirement for these two to be the same seems odd (docs without images, images without associated text, docs with multiple images)
-        # fusion_mode: str="sum", # will remove this as it should be required in the interface
         **kwargs: Any,
     ) -> np.ndarray:
         pass
