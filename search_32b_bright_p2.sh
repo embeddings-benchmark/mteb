22f49e276a6339f59372c5b3bd4b0b7beb13286b
oweller2 <oweller2@dsailogin.mgmt.ai.cluster>
2025-02-25 14:19:55 -0500

    cleanup

--- search_32b_bright_p2.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export VLLM_WORKER_MULTIPROC_METHOD=spawn
-python run_mteb.py -c "/home/oweller2/my_scratch/LLaMA-Factory/saves/qwen-32b-v2-short/lora/sft/checkpoint-750-full" -d BrightRetrieval -s biology -n 2
-python run_mteb.py -c "/home/oweller2/my_scratch/LLaMA-Factory/saves/qwen-32b-v2-short/lora/sft/checkpoint-1000-full" -d BrightRetrieval -s biology -n 2
-python run_mteb.py -c "/home/oweller2/my_scratch/LLaMA-Factory/saves/qwen-32b-v2-short/lora/sft/checkpoint-750-full" -d BrightRetrieval -s biology -n 2
--- search_32b_bright_p4.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export VLLM_WORKER_MULTIPROC_METHOD=spawn
-python run_mteb.py -c "/home/oweller2/my_scratch/LLaMA-Factory/saves/qwen-32b-v2-short/lora/sft/checkpoint-1750-full" -d BrightRetrieval -s biology -n 4
-python run_mteb.py -c "/home/oweller2/my_scratch/LLaMA-Factory/saves/qwen-32b-v2-short/lora/sft/checkpoint-2000-full" -d BrightRetrieval -s biology -n 4
-python run_mteb.py -c "/home/oweller2/my_scratch/LLaMA-Factory/saves/qwen-32b-v2/lora/sft/checkpoint-2250-full" -d BrightRetrieval -s biology -n 4
-
