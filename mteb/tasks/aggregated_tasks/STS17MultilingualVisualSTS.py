1f372ecaa744716cabab9e061e8c9fb030968a08
Isaac Chung <chungisaac1217@gmail.com>
2025-02-26 08:24:55 +0000

    fix test and mark VisualSTS17 as multilingual

--- mteb/tasks/aggregated_tasks/STS17MultilingualVisualSTS.py
+++ mteb/tasks/aggregated_tasks/STS17MultilingualVisualSTS.py
@@ -12,6 +12,7 @@ task_list_sts17: list[AbsTask] = [
 
 
 class STS17MultilingualVisualSTSEng(AbsTaskAggregate):
+    is_multilingual = (True,)
     metadata = AggregateTaskMetadata(
         name="VisualSTS17Eng",
         description="STS17MultilingualVisualSTS English only.",
@@ -57,6 +58,7 @@ task_list_sts17_multi: list[AbsTask] = [
 
 
 class STS17MultilingualVisualSTSMultilingual(AbsTaskAggregate):
+    is_multilingual = (True,)
     metadata = AggregateTaskMetadata(
         name="VisualSTS17Multilingual",
         description="STS17MultilingualVisualSTS multilingual.",
