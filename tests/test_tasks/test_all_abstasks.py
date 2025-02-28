465d22a78ea3085fa3379318e51bfa984f321416
sam021313 <40773225+sam021313@users.noreply.github.com>
2025-02-28 22:32:54 +0100

    dont run test_dataset_on_hf in every pr

--- tests/test_tasks/test_all_abstasks.py
+++ tests/test_tasks/test_all_abstasks.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import logging
 from unittest.mock import Mock, patch
-
+import os
 import pytest
 import requests
 
@@ -65,7 +65,7 @@ def test_load_data(
         if not task.is_multilingual:
             mock_dataset_transform.assert_called_once()
 
-
+@pytest.mark.skipif(condition=os.getenv("NO_SKIP", 0) == 1, reason="Just in separate CI Job")
 @pytest.mark.flaky(
     reruns=3,
     reruns_delay=5,
