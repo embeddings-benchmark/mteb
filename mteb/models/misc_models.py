f7ba296de15e988ab4cd521ba9496ea197df2387
Márton Kardos <power.up1163@gmail.com>
2025-02-28 16:22:13 +0100

    Added training data for Noinstruct

--- mteb/models/misc_models.py
+++ mteb/models/misc_models.py
@@ -1609,7 +1609,24 @@ avsolatorio__NoInstruct_small_Embedding_v0 = ModelMeta(
     reference="https://huggingface.co/avsolatorio/NoInstruct-small-Embedding-v0",
     similarity_fn_name="cosine",
     use_instructions=None,
-    training_datasets=None,
+    training_datasets={
+        **bge_training_data,
+        # not in mteb:
+        # MEDI
+        # all MTEB CLF datasets that has a train split:
+        "AmazonPolarityClassification": ["train"],
+        "AmazonReviewsClassification": ["train"],
+        "EmotionClassification": ["train"],
+        "ImdbClassification": ["train"],
+        "MTOPDomainClassification": ["train"],
+        "MTOPIntentClassification": ["train"],
+        "MassiveIntentClassification": ["train"],
+        "MassiveScenarioClassification": ["train"],
+        "ToxicConversationsClassification": ["train"],
+        "TweetSentimentExtractionClassification": ["train"],
+        "Banking77Classification": ["train"],
+        "AmazonCounterfactualClassification": ["train"],
+    },
     adapted_from=None,
     superseded_by=None,
 )
