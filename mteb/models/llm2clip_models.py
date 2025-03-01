db922b77a680ef09f713d2012dfb36af8f783292
Isaac Chung <chungisaac1217@gmail.com>
2025-03-01 08:22:51 +0000

    add image_emb, fused_emb, and calc probs methods

--- mteb/models/llm2clip_models.py
+++ mteb/models/llm2clip_models.py
@@ -5,11 +5,18 @@ from pathlib import Path
 from typing import Any
 
 import torch
+from PIL import Image
+from torch.utils.data import DataLoader
 from tqdm import tqdm
 from transformers import AutoConfig, AutoModel, AutoTokenizer, CLIPImageProcessor
 
 from mteb.encoder_interface import PromptType
 from mteb.model_meta import ModelMeta
+
+MODEL2PROCESSOR = {
+    "microsoft/LLM2CLIP-Openai-L-14-336": "openai/clip-vit-large-patch14-336",
+    "microsoft/LLM2CLIP-Openai-B-16": "openai/clip-vit-base-patch16",
+}
 
 
 def llm2clip_loader(**kwargs):
@@ -31,11 +38,10 @@ def llm2clip_loader(**kwargs):
             self.device = device
             from huggingface_hub import snapshot_download
 
-            repo_id = "microsoft/LLM2CLIP-Openai-L-14-336"
-            model_folder_path = snapshot_download(repo_id=repo_id)
+            model_folder_path = snapshot_download(repo_id=model_name)
             model_name_or_path = Path(model_folder_path)
             self.processor = CLIPImageProcessor.from_pretrained(
-                "openai/clip-vit-large-patch14-336"
+                MODEL2PROCESSOR[model_name]
             )
             self.model = (
                 AutoModel.from_pretrained(
@@ -47,7 +53,9 @@ def llm2clip_loader(**kwargs):
                 .eval()
             )
 
-            llm_model_name = "microsoft/LLM2CLIP-Llama-3-8B-Instruct-CC-Finetuned"
+            llm_model_name = (
+                "microsoft/LLM2CLIP-Llama-3-8B-Instruct-CC-Finetuned"  # constant
+            )
             config = AutoConfig.from_pretrained(llm_model_name, trust_remote_code=True)
             llm_model = AutoModel.from_pretrained(
                 llm_model_name,
@@ -56,9 +64,7 @@ def llm2clip_loader(**kwargs):
                 trust_remote_code=True,
             )
             tokenizer = AutoTokenizer.from_pretrained(llm_model_name)
-            llm_model.config._name_or_path = (
-                "meta-llama/Meta-Llama-3-8B-Instruct"  #  Workaround for LLM2VEC
-            )
+            llm_model.config._name_or_path = "meta-llama/Meta-Llama-3-8B-Instruct"  #  Workaround for LLM2VEC. constant.
             self.l2v = LLM2Vec(
                 llm_model,
                 tokenizer,
@@ -92,6 +98,94 @@ def llm2clip_loader(**kwargs):
 
             return all_text_embeddings
 
+        def get_image_embeddings(
+            self,
+            images: list[Image.Image] | DataLoader,
+            *,
+            task_name: str | None = None,
+            prompt_type: PromptType | None = None,
+            batch_size: int = 32,
+            **kwargs: Any,
+        ):
+            all_image_embeddings = []
+            if isinstance(images, DataLoader):
+                import torchvision.transforms.functional as F
+
+                with torch.no_grad(), torch.amp.autocast("cuda"):
+                    for batch in tqdm(images):
+                        input_pixels = self.processor(
+                            images=[F.to_pil_image(b) for b in batch],
+                            return_tensors="pt",
+                        ).pixel_values.to(self.device)
+                        image_features = self.model.get_image_features(input_pixels)
+                        image_features /= image_features.norm(dim=-1, keepdim=True)
+                        all_image_embeddings.append(
+                            image_features.cpu().to(torch.float32)
+                        )
+            else:
+                with torch.no_grad(), torch.cuda.amp.autocast():
+                    for i in tqdm(range(0, len(images), batch_size)):
+                        batch_images = images[i : i + batch_size]
+                        input_pixels = self.processor(
+                            images=batch_images, return_tensors="pt"
+                        ).pixel_values.to(self.device)
+                        image_features = self.model.get_image_features(input_pixels)
+                        image_features /= image_features.norm(dim=-1, keepdim=True)
+                        all_image_embeddings.append(
+                            image_features.cpu().to(torch.float32)
+                        )
+
+            all_image_embeddings = torch.cat(all_image_embeddings, dim=0)
+            return all_image_embeddings
+
+        def calculate_probs(self, text_embeddings, image_embeddings):
+            text_embeddings = text_embeddings / text_embeddings.norm(
+                dim=-1, keepdim=True
+            )
+            image_embeddings = image_embeddings / image_embeddings.norm(
+                dim=-1, keepdim=True
+            )
+            logits = torch.matmul(image_embeddings, text_embeddings.T)
+            probs = (logits * 100).softmax(dim=-1)
+            return probs
+
+        def get_fused_embeddings(
+            self,
+            texts: list[str] = None,
+            images: list[Image.Image] | DataLoader = None,
+            fusion_mode="sum",
+            **kwargs: Any,
+        ):
+            if texts is None and images is None:
+                raise ValueError("Either texts or images must be provided")
+
+            text_embeddings = None
+            image_embeddings = None
+
+            if texts is not None:
+                text_embeddings = self.get_text_embeddings(texts, **kwargs)
+
+            if images is not None:
+                image_embeddings = self.get_image_embeddings(images, **kwargs)
+
+            if text_embeddings is not None and image_embeddings is not None:
+                if len(text_embeddings) != len(image_embeddings):
+                    raise ValueError(
+                        "The number of texts and images must have the same length"
+                    )
+                if fusion_mode == "sum":
+                    fused_embeddings = text_embeddings + image_embeddings
+                else:
+                    # to do: add other fusion mode
+                    raise ValueError(
+                        f"fusion mode {fusion_mode} hasn't been implemented"
+                    )
+                return fused_embeddings
+            elif text_embeddings is not None:
+                return text_embeddings
+            elif image_embeddings is not None:
+                return image_embeddings
+
     return LLM2CLIPWrapper(**kwargs)
 
 
@@ -123,6 +217,6 @@ llm2clip_openai_l_14_336 = ModelMeta(
 
 if __name__ == "__main__":
     m = llm2clip_loader()
-    m.get_text_embeddings(
+    emb = m.get_text_embeddings(
         texts=["what is going on blah?", "this is a test for this model."]
     )
