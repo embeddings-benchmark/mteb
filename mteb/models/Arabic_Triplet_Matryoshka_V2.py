b582a1fe2be9121bf46615aa916913783406ffaf
Eng. Omar Najar <eng.omarnj@gmail.com>
2025-03-06 16:17:21 +0300

    added correct memory usage

--- mteb/models/Arabic_Triplet_Matryoshka_V2.py
+++ mteb/models/Arabic_Triplet_Matryoshka_V2.py
@@ -7,7 +7,7 @@ arabic_triplet_matryoshka = ModelMeta(
     revision="ed357f222f0b6ea6670d2c9b5a1cb93950d34200",  
     release_date="2024-07-28",
     n_parameters=135_000_000, 
-    memory_usage_mb = 0.5,
+    memory_usage_mb = 516,
     embed_dim=768, 
     license="apache-2.0",  
     max_tokens=768, 
@@ -18,4 +18,9 @@ arabic_triplet_matryoshka = ModelMeta(
     public_training_code=None,
     adapted_from="aubmindlab/bert-base-arabertv02",
     public_training_data="akhooli/arabic-triplets-1m-curated-sims-len",  
+    training_datasets={"akhooli/arabic-triplets-1m-curated-sims-len": ["train"]},
 )
+
+# ✅ Correct way to calculate memory usage
+memory_usage = arabic_triplet_matryoshka.calculate_memory_usage_mb()
+print("Calculated Memory Usage (MB):", memory_usage)
