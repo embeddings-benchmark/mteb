9f9bb580e940f758ad7cf608d55d222e7edb4697
Eng. Omar Najar <eng.omarnj@gmail.com>
2025-03-06 16:03:52 +0300

    Update memory_usage_mb with correct calculated value

--- mteb/models/Arabic_Triplet_Matryoshka_V2.py
+++ mteb/models/Arabic_Triplet_Matryoshka_V2.py
@@ -1,13 +1,13 @@
 from mteb.model_meta import ModelMeta
 
 arabic_triplet_matryoshka = ModelMeta(
-    name="Arabic-Triplet-Matryoshka-V2",
+    name="Omartificial-Intelligence-Space/Arabic-Triplet-Matryoshka-V2",
     languages=["ara-Arab"],  # Arabic in correct format
     open_weights=True,
     revision="ed357f222f0b6ea6670d2c9b5a1cb93950d34200",  # Your actual revision hash
     release_date="2024-07-28",
     n_parameters=135_000_000,  # Update if different
-    memory_usage_mb=0.5,  # Update if different
+    memory_usage_mb = 0.5,
     embed_dim=768,  # Update if different
     license="apache-2.0",  # Update if different
     max_tokens=768,  # Update if different
@@ -16,7 +16,6 @@ arabic_triplet_matryoshka = ModelMeta(
     framework=["Sentence Transformers", "PyTorch"],
     use_instructions=False,
     public_training_code=None,
-    public_training_data=None,  # Update dataset link
-    training_datasets=None,
+    adapted_from="aubmindlab/bert-base-arabertv02",
+    public_training_data="akhooli/arabic-triplets-1m-curated-sims-len",  # Update dataset link
 )
-
