b233801d9d0d282226c5bcd2ab45bc6f7080fe8e
Mina Parham <36207068+mina-parham@users.noreply.github.com>
2025-03-12 20:48:24 -0700

    Update n_samples
    Co-authored-by: Isaac Chung <chungisaac1217@gmail.com>

--- mteb/tasks/Audio/AudioClassification/eng/VocalSound.py
+++ mteb/tasks/Audio/AudioClassification/eng/VocalSound.py
@@ -39,7 +39,7 @@ class VocalSoundClassification(AbsTaskAudioClassification):
             month=may }
                 }""",
         descriptive_stats={
-            "n_samples": {"validation": 1860, "test": 3590},
+            "n_samples": {"validation": 1860, "test": 3594},
         },
     )
 
