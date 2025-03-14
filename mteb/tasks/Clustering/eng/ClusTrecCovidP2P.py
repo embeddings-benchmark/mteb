fa9162fcdba6f53f9508ee6f5310cc1da0a45a29
katzurik <urikacid@gmail.com>
2025-03-14 15:25:03 +0200

    fix

--- mteb/tasks/Clustering/eng/ClusTrecCovidP2P.py
+++ mteb/tasks/Clustering/eng/ClusTrecCovidP2P.py
@@ -18,14 +18,18 @@ class ClusTrecCovidP2PFast(AbsTaskClusteringFast):
         description="A Topical Clustering Benchmark for COVID-19 Scientific Research across 50 covid-19 related topics.",
         reference="https://github.com/katzurik/Knowledge_Navigator/tree/main/Benchmarks/CLUSTREC%20COVID",
         dataset={
-            "path": "Uri-ka/ClusTREC-Covid-P2P",
-            "revision": "f5a9420d2bd39d472ca0d3c268ea88d7e9a10fa4",
+            "path": "Uri-ka/ClusTREC-Covid",
+            "revision": "7f3489153b8dad7336a54f63202deb1414c33309",
         },
         type="Clustering",
         category="p2p",
         modalities=["text"],
         eval_splits=["test"],
-        eval_langs=["eng-Latn"],
+        HFSubset="title and abstract",
+        eval_langs={
+            "title and abstract": ["eng-Latn"],
+            "title": ["eng-Latn"]
+        },
         main_score="v_measure",
         date=("2020-04-10", "2020-07-16"),
         domains=["Academic", "Medical", "Written"],
@@ -52,53 +56,54 @@ class ClusTrecCovidP2PFast(AbsTaskClusteringFast):
         adapted_from=["ClusTrecCovidP2P"],
     )
 
-    def dataset_transform(self):
-        ds = {}
-        for split in self.metadata.eval_splits:
-            labels = list(itertools.chain.from_iterable(self.dataset[split]["labels"]))
-            sentences = list(
-                itertools.chain.from_iterable(self.dataset[split]["sentences"])
-            )
-            check_label_distribution(self.dataset[split])
-            ds[split] = Dataset.from_dict({"labels": labels, "sentences": sentences})
-        self.dataset = DatasetDict(ds)
 
+# class ClusTrecCovidP2P(AbsTaskClustering):
+#     metadata = TaskMetadata(
+#         name="ClusTREC-CovidP2P",
+#         description="A Topical Clustering Benchmark for COVID-19 Scientific Research across 50 covid-19 related topics.",
+#         reference="https://github.com/katzurik/Knowledge_Navigator/tree/main/Benchmarks/CLUSTREC%20COVID",
+#         dataset={
+#             "path": "Uri-ka/ClusTREC-Covid-P2P",
+#             "revision": "f5a9420d2bd39d472ca0d3c268ea88d7e9a10fa4",
+#         },
+#         type="Clustering",
+#         category="p2p",
+#         modalities=["text"],
+#         eval_splits=["test"],
+#         eval_langs={
+#             "title and abstract": ["eng-Latn"],
+#             "title": ["eng-Latn"]
+#         },
+#         main_score="v_measure",
+#         date=("2020-04-10", "2020-07-16"),
+#         domains=["Academic", "Written"],
+#         task_subtypes=["Thematic clustering"],
+#         license="cc-by-sa-4.0",
+#         annotations_creators="expert-annotated",
+#         dialect=[],
+#         sample_creation="created",
+#         bibtex_citation="""@inproceedings{katz-etal-2024-knowledge,
+#     title = "Knowledge Navigator: {LLM}-guided Browsing Framework for Exploratory Search in Scientific Literature",
+#     author = "Katz, Uri  and
+#       Levy, Mosh  and
+#       Goldberg, Yoav",
+#     booktitle = "Findings of the Association for Computational Linguistics: EMNLP 2024",
+#     month = nov,
+#     year = "2024",
+#     address = "Miami, Florida, USA",
+#     publisher = "Association for Computational Linguistics",
+#     url = "https://aclanthology.org/2024.findings-emnlp.516",
+#     pages = "8838--8855",
+# }
+#         """,
+#         prompt="Identify the main category of the covid-19 papers based on the titles and abstracts",
+#     )
 
-class ClusTrecCovidP2P(AbsTaskClustering):
-    metadata = TaskMetadata(
-        name="ClusTREC-CovidP2P",
-        description="A Topical Clustering Benchmark for COVID-19 Scientific Research across 50 covid-19 related topics.",
-        reference="https://github.com/katzurik/Knowledge_Navigator/tree/main/Benchmarks/CLUSTREC%20COVID",
-        dataset={
-            "path": "Uri-ka/ClusTREC-Covid-P2P",
-            "revision": "f5a9420d2bd39d472ca0d3c268ea88d7e9a10fa4",
-        },
-        type="Clustering",
-        category="p2p",
-        modalities=["text"],
-        eval_splits=["test"],
-        eval_langs=["eng-Latn"],
-        main_score="v_measure",
-        date=("2020-04-10", "2020-07-16"),
-        domains=["Academic", "Written"],
-        task_subtypes=["Thematic clustering"],
-        license="cc-by-sa-4.0",
-        annotations_creators="expert-annotated",
-        dialect=[],
-        sample_creation="created",
-        bibtex_citation="""@inproceedings{katz-etal-2024-knowledge,
-    title = "Knowledge Navigator: {LLM}-guided Browsing Framework for Exploratory Search in Scientific Literature",
-    author = "Katz, Uri  and
-      Levy, Mosh  and
-      Goldberg, Yoav",
-    booktitle = "Findings of the Association for Computational Linguistics: EMNLP 2024",
-    month = nov,
-    year = "2024",
-    address = "Miami, Florida, USA",
-    publisher = "Association for Computational Linguistics",
-    url = "https://aclanthology.org/2024.findings-emnlp.516",
-    pages = "8838--8855",
-}
-        """,
-        prompt="Identify the main category of the covid-19 papers based on the titles and abstracts",
-    )
+if __name__ == "__main__":
+    # testing the task with a model:
+    from sentence_transformers import SentenceTransformer
+    from mteb.evaluation.MTEB import MTEB
+
+    model = SentenceTransformer("average_word_embeddings_komninos")
+    evaluation = MTEB(tasks=[ClusTrecCovidP2PFast()])
+    evaluation.run(model)
