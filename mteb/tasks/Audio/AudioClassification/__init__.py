3e6890ca5fb2f3e108ae1c605132887861ed5a57
silky1708 <silky1708@gmail.com>
2025-03-16 11:58:23 -0700

    update Taskmetadata

--- mteb/tasks/Audio/AudioClassification/__init__.py
+++ mteb/tasks/Audio/AudioClassification/__init__.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from .eng.BeijingOpera import *
 from .eng.CREMA_D import *
 from .eng.ESC50 import *
+from .eng.FSDD import *
 from .eng.GTZANGenre import *
 from .eng.GunshotTriangulation import *
 from .eng.LibriCount import *
