5bfac542467d97689034d4554738b1ddfbc538dc
LEE-kanghyeun <kanghyeun7@gmail.com>
2025-03-25 08:25:53 +0900

    edit task_subtypes

--- mteb/tasks/Clustering/kor/KlueMrcDomainClustering.py
+++ mteb/tasks/Clustering/kor/KlueMrcDomainClustering.py
@@ -23,7 +23,7 @@ class KlueMrcDomainClustering(AbsTaskClustering):
         },
         date=("2016-01-01", "2020-12-31"),
         domains=["News", "Written"],
-        task_subtypes=None,
+        task_subtypes=[],
         license="cc-by-sa-4.0",
         annotations_creators="human-annotated",
         dialect=[],
