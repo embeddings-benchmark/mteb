97c19fb4cdd4cb60895170878e64be8a1989b10e
TA <adewole63@gmail.com>
2025-03-24 11:36:02 -0700

    changed sentence transformer loader to mteb default loader and passed instructions s model prompts

--- mteb/models/searchmap_models.py
+++ mteb/models/searchmap_models.py
@@ -1,29 +1,29 @@
 from __future__ import annotations
 from functools import partial
-from mteb import ModelMeta
-from sentence_transformers import SentenceTransformer
-import torch
+from mteb.model_meta import ModelMeta, sentence_transformers_loader
 
 # Define task instructions with specific task names
-def searchmap_loader():
-    model = SentenceTransformer(
-        "VPLabs/SearchMap_Preview", 
-        trust_remote_code=True
-    )
-
-    if hasattr(model, "encoder"):
-        # Configure for inference only
-        model.encoder.eval()  # Ensure eval mode
-        torch.set_grad_enabled(False)  # Disable gradients
-    return model
+task_instructions = {
+    "Classification": "Generate a representation for this text that can be used for classification:",
+    "Clustering": "Generate a representation for this text that can be used for clustering:",
+    "Retrieval": "Generate a representation for this text that can be used for retrieval:",
+    "STS": "Generate a representation for this text that can be used for semantic similarity:",
+    "PairClassification": "Generate a representation for this text pair that can be used for classification:",
+    "Reranking": "Generate a representation for this text that can be used for reranking:",
+    "Summarization": "Generate a representation for this text that can be used for summarization:"
+}
 
 searchmap_preview = ModelMeta(
-    loader=searchmap_loader,
+    loader=partial(  
+        sentence_transformers_loader,
+        model_name="VPLabs/SearchMap_Preview",
+        revision="69de17ef48278ed08ba1a4e65ead8179912b696e",
+        model_prompts=task_instructions,  
+    ),
     name="VPLabs/SearchMap_Preview",
     languages=["eng_Latn"],
     open_weights=True,
     use_instructions=True,
-    revision="69de17ef48278ed08ba1a4e65ead8179912b696e",
     release_date="2025-03-05",
     n_parameters=435_000_000,
     memory_usage_mb=1660,
