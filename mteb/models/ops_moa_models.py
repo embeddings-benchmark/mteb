77236ddb36303d0ebecc5f162e9f9b4950affb5f
kunka.xgw <kunka.xgw@taobao.com>
2025-03-28 23:10:43 +0800

    support SentenceTransformers

--- mteb/models/ops_moa_models.py
+++ mteb/models/ops_moa_models.py
@@ -9,49 +9,12 @@ import torch.nn as nn
 from huggingface_hub import snapshot_download
 
 
-class YuanWrapper(Wrapper):
-    def __init__(self):
+class CustomWrapper(Wrapper):
+    def __init__(self, model_name, revision):
         super().__init__()
         self.model = SentenceTransformer(
-            "IEITYuan/Yuan-embedding-1.0", trust_remote_code=True
+            model_name, revision=revision, trust_remote_code=True
         )
-
-        model_path = snapshot_download(
-            repo_id="OpenSearch-AI/Ops-MoA-Yuan-embedding-1.0",
-            local_dir="./Ops_MoA_Yuan/",
-        )
-        from Ops_MoA_Yuan.modeling_adaptor import MixtureOfAdaptors
-
-        adaptor = MixtureOfAdaptors(5, 1792)
-
-        adaptor.load_state_dict(torch.load(f"Ops_MoA_Yuan/yuan-adaptors.pth"))
-
-        self.model.add_module("adaptor", adaptor)
-        self.output_dim = 1536
-
-    def encode(self, sentences: list[str], **kwargs) -> np.ndarray:
-        embeddings = self.model.encode(sentences, **kwargs)
-        return embeddings[:, : self.output_dim]
-
-
-class ConanWrapper(Wrapper):
-    def __init__(self):
-        super().__init__()
-        self.model = SentenceTransformer(
-            "TencentBAC/Conan-embedding-v1", trust_remote_code=True
-        )
-
-        model_path = snapshot_download(
-            repo_id="OpenSearch-AI/Ops-MoA-Conan-embedding-v1",
-            local_dir="./Ops_MoA_Conan",
-        )
-        from Ops_MoA_Conan.modeling_adaptor import MixtureOfAdaptors
-
-        adaptor = MixtureOfAdaptors(5, 1792)
-
-        adaptor.load_state_dict(torch.load(f"Ops_MoA_Conan/conan-adaptors.pth"))
-
-        self.model.add_module("adaptor", adaptor)
         self.output_dim = 1536
 
     def encode(self, sentences: list[str], **kwargs) -> np.ndarray:
@@ -61,11 +24,13 @@ class ConanWrapper(Wrapper):
 
 ops_moa_conan_embedding = ModelMeta(
     name="OpenSearch-AI/Ops-MoA-Conan-embedding-v1",
-    revision="cd42de6d61c103047b7bcd780ef0dbaa9a9d0472",
+    revision="46dcd58753f3daa920c66f89e47086a534089350",
     release_date="2025-03-26",
     languages=["zho_Hans"],
     loader=partial(
-        ConanWrapper,
+        CustomWrapper,
+        "OpenSearch-AI/Ops-MoA-Conan-embedding-v1",
+        "46dcd58753f3daa920c66f89e47086a534089350",
     ),
     n_parameters=343 * 1e6,
     memory_usage_mb=2e3,
@@ -85,11 +50,13 @@ ops_moa_conan_embedding = ModelMeta(
 
 ops_moa_yuan_embedding = ModelMeta(
     name="OpenSearch-AI/Ops-MoA-Yuan-embedding-1.0",
-    revision="09b8857bbd74f189d9bc45bf59adaf34f9378e17",
+    revision="23712d0766417b0eb88a2513c6e212a58b543268",
     release_date="2025-03-26",
     languages=["zho_Hans"],
     loader=partial(
-        YuanWrapper,
+        CustomWrapper,
+        "OpenSearch-AI/Ops-MoA-Yuan-embedding-1.0",
+        "23712d0766417b0eb88a2513c6e212a58b543268",
     ),
     n_parameters=343 * 1e6,
     memory_usage_mb=2e3,
