50d7e9e139729d0373030884255bc78be4ed17b0
Munot Ayush Sunil <munotayush6@kgpian.iitkgp.ac.in>
2025-04-16 22:55:48 +0530

    fix unintentional working of filters on leaderboard (#2535)
    * fix unintentional working of filters on leaderboard
    
    * address comments
    
    * make lint
    
    * address comments
    
    * rollback unnecessary changes

--- mteb/leaderboard/app.py
+++ mteb/leaderboard/app.py
@@ -579,11 +579,17 @@ def get_leaderboard_app() -> gr.Blocks:
             for task in mteb.get_benchmark(benchmark_name).tasks:
                 if task.metadata.type not in type_select:
                     continue
-                if not (set(task.metadata.domains or []) & set(domain_select)):
+                if task.metadata.domains is not None and not (
+                    set(task.metadata.domains) & set(domain_select)
+                ):
                     continue
-                if not (set(task.languages or []) & set(lang_select)):
+                if task.languages is not None and not (
+                    set(task.languages) & set(lang_select)
+                ):
                     continue
-                if not (set(task.metadata.modalities or []) & set(modality_select)):
+                if task.metadata.modalities and not (
+                    set(task.metadata.modalities) & set(modality_select)
+                ):
                     continue
                 tasks_to_keep.append(task.metadata.name)
             elapsed = time.time() - start_time
