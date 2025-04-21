efe326dd6c40ef9a0c438d3072e8787f3800ea05
Shuu <136542198+Shun0212@users.noreply.github.com>
2025-04-22 04:22:31 +0900

    Add files via upload

---
+++ mteb/models/shuu_model.py
@@ -0,0 +1,34 @@
+from functools import partial
+from mteb.model_meta import ModelMeta
+from mteb.models.sentence_transformers_wrapper import SentenceTransformerWrapper
+
+codemodernbert_crow_meta = ModelMeta(
+    name="Shuu12121/CodeSearch-ModernBERT-Crow-Plus",
+    languages=["eng-Latn"],
+    loader=partial(
+        SentenceTransformerWrapper,
+        model="Shuu12121/CodeSearch-ModernBERT-Crow-Plus",
+        revision="fd3d662ca9ba8d0e6be981cb3be21aee7a80096e",
+    ),
+    open_weights=True,
+    release_date="2025-04-21",
+    n_parameters=151668480,
+    memory_usage_mb=None,
+    embed_dim=768,
+    license="apache-2.0",
+    max_tokens=1028,
+    reference="https://huggingface.co/Shuu12121/CodeSearch-ModernBERT-Crow-Plus",
+    similarity_fn_name="cosine",
+    framework=["Sentence Transformers", "PyTorch"],
+    use_instructions=False,
+    public_training_code=None,
+    public_training_data=None,
+    training_datasets={
+        "code-search-net/code_search_net": ["train"],
+        "Shuu12121/python-codesearch-filtered": ["train"],
+        "Shuu12121/ruby-codesearch-filtered": ["train"],
+        "Shuu12121/java-codesearch-filtered": ["train"],
+        "Shuu12121/javascript-codesearch-filtered": ["train"],
+        "Shuu12121/rust-codesearch-filtered": ["train"],
+    },
+)
