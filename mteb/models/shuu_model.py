366b227f374715a7e8c7436ca06e962e7429d309
Shuu <136542198+Shun0212@users.noreply.github.com>
2025-04-22 11:44:42 +0900

    Update shuu_model.py

--- mteb/models/shuu_model.py
+++ mteb/models/shuu_model.py
@@ -3,21 +3,21 @@ from functools import partial
 from mteb.model_meta import ModelMeta, sentence_transformers_loader
 
 codemodernbert_crow_meta = ModelMeta(
-    name="Shuu12121/CodeSearch-ModernBERT-Crow-Plus",
-    revision="fd3d662ca9ba8d0e6be981cb3be21aee7a80096e",
-    languages=["eng-Latn"],
     loader=partial(
         sentence_transformers_loader,
         model="Shuu12121/CodeSearch-ModernBERT-Crow-Plus",
         revision="fd3d662ca9ba8d0e6be981cb3be21aee7a80096e",
     ),
+    name="Shuu12121/CodeSearch-ModernBERT-Crow-Plus",
+    languages=["eng-Latn"],
     open_weights=True,
+    revision="fd3d662ca9ba8d0e6be981cb3be21aee7a80096e",
     release_date="2025-04-21",
     n_parameters=151668480,
     memory_usage_mb=None,
     embed_dim=768,
     license="apache-2.0",
-    max_tokens=1028,
+    max_tokens=1024,
     reference="https://huggingface.co/Shuu12121/CodeSearch-ModernBERT-Crow-Plus",
     similarity_fn_name="cosine",
     framework=["Sentence Transformers", "PyTorch"],
