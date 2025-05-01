d8a9d9c3a2324416c5c926b96efd883eb8eb78f7
namespace-Pt <namespace.pt@gmail.com>
2025-05-01 21:49:18 +0800

    update link

--- mteb/models/seed_models.py
+++ mteb/models/seed_models.py
@@ -157,7 +157,7 @@ seed_embedding = ModelMeta(
     n_parameters=None,
     memory_usage_mb=None,
     license=None,
-    reference="https://console.volcengine.com/ark/region:ark+cn-beijing/model/detail?Id=doubao-1-5-embedding",
+    reference="https://huggingface.co/ByteDance-Seed/Doubao-1.5-Embedding",
     similarity_fn_name="cosine",
     framework=["API"],
     use_instructions=True,
