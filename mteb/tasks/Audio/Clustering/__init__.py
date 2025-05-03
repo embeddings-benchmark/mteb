4f077e2f47f68d62b488a98bd81ffd5e2021dd70
Adnan El Assadi <aassadi22@ku.edu.tr>
2025-05-03 17:46:39 -0400

    Added missing inits

--- mteb/tasks/Audio/Clustering/__init__.py
+++ mteb/tasks/Audio/Clustering/__init__.py
@@ -3,3 +3,5 @@ from __future__ import annotations
 from .eng.MusicGenre import *
 from .eng.VehicleSoundClustering import *
 from .eng.VoiceGender import *
+from .eng.VoxPopuliAccentClustering import *
+from .eng.VoxPopuliGenderClustering import *
