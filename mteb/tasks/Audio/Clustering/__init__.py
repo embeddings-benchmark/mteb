1dcd1f0a82e3fa1f063e0d76405e0c27fa92a1c0
Adnan El Assadi <aassadi22@ku.edu.tr>
2025-05-03 19:22:50 -0400

    ran lint

--- mteb/tasks/Audio/Clustering/__init__.py
+++ mteb/tasks/Audio/Clustering/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
+from .eng.GTZANGenreClustering import *
 from .eng.MusicGenre import *
 from .eng.VehicleSoundClustering import *
 from .eng.VoiceGender import *
-from .eng.GTZANGenreClustering import *
