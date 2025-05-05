c5c0fa33a30be66e5ec9ce272f40f5db9a9e027e
Adnan El Assadi <aassadi22@ku.edu.tr>
2025-05-05 12:07:57 -0400

    Bibtex + TaskMetadata + Descritption

--- mteb/tasks/Audio/Clustering/eng/ESC50Clustering.py
+++ mteb/tasks/Audio/Clustering/eng/ESC50Clustering.py
@@ -8,7 +8,13 @@ class ESC50Clustering(AbsTaskAudioClustering):
     label_column_name: str = "target"
     metadata = TaskMetadata(
         name="ESC50Clustering",
-        description="Clustering task based on the Environmental Sound Classification Dataset with 50 classes.",
+        description=(
+            "The ESC-50 dataset contains 2,000 labeled environmental audio recordings "
+            "evenly distributed across 50 classes (40 clips per class). "
+            "These classes are organized into 5 broad categories: animal sounds, natural soundscapes & water sounds, "
+            "human (non-speech) sounds, interior/domestic sounds, and exterior/urban noises. "
+            "This task evaluates unsupervised clustering performance on environmental audio recordings."
+        ),
         reference="https://huggingface.co/datasets/ashraq/esc50",
         dataset={
             "path": "ashraq/esc50",
@@ -27,16 +33,18 @@ class ESC50Clustering(AbsTaskAudioClustering):
         dialect=[],
         modalities=["audio"],
         sample_creation="found",
-        bibtex_citation="""@inproceedings{piczak2015dataset,
-            title = {{ESC}: {Dataset} for {Environmental Sound Classification}},
-            author = {Piczak, Karol J.},
-            booktitle = {Proceedings of the 23rd {Annual ACM Conference} on {Multimedia}},
-            date = {2015-10-13},
-            url = {http://dl.acm.org/citation.cfm?doid=2733373.2806390},
-            doi = {10.1145/2733373.2806390},
-            location = {{Brisbane, Australia}},
-            isbn = {978-1-4503-3459-4},
-            publisher = {{ACM Press}},
-            pages = {1015--1018}
-        }""",
+        bibtex_citation=r"""
+@inproceedings{piczak2015dataset,
+  author = {Piczak, Karol J.},
+  booktitle = {Proceedings of the 23rd {Annual ACM Conference} on {Multimedia}},
+  date = {2015-10-13},
+  doi = {10.1145/2733373.2806390},
+  isbn = {978-1-4503-3459-4},
+  location = {{Brisbane, Australia}},
+  pages = {1015--1018},
+  publisher = {{ACM Press}},
+  title = {{ESC}: {Dataset} for {Environmental Sound Classification}},
+  url = {http://dl.acm.org/citation.cfm?doid=2733373.2806390},
+}
+""",
     )
