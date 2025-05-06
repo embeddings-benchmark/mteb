5b65837dada8b119245314a7608f0668d95cdb68
Ali Sartaz Khan <alisartazkhan@gmail.com>
2025-05-05 23:14:00 -0700

    added qwen2 model

--- run.py
+++
@@ -1,9 +0,0 @@
-import mteb
-model_name = "Qwen/Qwen2-Audio-7B"
-model = mteb.get_model(model_name=model_name)
-print("model loaded")
-tasks = mteb.get_tasks(tasks=["BeijingOpera"])
-print("task loaded")
-evaluation = mteb.MTEB(tasks=tasks)
-results = evaluation.run(model)
-print("eval complete")
