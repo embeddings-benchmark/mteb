4c5382386cf2c5906a2729d91f737c7c0d9e3690
AdnanElAssadi56 <115242814+AdnanElAssadi56@users.noreply.github.com>
2025-05-06 15:52:23 -0400

    Added SpeechCommands Dataset (Subset) (#2645)
    * Added SpeechCommands Dataset
    
    * Bibtex + Feedback
    
    * Description
    
    * TaskMetadata "Remove"

--- mteb/abstasks/TaskMetadata.py
+++ mteb/abstasks/TaskMetadata.py
@@ -21,6 +21,7 @@ from ..languages import (
 
 TASK_SUBTYPE = Literal[
     "Article retrieval",
+    "Accent identification",
     "Conversational retrieval",
     "Dialect pairing",
     "Dialog Systems",
@@ -61,6 +62,7 @@ TASK_SUBTYPE = Literal[
     "Stroke Classification of Musical Instrument",
     "Tonic Classification of Musical Instrument",
     "Speaker Count Identification",
+    "Species Classification",
     "Spoken Digit Classification",
     "Gender Clustering",
     "Music Clustering",
@@ -68,6 +70,7 @@ TASK_SUBTYPE = Literal[
     "Sentiment Analysis",
     "Intent Classification",
     "Vehicle Clustering",
+    "Environment Sound Clustering",
     "Rendered semantic textual similarity",
     "Gender Classification",
     "Age Classification",
@@ -75,6 +78,7 @@ TASK_SUBTYPE = Literal[
 
 TASK_DOMAIN = Literal[
     "Academic",
+    "Bioacoustics",
     "Blog",
     "Constructed",
     "Encyclopaedic",
