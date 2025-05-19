86fc7bc27509d36f5112e43fe89bcefe701266bd
Feiyang <feiyangc@google.com>
2025-05-19 22:12:48 +0000

    Rename gemini-embedding-exp-03-07 to gemini-embedding-001

--- mteb/models/google_models.py
+++ mteb/models/google_models.py
@@ -215,13 +215,13 @@ google_text_multilingual_emb_002 = ModelMeta(
     training_datasets=GECKO_TRAINING_DATA,
 )
 
-google_gemini_embedding_exp_03_07 = ModelMeta(
+google_gemini_embedding_001 = ModelMeta(
     loader=partial(
         GoogleTextEmbeddingModel,
-        model_name="gemini-embedding-exp-03-07",
+        model_name="gemini-embedding-001",
         model_prompts=MODEL_PROMPTS,
     ),
-    name="google/gemini-embedding-exp-03-07",
+    name="google/gemini-embedding-001",
     languages=MULTILINGUAL_EVALUATED_LANGUAGES,
     open_weights=False,
     revision="1",
