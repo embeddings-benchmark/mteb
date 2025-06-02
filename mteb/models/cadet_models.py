bf73ae4a23d3e3d95762c7b49b9866e1ba0f426e
manveertamber <manveertamber@gmail.com>
2025-06-02 13:09:13 -0400

    update

--- mteb/models/cadet_models.py
+++ mteb/models/cadet_models.py
@@ -1,19 +1,20 @@
 from __future__ import annotations
 
-from mteb.encoder_interface import PromptType
-from mteb.model_meta import ModelMeta, sentence_transformers_loader
 from functools import partial
+
+from mteb.model_meta import ModelMeta, sentence_transformers_loader
 from mteb.models.bge_models import bge_m3_training_data
 
 cadet_training_data = {
-    # we train with the corpora of FEVER, MSMARCO, and DBPEDIA. We only train with synthetic generated queries. However, we do use queries from MSMARCO as examples for synthetic query generation.
+    # we train with the corpora of FEVER, MSMARCO, and DBPEDIA. We only train with synthetic generated queries.
+    # However, we do use queries from MSMARCO as examples for synthetic query generation.
     "MSMARCO": ["train"],
     "DBPedia": [],
-    
-    # We distill from RankT5 which trains using MSMARCO + NQ. source: https://arxiv.org/pdf/2210.10634
+    # We distill from RankT5 which trains using MSMARCO + NQ.
+    # source: https://arxiv.org/pdf/2210.10634
     "NQ": ["train"],
-
-    # We also distill from bge-rerankerv2.5-gemma2-lightweight, which utilizes the BGE-M3 dataset, along with Arguana, HotpotQA, and FEVER for training. source: https://arxiv.org/pdf/2409.15700
+    # We also distill from bge-rerankerv2.5-gemma2-lightweight, which utilizes the BGE-M3 dataset for training, along with Arguana, HotpotQA, and FEVER.
+    # source: https://arxiv.org/pdf/2409.15700
     "FEVER": ["train"],
     "HotpotQA": ["train"],
     "ArguAna": ["train"],
@@ -23,8 +24,7 @@ for k, v in bge_m3_training_data.items():
     cadet_training_data.setdefault(k, []).extend(v)
 # deduplicate
 cadet_training_data = {
-    k: list(dict.fromkeys(v))
-    for k, v in cadet_training_data.items()
+    k: list(dict.fromkeys(v)) for k, v in cadet_training_data.items()
 }
 
 
@@ -34,8 +34,8 @@ cadet_embed = ModelMeta(
         model_name="manveertamber/cadet-embed-base-v1",
         revision="8056d118be37a566f20972a5f35cda815f6bc47e",
         model_prompts={
-           "query": "query: ",
-           "passage": "passage: ",
+            "query": "query: ",
+            "passage": "passage: ",
         },
     ),
     name="manveertamber/cadet-embed-base-v1",
@@ -53,7 +53,8 @@ cadet_embed = ModelMeta(
     framework=["Sentence Transformers", "PyTorch"],
     use_instructions=True,
     public_training_code="https://github.com/manveertamber/cadet-dense-retrieval",
-    public_training_data="https://github.com/manveertamber/cadet-dense-retrieval", # we provide the code to generate the training data
+    # we provide the code to generate the training data
+    public_training_data="https://github.com/manveertamber/cadet-dense-retrieval",
     training_datasets=cadet_training_data,
     adapted_from="intfloat/e5-base-unsupervised",
 )
