69d67e41d182817e493b91b1c470b96d384d282f
AdnanElAssadi56 <115242814+AdnanElAssadi56@users.noreply.github.com>
2025-06-03 06:38:59 -0400

    Added Crema_d Dataset (#2651)
    * Added Crema_d Dataset
    
    * Bibtex + Metadata
    
    ---------
    
    Co-authored-by: Isaac Chung <chungisaac1217@gmail.com>

--- mteb/abstasks/TaskMetadata.py
+++ mteb/abstasks/TaskMetadata.py
@@ -66,6 +66,7 @@ TASK_SUBTYPE = Literal[
     "Spoken Digit Classification",
     "Gender Clustering",
     "Music Clustering",
+    "Emotion Clustering",
     "Rendered semantic textual similarity",
     "Sentiment Analysis",
     "Intent Classification",
