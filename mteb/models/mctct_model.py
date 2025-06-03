33b5f3da9b88510f27d43949b09ee50dc680e7ab
Adnan El Assadi <aassadi22@ku.edu.tr>
2025-06-02 22:27:28 -0400

    lang

--- mteb/models/mctct_model.py
+++ mteb/models/mctct_model.py
@@ -22,8 +22,8 @@ COMMON_VOICE_LANGUAGES = [
     "eus-Latn",  # Basque
     "bre-Latn",  # Breton
     "cat-Latn",  # Catalan
-    "zho-Hans-CN",  # Chinese (China)
-    "zho-Hant-HK",  # Chinese (Hong Kong) + (Taiwan)
+    "zho-Hans",  # Chinese (China)
+    "zho-Hant",  # Chinese (Hong Kong) + (Taiwan)
     "chv-Cyrl",  # Chuvash
     "ces-Latn",  # Czech
     "div-Thaa",  # Dhivehi
