44d30a9287f9f0edd7db670700e0265f3d9a8377
Adnan El Assadi <aassadi22@ku.edu.tr>
2025-06-02 20:24:42 -0400

    Languages Added + Feedback

--- mteb/models/mctct_model.py
+++ mteb/models/mctct_model.py
@@ -15,6 +15,69 @@ from mteb.encoder_interface import AudioBatch, AudioData, PromptType
 from mteb.model_meta import ModelMeta
 from mteb.models.wrapper import Wrapper
 
+COMMON_VOICE_LANGUAGES = [
+    "ab-Cyrl",  # Abkhaz
+    "ar-Arab",  # Arabic
+    "as-Beng",  # Assamese
+    "eu-Latn",  # Basque
+    "br-Latn",  # Breton
+    "ca-Latn",  # Catalan
+    "zh-Hans-CN",  # Chinese (China)
+    "zh-Hant-HK",  # Chinese (Hong Kong)
+    "zh-Hant-TW",  # Chinese (Taiwan)
+    "cv-Cyrl",  # Chuvash
+    "cs-Latn",  # Czech
+    "dv-Thaa",  # Dhivehi
+    "nl-Latn",  # Dutch
+    "en-Latn",  # English
+    "eo-Latn",  # Esperanto
+    "et-Latn",  # Estonian
+    "fi-Latn",  # Finnish
+    "fr-Latn",  # French
+    "fy-Latn",  # Frisian
+    "ka-Geor",  # Georgian
+    "de-Latn",  # German
+    "el-Grek",  # Greek
+    "cfm-Latn",  # Hakha Chin
+    "hi-Deva",  # Hindi
+    "hu-Latn",  # Hungarian
+    "id-Latn",  # Indonesian
+    "ia-Latn",  # Interlingua
+    "ga-Latn",  # Irish
+    "it-Latn",  # Italian
+    "ja-Jpan",  # Japanese
+    "kab-Latn",  # Kabyle
+    "rw-Latn",  # Kinyarwanda
+    "ky-Cyrl",  # Kyrgyz
+    "lv-Latn",  # Latvian
+    "lt-Latn",  # Lithuanian
+    "lg-Latn",  # Luganda
+    "mt-Latn",  # Maltese
+    "mn-Cyrl",  # Mongolian
+    "or-Orya",  # Odia
+    "fa-Arab",  # Persian
+    "pl-Latn",  # Polish
+    "pt-Latn",  # Portuguese
+    "pa-Guru",  # Punjabi
+    "ro-Latn",  # Romanian
+    "rm-Surs",  # Romansh Sursilvan
+    "rm-Vall",  # Romansh Vallader
+    "ru-Cyrl",  # Russian
+    "sah-Cyrl",  # Sakha
+    "sl-Latn",  # Slovenian
+    "hsb-Latn",  # Upper Sorbian
+    "es-Latn",  # Spanish
+    "sv-Latn",  # Swedish
+    "ta-Taml",  # Tamil
+    "tt-Cyrl",  # Tatar
+    "th-Thai",  # Thai
+    "tr-Latn",  # Turkish
+    "uk-Cyrl",  # Ukrainian
+    "vi-Latn",  # Vietnamese
+    "vot-Latn",  # Votic
+    "cy-Latn",  # Welsh
+]
+
 
 class MCTCTWrapper(Wrapper):
     def __init__(
@@ -155,14 +218,14 @@ mctct_large = ModelMeta(
         model_name="speechbrain/m-ctc-t-large",
     ),
     name="speechbrain/m-ctc-t-large",
-    languages=["eng-Latn"],  # Supports 60 languages found in reference
+    languages=COMMON_VOICE_LANGUAGES,  # Supports 60 languages
     open_weights=True,
     revision="ed014c8255cea2c36f87a71cf2533b665ba00863",
     release_date="2022-01-10",
     max_tokens=None,
     n_parameters=1_058_978_691,
     memory_usage_mb=4039,
-    embed_dim=1536,  # Hidden dimension
+    embed_dim=1536,
     license="apache-2.0",
     reference="https://huggingface.co/speechbrain/m-ctc-t-large",
     similarity_fn_name="cosine",
@@ -170,6 +233,6 @@ mctct_large = ModelMeta(
     use_instructions=False,
     public_training_code="https://github.com/speechbrain/speechbrain",
     public_training_data="https://github.com/speechbrain/speechbrain",
-    training_datasets={},  # {"Common Voice": ["train"], "VoxPopuli": ["train"]},
+    training_datasets={"Common Voice": ["train"], "VoxPopuli": ["train"]},
     modalities=["audio"],
 )
