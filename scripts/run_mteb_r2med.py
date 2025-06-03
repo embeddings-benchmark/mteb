559c68247f744db4fd7e7468d840d96e28f61e23
Li Lei <34205771+ll0ruc@users.noreply.github.com>
2025-06-03 18:10:08 +0800

    Update run_mteb_r2med.py

--- scripts/run_mteb_r2med.py
+++ scripts/run_mteb_r2med.py
@@ -1,11 +1,1 @@
-from __future__ import annotations
 
-from sentence_transformers import SentenceTransformer
-
-from mteb import MTEB
-from mteb.tasks.Retrieval.eng.R2MEDRetrieval import R2MEDRetrieval
-
-# testing the task with a model:
-model = SentenceTransformer("sentence-transformers/all-mpnet-base-v2")
-evaluation = MTEB(tasks=[R2MEDRetrieval()])
-evaluation.run(model, output_folder="results")
