c5d8484236a7e4c9ffe2f6e25c898185979ced26
AdnanElAssadi56 <115242814+AdnanElAssadi56@users.noreply.github.com>
2025-06-03 03:49:47 -0400

    Added GTZAN Clustering Dataset (#2653)
    * Added GTZAN Clustering Dataset
    
    * ran lint
    
    * Bibtex + Metadata

--- mteb/tasks/Audio/Clustering/__init__.py
+++ mteb/tasks/Audio/Clustering/__init__.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from .eng.GTZANGenreClustering import *
 from .eng.ESC50Clustering import *
 from .eng.MusicGenre import *
 from .eng.VehicleSoundClustering import *
