58d1bcec8fb47f2049622cf6daeab5b1bdc6ffea
fangxq6640 <fangxq6640@hnu.edu.cn>
2025-06-05 15:44:53 +0800

    update

--- mteb/models/xyz_models.py
+++ mteb/models/xyz_models.py
@@ -20,5 +20,18 @@ xyz_embedding = ModelMeta(
     use_instructions=False,
     public_training_code=None,
     public_training_data=None,
-    training_datasets=None, 
-)
+    training_datasets={
+    ##
+    "BQ": ["train"],
+    "LCQMC": ["train"],
+    "PAWSX": ["train"],
+    "STS-B": ["train"],
+    "DuRetrieval": ["train"],
+    "AFQMC": ["train"],
+    "Cmnli": ["train"],
+    "Ocnli": ["train"],
+    "T2Retrieval": ["train"],
+    "T2Reranking": ["train"],
+    "MMarcoReranking": ["train"],
+    "CMedQAv2-reranking": ["train"],
+    "Multi-CPR":"http://github.com/Alibaba-NLP/Multi-CPR"},)
