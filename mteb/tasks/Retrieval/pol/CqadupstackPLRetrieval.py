b5d31c1e44313e1d3ba7ab212f2b95197c568974
Kenneth Enevoldsen <kennethcenevoldsen@gmail.com>
2025-06-09 11:43:38 +0200

    prevent import of TaskMetadata from mteb.abstasks

--- mteb/tasks/Retrieval/pol/CqadupstackPLRetrieval.py
+++ mteb/tasks/Retrieval/pol/CqadupstackPLRetrieval.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
-from mteb.abstasks import AbsTaskRetrieval, TaskMetadata
+from mteb.abstasks import AbsTaskRetrieval
+from mteb.abstasks.TaskMetadata import TaskMetadata
 
 
 class CQADupstackWordpressRetrievalPL(AbsTaskRetrieval):
