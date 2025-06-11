6de9a63db5949a9baa2c883a893907cc2a19933f
Kenneth Enevoldsen <kennethcenevoldsen@gmail.com>
2025-06-11 10:07:46 +0200

    fix import issue in tests

--- tests/test_load_results/test_task_results.py
+++ tests/test_load_results/test_task_results.py
@@ -5,7 +5,8 @@ from pathlib import Path
 
 import pytest
 
-from mteb.abstasks import AbsTask, TaskMetadata
+from mteb.abstasks import AbsTask
+from mteb.abstasks.TaskMetadata import TaskMetadata
 from mteb.load_results.task_results import TaskResult
 
 tests_folder = Path(__file__).parent.parent
