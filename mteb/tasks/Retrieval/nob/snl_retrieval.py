31dc6a040c6cc5fcee413a8637c635af8563f35e
Kenneth Enevoldsen <kennethcenevoldsen@gmail.com>
2025-06-15 20:09:24 +0200

    format

--- mteb/tasks/Retrieval/nob/snl_retrieval.py
+++ mteb/tasks/Retrieval/nob/snl_retrieval.py
@@ -10,7 +10,7 @@ class SNLRetrieval(AbsTaskRetrieval):
     metadata = TaskMetadata(
         name="SNLRetrieval",
         dataset={
-            "path": "adrlau/navjordj-SNL_summarization_copy", # TODO: replace with mteb/SNLRetrieval after #2820 is resolved.
+            "path": "adrlau/navjordj-SNL_summarization_copy",  # TODO: replace with mteb/SNLRetrieval after #2820 is resolved.
             "revision": "3d3d27aa7af8941408cefc3991ada5d12a4273d1",
         },
         description="Webscrabed articles and ingresses from the Norwegian lexicon 'Det Store Norske Leksikon'.",
