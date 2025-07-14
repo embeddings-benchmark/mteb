43a8ceb93288f31f046d2f88a07b5d1655c68c3f
Alexey Vatolin <vatolinalex@gmail.com>
2025-07-14 11:59:01 +0000

    fix lint

--- mteb/tasks/Classification/rus/RuSciBenchGRNTIClassification.py
+++ mteb/tasks/Classification/rus/RuSciBenchGRNTIClassification.py
@@ -5,7 +5,7 @@ from mteb.abstasks.TaskMetadata import TaskMetadata
 
 
 class RuSciBenchGRNTIClassification(AbsTaskClassification):
-    superseded_by="RuSciBenchGRNTIOrigClassification"
+    superseded_by = "RuSciBenchGRNTIOrigClassification"
 
     metadata = TaskMetadata(
         name="RuSciBenchGRNTIClassification",
--- mteb/tasks/Classification/rus/RuSciBenchOECDClassification.py
+++ mteb/tasks/Classification/rus/RuSciBenchOECDClassification.py
@@ -5,7 +5,7 @@ from mteb.abstasks.TaskMetadata import TaskMetadata
 
 
 class RuSciBenchOECDClassification(AbsTaskClassification):
-    superseded_by="RuSciBenchOECDOrigClassification"
+    superseded_by = "RuSciBenchOECDOrigClassification"
 
     metadata = TaskMetadata(
         name="RuSciBenchOECDClassification",
