0dab822dc456cb33d24d889e9803e80ff9856e31
abdurrahmanbutler <abdurrahmanbutler@gmail.com>
2025-07-21 13:00:23 +1000

    updated details

--- mteb/tasks/Retrieval/eng/BarExamQA.py
+++ mteb/tasks/Retrieval/eng/BarExamQA.py
@@ -1,5 +1,9 @@
 from __future__ import annotations
 
+from sentence_transformers import SentenceTransformer
+
+from mteb import MTEB
+from mteb.abstasks.AbsTaskRetrieval import AbsTaskRetrieval
 from mteb.abstasks.TaskMetadata import TaskMetadata
 
 from ....abstasks.AbsTaskRetrieval import AbsTaskRetrieval
@@ -7,31 +11,42 @@ from ....abstasks.AbsTaskRetrieval import AbsTaskRetrieval
 
 class BarExamQA(AbsTaskRetrieval):
     metadata = TaskMetadata(
+        dataset={
+            "path": "isaacus/mteb-barexam-qa",
+            "revision": "49c07b6",
+        },
         name="BarExamQA",
-        description="The dataset includes questions from multistate bar exams and answers sourced from expert annotations.",
-        reference="https://reglab.github.io/legal-rag-benchmarks/",
+        description="A benchmark for retrieving legal provisions that answer US bar exam questions.",
+        reference="https://huggingface.co/datasets/reglab/barexam_qa",
         type="Retrieval",
         category="t2t",
         modalities=["text"],
         eval_splits=["test"],
         eval_langs=["eng-Latn"],
         main_score="ndcg_at_10",
-        dataset={
-            "path": "isaacus/mteb-barexam-qa",
-            "revision": "4246981",
-        },
         date=("2024-08-14", "2025-07-18"),
         domains=["Legal", "Academic"],
-        task_subtypes=["Question answering"],
+        task_subtypes=["Text Retrieval", "Question Answering"],
         license="cc-by-sa-4.0",
-        annotations_creators="derived",
+        annotations_creators="expert-annotated",
         dialect=[],
         sample_creation="found",
-        bibtex_citation="""
-@inproceedings{zheng2025,
-  author = {Zheng, Lucia and Guha, Neel and Arifov, Javokhir and Zhang, Sarah and Skreta, Michal and Manning, Christopher D. and Henderson, Peter and Ho, Daniel E.},
-  title = {A Reasoning-Focused Legal Retrieval Benchmark},
-  year = {2025},
-  series = {CSLAW '25 (forthcoming)}
-""",
+        bibtex_citation="""\
+@inproceedings{Zheng_2025, series={CSLAW ’25},
+   title={A Reasoning-Focused Legal Retrieval Benchmark},
+   url={http://dx.doi.org/10.1145/3709025.3712219},
+   DOI={10.1145/3709025.3712219},
+   booktitle={Proceedings of the Symposium on Computer Science and Law on ZZZ},
+   publisher={ACM},
+   author={Zheng, Lucia and Guha, Neel and Arifov, Javokhir and Zhang, Sarah and Skreta, Michal and Manning, Christopher D. and Henderson, Peter and Ho, Daniel E.},
+   year={2025},
+   month=mar, pages={169–193},
+   collection={CSLAW ’25},
+   eprint={2505.03970}
+}""",
     )
+
+
+model = SentenceTransformer("intfloat/multilingual-e5-small")
+evaluation = MTEB(tasks=[BarExamQA()])
+evaluation.run(model)
