698500d141604c6b52544314dc35f5ab775de8a1
AdnanElAssadi56 <115242814+AdnanElAssadi56@users.noreply.github.com>
2025-07-21 11:41:27 -0400

    Audio Retrieval Dataset: MACS (#2921)
    * Added MACS Retrieval Dataset
    
    * license link
    
    * update task subtype
    
    ---------
    
    Co-authored-by: Isaac Chung <chungisaac1217@gmail.com>

--- mteb/tasks/Audio/Any2AnyRetrieval/__init__.py
+++ mteb/tasks/Audio/Any2AnyRetrieval/__init__.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from .AudioCaps import *
 from .Clotho import *
+from .MACS import *
 from .multilingual.JamAlt import *
 from .SoundDescs import *
 from .UrbanSound8kRetrieval import *
