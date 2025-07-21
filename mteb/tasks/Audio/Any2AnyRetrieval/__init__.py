7a4be45f0e56cc6e7fcdf3c8da5db3fb40409ed7
AdnanElAssadi56 <115242814+AdnanElAssadi56@users.noreply.github.com>
2025-07-21 13:03:23 -0400

    Audio Retrieval Dataset: HiFiTTS (#2924)
    * Added HiFiTTS Retrieval Dataset
    
    * remove dialect
    
    * clean up metadata
    
    ---------
    
    Co-authored-by: Isaac Chung <chungisaac1217@gmail.com>

--- mteb/tasks/Audio/Any2AnyRetrieval/__init__.py
+++ mteb/tasks/Audio/Any2AnyRetrieval/__init__.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from .AudioCaps import *
 from .Clotho import *
 from .EmoVDB import *
+from .HiFiTTS import *
 from .MACS import *
 from .multilingual.JamAlt import *
 from .SoundDescs import *
