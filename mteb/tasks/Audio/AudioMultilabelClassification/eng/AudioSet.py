e28712b624ce151c432213ba4bf4fcd0a8aba5eb
Isaac Chung <chungisaac1217@gmail.com>
2025-07-27 23:58:48 +0300

    add audioset mini

--- mteb/tasks/Audio/AudioMultilabelClassification/eng/AudioSet.py
+++ mteb/tasks/Audio/AudioMultilabelClassification/eng/AudioSet.py
@@ -7,6 +7,7 @@ from mteb.abstasks.TaskMetadata import TaskMetadata
 
 
 class AudioSetMultilingualClassification(AbsTaskAudioMultilabelClassification):
+    superseded_by = "AudioSetMini"
     metadata = TaskMetadata(
         name="AudioSet",
         description="AudioSet consists of an expanding ontology of 632 audio event classes and a collection of 2,084,320 human-labeled 10-second sound clips drawn from YouTube videos.",
@@ -48,4 +49,50 @@ class AudioSetMultilingualClassification(AbsTaskAudioMultilabelClassification):
     )
 
     audio_column_name: str = "audio"
-    label_column_name: str = "labels"
+    label_column_name: str = "human_labels"
+
+
+# Sampled using scripts/data/audioset/create_data.ipynb
+class AudioSetMiniMultilingualClassification(AbsTaskAudioMultilabelClassification):
+    metadata = TaskMetadata(
+        name="AudioSetMini",
+        description="AudioSet consists of an expanding ontology of 632 audio event classes and a collection of 2,084,320 human-labeled 10-second sound clips drawn from YouTube videos. This is a mini version that is sampled from the original dataset.",
+        reference="https://huggingface.co/datasets/agkphysics/AudioSet",
+        dataset={
+            "path": "mteb/audioset",
+            "revision": "168a7e681ee40609129535d49855c7e3e77e5efa",
+        },
+        type="AudioMultilabelClassification",
+        category="a2t",
+        eval_splits=["test"],
+        eval_langs=["eng-Latn"],
+        main_score="accuracy",
+        date=(
+            "2016-01-01",
+            "2017-01-30",
+        ),
+        domains=["Web", "Music", "Speech", "Scene"],
+        task_subtypes=[
+            "Environment Sound Classification",
+            "Music Instrument Recognition",
+            "Vocal Sound Classification",
+            "Gunshot Audio Classification",
+        ],
+        license="cc-by-4.0",
+        annotations_creators="human-annotated",
+        dialect=[],
+        modalities=["audio"],
+        sample_creation="found",
+        bibtex_citation=r"""
+@inproceedings{45857,
+  address = {New Orleans, LA},
+  author = {Jort F. Gemmeke and Daniel P. W. Ellis and Dylan Freedman and Aren Jansen and Wade Lawrence and R. Channing Moore and Manoj Plakal and Marvin Ritter},
+  booktitle = {Proc. IEEE ICASSP 2017},
+  title = {Audio Set: An ontology and human-labeled dataset for audio events},
+  year = {2017},
+}
+""",
+    )
+
+    audio_column_name: str = "audio"
+    label_column_name: str = "human_labels"
