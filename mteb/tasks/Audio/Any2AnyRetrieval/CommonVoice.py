fcc8eab5593da8c4c44337b9df13253441d0af2f
murphy <hepengfe@gmail.com>
2025-07-28 23:38:36 -0700

    fixed class name, superclass and eval languages

--- mteb/tasks/Audio/Any2AnyRetrieval/CommonVoice.py
+++ mteb/tasks/Audio/Any2AnyRetrieval/CommonVoice.py
@@ -1,14 +1,66 @@
 from __future__ import annotations
 
-from mteb.abstasks.Image.AbsTaskAny2AnyRetrieval import AbsTaskAny2AnyRetrieval
-from mteb.abstasks.TaskMetadata import TaskMetadata
+from collections import defaultdict
+
 import datasets
-from datasets import Dataset, DatasetDict, load_dataset
+from datasets import Dataset, DatasetDict
 from tqdm import tqdm
 
-class CommonVoiceA2TRetrieval(AbsTaskAny2AnyRetrieval):
+from mteb.abstasks.Image.AbsTaskAny2AnyRetrieval import AbsTaskAny2AnyRetrieval
+from mteb.abstasks.MultilingualTask import MultilingualTask
+from mteb.abstasks.TaskMetadata import TaskMetadata
+
+_EVAL_LANGS =  {
+    'ab': ['Abkhaz'],
+    'af': ['Afrikaans'],
+    'am': ['Amharic'],
+    'ar': ['Arabic'],
+    'as': ['Assamese'],
+    'ast': ['Asturian'],
+    'az': ['Azerbaijani'],
+    'ba': ['Bashkir'],
+    'bas': ['Basaa'],
+    'be': ['Belarusian'],
+    'bg': ['Bulgarian'],
+    'bn': ['Bengali'],
+    'br': ['Breton'],
+    'ca': ['Catalan'],
+    'ckb': ['Central Kurdish (Sorani)'],
+    'cnh': ['Hakha Chin'],
+    'cs': ['Czech'],
+    'cv': ['Chuvash'],
+    'cy': ['Welsh'],
+    'da': ['Danish'],
+    'de': ['German'],
+    'dv': ['Divehi'],
+    'dyu': ['Dyula'],
+    'el': ['Greek'],
+    'en': ['English'],
+    'eo': ['Esperanto'],
+    'es': ['Spanish'],
+    'et': ['Estonian'],
+    'eu': ['Basque'],
+    'fa': ['Persian'],
+    'fi': ['Finnish'],
+    'fr': ['French'],
+    'fy-NL': ['Frisian (Netherlands)'],
+    'ga-IE': ['Irish (Ireland)'],
+    'gl': ['Galician'],
+    'gn': ['Guarani'],
+    'ha': ['Hausa'],
+    'he': ['Hebrew'],
+    'hi': ['Hindi'],
+    'hsb': ['Upper Sorbian'],
+    'hu': ['Hungarian'],
+    'hy-AM': ['Armenian (Armenia)'],
+    'ia': ['Interlingua'],
+    'id': ['Indonesian'],
+    'ig': ['Igbo']
+}
+
+class CommonVoice17A2TRetrieval(MultilingualTask, AbsTaskAny2AnyRetrieval):
     metadata = TaskMetadata(
-        name="CommonVoiceA2TRetrieval",
+        name="CommonVoice17A2TRetrieval",
         description="Speech recordings with corresponding text transcriptions from CommonVoice dataset.",
         reference="https://commonvoice.mozilla.org/",
         dataset={
@@ -19,7 +71,7 @@ class CommonVoiceA2TRetrieval(AbsTaskAny2AnyRetrieval):
         category="a2t",
         modalities=["text", "audio"],
         eval_splits=["test"],
-        eval_langs=["af"],
+        eval_langs=_EVAL_LANGS,
         main_score="cv_recall_at_5",
         date=("2020-01-01", "2024-12-31"),
         domains=["Spoken"],
@@ -43,69 +95,64 @@ class CommonVoiceA2TRetrieval(AbsTaskAny2AnyRetrieval):
         """Load dataset from HuggingFace hub"""
         if self.data_loaded:
             return
-        self.dataset = datasets.load_dataset(
-                                             self.metadata.dataset["path"],
-                                             name = self.metadata.eval_langs[0],
-                                             revision=self.metadata.dataset.get("revision"),
-                                             )  # type: ignore
+        self.corpus = defaultdict(DatasetDict)
+        self.queries = defaultdict(DatasetDict)
+        self.relevant_docs = defaultdict(DatasetDict)
         self.dataset_transform()
         self.data_loaded = True
-        
-
 
     def dataset_transform(self, id_col="path", text_col="sentence", audio_col="audio"):
-        """
-        Transform Common Voice dataset to MTEB t2a retrieval format.
+        """Transform Common Voice dataset to MTEB t2a retrieval format.
         Returns (corpus, queries, relevant_docs) as DatasetDicts.
         """
-
         queries_ = {"id": [], "modality": [], "audio": []}
         corpus_ = {"id": [], "modality": [], "text": []}
         relevant_docs_ = {"query-id": [], "corpus-id": [], "score": []}
         relevant_docs_ = {}
-        self.corpus = DatasetDict()
-        self.queries = DatasetDict()
-        self.relevant_docs = DatasetDict()
-        
+
         qid = set()
         did = set()
-        for split in self.metadata.eval_splits:
-            for row in tqdm(self.dataset[split], total=len(self.dataset[split])):
-                # Use the "path" field as a unique id for both query and doc
+        for lang in self.metadata.eval_langs:
+            lang_dataset = datasets.load_dataset(
+                self.metadata.dataset["path"],
+                lang,
+                revision=self.metadata.dataset.get("revision"),
+            ) 
+            for split in self.metadata.eval_splits:
+                for row in tqdm(lang_dataset[split], total=len(lang_dataset[split])):
+                    # Use the "path" field as a unique id for both query and doc
 
-                query_id = str(row[id_col])
-                doc_id = str(row[id_col])
-                text = row[text_col]
-                audio = row[audio_col]
+                    query_id = str(row[id_col])
+                    doc_id = str(row[id_col])
+                    text = row[text_col]
+                    audio = row[audio_col]
 
-                if query_id not in qid:
-                    qid.add(query_id)
-                    queries_["id"].append(query_id)
-                    queries_['audio'].append(audio)
-                    queries_["modality"].append("audio")
-                    
+                    if query_id not in qid:
+                        qid.add(query_id)
+                        queries_["id"].append(query_id)
+                        queries_["audio"].append(audio)
+                        queries_["modality"].append("audio")
 
-                if doc_id not in did:
-                    did.add(doc_id)
-                    corpus_["id"].append(doc_id)
-                    corpus_["text"].append(text)
-                    corpus_["modality"].append("text")
+                    if doc_id not in did:
+                        did.add(doc_id)
+                        corpus_["id"].append(doc_id)
+                        corpus_["text"].append(text)
+                        corpus_["modality"].append("text")
 
-                if query_id not in relevant_docs_:
-                    relevant_docs_[query_id] = {}
-                relevant_docs_[query_id][doc_id] = 1
-                
-            #  dict[str, dict[str, int]],
-            self.corpus[split] = Dataset.from_dict(corpus_)
-            self.queries[split] = Dataset.from_dict(queries_)
-            self.relevant_docs[split] = relevant_docs_ # Dataset.from_dict(relevant_docs_)
+                    if query_id not in relevant_docs_:
+                        relevant_docs_[query_id] = {}
+                    relevant_docs_[query_id][doc_id] = 1
 
-
+                self.corpus[lang][split] = Dataset.from_dict(corpus_)
+                self.queries[lang][split] = Dataset.from_dict(queries_)
+                self.relevant_docs[lang][split] = (
+                    relevant_docs_  # Dataset.from_dict(relevant_docs_)
+                )
 
 
-class CommonVoiceT2ARetrieval(AbsTaskAny2AnyRetrieval):
+class CommonVoice17T2ARetrieval(MultilingualTask, AbsTaskAny2AnyRetrieval):
     metadata = TaskMetadata(
-        name="CommonVoiceT2ARetrieval",
+        name="CommonVoice17T2ARetrieval",
         description="Speech recordings with corresponding text transcriptions from CommonVoice dataset.",
         reference="https://commonvoice.mozilla.org/",
         dataset={
@@ -116,7 +163,7 @@ class CommonVoiceT2ARetrieval(AbsTaskAny2AnyRetrieval):
         category="t2a",
         modalities=["text", "audio"],
         eval_splits=["test"],
-        eval_langs=["af"],
+        eval_langs=_EVAL_LANGS,
         main_score="cv_recall_at_5",
         date=("2020-01-01", "2024-12-31"),
         domains=["Spoken"],
@@ -135,56 +182,57 @@ class CommonVoiceT2ARetrieval(AbsTaskAny2AnyRetrieval):
 }
 """,
     )
-    
+
     def load_data(self, **kwargs):
         """Load dataset from HuggingFace hub"""
         if self.data_loaded:
             return
-        self.dataset = datasets.load_dataset(
-                                             self.metadata.dataset["path"],
-                                             name = self.metadata.eval_langs[0],
-                                             revision=self.metadata.dataset.get("revision"),
-                                             )  # type: ignore
+        self.corpus = defaultdict(DatasetDict)
+        self.queries = defaultdict(DatasetDict)
+        self.relevant_docs = defaultdict(DatasetDict)
         self.dataset_transform()
         self.data_loaded = True
 
     def dataset_transform(self, id_col="path", text_col="sentence", audio_col="audio"):
-        """
-        For T2A: query=text, corpus=audio.
+        """For T2A: query=text, corpus=audio.
         """
         queries_ = {"id": [], "modality": [], "text": []}
         corpus_ = {"id": [], "modality": [], "audio": []}
         relevant_docs_ = {}
 
-        self.corpus = DatasetDict()
-        self.queries = DatasetDict()
-        self.relevant_docs = DatasetDict()
+
 
         qid = set()
         did = set()
-        for split in self.metadata.eval_splits:
-            for row in tqdm(self.dataset[split], total=len(self.dataset[split])):
-                query_id = str(row[id_col])
-                doc_id = str(row[id_col])
-                text = row[text_col]
-                audio = row[audio_col]
+        for lang in self.metadata.eval_langs:
+            lang_dataset = datasets.load_dataset(
+                self.metadata.dataset["path"],
+                lang,
+                revision=self.metadata.dataset.get("revision"),
+            ) 
+            for split in self.metadata.eval_splits:
+                for row in tqdm(lang_dataset[split], total=len(lang_dataset[split])):
+                    query_id = str(row[id_col])
+                    doc_id = str(row[id_col])
+                    text = row[text_col]
+                    audio = row[audio_col]
 
-                if query_id not in qid:
-                    qid.add(query_id)
-                    queries_["id"].append(query_id)
-                    queries_["text"].append(text)
-                    queries_["modality"].append("text")
+                    if query_id not in qid:
+                        qid.add(query_id)
+                        queries_["id"].append(query_id)
+                        queries_["text"].append(text)
+                        queries_["modality"].append("text")
 
-                if doc_id not in did:
-                    did.add(doc_id)
-                    corpus_["id"].append(doc_id)
-                    corpus_["audio"].append(audio)
-                    corpus_["modality"].append("audio")
+                    if doc_id not in did:
+                        did.add(doc_id)
+                        corpus_["id"].append(doc_id)
+                        corpus_["audio"].append(audio)
+                        corpus_["modality"].append("audio")
 
-                if query_id not in relevant_docs_:
-                    relevant_docs_[query_id] = {}
-                relevant_docs_[query_id][doc_id] = 1
+                    if query_id not in relevant_docs_:
+                        relevant_docs_[query_id] = {}
+                    relevant_docs_[query_id][doc_id] = 1
 
-            self.corpus[split] = Dataset.from_dict(corpus_)
-            self.queries[split] = Dataset.from_dict(queries_)
-            self.relevant_docs[split] = relevant_docs_
+                self.corpus[lang][split] = Dataset.from_dict(corpus_)
+                self.queries[lang][split] = Dataset.from_dict(queries_)
+                self.relevant_docs[lang][split] = relevant_docs_
