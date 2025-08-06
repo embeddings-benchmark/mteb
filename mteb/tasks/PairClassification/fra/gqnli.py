608262d2baa43694d34f1769722641555a28089f
MedAmineYoussef <MAYOU29@ulaval.ca>
2025-08-06 10:37:56 -0400

    Update gqnli.py

--- mteb/tasks/PairClassification/fra/gqnli.py
+++ mteb/tasks/PairClassification/fra/gqnli.py
@@ -14,7 +14,7 @@ class GqnliTask(AbsTaskPairClassification):
         reference="https://huggingface.co/datasets/maximoss/gqnli-fr",
         dataset={
             "path": "maximoss/gqnli-fr",
-            "revision": "main"
+            "revision": "3089f0b591628692e7c4d4122804f6995380bdad"
         },
         type="PairClassification",
         category="s2s",
@@ -29,12 +29,28 @@ class GqnliTask(AbsTaskPairClassification):
         annotations_creators="human-annotated",
         dialect=[],
         sample_creation="found",
-        bibtex_citation=r"""@article{your_citation_here,
-  title={Your Title Here},
-  author={Your Author Here},
-  journal={Your Journal Here},
-  year={2025}
-}""",
+        bibtex_citation=r"""@inproceedings{skandalis-etal-2024-new-datasets,
+    title = "New Datasets for Automatic Detection of Textual Entailment and of Contradictions between Sentences in {F}rench",
+    author = "Skandalis, Maximos  and
+      Moot, Richard  and
+      Retor{\'e}, Christian  and
+      Robillard, Simon",
+    editor = "Calzolari, Nicoletta  and
+      Kan, Min-Yen  and
+      Hoste, Veronique  and
+      Lenci, Alessandro  and
+      Sakti, Sakriani  and
+      Xue, Nianwen",
+    booktitle = "Proceedings of the 2024 Joint International Conference on Computational Linguistics, Language Resources and Evaluation (LREC-COLING 2024)",
+    month = may,
+    year = "2024",
+    address = "Torino, Italy",
+    publisher = "ELRA and ICCL",
+    url = "https://aclanthology.org/2024.lrec-main.1065",
+    pages = "12173--12186",
+    abstract = "This paper introduces DACCORD, an original dataset in French for automatic detection of contradictions between sentences. It also presents new, manually translated versions of two datasets, namely the well known dataset RTE3 and the recent dataset GQNLI, from English to French, for the task of natural language inference / recognising textual entailment, which is a sentence-pair classification task. These datasets help increase the admittedly limited number of datasets in French available for these tasks. DACCORD consists of 1034 pairs of sentences and is the first dataset exclusively dedicated to this task and covering among others the topic of the Russian invasion in Ukraine. RTE3-FR contains 800 examples for each of its validation and test subsets, while GQNLI-FR is composed of 300 pairs of sentences and focuses specifically on the use of generalised quantifiers. Our experiments on these datasets show that they are more challenging than the two already existing datasets for the mainstream NLI task in French (XNLI, FraCaS). For languages other than English, most deep learning models for NLI tasks currently have only XNLI available as a training set. Additional datasets, such as ours for French, could permit different training and evaluation strategies, producing more robust results and reducing the inevitable biases present in any single dataset.",
+}
+""",
     )
 
     def dataset_transform(self):
