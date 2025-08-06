0282c3967dca6e9d8cd927818dccc8258e50f767
MedAmineYoussef <MAYOU29@ulaval.ca>
2025-08-06 10:51:28 -0400

    Update FraCaS.py

--- mteb/tasks/PairClassification/fra/FraCaS.py
+++ mteb/tasks/PairClassification/fra/FraCaS.py
@@ -10,11 +10,11 @@ class FracasTask(AbsTaskPairClassification):
             "prédit si une hypothèse découle (entailment) ou non d'une prémisse."
         ),
         reference="https://huggingface.co/datasets/maximoss/fracas",
-        dataset={"path": "maximoss/fracas", "revision": "main"},
+        dataset={"path": "maximoss/fracas", "revision": "2506e60be409b124bd72336038dea6f9460ea70c"},
         type="PairClassification",
         category="s2s",
         modalities=["text"],
-        eval_splits=["train"],        # FRACAS ne propose que ce split
+        eval_splits=["train"],
         eval_langs=["fra-Latn"],
         main_score="max_accuracy",
         date=("2025-08-05", "2025-08-05"),
@@ -25,57 +25,56 @@ class FracasTask(AbsTaskPairClassification):
         dialect=[],
         sample_creation="found",
         bibtex_citation=r"""
-@inproceedings{fracas2025,
-  author    = {Maxim Oss and Collaborateurs},
-  title     = {FRACAS: A French NLI dataset},
-  booktitle = {Imaginary Conference on French NLP},
-  year      = {2025},
+@inproceedings{amblard-etal-2020-french,
+    title = "A {F}rench Version of the {F}ra{C}a{S} Test Suite",
+    author = "Amblard, Maxime  and
+      Beysson, Cl{\'e}ment  and
+      de Groote, Philippe  and
+      Guillaume, Bruno  and
+      Pogodalla, Sylvain",
+    editor = "Calzolari, Nicoletta  and
+      B{\'e}chet, Fr{\'e}d{\'e}ric  and
+      Blache, Philippe  and
+      Choukri, Khalid  and
+      Cieri, Christopher  and
+      Declerck, Thierry  and
+      Goggi, Sara  and
+      Isahara, Hitoshi  and
+      Maegaard, Bente  and
+      Mariani, Joseph  and
+      Mazo, H{\'e}l{\`e}ne  and
+      Moreno, Asuncion  and
+      Odijk, Jan  and
+      Piperidis, Stelios",
+    booktitle = "Proceedings of the Twelfth Language Resources and Evaluation Conference",
+    month = may,
+    year = "2020",
+    address = "Marseille, France",
+    publisher = "European Language Resources Association",
+    url = "https://aclanthology.org/2020.lrec-1.721",
+    pages = "5887--5895",
+    abstract = "This paper presents a French version of the FraCaS test suite. This test suite, originally written in English, contains problems illustrating semantic inference in natural language. We describe linguistic choices we had to make when translating the FraCaS test suite in French, and discuss some of the issues that were raised by the translation. We also report an experiment we ran in order to test both the translation and the logical semantics underlying the problems of the test suite. This provides a way of checking formal semanticists{'} hypotheses against actual semantic capacity of speakers (in the present case, French speakers), and allow us to compare the results we obtained with the ones of similar experiments that have been conducted for other languages.",
+    language = "English",
+    ISBN = "979-10-95546-34-4",
 }
+
 """,
     )
 
-    def load_data(self, **kwargs):
-        """Charge le DatasetDict HF puis transforme en self.dataset."""
-        if getattr(self, "data_loaded", False):
-            return
-        self.dataset = load_dataset(
-            self.metadata.dataset["path"],
-            revision=self.metadata.dataset["revision"],
-        )
-        self.dataset_transform()
-        self.data_loaded = True
-
     def dataset_transform(self):
-        """
-        Construit self.dataset sous la forme :
-        {
-          'fra-Latn': {
-             'train': [
-               {
-                 'sentence1': [...],  # liste de prémisses
-                 'sentence2': [...],  # liste d’hypothèses
-                 'labels':    [...],  # liste de 0/1
-               }
-             ]
-          }
-        }
-        """
+
         out: dict[str, dict[str, list[dict[str, list]]]] = {}
-        for lang in self.hf_subsets:                 # ['fra-Latn']
+        for lang in self.hf_subsets:
             out[lang] = {}
-            for split in self.metadata.eval_splits:  # ['train']
+            for split in self.metadata.eval_splits:
                 ds = self.dataset[split]
-                # Affiche les labels pour debugging
-                # Filtrer hors 'undef'
                 ds = ds.filter(lambda x: x["label"] != "undef")
-                # Remapper '1'→1 (positif), '0' et '2'→0 (négatif)
                 ds = ds.map(lambda ex: {"label": 1 if ex["label"] == "1" else 0})
-                # Construire la liste contenant UN dict de listes alignées
                 out[lang][split] = [
                     {
                         "sentence1": ds["premises"],
                         "sentence2": ds["hypothesis"],
-                        "labels":    ds["label"],
+                        "labels": ds["label"],
                     }
                 ]
         self.dataset = out
