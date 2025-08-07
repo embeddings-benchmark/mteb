0f3b9f250afa8586c45b78283a92cc400064cd4e
Kenneth Enevoldsen <kennethcenevoldsen@gmail.com>
2025-08-07 15:20:47 +0200

    format

--- mteb/evaluation/evaluators/image/__init__.py
+++ mteb/evaluation/evaluators/image/__init__.py
@@ -1,9 +1,9 @@
 from __future__ import annotations
 
+from .image_multi_choice_evaluator import Any2AnyMultiChoiceEvaluator
 from .image_text_pair_classification_evaluator import (
     ImageTextPairClassificationEvaluator,
 )
-from .image_multi_choice_evaluator import Any2AnyMultiChoiceEvaluator
 from .image_text_retrieval_evaluator import ImageTextRetrievalEvaluator
 
 __all__ = [
