bb38eb2d24eb26864c077f4654aa82c52c279022
fzoll <fodizoltan@gmail.com>
2025-08-17 23:51:37 +0200

    Add FreshStackRetrieval

--- mteb/tasks/Retrieval/__init__.py
+++ mteb/tasks/Retrieval/__init__.py
@@ -12,6 +12,7 @@ from .code.CodeTransOceanContestRetrieval import *
 from .code.CodeTransOceanDLRetrieval import *
 from .code.COIRCodeSearchNetRetrieval import *
 from .code.CosQARetrieval import *
+from .code.FreshStackRetrieval import *
 from .code.StackOverflowQARetrieval import *
 from .code.SyntheticText2SqlRetrieval import *
 from .dan.DanFeverRetrieval import *
