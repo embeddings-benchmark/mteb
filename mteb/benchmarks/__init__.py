656c25cddb3dcc1bfb666eeb84457a44382daf0a
fzoll <fodizoltan@gmail.com>
2025-08-26 09:46:59 +0200

    Add the rteb_benchmarks to the BENCHMARK_REGISTRY

--- mteb/benchmarks/__init__.py
+++ mteb/benchmarks/__init__.py
@@ -2,12 +2,12 @@ from __future__ import annotations
 
 from mteb.benchmarks.benchmark import Benchmark
 from mteb.benchmarks.benchmarks import *
+from mteb.benchmarks.rteb_benchmarks import * # pylint: disable=Required to be here
 from mteb.benchmarks.get_benchmark import (
     BENCHMARK_REGISTRY,
     get_benchmark,
     get_benchmarks,
 )
-from mteb.benchmarks.rteb_benchmarks import *
 
 __all__ = [
     "BENCHMARK_REGISTRY",
