9f6642bc9369933cc310c46ad447687eb90f1f23
atheer <atheerwaled66@hotmail.se>
2025-08-26 21:51:36 +0200

    fix: update mteb task according to feedback

--- mteb/tasks/MultiLabelClassification/swe/SwedishPatentCPCSubclassClassification.py
+++ mteb/tasks/MultiLabelClassification/swe/SwedishPatentCPCSubclassClassification.py
@@ -16,13 +16,13 @@ class SwedishPatentCPCSubclassClassification(AbsTaskMultilabelClassification):
 		category="s2s",
 		modalities=["text"],
 		eval_splits=["train"],
-		eval_langs=["swe"],
+		eval_langs=["swe-Latn"],
 		main_score="accuracy",
 		dataset = {
 			"path": "atheer2104/swedish-patent-cpc-subclass",
 			"revision": "cf7b50cc195c609f2cb8a0f6b6651335794eccb4",
 		},
-		date = ("2025-01-01", "2025-04-01"),
+		date = ("1885-01-01", "1972-01-01"),
 		domains = ["Legal", "Government"],
 		task_subtypes = [],
 		license = "mit",
@@ -39,7 +39,6 @@ class SwedishPatentCPCSubclassClassification(AbsTaskMultilabelClassification):
 		series = {TRITA-EECS-EX},
 		number = {2025:571},
 		keywords = {Multi-label Text Classification, Machine Learning, Patent Classification, Deep Learning, Natural Language Processing, Textklassificering med flera Klasser, Maskininlärning, Patentklassificering, Djupinlärning, Språkteknologi},
-		abstract = {Patents are essential for protecting intellectual property and advancing innovation, but the accessibility of historical patents is often limited by outdated classification systems. In Sweden, many older patents are classified solely under the now-obsolete German Patent Classification (DPK) system, making them difficult to retrieve using the modern Cooperative Patent Classification (CPC) system. This lack of accessibility can hinder prior art searches and public access to technical knowledge. The thesis investigates whether incorporating DPK information alongside patent claims improves the performance of machine learning models in classifying historical Swedish patents into the CPC system. Three models - Linear, XML-CNN, and CNN-BiLSTM - were trained on four datasets: two using only patent claims and two combining patent claims with DPK information for both CPC subclass and group-level predictions. Model performance was evaluated using precision at k, recall at k, F1-score at k, normalized discounted cumulated gains (nDCG@k) for k = 1, 3, 5, and micro F1-score. The results show that incorporating DPK information consistently enhances classification performance across all metrics and models, with statistical significance improvements. While group-level predictions were more challenging due to label imbalance and fewer samples per label, DPK information improved results at both hierarchy levels. These findings demonstrate that leveraging legacy classification data can significantly improve the reclassification of historical patents, thereby enhancing their accessibility and supporting more effective prior art searches. },
 		year = {2025},
 		url = "https://urn.kb.se/resolve?urn=urn:nbn:se:kth:diva-368254"
 		}
