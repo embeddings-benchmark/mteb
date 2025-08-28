f3efbc694f19503cc60f6c1c5ab95a9e4e88230b
mathlesage <matheo.qtb@gmail.com>
2025-08-28 16:47:59 +0200

    Add training_datasets (common_corpus, fineweb, wiki_fr, private LLM-synth)

--- mteb/models/ordalietech_solon_embeddings_mini_beta_1_1.py
+++ mteb/models/ordalietech_solon_embeddings_mini_beta_1_1.py
@@ -2,17 +2,25 @@ from mteb.model_meta import ModelMeta
 
 my_model = ModelMeta(
     name="OrdalieTech/Solon-embeddings-mini-beta-1.1",
-    languages=["fra-Latn"],                 # ajoute "eng-Latn" si bilingue
+    languages=["fra-Latn"],                
     open_weights=True,
     revision="8e4ea66eb7eb6109b47b7d97d7556f154d9aec4a",
     release_date="2025-01-01",
-    n_parameters=210_000_000,              # ajuste si tu as la valeur exacte
-    embed_dim=768,                          # <<< mets la vraie dimension si différente
+    n_parameters=210_000_000,              
+    embed_dim=768,                          
     license="apache-2.0",
-    max_tokens=8192,                        # <<< ajuste si tu as la valeur exacte
+    max_tokens=8192,                        
     reference="https://huggingface.co/OrdalieTech/Solon-embeddings-mini-beta-1.1",
     similarity_fn_name="cosine",
     framework=["Sentence Transformers", "PyTorch"],
     use_instructions=False,
+    training_datasets={
+        "PleIAs/common_corpus": ["train"],
+        "HuggingFaceFW/fineweb": ["train"],
+        "OrdalieTech/wiki_fr": ["train"],
+        "llm_synthetic_private": ["train"],
+    },
+
+
 )
 
