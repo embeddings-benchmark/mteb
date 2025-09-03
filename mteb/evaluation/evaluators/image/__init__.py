dc495cbb17b9e743b04eed33432895ba2881c17b
Solomatin Roman <samoed.roman@gmail.com>
2025-09-04 00:00:50 +0300

    fix imports

---
+++ mteb/evaluation/evaluators/image/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import annotations
+
+from .imagetext_pairclassification_evaluator import ImageTextPairClassificationEvaluator
+
+__all__ = [
+    "ImageTextPairClassificationEvaluator",
+]
