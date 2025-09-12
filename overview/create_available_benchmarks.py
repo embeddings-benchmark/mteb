7a78ef6b252571b12f1e0103ca6a1d952feab50d
Kenneth Enevoldsen <kennethcenevoldsen@gmail.com>
2025-09-12 10:08:03 +0200

    Deployed ada80e2ab with MkDocs version: 1.6.1

--- overview/create_available_benchmarks.py
+++ overview/create_available_benchmarks.py
@@ -3,8 +3,10 @@
 from __future__ import annotations
 
 from pathlib import Path
+from typing import cast
 
 import mteb
+from mteb.overview import MTEBTasks
 
 START_INSERT = "<!-- START TASK DESCRIPTION -->"
 END_INSERT = "<!-- END TASK DESCRIPTION -->"
@@ -14,11 +16,51 @@ benchmark_entry = """
 
 {description}
 
-??? info Tasks
+??? info "Tasks"
 
 {task_table}
 """
 learn_more = "[Learn more →]({reference})"
+
+citation_admonition = """
+
+??? quote "Citation"
+
+{citation_chunk}
+
+"""
+
+citation_chunk = """
+```bibtex
+{bibtex_citation}
+```
+"""
+
+
+def pretty_long_list(items: list[str], max_items: int = 5) -> str:
+    if len(items) <= max_items:
+        return ", ".join(items)
+    return ", ".join(items[:max_items]) + f", ... ({len(items)})"
+
+
+def create_table(benchmark: mteb.Benchmark) -> str:
+    """Create a markdown table of tasks in the benchmark."""
+    tasks = benchmark.tasks
+    tasks = cast(MTEBTasks, tasks)
+    df = tasks.to_dataframe(["name", "type", "modalities", "languages"])
+
+    # add links to task names:
+    # format: http://127.0.0.1:8000/overview/available_tasks/retrieval/#treccovid
+    df["name"] = df.apply(
+        lambda row: f"[{row['name']}](./available_tasks/{row['type'].lower()}/#{row['name'].lower()})",
+        axis=1,
+    )
+    df["modalities"] = df["modalities"].apply(lambda x: pretty_long_list(x))
+    df["languages"] = df["languages"].apply(lambda x: pretty_long_list(x))
+
+    tasks_md = df.to_markdown(index=False)
+
+    return tasks_md
 
 
 def format_benchmark_entry(benchmark: mteb.Benchmark) -> str:
@@ -29,15 +71,20 @@ def format_benchmark_entry(benchmark: mteb.Benchmark) -> str:
             "\n\n" + learn_more.format(reference=benchmark.reference) + "\n\n"
         )
 
-    tasks_md = benchmark.tasks.to_markdown(["type", "modalities"])
-
+    tasks_md = create_table(benchmark)
     task_md_indented = "\n".join([f"    {line}" for line in tasks_md.split("\n")])
 
-    return benchmark_entry.format(
+    entry = benchmark_entry.format(
         benchmark_name=benchmark.name,
         description=description,
         task_table=task_md_indented,
     )
+    if benchmark.citation:
+        citation = citation_chunk.format(bibtex_citation=benchmark.citation)
+        citation = "\n".join([f"    {line}" for line in citation.split("\n")])
+        entry += citation_admonition.format(citation_chunk=citation)
+
+    return entry
 
 
 def insert_between_markers(
