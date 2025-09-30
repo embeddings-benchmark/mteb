cefd826a096fe4a9b47ffe5dcc9fac88c23000ef
q275343119 <275343119@qq.com>
2025-09-30 16:17:26 +0800

    feat - getattr

--- mteb/load_results/task_results.py
+++ mteb/load_results/task_results.py
@@ -168,6 +168,8 @@ class TaskResult(BaseModel):
                     "languages": eval_langs,
                 }
                 flat_scores[split].append(_scores)
+        # AttributeError: 'Namespace' object has no attribute 'is_public'
+        is_public = getattr(task_meta, "is_public", True)
 
         return TaskResult(
             dataset_revision=task.metadata.revision,
@@ -176,7 +178,7 @@ class TaskResult(BaseModel):
             scores=flat_scores,
             evaluation_time=evaluation_time,
             kg_co2_emissions=kg_co2_emissions,
-            is_pubic=task.metadata.is_public,
+            is_pubic=is_public,
         )
 
     @field_validator("scores")
