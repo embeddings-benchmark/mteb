b80ca1c31476b2133565c35297802b7ef541db23
Fedor Yaronskiy <yaronskij.fm@phystech.edu>
2025-10-09 22:10:05 +0300

    Add en code retriever model

---
+++ mteb/models/en_code_retriever.py
@@ -0,0 +1,39 @@
+from __future__ import annotations
+
+from functools import partial
+
+from mteb.encoder_interface import PromptType
+from mteb.model_meta import ModelMeta, sentence_transformers_loader
+
+model_name = "fyaronskiy/english_code_retriever"
+revision = "be653fab7d27a7348a0c2c3d16b9f92a7f10cb0c"
+
+
+english_code_retriever = ModelMeta(
+    loader=partial(
+        sentence_transformers_loader,
+        model_name=model_name,
+        revision=revision,
+        model_prompts={
+            PromptType.query.value: "search_query: ",
+            PromptType.document.value: "search_document: ",
+        },
+    ),
+    name=model_name,
+    languages=["eng-Latn"],
+    open_weights=True,
+    revision=revision,
+    release_date="2025-07-10",
+    n_parameters=149_000_000,
+    memory_usage_mb=568,
+    embed_dim=768,
+    license="mit",
+    max_tokens=8192,
+    reference="https://huggingface.co/fyaronskiy/english_code_retriever",
+    similarity_fn_name="cosine",
+    framework=["Sentence Transformers", "PyTorch"],
+    use_instructions=True,
+    public_training_code=None,
+    public_training_data="https://huggingface.co/datasets/code-search-net/code_search_net",
+    training_datasets={"CodeSearchNet": ["train"]},
+)
