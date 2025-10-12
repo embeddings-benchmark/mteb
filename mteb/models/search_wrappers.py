50a70606733b26198ca9d890fb9e5b5d31a2f6ef
Kenneth Enevoldsen <kennethcenevoldsen@gmail.com>
2025-10-12 20:16:37 +0200

    fixed cases where torch output was assumed to handle numpy

--- mteb/models/search_wrappers.py
+++ mteb/models/search_wrappers.py
@@ -176,7 +176,7 @@ class SearchEncoderWrapper:
 
             # get top-k values
             cos_scores_top_k_values, cos_scores_top_k_idx = torch.topk(
-                scores,
+                torch.tensor(scores),
                 min(
                     top_k + 1,
                     len(scores[1]) if len(scores) > 1 else len(scores[-1]),
