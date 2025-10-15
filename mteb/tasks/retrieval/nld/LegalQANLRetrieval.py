d3d6e6863d50d59affc6c577913b835b80950dd5
Nikolay Banar <nikc20008@gmail.com>
2025-10-15 17:16:41 +0200

    LegalQANLRetrieval the HF location fixed

--- mteb/tasks/retrieval/nld/LegalQANLRetrieval.py
+++ mteb/tasks/retrieval/nld/LegalQANLRetrieval.py
@@ -1,5 +1,3 @@
-import datasets
-
 from mteb.abstasks.retrieval import AbsTaskRetrieval
 from mteb.abstasks.task_metadata import TaskMetadata
 
@@ -13,8 +11,8 @@ class LegalQANLRetrieval(AbsTaskRetrieval):
         "with attributions to Dutch law articles.",
         reference="https://aclanthology.org/2024.nllp-1.12/",
         dataset={
-            "path": "clips/mteb-nl-legalqa",
-            "revision": "099ecf6131a826c6f81cc74f0a3a2452fa51644d",
+            "path": "clips/mteb-nl-legalqa-pr",
+            "revision": "8f593522dfbe7ec07055ca9d38a700e7643d3882",
         },
         type="Retrieval",
         category="t2t",
@@ -29,63 +27,13 @@ class LegalQANLRetrieval(AbsTaskRetrieval):
         annotations_creators="expert-annotated",
         dialect=[],
         sample_creation="found",
-        bibtex_citation=r"""""",
+        bibtex_citation=r"""
+@inproceedings{redelaar2024attributed,
+  author = {Redelaar, Felicia and Van Drie, Romy and Verberne, Suzan and De Boer, Maaike},
+  booktitle = {Proceedings of the natural legal language processing workshop 2024},
+  pages = {154--165},
+  title = {Attributed Question Answering for Preconditions in the Dutch Law},
+  year = {2024},
+}
+""",
     )
-
-    def load_data(self, **kwargs):
-        if self.data_loaded:
-            return
-        # fetch both subsets of the dataset, only test split
-        corpus_raw = datasets.load_dataset(
-            name="corpus",
-            split="corpus",
-            **self.metadata.dataset,
-        )
-        queries_raw = datasets.load_dataset(
-            name="queries",
-            split="queries",
-            **self.metadata.dataset,
-        )
-
-        self.queries = {
-            self.metadata.eval_splits[0]: {
-                str(q["question_id"]): q["question"].strip() for q in queries_raw
-            }
-        }
-
-        self.corpus = {
-            self.metadata.eval_splits[0]: {
-                str(d["DOC_ID"]): {
-                    "text": ", ".join(
-                        field
-                        for field in (
-                            d.get("law_name"),
-                            d.get("hoofdstuk"),
-                            d.get("hoofdstuk_titel"),
-                            d.get("afdeling"),
-                            d.get("afdeling_titel"),
-                            d.get("paragraaf"),
-                            d.get("paragraaf_titel"),
-                            d.get("subparagraaf_titel"),
-                            d.get("titel_titel"),
-                            d.get("artikel"),
-                            d.get("article_name"),
-                        )
-                        if field and field.strip()
-                    )
-                    + "\n"
-                    + d.get("text")
-                }
-                for d in corpus_raw
-            }
-        }
-
-        self.relevant_docs = {"test": {str(q["question_id"]): {} for q in queries_raw}}
-
-        for q in queries_raw:
-            for doc_id in q["human_attribution"].split(","):
-                self.relevant_docs[self.metadata.eval_splits[0]][str(q["question_id"])][
-                    doc_id
-                ] = 1
-
-        self.data_loaded = True
