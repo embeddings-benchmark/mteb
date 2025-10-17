240f182fc73f4a3a8344884a254a80622e3278be
Roman Solomatin <36135455+Samoed@users.noreply.github.com>
2025-10-17 10:54:07 +0300

    fix import

--- mteb/tasks/aggregated_tasks/eng/__init__.py
+++ mteb/tasks/aggregated_tasks/eng/__init__.py
@@ -1,9 +1,8 @@
-from sts_benchmark_multilingual_visual_sts_eng import (
-    STSBenchmarkMultilingualVisualSTSEng,
-)
-
 from .cqadupstack_retrieval import CQADupstackRetrieval
 from .sts17_multilingual_visual_sts_eng import STS17MultilingualVisualSTSEng
+from .sts_benchmark_multilingual_visual_sts_eng import (
+    STSBenchmarkMultilingualVisualSTSEng,
+)
 
 __all__ = [
     "CQADupstackRetrieval",
