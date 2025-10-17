361b4fec682c6201f085191309c7a28c6ddf3d03
Roman Solomatin <samoed.roman@gmail.com>
2025-10-17 12:31:04 +0300

    Restructure agg tasks into lang subfolders (#3394)
    * restructure agg tasks into lang subfolders
    
    * add inits
    
    * fix import
    
    * import multilingual tasks

---
+++ mteb/tasks/aggregated_tasks/fas/__init__.py
@@ -0,0 +1,7 @@
+from .cqadupstack_retrieval_fa import CQADupstackRetrievalFa
+from .syn_per_chatbot_conv_sa_classification import SynPerChatbotConvSAClassification
+
+__all__ = [
+    "CQADupstackRetrievalFa",
+    "SynPerChatbotConvSAClassification",
+]
