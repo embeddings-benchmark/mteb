6ca37e38b342b3db8ad2a4443df600f9f38dc24e
spartan <39662153+spartan8806@users.noreply.github.com>
2025-11-16 09:19:17 -0600

    Update spartan8806_atles_champion.py with the latest content

--- mteb/models/spartan8806_atles_champion.py
+++ mteb/models/spartan8806_atles_champion.py
@@ -1,32 +1,16 @@
-"""ATLES Champion Embedding Model wrapper for MTEB."
-
-from __future__ import annotations
+"""ATLES Champion Embedding Model for MTEB."""
 
 from mteb.model_meta import ModelMeta
-
-from .wrapper import Wrapper
-
-
-class SpartanATLESChampionEmbedding(Wrapper):
-    """ATLES Champion Embedding model wrapper for MTEB benchmarking."""
-
-    def __init__(self, **kwargs):
-        """Initialize the ATLES Champion model."""
-        super().__init__(
-            model_name_or_path="spartan8806/atles-champion-embedding",
-            **kwargs
-        )
-
+from mteb.models.sentence_transformer_wrapper import sentence_transformers_loader
 
 spartan8806_atles_champion_embedding = ModelMeta(
-    loader=lambda **kwargs: SpartanATLESChampionEmbedding(**kwargs),
+    loader=sentence_transformers_loader,
     name="spartan8806/atles-champion-embedding",
-    languages=["eng_Latn"],
+    languages=["eng-Latn"],
     open_source=True,
     revision="main",
     release_date="2025-01-16",
     n_parameters=110_000_000,
-    memory_usage=None,
     max_tokens=512,
     embed_dim=768,
     license="apache-2.0",
