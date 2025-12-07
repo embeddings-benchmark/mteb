e3a21f2038dbf88c38a680818dbfd2c4a74c77e3
Roman Solomatin <36135455+Samoed@users.noreply.github.com>
2025-12-07 16:12:06 +0300

    v4 -> v3

--- mteb/tasks/classification/heb/__init__.py
+++ mteb/tasks/classification/heb/__init__.py
@@ -2,12 +2,10 @@ from .hebrew_sentiment_analysis import (
     HebrewSentimentAnalysis,
     HebrewSentimentAnalysisV2,
     HebrewSentimentAnalysisV3,
-    HebrewSentimentAnalysisV4,
 )
 
 __all__ = [
     "HebrewSentimentAnalysis",
     "HebrewSentimentAnalysisV2",
     "HebrewSentimentAnalysisV3",
-    "HebrewSentimentAnalysisV4",
 ]
