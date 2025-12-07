e8ef2ab985427cc014d2059b764a28cda8ed4bb5
kowshik24 <kowshikcseruet1998@gmail.com>
2025-12-07 23:40:59 +0600

    fix: update import path for ModelMeta in kowshik24_models.py

--- mteb/models/model_implementations/kowshik24_models.py
+++ mteb/models/model_implementations/kowshik24_models.py
@@ -1,4 +1,4 @@
-from mteb.model_meta import ModelMeta, sentence_transformers_loader
+from mteb.models import ModelMeta, sentence_transformers_loader
 
 kowshik24_bangla_embedding_model = ModelMeta(
     loader=sentence_transformers_loader,
@@ -6,9 +6,9 @@ kowshik24_bangla_embedding_model = ModelMeta(
     languages=["ben-Beng"],  # Bengali using Bengali script
     open_weights=True,
     revision="6689c21e69be5950596bad084457cbaa138728d8",
-    release_date="2025-11-10",  
-    n_parameters=278_000_000,   
-    memory_usage_mb=1061,       
+    release_date="2025-11-10",
+    n_parameters=278_000_000,
+    memory_usage_mb=1061,
     embed_dim=768,
     license="apache-2.0",
     max_tokens=128,
