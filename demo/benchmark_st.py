003bd20e8acf4862751f3b6e164cdcc75b64014c
wang.yuqi <yuqi.wang@daocloud.io>
2025-12-26 14:18:52 +0800

    + benchmark
    Signed-off-by: wang.yuqi <yuqi.wang@daocloud.io>

---
+++ demo/benchmark_st.py
@@ -0,0 +1,29 @@
+import time
+import mteb
+import torch
+
+"""
+model_name = "BAAI/bge-m3"
+model = mteb.get_model(model_name)
+tasks = mteb.get_tasks(tasks=["T2Reranking"])
+start = time.perf_counter()
+# When suing batch size is 12, the gpu memory usage is 18.669 GB, and it is estimated to take 13 hours to complete.
+results = mteb.evaluate(model, tasks=tasks, encode_kwargs={"batch_size": 12, "show_progress_bar": True})
+end = time.perf_counter()
+elapsed_time = end - start
+main_score = results[0].scores["dev"][0]["main_score"]
+print("elapsed_time", elapsed_time, "main_score", main_score)
+"""
+
+
+model_name = "BAAI/bge-m3"
+model = mteb.get_model(model_name, config_kwargs={"dtype":"float16"})
+tasks = mteb.get_tasks(tasks=["T2Reranking"])
+start = time.perf_counter()
+# There is not much speed improvement even when using float16. it is estimated to take 12 hours to complete.
+results = mteb.evaluate(model, tasks=tasks, encode_kwargs={"batch_size": 12, "show_progress_bar": True})
+end = time.perf_counter()
+elapsed_time = end - start
+main_score = results[0].scores["dev"][0]["main_score"]
+print("elapsed_time", elapsed_time, "main_score", main_score)
+
