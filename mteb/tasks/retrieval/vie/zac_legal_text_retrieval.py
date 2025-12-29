50f7b07a2a6c8e7a114f51c6dda912dcf8db37e2
BaoLocPham <phambaoloc163@gmail.com>
2025-12-29 06:35:32 +0000

    [UPDATE] bibtext

--- mteb/tasks/retrieval/vie/zac_legal_text_retrieval.py
+++ mteb/tasks/retrieval/vie/zac_legal_text_retrieval.py
@@ -25,19 +25,19 @@ class ZacLegalTextRetrieval(AbsTaskRetrieval):
         dialect=[],
         sample_creation="found",
         bibtex_citation=r"""
-@InProceedings{10.1007/978-981-95-1746-6_17,
-author="Pham, Bao Loc
+@inproceedings{10.1007/978-981-95-1746-6_17,
+  abstract = {Information retrieval often comes in plain text, lacking semi-structured text such as HTML and markdown, retrieving data that contains rich format such as table became non-trivial. In this paper, we tackle this challenge by introducing a new dataset, GreenNode Table Retrieval VN (GN-TRVN), which is collected from a massive corpus, a wide range of topics, and a longer context compared to ViQuAD2.0. To evaluate the effectiveness of our proposed dataset, we introduce two versions, M3-GN-VN and M3-GN-VN-Mixed, by fine-tuning the M3-Embedding model on this dataset. Experimental results show that our models consistently outperform the baselines, including the base model, across most evaluation criteria on various datasets such as VieQuADRetrieval, ZacLegalTextRetrieval, and GN-TRVN. In general, we release a more comprehensive dataset and two model versions that improve response performance for Vietnamese Markdown Table Retrieval.},
+  address = {Singapore},
+  author = {Pham, Bao Loc
 and Hoang, Quoc Viet
 and Luu, Quy Tung
-and Vo, Trong Thu",
-title="GN-TRVN: A Benchmark for Vietnamese Table Markdown Retrieval Task",
-booktitle="Proceedings of the Fifth International Conference on Intelligent Systems and Networks",
-year="2026",
-publisher="Springer Nature Singapore",
-address="Singapore",
-pages="153--163",
-abstract="Information retrieval often comes in plain text, lacking semi-structured text such as HTML and markdown, retrieving data that contains rich format such as table became non-trivial. In this paper, we tackle this challenge by introducing a new dataset, GreenNode Table Retrieval VN (GN-TRVN), which is collected from a massive corpus, a wide range of topics, and a longer context compared to ViQuAD2.0. To evaluate the effectiveness of our proposed dataset, we introduce two versions, M3-GN-VN and M3-GN-VN-Mixed, by fine-tuning the M3-Embedding model on this dataset. Experimental results show that our models consistently outperform the baselines, including the base model, across most evaluation criteria on various datasets such as VieQuADRetrieval, ZacLegalTextRetrieval, and GN-TRVN. In general, we release a more comprehensive dataset and two model versions that improve response performance for Vietnamese Markdown Table Retrieval.",
-isbn="978-981-95-1746-6"
+and Vo, Trong Thu},
+  booktitle = {Proceedings of the Fifth International Conference on Intelligent Systems and Networks},
+  isbn = {978-981-95-1746-6},
+  pages = {153--163},
+  publisher = {Springer Nature Singapore},
+  title = {GN-TRVN: A Benchmark for Vietnamese Table Markdown Retrieval Task},
+  year = {2026},
 }
 """,
     )
