357ac90f92d1d0f30a93627524c2c5ae14933290
AdnanElAssadi56 <115242814+AdnanElAssadi56@users.noreply.github.com>
2026-01-07 17:17:34 -0500

    [MAEB] Remove AudioCapsMiniReranking Task (#3883)
    remove audio_caps_mini_reranking

--- mteb/tasks/audio/audio_reranking/eng/__init__.py
+++ mteb/tasks/audio/audio_reranking/eng/__init__.py
@@ -1,4 +1,3 @@
-from .audio_caps_mini_reranking import AudioCapsMiniReranking
 from .esc50_audio_reranking import ESC50AudioReranking
 from .fs_dnoisy18k_audio_reranking import FSDnoisy18kAudioReranking
 from .gtzan_audio_reranking import GTZANAudioReranking
@@ -6,7 +5,6 @@ from .urban_sound8_k_audio_reranking import UrbanSound8KAudioReranking
 from .vocal_sound_audio_reranking import VocalSoundAudioReranking
 
 __all__ = [
-    "AudioCapsMiniReranking",
     "ESC50AudioReranking",
     "FSDnoisy18kAudioReranking",
     "GTZANAudioReranking",
