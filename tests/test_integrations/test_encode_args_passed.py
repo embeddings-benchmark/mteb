8c95e8f71a58931845319e5cb2c3a21a2f0c48fe
Roman Solomatin <36135455+Samoed@users.noreply.github.com>
2026-01-30 19:02:12 +0300

    fix test import

--- tests/test_integrations/test_encode_args_passed.py
+++ tests/test_integrations/test_encode_args_passed.py
@@ -1,5 +1,7 @@
 """test that arguments, encode_kwargs are correctly called and passed to the encoders"""
 
+from __future__ import annotations
+
 import logging
 from pathlib import Path
 from typing import TYPE_CHECKING, Any
