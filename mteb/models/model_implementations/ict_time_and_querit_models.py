fc747e20c33eaf5524afc4f01a7411c9e573740a
HengranZhang <54845883+hengran@users.noreply.github.com>
2026-02-01 00:51:07 +0800

    Remove the description comment at the beginning and update the n_parameters

--- mteb/models/model_implementations/ict_time_and_querit_models.py
+++ mteb/models/model_implementations/ict_time_and_querit_models.py
@@ -1,6 +1,3 @@
-"""
-Models from the ICT-TIME and Querit organizations.
-"""
 
 from __future__ import annotations
 
@@ -23,7 +20,7 @@ def instruction_template(
     return f"Instruct: {instruction}\nQuery:"
 
 
-# Multilingual language codes
+
 multilingual_langs = [
     "deu-Latn",
     "ita-Latn",
@@ -45,7 +42,6 @@ multilingual_langs = [
     "tha-Thai",
 ]
 
-# Training datasets
 training_data = [
     "FEVER",
     "DuRetrieval",
@@ -75,7 +71,7 @@ boom_4b_v1 = ModelMeta(
     open_weights=True,
     revision="33fb345468120e37c81eed2369aefe08b8f8222b",
     release_date="2026-01-31",
-    n_parameters=4_021_774_336,
+    n_parameters=4021774336,
     n_embedding_parameters=None,
     memory_usage_mb=7671,
     embed_dim=2560,
