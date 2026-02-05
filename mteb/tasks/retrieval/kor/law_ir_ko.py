21d6c606f0b3af44ff657f77a4396957d5d4c421
LEE-kanghyeun <kanghyeun7@gmail.com>
2026-02-06 00:05:36 +0900

    law_ir_ko

---
+++ mteb/tasks/retrieval/kor/law_ir_ko.py
@@ -0,0 +1,35 @@
+from mteb.abstasks.retrieval import AbsTaskRetrieval
+from mteb.abstasks.task_metadata import TaskMetadata
+
+
+class LawIRKo(AbsTaskRetrieval):
+    metadata = TaskMetadata(
+        name="law_ir-ko",
+        description="law_ir-ko",
+        reference=None,
+        dataset={
+            "path": "on-and-on/lawgov_ir-ko",
+            "revision": "bd5361e486ef4be7052c506adfdf0610d04abbfe",
+        },
+        type="Retrieval",
+        category="t2t",
+        modalities=["text"],
+        eval_splits=["test"],
+        eval_langs=["kor-Hang"],
+        main_score="ndcg_at_10",
+        date=None,
+        domains=None,
+        task_subtypes=None,
+        license=None,
+        annotations_creators=None,
+        dialect=None,
+        sample_creation=None,
+        bibtex_citation=r"""
+@misc{law_ko_ir_on,
+  author = {on-and-on},
+  note = {A Benchmark Dataset for Korean Legal Information Retrieval and QA},
+  howpublished = {\url{https://www.law.go.kr/LSW/main.html}},
+  year = {2026},
+}
+""",
+    )
