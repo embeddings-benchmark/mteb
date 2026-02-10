3348864030710ff0bda912bf3cc2e8850dfe8f1d
Kenneth Enevoldsen <kennethcenevoldsen@gmail.com>
2026-02-10 21:26:46 +0100

    docs: fix `AbsTaskClassification` docstring formatting and improve docstrings for some of the main tasks

--- mteb/abstasks/abstask.py
+++ mteb/abstasks/abstask.py
@@ -71,7 +71,7 @@ def _multilabel_subsampling(
 
 
 class AbsTask(ABC):
-    """The abstract class for the tasks
+    """The abstract class for the tasks. All tasks in `mteb` inherits from this class.
 
     Attributes:
         metadata: The metadata describing the task
