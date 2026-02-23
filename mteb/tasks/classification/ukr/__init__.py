0077e6c68209e2f67caf1e167cc0d0c65d5f0f39
YevhenKost <kosteugeneo@gmail.com>
2026-02-23 15:10:59 +0100

    fix: missimg init change

--- mteb/tasks/classification/ukr/__init__.py
+++ mteb/tasks/classification/ukr/__init__.py
@@ -2,9 +2,11 @@ from .ukr_formality_classification import (
     UkrFormalityClassification,
     UkrFormalityClassificationV2,
 )
+from .ukr_news_classification import UANewsTitleClassification
 from .ukr_toxicity_classification import UkrTweetToxicityBinaryClassification
 
 __all__ = [
+    "UANewsTitleClassification",
     "UkrFormalityClassification",
     "UkrFormalityClassificationV2",
     "UkrTweetToxicityBinaryClassification",
