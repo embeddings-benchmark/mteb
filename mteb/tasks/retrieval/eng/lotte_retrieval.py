57a309a47b1b89b085843a10156a22f8300797c1
Roman Solomatin <36135455+Samoed@users.noreply.github.com>
2026-02-24 12:41:08 +0300

    fix name

--- mteb/tasks/retrieval/eng/lotte_retrieval.py
+++ mteb/tasks/retrieval/eng/lotte_retrieval.py
@@ -28,7 +28,7 @@ class LoTTERetrieval(AbsTaskRetrieval):
         # in paper mentioned as `Success@5 (sometimes referred to as Recall@5)`,
         # which is hit_rate@5 in code https://github.com/stanford-futuredata/ColBERT/blob/cc4f3dc91c0b45d2d08c251d9d95178285c65f1c/utility/evaluate/evaluate_lotte_rankings.py#L39-L40
         # and mteb have cv_recall_at_5 which is actually hit_rate
-        main_score="hit_rate@5",
+        main_score="hit_rate_at_5",
         date=("2021-12-02", "2022-06-10"),
         domains=["Academic", "Web", "Social"],
         task_subtypes=["Article retrieval"],
