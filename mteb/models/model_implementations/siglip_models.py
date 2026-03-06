d15a02047261ed0761fac71dfb43e9654006f46e
Your Name <you@example.com>
2026-03-06 11:21:00 +0100

    add mean pool to siglip

--- mteb/models/model_implementations/siglip_models.py
+++ mteb/models/model_implementations/siglip_models.py
@@ -68,7 +68,8 @@ class SiglipModelWrapper(AbsEncoder):
                 )
                 inputs = {k: v.to(self.device) for k, v in inputs.items()}
                 text_outputs = self.model.get_text_features(**inputs)
-                all_text_embeddings.append(text_outputs["last_hidden_state"].cpu())
+                embeddings = torch.mean(text_outputs["last_hidden_state"], dim=1) # mean pool
+                all_text_embeddings.append(embeddings.cpu())
 
         all_text_embeddings = torch.cat(all_text_embeddings, dim=0)
         return all_text_embeddings
@@ -88,7 +89,8 @@ class SiglipModelWrapper(AbsEncoder):
                 )
                 inputs = {k: v.to(self.device) for k, v in inputs.items()}
                 image_outputs = self.model.get_image_features(**inputs)
-                all_image_embeddings.append(image_outputs.cpu())
+                embeddings = torch.mean(image_outputs["last_hidden_state"], dim=1) # mean pool
+                all_image_embeddings.append(embeddings.cpu())
         all_image_embeddings = torch.cat(all_image_embeddings, dim=0)
         return all_image_embeddings
 
