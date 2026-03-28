e15fbadc5b6aad7b0d3bc6f40e7804be0d22e465
ayush1298 <munotayush6@kgpian.iitkgp.ac.in>
2026-03-28 18:19:29 +0530

    fix indentation

--- mteb/cache/result_cache.py
+++ mteb/cache/result_cache.py
@@ -1033,13 +1033,12 @@ class ResultCache:
 
         checklist = textwrap.dedent("""
         ### Checklist
-
-        - [ ] My model has a model sheet, report, or similar
-        - [ ] My model has a reference implementation in [`mteb/models/model_implementations/`](https://github.com/embeddings-benchmark/mteb/tree/main/mteb/models/model_implementations), this can be as an API. Instruction on how to add a model can be found [here](https://embeddings-benchmark.github.io/mteb/contributing/adding_a_model/)
-          - [ ] No, but there is an existing PR ___
-        - [ ] The results submitted are obtained using the reference implementation
-        - [ ] My model is available, either as a publicly accessible API or publicly on e.g., Huggingface
-        - [ ] I *solemnly swear* that for all results submitted I have not trained on the evaluation dataset including training splits. If I have, I have disclosed it clearly.
+- [ ] My model has a model sheet, report, or similar
+- [ ] My model has a reference implementation in [`mteb/models/model_implementations/`](https://github.com/embeddings-benchmark/mteb/tree/main/mteb/models/model_implementations), this can be as an API. Instruction on how to add a model can be found [here](https://embeddings-benchmark.github.io/mteb/contributing/adding_a_model/)
+  - [ ] No, but there is an existing PR ___
+- [ ] The results submitted are obtained using the reference implementation
+- [ ] My model is available, either as a publicly accessible API or publicly on e.g., Huggingface
+- [ ] I *solemnly swear* that for all results submitted I have not trained on the evaluation dataset including training splits. If I have, I have disclosed it clearly.
         """).strip()
 
         body = textwrap.dedent(f"""
