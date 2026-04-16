e45cbaae39100d4362fb195dbab99c9b090f5cf3
Roman Solomatin <samoed.roman@gmail.com>
2026-04-16 12:29:59 +0300

    fix: Handle quantization in sentence transformers as an experiment (#4367)
    * handle sbert quants
    
    * move inside model encode
    
    * update type of prompt type
    
    * fix tests

--- tests/test_evaluate.py
+++ tests/test_evaluate.py
@@ -7,10 +7,12 @@ import pytest
 from datasets.exceptions import DatasetNotFoundError
 
 import mteb
+from mteb import SentenceTransformerEncoderWrapper
 from mteb.abstasks.abstask import AbsTask
 from mteb.cache import ResultCache
 from mteb.models import ModelMeta
 from mteb.models.models_protocols import EncoderProtocol
+from mteb.types import OutputDType
 from tests.mock_models import MockSentenceTransformer
 from tests.mock_tasks import (
     MockAggregatedTask,
@@ -367,3 +369,13 @@ def test_mrl_unsupported_dim():
             "intfloat/multilingual-e5-small",
             embed_dim=100,
         )
+
+
+def test_precision_arg():
+    model = SentenceTransformerEncoderWrapper(MockSentenceTransformer())
+    task = MockRetrievalTask()
+    mteb.evaluate(model, task, cache=None, encode_kwargs={"precision": "float16"})
+
+    assert (
+        model.mteb_model_meta.experiment_kwargs["output_dtypes"] == OutputDType.FLOAT16
+    )
