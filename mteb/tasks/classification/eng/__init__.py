40e009314f0e3ef64e390e52ae52ca4fdfec1029
Shriya Sudhakar <shriya.sudhakar314@gmail.com>
2026-04-15 23:58:41 -0400

    dataset: Add RAVDESS_AV classification task

--- mteb/tasks/classification/eng/__init__.py
+++ mteb/tasks/classification/eng/__init__.py
@@ -206,6 +206,7 @@ from .poem_sentiment_classification import (
     PoemSentimentClassification,
     PoemSentimentClassificationV2,
 )
+from .ravdess_av_classification import RAVDESSAVClassification
 from .resisc45_classification import RESISC45Classification
 from .sds_eye_protection_classification import (
     SDSEyeProtectionClassification,
@@ -458,6 +459,7 @@ __all__ = [
     "PersonalJurisdictionLegalBenchClassification",
     "PoemSentimentClassification",
     "PoemSentimentClassificationV2",
+    "RAVDESSAVClassification",
     "RESISC45Classification",
     "SCDBPAccountabilityLegalBenchClassification",
     "SCDBPAuditsLegalBenchClassification",
