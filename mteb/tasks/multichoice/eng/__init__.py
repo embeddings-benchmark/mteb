b2026a3af585fec3dc448f4e57d274d8be27c11e
Yashwanth Devavarapu <yashwanthdevavarapu@Yashwanths-MacBook-Pro.local>
2026-04-28 02:56:11 +0530

    [MVEB] Add PerceptionTestVideoAudioCentricQA task and fix modalities

--- mteb/tasks/multichoice/eng/__init__.py
+++ mteb/tasks/multichoice/eng/__init__.py
@@ -2,7 +2,7 @@ from .blink_it2i_multi_choice import BLINKIT2IMultiChoice
 from .blink_it2t_multi_choice import BLINKIT2TMultiChoice
 from .cv_bench import CVBenchCount, CVBenchDepth, CVBenchDistance, CVBenchRelation
 from .nextqa import NExTQAVideoCentricQA
-from .perception_test import PerceptionTestVideoCentricQA
+from .perception_test import PerceptionTestVideoAudioCentricQA, PerceptionTestVideoCentricQA
 
 __all__ = [
     "BLINKIT2IMultiChoice",
@@ -12,5 +12,6 @@ __all__ = [
     "CVBenchDistance",
     "CVBenchRelation",
     "NExTQAVideoCentricQA",
+    "PerceptionTestVideoAudioCentricQA",
     "PerceptionTestVideoCentricQA",
 ]
