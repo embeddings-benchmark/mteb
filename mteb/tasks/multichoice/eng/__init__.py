5b42120d5b13d955f71a1fad865865ae54efdbc6
Rakshitha Ireddi <your.email@example.com>
2026-04-28 02:43:31 +0530

    [MVEB] Add WorldSense 1min video-centric QA task

--- mteb/tasks/multichoice/eng/__init__.py
+++ mteb/tasks/multichoice/eng/__init__.py
@@ -3,6 +3,7 @@ from .blink_it2t_multi_choice import BLINKIT2TMultiChoice
 from .cv_bench import CVBenchCount, CVBenchDepth, CVBenchDistance, CVBenchRelation
 from .egoschema import EgoSchemaVideoCentricQA
 from .nextqa import NExTQAVideoCentricQA
+from .worldsense import WorldSense1MinVideoCentricQA
 
 __all__ = [
     "BLINKIT2IMultiChoice",
@@ -13,4 +14,5 @@ __all__ = [
     "CVBenchRelation",
     "EgoSchemaVideoCentricQA",
     "NExTQAVideoCentricQA",
+    "WorldSense1MinVideoCentricQA",
 ]
