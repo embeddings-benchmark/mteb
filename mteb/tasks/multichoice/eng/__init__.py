c2f828f83677f9a36f25e21ac830afc95a931f68
Yashwanth Devavarapu <yashwanthdevavarapu@Yashwanths-MacBook-Pro.local>
2026-04-28 02:42:58 +0530

    [MVEB] Add AVMemeExamVideoAudioCentricQA task and fix modalities

--- mteb/tasks/multichoice/eng/__init__.py
+++ mteb/tasks/multichoice/eng/__init__.py
@@ -1,10 +1,11 @@
-from .avmeme_exam import AVMemeExamVideoCentricQA
+from .avmeme_exam import AVMemeExamVideoAudioCentricQA, AVMemeExamVideoCentricQA
 from .blink_it2i_multi_choice import BLINKIT2IMultiChoice
 from .blink_it2t_multi_choice import BLINKIT2TMultiChoice
 from .cv_bench import CVBenchCount, CVBenchDepth, CVBenchDistance, CVBenchRelation
 from .nextqa import NExTQAVideoCentricQA
 
 __all__ = [
+    "AVMemeExamVideoAudioCentricQA",
     "AVMemeExamVideoCentricQA",
     "BLINKIT2IMultiChoice",
     "BLINKIT2TMultiChoice",
