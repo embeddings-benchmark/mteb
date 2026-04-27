bbba869fae2e8759b201059f75be0393ffee1923
Yashwanth Devavarapu <yashwanthdevavarapu@Yashwanths-MacBook-Pro.local>
2026-04-28 01:47:36 +0530

    [MVEB] Add AVMeme-Exam video-centric QA task

--- mteb/tasks/multichoice/eng/__init__.py
+++ mteb/tasks/multichoice/eng/__init__.py
@@ -1,9 +1,11 @@
+from .avmeme_exam import AVMemeExamVideoCentricQA
 from .blink_it2i_multi_choice import BLINKIT2IMultiChoice
 from .blink_it2t_multi_choice import BLINKIT2TMultiChoice
 from .cv_bench import CVBenchCount, CVBenchDepth, CVBenchDistance, CVBenchRelation
 from .nextqa import NExTQAVideoCentricQA
 
 __all__ = [
+    "AVMemeExamVideoCentricQA",
     "BLINKIT2IMultiChoice",
     "BLINKIT2TMultiChoice",
     "CVBenchCount",
