dea82e0deb2ef1dee5c34193d541226762fba6e1
Nguyen Tai <tainguyen7597@gmail.com>
2026-04-29 20:40:18 -0700

    [MVEB] Add HMDB51ZeroShot for video zero-shot classification (#4532)
    add HMDB51ZeroShot for video zero-shot classification

--- mteb/tasks/zeroshot_classification/eng/__init__.py
+++ mteb/tasks/zeroshot_classification/eng/__init__.py
@@ -14,6 +14,7 @@ from .fer2013 import FER2013ZeroShotClassification
 from .fgvc_aircraft import FGVCAircraftZeroShotClassification
 from .food101 import Food101ZeroShotClassification
 from .gtsrb import GTSRBZeroShotClassification
+from .hmdb51 import HMDB51ZeroShotClassification
 from .human_animal_cartoon import HumanAnimalCartoonZeroShotClassification
 from .imagenet1k import Imagenet1kZeroShotClassification
 from .kinetics400 import (
@@ -69,6 +70,7 @@ __all__ = [
     "FGVCAircraftZeroShotClassification",
     "Food101ZeroShotClassification",
     "GTSRBZeroShotClassification",
+    "HMDB51ZeroShotClassification",
     "HumanAnimalCartoonZeroShotClassification",
     "Imagenet1kZeroShotClassification",
     "Kinetics400VAZeroShotClassification",
