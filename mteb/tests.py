0826c5b500adc9aa63a3f25169dc1706dce3ddc7
ayush1298 <munotayush6@kgpian.iitkgp.ac.in>
2026-06-16 00:09:42 +0530

    move mocktasks under mteb/tests_mock

--- mteb/tests.py
+++ mteb/tests.py
@@ -1,24 +1,19 @@
 from __future__ import annotations
 
-try:
-    from tests.task_grid import (
-        ALL_TASK_TEST_GRID,
-        MOCK_MAEB_TASK_GRID,
-        MOCK_MIEB_TASK_GRID,
-        MOCK_MVEB_TASK_GRID,
-        MOCK_TASK_TEST_GRID,
-    )
+from mteb.tests_mock.task_grid import (
+    ALL_TASK_TEST_GRID,
+    MOCK_MAEB_TASK_GRID,
+    MOCK_MIEB_TASK_GRID,
+    MOCK_MVEB_TASK_GRID,
+    MOCK_TASK_TEST_GRID,
+)
 
-    all_test_tasks = ALL_TASK_TEST_GRID
-    test_tasks_audio = MOCK_MAEB_TASK_GRID
-    test_tasks_image = MOCK_MIEB_TASK_GRID
-    test_tasks_video = MOCK_MVEB_TASK_GRID
-    test_tasks = MOCK_TASK_TEST_GRID
-    test_tasks_text = MOCK_TASK_TEST_GRID
-except ImportError as e:
-    raise ImportError(
-        "mteb.tests is only available for local development and requires the tests folder from the repository."
-    ) from e
+all_test_tasks = ALL_TASK_TEST_GRID
+test_tasks_audio = MOCK_MAEB_TASK_GRID
+test_tasks_image = MOCK_MIEB_TASK_GRID
+test_tasks_video = MOCK_MVEB_TASK_GRID
+test_tasks = MOCK_TASK_TEST_GRID
+test_tasks_text = MOCK_TASK_TEST_GRID
 
 __all__ = [
     "all_test_tasks",
