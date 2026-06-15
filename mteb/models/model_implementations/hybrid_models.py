8c66fbb0915363e10e52d0061d9c5ae56010ab17
ayush1298 <munotayush6@kgpian.iitkgp.ac.in>
2026-06-15 12:21:14 +0530

    Register bm25+e5 small models using other fusion strategy

--- mteb/models/model_implementations/hybrid_models.py
+++ mteb/models/model_implementations/hybrid_models.py
@@ -39,6 +39,76 @@ hybrid_bm25s_e5 = ModelMeta(
 )
 
 
+def hybrid_bm25s_e5_dbsf_loader(model_name: str, **kwargs) -> HybridSearch:
+    bm25 = mteb.get_model("mteb/baseline-bm25s")
+    dense = mteb.get_model("intfloat/multilingual-e5-small")
+    return HybridSearch(
+        models=[bm25, dense],
+        fusion_strategy="dbsf",
+    )
+
+
+hybrid_bm25s_e5_dbsf = ModelMeta(
+    loader=hybrid_bm25s_e5_dbsf_loader,
+    name="mteb/hybrid-bm25s-e5-small-dbsf",
+    model_type=["hybrid"],
+    languages=["eng-Latn"],
+    open_weights=True,
+    revision="0.1.0",
+    release_date="2024-02-08",
+    n_parameters=118_000_000,
+    n_embedding_parameters=96_014_208,
+    memory_usage_mb=None,
+    embed_dim=None,
+    license=None,
+    max_tokens=None,
+    reference=None,
+    similarity_fn_name=None,
+    framework=[],
+    use_instructions=False,
+    public_training_code=None,
+    public_training_data=None,
+    training_datasets=None,
+    citation=None,
+)
+
+
+def hybrid_bm25s_e5_relative_score_fusion_loader(
+    model_name: str, **kwargs
+) -> HybridSearch:
+    bm25 = mteb.get_model("mteb/baseline-bm25s")
+    dense = mteb.get_model("intfloat/multilingual-e5-small")
+    return HybridSearch(
+        models=[bm25, dense],
+        fusion_strategy="relative-score-fusion",
+    )
+
+
+hybrid_bm25s_e5_relative_score_fusion = ModelMeta(
+    loader=hybrid_bm25s_e5_relative_score_fusion_loader,
+    name="mteb/hybrid-bm25s-e5-small-relative-score-fusion",
+    model_type=["hybrid"],
+    languages=["eng-Latn"],
+    open_weights=True,
+    revision="0.1.0",
+    release_date="2024-02-08",
+    n_parameters=118_000_000,
+    n_embedding_parameters=96_014_208,
+    memory_usage_mb=None,
+    embed_dim=None,
+    license=None,
+    max_tokens=None,
+    reference=None,
+    similarity_fn_name=None,
+    framework=[],
+    use_instructions=False,
+    public_training_code=None,
+    public_training_data=None,
+    training_datasets=None,
+    citation=None,
+)
+
+
 def hybrid_baseline_encoder_e5_loader(model_name: str, **kwargs) -> HybridSearch:
     bm25 = mteb.get_model("mteb/baseline-random-encoder")
     dense = mteb.get_model("intfloat/multilingual-e5-small")
