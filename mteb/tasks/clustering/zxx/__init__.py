3c57b1b8e23dc6c1a3d2f6dd859fce36d4f68bb3
Rakshitha Ireddi <rakshithaireddi@Rakshithas-MacBook-Pro.local>
2026-07-25 17:52:10 +0530

    Add UrbanSound8K Audio Clustering task (closes #5018)
      Part of MOEB: Massive Omni Embedding Benchmark (tracking issue #4842)

--- mteb/tasks/clustering/zxx/__init__.py
+++ mteb/tasks/clustering/zxx/__init__.py
@@ -1,6 +1,7 @@
 from .esc50_clustering import ESC50Clustering
 from .gtzan_genre_clustering import GTZANGenreClustering
 from .music_genre import MusicGenreClustering
+from .urban_sound8k_clustering import UrbanSound8kClustering
 from .vehicle_sound_clustering import VehicleSoundClustering
 from .vim_sketch_clustering import VimSketchImitationClustering
 
@@ -8,6 +9,7 @@ __all__ = [
     "ESC50Clustering",
     "GTZANGenreClustering",
     "MusicGenreClustering",
+    "UrbanSound8kClustering",
     "VehicleSoundClustering",
     "VimSketchImitationClustering",
 ]
