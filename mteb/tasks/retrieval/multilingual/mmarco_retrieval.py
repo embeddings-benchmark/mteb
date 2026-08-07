38e563a0a5bc25d045332f65d71fe43ed78c02ed
Lorenzo Pozzi <lpozzi@pcopernico.it>
2026-08-07 12:04:08 +0200

    MMarcoRetrievalMultilingual task contributed_by=None

--- mteb/tasks/retrieval/multilingual/mmarco_retrieval.py
+++ mteb/tasks/retrieval/multilingual/mmarco_retrieval.py
@@ -62,6 +62,6 @@ Rodrigo Frassetto Nogueira},
             "query": "Given a web search query, retrieve relevant passages that answer the query"
         },
         adapted_from=["MMarcoRetrieval"],
-        contributed_by="lopozz",
+        contributed_by=None,
         is_beta=True,
     )
