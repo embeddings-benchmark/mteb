1b558ae00587d8548227b81c47f7e038669da666
Yongbin Choi <whybe.choi@gmail.com>
2026-08-08 12:28:12 +0000

    dataset: add REAL-MM-RAG

--- mteb/tasks/retrieval/eng/__init__.py
+++ mteb/tasks/retrieval/eng/__init__.py
@@ -298,6 +298,12 @@ from .r_paris_i2i_retrieval import (
 from .ra_rb_code_retrieval import RARbCode
 from .ra_rb_math_retrieval import RARbMath
 from .re_mu_q_it2t_retrieval import ReMuQIT2TRetrieval
+from .real_mm_rag_retrieval import (
+    RealMMRAGFinReportRetrieval,
+    RealMMRAGFinSlidesRetrieval,
+    RealMMRAGTechReportRetrieval,
+    RealMMRAGTechSlidesRetrieval,
+)
 from .rp2k_i2i_retrieval import RP2kI2IRetrieval
 from .sci_fact_retrieval import SciFact
 from .sci_mmir_i2t_retrieval import SciMMIRI2TRetrieval
@@ -699,6 +705,10 @@ __all__ = [
     "RParisMediumI2IRetrieval",
     "ReMe",
     "ReMuQIT2TRetrieval",
+    "RealMMRAGFinReportRetrieval",
+    "RealMMRAGFinSlidesRetrieval",
+    "RealMMRAGTechReportRetrieval",
+    "RealMMRAGTechSlidesRetrieval",
     "SHS100KA2ARetrieval",
     "SOPI2IRetrieval",
     "SciFact",
