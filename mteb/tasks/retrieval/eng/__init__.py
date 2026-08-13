0e07d11917181216581ec6b575d669ffd1c59d01
nehal309 <nehalkathrotia@gmail.com>
2026-08-13 02:08:51 -0700

    Add Flickr dataset I2A and A2I (#5137)
    * Add Flickr dataset I2A and A2I
    
    Co-Authored-By: Deep Shah <21212684+deep9539@users.noreply.github.com>
    
    * refactor and directory update
    
    * 1. Add descriptive stats and 2. HF commit version when reading
    
    * format lint files
    
    ---------
    
    Co-authored-by: Deep Shah <21212684+deep9539@users.noreply.github.com>

--- mteb/tasks/retrieval/eng/__init__.py
+++ mteb/tasks/retrieval/eng/__init__.py
@@ -160,6 +160,10 @@ from .flare_retrieval import (
 )
 from .flickr30k_i2t_retrieval import Flickr30kI2TRetrieval
 from .flickr30k_t2i_retrieval import Flickr30kT2IRetrieval
+from .flickr_audio_image_retrieval import (
+    FlickrAudioToImageRetrieval,
+    FlickrImageToAudioRetrieval,
+)
 from .forb_i2i_retrieval import FORBI2I
 from .giga_speech import GigaSpeechA2TRetrieval, GigaSpeechT2ARetrieval
 from .gl_dv2_i2i_retrieval import GLDv2I2IRetrieval
@@ -610,6 +614,8 @@ __all__ = [
     "FinanceBenchRetrieval",
     "Flickr30kI2TRetrieval",
     "Flickr30kT2IRetrieval",
+    "FlickrAudioToImageRetrieval",
+    "FlickrImageToAudioRetrieval",
     "GLDv2I2IRetrieval",
     "GLDv2I2TRetrieval",
     "GigaSpeechA2TRetrieval",
