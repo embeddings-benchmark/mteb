43ed975890b424c004a0c6971cbee895bcc1463f
Wissam Siblini <36303760+wissam-sib@users.noreply.github.com>
2026-08-24 16:35:53 +0200

    dataset: Add EMID A2I + I2A retrieval datasets (#5279)
    * [MOEB] Add EMID pair classification and EMID a2i + i2a retreival datasets
    
    * add stats
    
    * fix types
    
    * reduce size and dedup
    
    * remove pc files
    
    * move data to mteb org

--- mteb/tasks/retrieval/eng/__init__.py
+++ mteb/tasks/retrieval/eng/__init__.py
@@ -142,6 +142,7 @@ from .didemo_retrieval import (
 )
 from .edis_t2it_retrieval import EDIST2ITRetrieval
 from .elastic_kb_retrieval import ElasticKBRetrieval
+from .emid_retrieval import EMIDA2IRetrieval, EMIDI2ARetrieval
 from .emo_vdb import EmoVDBA2TRetrieval, EmoVDBT2ARetrieval
 from .encyclopedia_vqa_it2it_retrieval import EncyclopediaVQAIT2ITRetrieval
 from .english_finance1_retrieval import EnglishFinance1Retrieval
@@ -607,6 +608,8 @@ __all__ = [
     "DiDeMoVA2TRetrieval",
     "DiDeMoVT2ARetrieval",
     "EDIST2ITRetrieval",
+    "EMIDA2IRetrieval",
+    "EMIDI2ARetrieval",
     "EPBench",
     "ESGReports",
     "ElasticKBRetrieval",
