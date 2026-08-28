7aeb4dee907deaa8c738970b0aadcfc053342e18
Clemente <53945706+Clemente-H@users.noreply.github.com>
2026-08-28 14:20:01 -0400

    dataset: add Spanish Wiki clustering v2 task

--- mteb/tasks/clustering/spa/__init__.py
+++ mteb/tasks/clustering/spa/__init__.py
@@ -1,9 +1,11 @@
 from .spanish_news_clustering_p2p import SpanishNewsClusteringP2P
+from .spanish_wiki_clustering_p2p_v2 import SpanishWikiClusteringP2PV2
 from .spanish_wikinews_clustering_p2p import SpanishWikinewsClusteringP2P
 from .spanish_wikinews_clustering_s2s import SpanishWikinewsClusteringS2S
 
 __all__ = [
     "SpanishNewsClusteringP2P",
+    "SpanishWikiClusteringP2PV2",
     "SpanishWikinewsClusteringP2P",
     "SpanishWikinewsClusteringS2S",
 ]
