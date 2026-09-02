4306a1beabd92ddcaa57aa52e196f5d086181dfa
Tommaso Cerruti <79256764+tommasocerruti@users.noreply.github.com>
2026-09-02 14:00:42 +0200

    task: add Lombard GRID audiovisual retrieval tasks (#5337)
    * task: add Lombard GRID i2va retrieval
    
    * task: add Lombard GRID audiovisual retrieval tasks

--- mteb/tasks/retrieval/eng/__init__.py
+++ mteb/tasks/retrieval/eng/__init__.py
@@ -250,6 +250,12 @@ from .lmeb_retrieval import (
     ReMe,
     ToolBench,
 )
+from .lombard_grid_retrieval import (
+    LombardGridA2VRetrieval,
+    LombardGridV2ARetrieval,
+    LombardGridV2VRetrieval,
+    LombardGridVA2VARetrieval,
+)
 from .lotte_retrieval import LoTTERetrieval
 from .macs import MACSA2TRetrieval, MACST2ARetrieval
 from .mars_vl_pairs import MarsVLPairsI2TRetrieval, MarsVLPairsT2IRetrieval
@@ -714,6 +720,10 @@ __all__ = [
     "LitSearchRetrieval",
     "LoCoMo",
     "LoTTERetrieval",
+    "LombardGridA2VRetrieval",
+    "LombardGridV2ARetrieval",
+    "LombardGridV2VRetrieval",
+    "LombardGridVA2VARetrieval",
     "LongMemEval",
     "LooGLE",
     "MACSA2TRetrieval",
