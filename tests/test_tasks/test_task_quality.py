d44304f0194c9c465427472737f6f19511cf4d1a
Rishabh Patil <rishabhpatil@gmail.com>
2026-09-07 21:15:44 -0700

    test: add duplicate_text exemption for ColDeRReranking in task quality tests

--- tests/test_tasks/test_task_quality.py
+++ tests/test_tasks/test_task_quality.py
@@ -459,6 +459,7 @@ KNOWN_ISSUES: dict[str, list[str]] = {
         "CodeSearchNetCCRetrieval",
         "CodeSearchNetRetrieval",
         "CodeTransOceanDL",
+        "ColDeRReranking",
         "Core17InstructionRetrieval",
         "CosQA",
         "CrisisMMDHumanitarianClassification",  # one tweet can pair with several separately annotated images
