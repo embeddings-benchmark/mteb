a8bce9d815225e162698983902d25350c848d3f0
khan <nextgenai.kr.system@gmail.com>
2026-09-10 14:02:27 +0900

    Add ModelMeta for NGA-KR/ko-embed-v0 (#5440)
    * Add ModelMeta for NGA-KR/ko-embed-v0
    
    * lint
    
    ---------
    
    Co-authored-by: Roman Solomatin <36135455+Samoed@users.noreply.github.com>

---
+++ mteb/models/model_implementations/nga_system_models.py
@@ -0,0 +1,32 @@
+from __future__ import annotations
+
+from mteb.models import ModelMeta, SentenceTransformerEncoderWrapper
+from mteb.models.model_meta import ScoringFunction
+
+ko_embed_v0 = ModelMeta(
+    loader=SentenceTransformerEncoderWrapper,
+    loader_kwargs=dict(
+        model_prompts={"query": "query: ", "document": "", "passage": "passage: "}
+    ),
+    name="NGA-KR/ko-embed-v0",
+    revision="f321741f682310b04b8d96afb3a36d177f1ab900",
+    release_date="2026-09-10",
+    languages=["eng-Latn", "kor-Hang"],
+    n_parameters=148731648,
+    n_embedding_parameters=38400000,
+    memory_usage_mb=284,
+    max_tokens=512,
+    embed_dim=768,
+    license="apache-2.0",
+    open_weights=True,
+    use_instructions=True,
+    public_training_code=None,
+    public_training_data=None,
+    framework=["Sentence Transformers", "safetensors"],
+    reference="https://huggingface.co/NGA-KR/ko-embed-v0",
+    similarity_fn_name=ScoringFunction.COSINE,
+    training_datasets={"MSMARCO", "GooAQ", "NQ", "HotpotQA"},
+    adapted_from="skt/A.X-Encoder-base",
+    modalities=["text"],
+    model_type=["dense"],
+)
