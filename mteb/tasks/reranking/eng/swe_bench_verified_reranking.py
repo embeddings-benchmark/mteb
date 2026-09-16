16ce838ad47d03cd9198c1072583324c262a1567
Kenneth Enevoldsen <kennethcenevoldsen@gmail.com>
2026-09-16 15:43:26 +0200

    re-add swebench cit

--- mteb/tasks/reranking/eng/swe_bench_verified_reranking.py
+++ mteb/tasks/reranking/eng/swe_bench_verified_reranking.py
@@ -27,5 +27,15 @@ class SWEbenchVerifiedReranking(AbsTaskRetrieval):
         prompt={
             "query": "Given a github issue, identify the code that needs to be changed to fix the issue."
         },
-        bibtex_citation="",
+        bibtex_citation=r"""
+@misc{jimenez2024swebenchlanguagemodelsresolve,
+      title={SWE-bench: Can Language Models Resolve Real-World GitHub Issues?},
+      author={Carlos E. Jimenez and John Yang and Alexander Wettig and Shunyu Yao and Kexin Pei and Ofir Press and Karthik Narasimhan},
+      year={2024},
+      eprint={2310.06770},
+      archivePrefix={arXiv},
+      primaryClass={cs.CL},
+      url={https://arxiv.org/abs/2310.06770}
+}
+""",
     )
