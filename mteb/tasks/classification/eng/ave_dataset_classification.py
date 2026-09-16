0d9d487fb340624e5760aac16864b84536c44817
Roman Solomatin <samoed.roman@gmail.com>
2026-09-16 10:59:15 +0300

    fix: fix citations (#5473)
    fix citations

--- mteb/tasks/classification/eng/ave_dataset_classification.py
+++ mteb/tasks/classification/eng/ave_dataset_classification.py
@@ -29,11 +29,11 @@ class AVEDatasetClassification(AbsTaskClassification):
         modalities=["video", "audio"],
         sample_creation="found",
         bibtex_citation=r"""
-@inproceedings{tian2018audio,
+@inproceedings{Tian_2018_ECCV,
   author = {Tian, Yapeng and Shi, Jing and Li, Bochen and Duan, Zhiyao and Xu, Chenliang},
-  booktitle = {Proceedings of the European conference on computer vision (ECCV)},
-  pages = {247--263},
-  title = {Audio-visual event localization in unconstrained videos},
+  booktitle = {The European Conference on Computer Vision (ECCV)},
+  month = {September},
+  title = {Audio-Visual Event Localization in Unconstrained Videos},
   year = {2018},
 }
 """,
@@ -70,11 +70,11 @@ class AVEDatasetVideoClassification(AbsTaskClassification):
         modalities=["video"],
         sample_creation="found",
         bibtex_citation=r"""
-@inproceedings{tian2018audio,
+@inproceedings{Tian_2018_ECCV,
   author = {Tian, Yapeng and Shi, Jing and Li, Bochen and Duan, Zhiyao and Xu, Chenliang},
-  booktitle = {Proceedings of the European conference on computer vision (ECCV)},
-  pages = {247--263},
-  title = {Audio-visual event localization in unconstrained videos},
+  booktitle = {The European Conference on Computer Vision (ECCV)},
+  month = {September},
+  title = {Audio-Visual Event Localization in Unconstrained Videos},
   year = {2018},
 }
 """,
