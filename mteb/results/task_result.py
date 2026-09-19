9de9d1cbd5d7c53f28bc83c230729b35c01ef70c
Rızgar Ozan <rizgarozan7@gmail.com>
2026-09-19 13:11:07 +0300

    fix: apply scripts filter in TaskResult.get_score (#5490)
    * fix: apply scripts filter in TaskResult.get_score
    
    * Apply suggestion from @Samoed
    
    * lint
    
    ---------
    
    Co-authored-by: Roman Solomatin <samoed.roman@gmail.com>
    Co-authored-by: Roman Solomatin <36135455+Samoed@users.noreply.github.com>

--- mteb/results/task_result.py
+++ mteb/results/task_result.py
@@ -574,7 +574,12 @@ class TaskResult(BaseModel):  # noqa: PLR0904
             for scores in self.scores[split]:
                 eval_langs = scores["languages"]
                 for lang in eval_langs:
-                    if lang_scripts.contains_language(lang):
+                    if lang_scripts.contains_language(lang) and (
+                        not scripts
+                        or lang_scripts.contains_script(
+                            lang.rsplit("-", maxsplit=1)[-1]
+                        )
+                    ):
                         values.append(getter(scores))
                         break
 
