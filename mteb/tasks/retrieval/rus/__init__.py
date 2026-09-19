93c5409b808c5c0aa1c8080ae624cb6c9d0c8f90
alekseevpavel04 <alekceevpavel@mail.ru>
2026-09-19 20:12:43 +0300

    Add RuLawRetrieval: Russian legal article retrieval task

--- mteb/tasks/retrieval/rus/__init__.py
+++ mteb/tasks/retrieval/rus/__init__.py
@@ -4,10 +4,12 @@ from .ria_news_retrieval import (
     RiaNewsRetrievalHardNegativesV2,
 )
 from .ru_bq_retrieval import RuBQRetrieval
+from .ru_law_retrieval import RuLawRetrieval
 
 __all__ = [
     "RiaNewsRetrieval",
     "RiaNewsRetrievalHardNegatives",
     "RiaNewsRetrievalHardNegativesV2",
     "RuBQRetrieval",
+    "RuLawRetrieval",
 ]
